0bc513f0d3da644603c489b7d2cc7876783fb095
Brian Nenninger <bnenning@gmail.com>
2016-04-24 14:21:43 -0400

    Use <md-icon> instead of <i class="material-icons"> in demos.

--- src/demo-app/list/list-demo.ts
+++ src/demo-app/list/list-demo.ts
@@ -1,12 +1,13 @@
 import {Component} from 'angular2/core';
 import {MdButton} from '../../components/button/button';
+import {MdIcon} from '../../components/icon/icon';
 import {MD_LIST_DIRECTIVES} from '../../components/list/list';
 
 @Component({
   selector: 'list-demo',
   templateUrl: 'demo-app/list/list-demo.html',
   styleUrls: ['demo-app/list/list-demo.css'],
-  directives: [MD_LIST_DIRECTIVES, MdButton]
+  directives: [MD_LIST_DIRECTIVES, MdButton, MdIcon]
 })
 export class ListDemo {
   items: string[] = [
