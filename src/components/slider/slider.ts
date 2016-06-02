4ea37bc93d8d8c0ec8981541c217ef97b9fe2492
Ivey Padgett <iveysaurrr@gmail.com>
2016-06-02 11:58:06 -0700

    Made a static slider

---
+++ src/components/slider/slider.ts
@@ -0,0 +1,15 @@
+import {
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'md-slider',
+  templateUrl: 'slider.html',
+  styleUrls: ['slider.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class MdSlider {
+
+}
---
+++ src/demo-app/slider/slider-demo.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import {MdSlider} from '@angular2-material/slider/slider';
+
+@Component({
+  moduleId: module.id,
+  selector: 'slider-demo',
+  templateUrl: 'slider-demo.html',
+  directives: [MdSlider],
+})
+export class SliderDemo { }
