bd7801ec76407e7785f0c0febad544a7920a6a3b
Ivey Padgett <iveysaurrr@gmail.com>
2016-06-03 15:50:01 -0700

    Move slider by clicking

--- src/components/slider/slider.ts
+++ src/components/slider/slider.ts
@@ -1,15 +1,68 @@
 import {
   Component,
+  ElementRef,
   ViewEncapsulation,
+  AfterContentInit,
 } from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'md-slider',
+  host: {
+    '(click)': 'test($event)'
+  },
   templateUrl: 'slider.html',
   styleUrls: ['slider.css'],
   encapsulation: ViewEncapsulation.None,
 })
-export class MdSlider {
+export class MdSlider implements AfterContentInit {
+  private _renderer: SliderRenderer = null;
 
+  private _sliderDimensions: ClientRect = null;
+
+  private _value: number = 0;
+
+  private _minValue: number = 0;
+
+  private _maxValue: number = 100;
+
+  private _percent: number = 0;
+
+  get value() {
+    return this._value;
+  }
+
+  set value(v: number) {
+    this._value = v;
+  }
+
+  constructor(private _elementRef: ElementRef) {
+    this._renderer = new SliderRenderer(_elementRef);
+  }
+
+  ngAfterContentInit() {
+    this._sliderDimensions = this._renderer.getSliderDimensions();
+  }
+
+  test(event: MouseEvent) {
+    let offset = this._sliderDimensions.left;
+    let size = this._sliderDimensions.width;
+    this._percent = (event.clientX - offset) / size;
+    let value = this._minValue + (this._percent * (this._maxValue - this._minValue));
+
+    this.value = value;
+  }
+
+  primaryTransform() {
+    let position = (this._percent * this._sliderDimensions.width) - 10;
+    return {transform: `translateX(${position}px) scale(1)`};
+  }
+}
+
+export class SliderRenderer {
+  constructor(private _elementRef: ElementRef) { }
+
+  getSliderDimensions() {
+    return this._elementRef.nativeElement.getBoundingClientRect();
+  }
 }
