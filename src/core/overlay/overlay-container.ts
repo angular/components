ff90e0547354bf9bfb76c27e6a20072718588d4e
Kara Erickson <karakara@google.com>
2016-07-21 16:46:11 -0700

    fix(overlay): lazily create container

--- src/core/overlay/overlay-container.ts
+++ src/core/overlay/overlay-container.ts
@@ -1,12 +1,21 @@
 
 
-/**
- * Create the overlay container element, which is simply a div
- * with the 'md-overlay-container' class on the document body.
- */
-export function createOverlayContainer(): Element {
-  let container = document.createElement('div');
-  container.classList.add('md-overlay-container');
-  document.body.appendChild(container);
-  return container;
+export class OverlayContainer {
+  private _containerElement: Element;
+
+  getContainerElement(): Element {
+    if (!this._containerElement) { this._createContainer(); }
+    return this._containerElement;
+  }
+
+  /**
+   * Create the overlay container element, which is simply a div
+   * with the 'md-overlay-container' class on the document body.
+   */
+  private _createContainer(): void {
+    let container = document.createElement('div');
+    container.classList.add('md-overlay-container');
+    document.body.appendChild(container);
+    this._containerElement = container;
+  }
 }
