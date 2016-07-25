a32f03ac011c3bd59f25f8aac2c0bbab4928e988
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-07-25 21:25:48 +0200

    address style changes

--- scripts/ci/create-plunker-bundle.js
+++ scripts/ci/create-plunker-bundle.js
@@ -7,9 +7,9 @@
  * The bundle will be used to load a Plunker Demo of Angular Material 2.
  */
 
-const spawn = require('child_process').spawnSync;
-const exec = require('child_process').execSync;
-const glob = require('glob').sync;
+const globSync = require('glob').sync;
+const spawnSync = require('child_process').spawnSync;
+const execSync = require('child_process').execSync;
 const firebase = require('firebase-tools');
 const path = require('path');
 const fse = require('fs-extra');
@@ -21,14 +21,14 @@ const DEPLOY_ROOT = path.join(DIST_ROOT, 'plunker-deploy/');
 
 const mainFile = path.join(DIST_ROOT, 'main.js');
 const latestTag = getLatestTag();
-const isRelease = getSHAFromTag(latestTag) === getLatestSHA();
+const isRelease = getShaFromTag(latestTag) === getLatestSha();
 const baseName = isRelease ? latestTag : 'HEAD';
 
 // Remove the distribution folder.
 fse.removeSync(DIST_ROOT);
 
 if (!buildProject()) {
-  console.error("An error occurred while building the project.");
+  console.error('An error occurred while building the project.');
   process.exit(1);
 }
 
@@ -46,17 +46,17 @@ firebase.deploy({
   token: process.env.MATERIAL_FIREBASE_TOKEN,
   public: 'dist/plunker-deploy'
 }).then(() => {
-  console.log("Firebase: Successfully deployed bundle to firebase.");
+  console.log('Firebase: Successfully deployed bundle to firebase.');
   process.exit(0);
 }).catch(err => {
-  console.error("Firebase: An error occurred while deploying to firebase.");
+  console.error('Firebase: An error occurred while deploying to firebase.');
   console.error(err);
   process.exit(1);
 });
 
 function inlineBundle() {
   let filePathFn = (sourceFile) => {
-    let sourceFiles = glob(`**/${sourceFile}`, { cwd: DIST_ROOT });
+    let sourceFiles = globSync(`**/${sourceFile}`, { cwd: DIST_ROOT });
     return path.resolve(DIST_ROOT, sourceFiles[0]);
   };
 
@@ -71,20 +71,20 @@ function inlineBundle() {
 function buildProject() {
   // Note: We can't use spawnSync here, because on some environments the Angular CLI
   // is not added to the System Paths and is only available in the locals.
-  let out = exec('npm run build:production').toString();
+  let out = execSync('npm run build:production').toString();
 
   return out.indexOf('successfully') !== -1;
 }
 
 function getLatestTag() {
-  let latestTagSHA = spawn('git', ['rev-list', '--tags', '--max-count=1']).stdout.toString().trim();
-  return spawn('git', ['describe', '--tags', latestTagSHA]).stdout.toString().trim();
+  let tagSHA = spawnSync('git', ['rev-list', '--tags', '--max-count=1']).stdout.toString().trim();
+  return spawnSync('git', ['describe', '--tags', tagSHA]).stdout.toString().trim();
 }
 
-function getLatestSHA() {
-  return spawn('git', ['rev-parse', 'master'])
+function getLatestSha() {
+  return spawnSync('git', ['rev-parse', 'master'])
 }
 
-function getSHAFromTag(tag) {
-  return spawn('git', ['rev-list', '-1', tag]).stdout.toString().trim();
+function getShaFromTag(tag) {
+  return spawnSync('git', ['rev-list', '-1', tag]).stdout.toString().trim();
 }
