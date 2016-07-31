31280130f929d57f750f8cada168c2a9be42b66e
Kara Erickson <karakara@google.com>
2016-07-31 15:22:08 -0700

    feat(menu): add keyboard events and accessibility

--- src/components/menu/menu.ts
+++ src/components/menu/menu.ts
@@ -2,14 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {OverlayModule} from '@angular2-material/core/core';
 import {MdMenu} from './menu-directive';
-import {MdMenuItem, MdMenuAnchor} from './menu-item';
+import {MdMenuItem} from './menu-item';
 import {MdMenuTrigger} from './menu-trigger';
 
 export {MdMenu} from './menu-directive';
-export {MdMenuItem, MdMenuAnchor} from './menu-item';
+export {MdMenuItem} from './menu-item';
 export {MdMenuTrigger} from './menu-trigger';
 
-export const MD_MENU_DIRECTIVES = [MdMenu, MdMenuItem, MdMenuTrigger, MdMenuAnchor];
+export const MD_MENU_DIRECTIVES = [MdMenu, MdMenuItem, MdMenuTrigger];
 
 
 @NgModule({
