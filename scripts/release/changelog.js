9dc7179e29d64ae0745307379c6d13e4ab205648
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-08-09 21:05:37 +0200

    chore(changelog): properly name tags for github compare links
    * The convention changelog currently auto-prefixes the previous semver tag with a `v`, which causes the github compare links to not work.
    * Manually resolve the previous and current tag from the git metadata. (As in https://github.com/angular/material/pull/9264)

--- scripts/release/changelog.js
+++ scripts/release/changelog.js
@@ -1,20 +1,26 @@
 #!/usr/bin/env node
 
 'use strict';
-/**
+
+/*
  * Creates a conventional changelog from the current git repository / metadata.
  */
 
-var fs = require('fs');
-var addStream = require('add-stream');
-var cl = require('conventional-changelog');
-var inStream = fs.createReadStream('CHANGELOG.md');
+const fs = require('fs');
+const addStream = require('add-stream');
+const changelog = require('conventional-changelog');
+const spawnSync = require('child_process').spawnSync;
+const npmVersion = require('../../package.json').version;
 
 /**
  * When the command line argument `--force` is provided, then the full changelog will created and overwritten.
  * By default, it will only create the changelog from the latest tag to head and prepends it to the changelog.
  */
-var isForce = process.argv.indexOf('--force') !== -1;
+const isForce = process.argv.indexOf('--force') !== -1;
+const inStream = fs.createReadStream('CHANGELOG.md');
+const gitTags = getTags();
+const currentTag = npmVersion === gitTags[1] ? gitTags[0] : npmVersion;
+const previousTag = npmVersion === gitTags[0] ? gitTags[1] : gitTags[0];
 
 inStream.on('error', function(err) {
   console.error('An error occurred, while reading the previous changelog file.\n' +
@@ -28,7 +34,12 @@ var config = {
   releaseCount: isForce ? 0 : 1
 };
 
-var stream = cl(config)
+var context = {
+  currentTag: currentTag,
+  previousTag: previousTag
+};
+
+var stream = changelog(config, context)
   .on('error', function(err) {
     console.error('An error occurred while generating the changelog: ' + err);
   })
@@ -44,4 +55,12 @@ if (!isForce) {
 
 function getOutputStream() {
   return fs.createWriteStream('CHANGELOG.md');
+}
+
+/**
+ * Resolves available tags over all branches from the repository metadata.
+ * @returns {Array.<String>} Array of available tags.
+ */
+function getTags() {
+  return spawnSync('git', ['tag']).stdout.toString().trim().split('\n').reverse();
 }
