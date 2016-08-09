3d56cd73e56278d919f1fa1504c1a5bbf3d54ba2
Jeremy Elbourn <jelbourn@google.com>
2016-08-09 12:13:13 -0700

    chore: package.json for tooltip, up compiler-cli version

--- src/demo-app/button-toggle/button-toggle-demo.ts
+++ src/demo-app/button-toggle/button-toggle-demo.ts
@@ -1,13 +1,9 @@
 import {Component} from '@angular/core';
-import {
-  MdUniqueSelectionDispatcher
-} from '@angular2-material/core/coordination/unique-selection-dispatcher';
 
 @Component({
   moduleId: module.id,
   selector: 'button-toggle-demo',
   templateUrl: 'button-toggle-demo.html',
-  providers: [MdUniqueSelectionDispatcher],
 })
 export class ButtonToggleDemo {
   favoritePie = 'Apple';
