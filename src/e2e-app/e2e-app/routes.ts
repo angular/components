d5f553e6f4dcda0f9f36e684e37a387268a5a5d8
Jesse Palmer <jesselpalmer@gmail.com>
2016-08-10 11:00:23 -0700

    style: cleans up code

--- src/e2e-app/e2e-app/routes.ts
+++ src/e2e-app/e2e-app/routes.ts
@@ -1,16 +1,18 @@
-import {provideRouter, RouterConfig} from '@angular/router';
-import {Home} from './e2e-app';
-import {ButtonE2E} from '../button/button-e2e';
-import {BasicTabs} from '../tabs/tabs-e2e';
-import {IconE2E} from '../icon/icon-e2e';
-import {MenuE2E} from '../menu/menu-e2e';
+import { provideRouter, RouterConfig } from '@angular/router';
+
+import { Home } from './e2e-app';
+import { ButtonE2E } from '../button/button-e2e';
+import { IconE2E } from '../icon/icon-e2e';
+import { MenuE2E } from '../menu/menu-e2e';
+import { TabsE2E } from '../tabs/tabs-e2e';
+
 
 export const routes: RouterConfig = [
-  {path: '', component: Home},
-  {path: 'button', component: ButtonE2E},
-  {path: 'menu', component: MenuE2E},
-  {path: 'icon', component: IconE2E},
-  {path: 'tabs', component: BasicTabs}
+  { path: '', component: Home },
+  { path: 'button', component: ButtonE2E },
+  { path: 'icon', component: IconE2E },
+  { path: 'menu', component: MenuE2E },
+  { path: 'tabs', component: TabsE2E }
 ];
 
 export const E2E_APP_ROUTE_PROVIDER = provideRouter(routes);
