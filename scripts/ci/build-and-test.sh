17c1131f73dc7a5373b9bf66c717693cb4d07c5e
Hans Larsen <hans@hansl.ca>
2016-08-18 14:55:30 -0700

    adding madge in gulp

--- scripts/ci/build-and-test.sh
+++ scripts/ci/build-and-test.sh
@@ -15,22 +15,13 @@ start_tunnel
 wait_for_tunnel
 if is_lint; then
   $(npm bin)/gulp ci:lint
-  $(npm bin)/gulp ci:forbidden-identifiers
 elif is_circular_deps_check; then
-  # Build first because madge needs the JavaScript output.
-  $(npm bin)/gulp build
-  npm run check-circular-deps
+  $(npm bin)/gulp ci:check-circular-deps
 elif is_e2e; then
-  # Run the e2e tests on the served e2e app.
-  echo "Starting e2e tests"
   $(npm bin)/gulp ci:e2e
 elif is_extract_metadata; then
   $(npm bin)/gulp ci:extract-metadata
 else
-  # Unit tests
   $(npm bin)/gulp ci:test
-#  npm run build
-#
-#  karma start test/karma.conf.js --single-run --no-auto-watch --reporters='dots'
 fi
 teardown_tunnel
--- tools/gulp/tasks/ci.ts
+++ tools/gulp/tasks/ci.ts
@@ -1,7 +1,8 @@
 import gulp = require('gulp');
+import {execTask} from '../task_helpers';
 
 
-gulp.task('ci:lint', ['lint']);
+gulp.task('ci:lint', ['ci:forbidden-identifiers', 'lint']);
 gulp.task('ci:test', ['test:single-run'], function() {
   process.exit(0);
 });
@@ -10,3 +11,5 @@ gulp.task('ci:extract-metadata', [':build:components:ngc']);
 gulp.task('ci:forbidden-identifiers', function() {
   require('../../../scripts/ci/forbidden-identifiers.js');
 });
+
+gulp.task('ci:check-circular-deps', ['build:release'], execTask('madge', ['--circular', './dist']));
