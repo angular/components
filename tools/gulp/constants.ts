2c2ad6bbaf2cdfa4291a6ebaf2bd2d2c88868dd4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-10-14 22:30:28 +0200

    chore: add autoprefixer to gulp
    * At the time of moving to gulp, the autoprefixer is no longer running against the CSS.
    
    * Re-introducing the autoprefixer for the "latest two versions" as in the README stated, does make sure that each properties are working within the supported browsers.

--- tools/gulp/constants.ts
+++ tools/gulp/constants.ts
@@ -6,6 +6,10 @@ export const SOURCE_ROOT = join(PROJECT_ROOT, 'src');
 export const DIST_ROOT = join(PROJECT_ROOT, 'dist');
 export const DIST_COMPONENTS_ROOT = join(DIST_ROOT, '@angular/material');
 
+export const SASS_AUTOPREFIXER_OPTIONS = {
+  browsers: ['last 2 versions'],
+  cascade: false,
+};
 
 export const NPM_VENDOR_FILES = [
   '@angular', 'core-js/client', 'hammerjs', 'rxjs', 'systemjs/dist', 'zone.js/dist'
