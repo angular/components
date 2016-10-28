b165c1e7efc65ff1c2cd838a381127bfa7874de7
Kacper Polak <kacper@hypequality.com>
2016-10-28 15:33:09 +0200

    tslint fix

--- src/lib/snack-bar/snack-bar-ref.ts
+++ src/lib/snack-bar/snack-bar-ref.ts
@@ -39,7 +39,7 @@ export class MdSnackBarRef<T> {
       });
     }
     // Clear autohide interval
-    if( this.autoHide ) {
+    if(this.autoHide !== false) {
       clearInterval(this.autoHide);
     }
   }
