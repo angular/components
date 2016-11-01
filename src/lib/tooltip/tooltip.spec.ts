6990b4aa2a414a1c21fbe32fb0f5e923d41680f4
Andrew Seguin <andrewjs@google.com>
2016-11-01 10:57:09 -0700

    remove fdescribe

--- src/lib/tooltip/tooltip.spec.ts
+++ src/lib/tooltip/tooltip.spec.ts
@@ -9,7 +9,7 @@ import {OverlayContainer} from '../core';
 
 const initialTooltipMessage = 'initial tooltip message';
 
-fdescribe('MdTooltip', () => {
+describe('MdTooltip', () => {
   let overlayContainerElement: HTMLElement;
 
 
