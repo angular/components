91cb21a4e235e5f71951d3b90d830fae2e23fc1a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-11-01 15:07:33 +0100

    chore(ci): only run within tunnel if necessary

--- scripts/ci/build-and-test.sh
+++ scripts/ci/build-and-test.sh
@@ -10,16 +10,12 @@ cd $(dirname $0)/../..
 source scripts/ci/sources/mode.sh
 source scripts/ci/sources/tunnel.sh
 
-start_tunnel
-
-wait_for_tunnel
 if is_lint; then
   $(npm bin)/gulp ci:lint
 elif is_e2e; then
-  $(npm bin)/gulp ci:e2e
+  run_in_tunnel "$(npm bin)/gulp ci:e2e"
 elif is_extract_metadata; then
   $(npm bin)/gulp ci:extract-metadata
 else
-  $(npm bin)/gulp ci:test
+  run_in_tunnel "$(npm bin)/gulp ci:test"
 fi
-teardown_tunnel
