7ae13d253674e416049317dba50f2e972bbd5728
Enmanuel <cxx.enlcxx@gmail.com>
2016-11-05 16:07:53 -0500

    fix

--- src/lib/module.ts
+++ src/lib/module.ts
@@ -30,7 +30,7 @@ import {MdToolbarModule} from './toolbar/index';
 import {MdTooltipModule} from './tooltip/index';
 import {MdMenuModule} from './menu/index';
 import {MdDialogModule} from './dialog/index';
-import {MdDatePickerModule} from './datePicker/index';
+import {MdDatepickerModule} from './datePicker/index';
 
 
 const MATERIAL_MODULES = [
@@ -61,7 +61,7 @@ const MATERIAL_MODULES = [
   RtlModule,
   A11yModule,
   StyleCompatibilityModule,
-  MdDatePickerModule,
+  MdDatepickerModule,
 ];
 
 @NgModule({
@@ -81,7 +81,7 @@ const MATERIAL_MODULES = [
     MdToolbarModule.forRoot(),
     PortalModule.forRoot(),
     RtlModule.forRoot(),
-    MdDatePickerModule.forRoot(),
+    MdDatepickerModule.forRoot(),
 
     // These modules include providers.
     A11yModule.forRoot(),
