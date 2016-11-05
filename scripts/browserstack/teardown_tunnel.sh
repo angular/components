91641fc26564d52972516618be5f9d53f9d03ade
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-11-05 19:23:57 +0100

    Wait for process to be dead (similar to Saucelabs teardown)

--- scripts/browserstack/teardown_tunnel.sh
+++ scripts/browserstack/teardown_tunnel.sh
@@ -5,8 +5,16 @@ set -e -o pipefail
 
 echo "Shutting down Browserstack tunnel"
 
+PID=$(cat $BROWSER_PROVIDER_READY_FILE);
+
 # Resolving the PID from the readyfile.
-kill $(cat $BROWSER_PROVIDER_READY_FILE)
+kill $PID
+
+
+while [[ -n `ps -ef | grep $PID | grep -v "grep"` ]]; do
+  printf "."
+  sleep .5
+done
 
 echo ""
 echo "Browserstack tunnel has been shut down"
