db9a11c918ce098a2f1a06929b82675e70acce42
Jeremy Elbourn <jelbourn@gmail.com>
2016-11-09 18:50:21 -0800

    Starter skeleton for Angular Material docs site (#1)

---
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/material-docs-app.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'material-docs-app',
+  templateUrl: 'material-docs-app.html',
+  styleUrls: ['material-docs-app.scss'],
+  host: {
+    '[class.docs-dark-theme]': 'isDarkTheme',
+  }
+})
+export class MaterialDocsApp {
+  isDarkTheme: boolean = false;
+}
