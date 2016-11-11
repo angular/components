34595c777086b58612fc14ab398a7df837d05009
Miles Malerba <mmalerba@google.com>
2016-11-10 17:48:24 -0800

    initial working demo

--- tools/gulp/tasks/docs.ts
+++ tools/gulp/tasks/docs.ts
@@ -1,7 +1,16 @@
 import gulp = require('gulp');
+const markdown = require('gulp-markdown');
+const transform = require('gulp-transform');
 
 
-gulp.task('docs', [':docs:md-to-html']);
-gulp.task(':docs:md-to-html', () => {
-  return gulp.src(['*.md'])
+const EXAMPLE_PATTERN = /<!--\W*example\(([^)]+)\)\W*-->/g;
+
+
+gulp.task('docs', () => {
+  return gulp.src(['src/lib/**/*.md'])
+      .pipe(markdown())
+      .pipe(transform((content: string) =>
+          content.toString().replace(EXAMPLE_PATTERN, (match: string, name: string) =>
+              `<div example="${name}"></div>`)))
+      .pipe(gulp.dest('dist/docs'));
 });
