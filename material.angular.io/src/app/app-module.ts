6d701e29ce7eb8a1514f789e20bffbac52f4d75a
tinayuangao <yuan.gao.tina@gmail.com>
2016-11-16 09:12:27 -0800

    Addressed comments (#9)

--- material.angular.io/src/app/app-module.ts
+++ material.angular.io/src/app/app-module.ts
@@ -5,17 +5,25 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {MaterialModule} from '@angular/material';
 import {MaterialDocsApp} from './material-docs-app';
+import {Homepage} from './pages/homepage/homepage';
+import {NavBar} from './shared/navbar/navbar';
+import {routing} from './routes';
+import {ComponentsList} from './pages/components/components';
 
 
 @NgModule({
   declarations: [
     MaterialDocsApp,
+    ComponentsList,
+    Homepage,
+    NavBar,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     MaterialModule.forRoot(),
+    routing,
   ],
   providers: [
     Location,
---
+++ material.angular.io/src/app/pages/components/components.ts
@@ -0,0 +1,18 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-components',
+  templateUrl: 'components.html',
+  styleUrls: ['components.scss']
+})
+export class ComponentsList {
+  componentItems = [
+    {name: 'Buttons', src: 'button'},
+    {name: 'Cards', src: 'card'},
+    {name: 'Chips', src: 'chip'},
+    {name: 'Grid lists', src: 'grid'},
+    {name: 'Menu', src: 'menu'},
+    {name: 'Tooltip', src: 'tooltip'},
+    {name: 'Progress', src: 'progress'},
+  ]
+}
