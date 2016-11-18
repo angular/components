b9856a1dd38a6c1d0c9085b1ca9f9986651d8a51
tinayuangao <yuan.gao.tina@gmail.com>
2016-11-18 13:25:27 -0800

    feat(homepage): Use static content in homepage (#16)

--- material.angular.io/src/app/pages/homepage/homepage.ts
+++ material.angular.io/src/app/pages/homepage/homepage.ts
@@ -5,40 +5,4 @@ import { Component} from '@angular/core';
   templateUrl: 'homepage.html',
   styleUrls: ['homepage.scss']
 })
-export class Homepage {
-  private _imagePath: string = '../assets/img/homepage/';
-
-  homePageContent = [
-    {
-      title: 'Sprint from Zero to App',
-      content: `Hit the ground running with comprehensive, modern UI components that work across
-        the web, mobile and desktop`,
-      reverse: false,
-      img: 'sprintzerotoapp.svg',
-    },
-    {
-      title: 'Fast and Consistent',
-      content: `Finely tunes performance, because every millisecond counts. Fully tested across
-        modern browsers.`,
-      reverse: true,
-      img: 'fastandconsistent.svg',
-    },
-    {
-      title: 'Versatile',
-      content: `Themable, for when you need to stay on brand or just have a facourite color.
-        Accessible and internationalized so that all users are welcome.`,
-      reverse: false,
-      img: 'versatile.svg',
-    },
-    {
-      title: 'Optimized for Angular',
-      content: 'Built by the Angular team to integrate seamlessly with Angular 2.',
-      reverse: true,
-      img: 'optimized.svg',
-    },
-  ];
-
-  getImagePath(srcSvg: string): string {
-    return this._imagePath + srcSvg;
-  }
-}
+export class Homepage {}
