8cd66422515c70f3cfef7791921dbcb333f81c8e
crisbeto <crisbeto@abv.bg>
2016-11-27 16:40:21 +0100

    feat(list): add keyboard navigation
    * Adds the ability to switch focus between list items using the up/down keys.
    * Splits up the various list-related components into separate files.
    
    Fixes #941.

--- e2e/components/list/list.e2e.ts
+++ e2e/components/list/list.e2e.ts
@@ -9,4 +9,38 @@ describe('list', () => {
     let container = element(by.css('md-list'));
     expect(container.isElementPresent(by.css('md-list-item'))).toBe(true);
   });
+
+  it('should be tabbable', () => {
+    pressKey(protractor.Key.TAB);
+    expectFocusOn(element(by.css('md-list')));
+  });
+
+  it('should shift focus between the list items', () => {
+    let items = element.all(by.css('md-list-item'));
+
+    pressKey(protractor.Key.TAB);
+    pressKey(protractor.Key.DOWN);
+    expectFocusOn(items.get(0));
+
+    pressKey(protractor.Key.DOWN);
+    expectFocusOn(items.get(1));
+
+    pressKey(protractor.Key.DOWN);
+    expectFocusOn(items.get(2));
+
+    pressKey(protractor.Key.UP);
+    expectFocusOn(items.get(1));
+
+    pressKey(protractor.Key.UP);
+    expectFocusOn(items.get(0));
+  });
+
+  // TODO: move to utility file. this was taken from the menu-page.ts
+  function expectFocusOn(el: any): void {
+    expect(browser.driver.switchTo().activeElement().getInnerHtml()).toBe(el.getInnerHtml());
+  }
+
+  function pressKey(key: string) {
+    browser.actions().sendKeys(key).perform();
+  }
 });
