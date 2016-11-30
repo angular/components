ccd7acb355788370151d3e6c4bb033bda4dbd0b0
Yuan Gao <tinagao@google.com>
2016-11-30 11:43:47 -0800

    Fix checked is not true when MdCheckbox initial checked value is true

--- src/lib/checkbox/checkbox.ts
+++ src/lib/checkbox/checkbox.ts
@@ -1,4 +1,5 @@
 import {
+    ChangeDetectorRef,
     ChangeDetectionStrategy,
     Component,
     ElementRef,
@@ -151,7 +152,9 @@ export class MdCheckbox implements ControlValueAccessor {
 
   hasFocus: boolean = false;
 
-  constructor(private _renderer: Renderer, private _elementRef: ElementRef) {
+  constructor(private _renderer: Renderer,
+              private _elementRef: ElementRef,
+              private _changeDetectorRef: ChangeDetectorRef,) {
     this.color = 'accent';
   }
 
@@ -169,6 +172,7 @@ export class MdCheckbox implements ControlValueAccessor {
       this._checked = checked;
       this._transitionCheckState(
           this._checked ? TransitionCheckState.Checked : TransitionCheckState.Unchecked);
+      this._changeDetectorRef.markForCheck();
     }
   }
 
