e5ece42ec3d6073483a40547929aa03c657019e2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-11-30 20:59:46 +0100

    Fix race conditions

--- scripts/browserstack/waitfor_tunnel.sh
+++ scripts/browserstack/waitfor_tunnel.sh
@@ -6,17 +6,20 @@ TUNNEL_LOG="$LOGS_DIR/browserstack-tunnel.log"
 # Time out if we wait for more than 2 minutes, so the process won't run forever.
 let "counter=0"
 
+# Exit the process if there are errors reported. Print the tunnel log to the console.
+if [ -f $BROWSER_PROVIDER_ERROR_FILE ]; then
+  echo
+  echo "An error occurred while starting the tunnel. See error:"
+  cat $TUNNEL_LOG
+  exit 5
+fi
+
 while [ ! -f $BROWSER_PROVIDER_READY_FILE ]; do
   let "counter++"
 
   if [ $counter -gt 240 ]; then
     echo
     echo "Timed out after 2 minutes waiting for tunnel ready file"
-    exit 5
-  elif [ -f $BROWSER_PROVIDER_ERROR_FILE ]; then
-    echo
-    echo "An error occurred while starting the tunnel. See error:"
-    cat $TUNNEL_LOG
     exit 5
   fi
 
