9537d1bf9c7f6a813f9668addc1535f5e9368fc5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-12-08 22:13:36 +0100

    Switch to new npm package

--- scripts/ci/after-success.sh
+++ scripts/ci/after-success.sh
@@ -3,12 +3,13 @@
 # Go to the project root directory
 cd $(dirname $0)/../..
 
-# Download and run the Travis After All script to retrieve information about other build modes.
-curl -Lo travis_after_all.py https://raw.github.com/dmakhno/travis_after_all/master/travis_after_all.py
-python travis_after_all.py
+RESULT=`$(npm bin)/travis-after-modes`
 
-if [ "$BUILD_LEADER" = "YES" ] && [ "$BUILD_AGGREGATE_STATUS" = "others_succeeded" ]; then
 
-  echo "All other builds succeeded. Publishing build"
-  # ./scripts/release/publish-build-artifacts.sh
+echo "Result: $RESULT"
+
+if [ "$RESULT" = "PASSED" ]; then
+  echo "Everything passed"
+else
+  echo "Something failed"
 fi
