4dc9394f73926966e729e80abb42980416132f2f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-12-09 21:05:00 +0100

    Address comments

--- scripts/ci/after-success.sh
+++ scripts/ci/after-success.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Script which always runs when the current Travis mode succeeds.
+# Used to run the travis-after-modes script, which checks if all other modes finished.
+
 # Go to the project root directory
 cd $(dirname $0)/../..
 
-RESULT=`$(npm bin)/travis-after-modes`
+npm_binaries=$(npm bin)
+ci_result=$($npm_binaries/travis-after-modes)
 
-if [ "$RESULT" = "PASSED" ] && [ -z "$TRAVIS_PULL_REQUEST" ]; then
+if [ "$ci_result" = "PASSED" ] && [ -z "$TRAVIS_PULL_REQUEST" ]; then
   echo "All travis modes passed. Publishing the build artifacts..."
   ./scripts/release/publish-build-artifacts.sh
 fi
--- scripts/release/publish-build-artifacts.sh
+++ scripts/release/publish-build-artifacts.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
 
+# Script to publish the build artifacts to a GitHub repository.
+# Builds will be automatically published once new changes are made to the repository.
+
 # Go to the project root directory
 cd $(dirname $0)/../..
 
-BUILD_DIR="dist/@angular/material"
-BUILD_VERSION=`sed -nE 's/^\s*"version": "(.*?)",$/\1/p' package.json`
+build_dir="dist/@angular/material"
+build_version=$(sed -nE 's/^\s*"version": "(.*?)",$/\1/p' package.json)
 
-COMMIT_SHA=`git rev-parse --short HEAD`
-COMMIT_AUTHOR=`git --no-pager show -s --format='%an <%ae>' HEAD`
-COMMIT_MESSAGE=`git log --oneline | head -n1`
+commit_sha=$(git rev-parse --short HEAD)
+commit_author=$(git --no-pager show -s --format='%an <%ae>' HEAD)
+commit_message=$(git log --oneline | head -n1)
 
-REPO_NAME="material-builds"
-REPO_URL="http://github.com/DevVersion/material-builds.git"
-REPO_DIR="tmp/$REPO_NAME"
+repo_name="material-builds"
+repo_url="http://github.com/DevVersion/material-builds.git"
+repo_dir="tmp/$repo_name"
 
 # Create a release of the current repository.
 $(npm bin)/gulp build:release
 
 # Prepare cloning the builds repository
-rm -rf $REPO_DIR
-mkdir -p $REPO_DIR
+rm -rf $repo_dir
+mkdir -p $repo_dir
 
 # Clone the repository
-git clone $REPO_URL $REPO_DIR
+git clone $repo_url $repo_dir
 
 # Copy the build files to the repository
-rm -rf $REPO_DIR/*
-cp -r $BUILD_DIR/* $REPO_DIR
+rm -rf $repo_dir/*
+cp -r $build_dir/* $repo_dir
 
 # Create the build commit and push the changes to the repository.
-cd $REPO_DIR &&
+cd $repo_dir &&
 
 # Setup the git repository authentication.
 git config credential.helper "store --file=.git/credentials" &&
 echo "$MATERIAL2_BUILDS_TOKEN" > .git/credentials
 
 git add -A &&
-git commit -m "$COMMIT_MESSAGE" --author "$COMMIT_AUTHOR" &&
-git tag "$BUILD_VERSION-$COMMIT_SHA" &&
+git commit -m "$commit_message" --author "$commit_author" &&
+git tag "$build_version-$commit_sha" &&
 git push origin master --tags
 
 echo "Finished publishing build artifacts"
