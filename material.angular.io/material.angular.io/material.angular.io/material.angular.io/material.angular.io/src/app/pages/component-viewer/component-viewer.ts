41bb3d1d7bf2f0e06f68a2d8925bb59d84abd0ae
Jeremy Elbourn <jelbourn@google.com>
2016-12-16 13:26:43 -0800

    Add skeleton component viewer

---
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/pages/component-viewer/component-viewer.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-components',
+  templateUrl: './component-viewer.html',
+  styleUrls: ['./component-viewer.scss']
+})
+export class ComponentViewer {
+}
