ec5cbb1af8f1799d4b3266a77c11599b1d92bd21
crisbeto <crisbeto@abv.bg>
2016-12-16 21:46:14 +0100

    Fix tabs CSS issue and switch mdDynamicHeight to dynamicHeight.

--- src/lib/tabs/tab-group.ts
+++ src/lib/tabs/tab-group.ts
@@ -48,6 +48,9 @@ export class MdTabChangeEvent {
   selector: 'md-tab-group',
   templateUrl: 'tab-group.html',
   styleUrls: ['tab-group.css'],
+  host: {
+    '[class.md-tab-group-dynamic-height]': '_dynamicHeight'
+  }
 })
 export class MdTabGroup {
   @ContentChildren(MdTab) _tabs: QueryList<MdTab>;
@@ -65,7 +68,7 @@ export class MdTabGroup {
 
   /** Whether the tab group should grow to the size of the active tab */
   private _dynamicHeight: boolean = false;
-  @Input('mdDynamicHeight') set dynamicHeight(value: boolean) {
+  @Input() set dynamicHeight(value: boolean) {
     this._dynamicHeight = coerceBooleanProperty(value);
   }
 
