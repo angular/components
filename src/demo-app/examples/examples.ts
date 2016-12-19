585acdc03e99d37df7117caafc639eb9701ace71
Miles Malerba <mmalerba@google.com>
2016-12-19 09:02:25 -0800

    refactor directories

--- src/demo-app/examples/examples.ts
+++ src/demo-app/examples/examples.ts
@@ -9,17 +9,17 @@ import {Component, ViewEncapsulation} from '@angular/core';
 })
 export class Examples {
   navItems = [
-    {name: 'Slider Overview', route: 'slider/overview'},
-    {name: 'Configurable Slider', route: 'slider/configurable'},
-    {name: 'Sidenav Overview', route: 'sidenav/overview'},
-    {name: 'Sidenav with FAB', route: 'sidenav/fab'},
-    {name: 'Input Overview', route: 'input/overview'},
-    {name: 'Input Form', route: 'input/form'},
-    {name: 'Button Overview', route: 'button/overview'},
-    {name: 'Button Types', route: 'button/types'},
-    {name: 'Card Overview', route: 'card/overview'},
-    {name: 'Fancy Card', route: 'card/fancy'},
-    {name: 'Checkbox Overview', route: 'checkbox/overview'},
-    {name: 'Configurable Checkbox', route: 'checkbox/configurable'},
+    {name: 'Slider Overview', route: 'slider-overview'},
+    {name: 'Configurable Slider', route: 'slider-configurable'},
+    {name: 'Sidenav Overview', route: 'sidenav-overview'},
+    {name: 'Sidenav with FAB', route: 'sidenav-fab'},
+    {name: 'Input Overview', route: 'input-overview'},
+    {name: 'Input Form', route: 'input-form'},
+    {name: 'Button Overview', route: 'button-overview'},
+    {name: 'Button Types', route: 'button-types'},
+    {name: 'Card Overview', route: 'card-overview'},
+    {name: 'Fancy Card', route: 'card-fancy'},
+    {name: 'Checkbox Overview', route: 'checkbox-overview'},
+    {name: 'Configurable Checkbox', route: 'checkbox-configurable'},
   ];
 }
