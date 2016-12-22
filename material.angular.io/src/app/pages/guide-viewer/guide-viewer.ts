29a946153c59386ce14d8b596a3a609ff5b28876
Jeremy Elbourn <jelbourn@gmail.com>
2016-12-22 09:13:25 -0800

    Add guides (getting-started, theming) (#51)
    * Add guides
    
    * Update homepage get-started link
    
    * Update second get-started link on homepage
    
    * fix test

--- material.angular.io/src/app/pages/guide-viewer/guide-viewer.ts
+++ material.angular.io/src/app/pages/guide-viewer/guide-viewer.ts
@@ -1,20 +1,19 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {GuideItems} from '../../shared/guide-items/guide-items';
+import {GuideItems, GuideItem} from '../../shared/guide-items/guide-items';
 
 
 @Component({
   selector: 'guide-viewer',
   templateUrl: './guide-viewer.html',
   styleUrls: ['./guide-viewer.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class GuideViewer {
-  documentUrl: string;
+  guide: GuideItem;
 
   constructor(private _route: ActivatedRoute, public guideItems: GuideItems) {
     _route.params.subscribe(p => {
-      this.documentUrl = guideItems.getItemById(p['id']).document;
+      this.guide = guideItems.getItemById(p['id']);
     });
   }
 }
