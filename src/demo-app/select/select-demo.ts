8d18e6e6bf47be877b1554816d26950283d591c9
crisbeto <crisbeto@abv.bg>
2016-12-30 14:33:09 +0200

    Separate the change event example from the ones that use the form directives.

--- src/demo-app/select/select-demo.ts
+++ src/demo-app/select/select-demo.ts
@@ -13,6 +13,7 @@ export class SelectDemo {
   isDisabled = false;
   showSelect = false;
   currentDrink: string;
+  latestChangeEvent: MdSelectChange;
   foodControl = new FormControl('pizza-1');
 
   foods = [
@@ -33,11 +34,13 @@ export class SelectDemo {
     {value: 'milk-8', viewValue: 'Milk'},
   ];
 
+  pokemon = [
+    {value: 'bulbasaur-0', viewValue: 'Bulbasaur'},
+    {value: 'charizard-1', viewValue: 'Charizard'},
+    {value: 'squirtle-2', viewValue: 'Squirtle'}
+  ];
+
   toggleDisabled() {
     this.foodControl.enabled ? this.foodControl.disable() : this.foodControl.enable();
   }
-
-  changeListener(event: MdSelectChange) {
-    console.log(event);
-  };
 }
