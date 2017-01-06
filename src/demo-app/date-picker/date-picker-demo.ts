60f35b4b8f21754211f28278fa7e1fe0a1c8ac98
Miles Malerba <mmalerba@google.com>
2017-01-05 16:09:03 -0800

    date picker initial commit

---
+++ src/demo-app/date-picker/date-picker-demo.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'date-picker-demo',
+  templateUrl: 'date-picker-demo.html',
+  styleUrls: ['date-picker-demo.css']
+})
+export class DatePickerDemo {}
