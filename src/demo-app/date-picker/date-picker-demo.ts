a8dd30872afceb47b1f522a2ba167383dfc95e63
Miles Malerba <mmalerba@google.com>
2017-01-06 16:33:20 -0800

    added month view functionality

--- src/demo-app/date-picker/date-picker-demo.ts
+++ src/demo-app/date-picker/date-picker-demo.ts
@@ -7,4 +7,7 @@ import {Component} from '@angular/core';
   templateUrl: 'date-picker-demo.html',
   styleUrls: ['date-picker-demo.css']
 })
-export class DatePickerDemo {}
+export class DatePickerDemo {
+  date = new Date();
+  selected = new Date('1/10/2017');
+}
