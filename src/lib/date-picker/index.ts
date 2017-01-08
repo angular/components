5ac9fa9b7d02ec70e248aebae5be007e52165bee
Miles Malerba <mmalerba@google.com>
2017-01-07 18:21:07 -0800

    base month view on calendar table

--- src/lib/date-picker/index.ts
+++ src/lib/date-picker/index.ts
@@ -1,14 +1,17 @@
-import {ModuleWithProviders, NgModule} from '@angular/core';
+import {NgModule, ModuleWithProviders} from '@angular/core';
 import {MdMonthView} from './month-view';
 import {DefaultStyleCompatibilityModeModule} from '../core/compatibility/default-mode';
 import {CommonModule} from '@angular/common';
+import {MdCalendarTable} from './calendar-table';
 
 export * from './month-view';
+export * from './calendar-table';
+
 
 @NgModule({
   imports: [CommonModule, DefaultStyleCompatibilityModeModule],
-  exports: [MdMonthView, DefaultStyleCompatibilityModeModule],
-  declarations: [MdMonthView],
+  exports: [MdMonthView, MdCalendarTable, DefaultStyleCompatibilityModeModule],
+  declarations: [MdMonthView, MdCalendarTable],
 })
 export class MdDatePickerModule {
   static forRoot(): ModuleWithProviders {
