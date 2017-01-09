266622f46b320a094960932463cdb35300624748
Miles Malerba <mmalerba@google.com>
2017-01-09 15:45:06 -0800

    fixed aot and added comment

--- src/demo-app/date-picker/date-picker-demo.ts
+++ src/demo-app/date-picker/date-picker-demo.ts
@@ -4,8 +4,7 @@ import {Component} from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'date-picker-demo',
-  templateUrl: 'date-picker-demo.html',
-  styleUrls: ['date-picker-demo.css']
+  templateUrl: 'date-picker-demo.html'
 })
 export class DatePickerDemo {
   date = new Date();
