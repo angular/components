1e078ab3b4063a6e300a63665292359a0f28e976
Kara Erickson <karakara@google.com>
2017-01-13 11:04:11 -0800

    feat(autocomplete): add value support

--- src/lib/autocomplete/autocomplete-trigger.ts
+++ src/lib/autocomplete/autocomplete-trigger.ts
@@ -1,10 +1,15 @@
-import {Directive, ElementRef, Input, ViewContainerRef, OnDestroy} from '@angular/core';
+import {
+  Directive, ElementRef, Input, ViewContainerRef, Optional, OnDestroy
+} from '@angular/core';
+import {NgControl} from '@angular/forms';
 import {Overlay, OverlayRef, OverlayState, TemplatePortal} from '../core';
 import {MdAutocomplete} from './autocomplete';
 import {PositionStrategy} from '../core/overlay/position/position-strategy';
 import {Observable} from 'rxjs/Observable';
-import {Subscription} from 'rxjs/Subscription';
+import {MdOptionSelectEvent} from '../core/option/option';
 import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/switchMap';
 
 /** The panel needs a slight y-offset to ensure the input underline displays. */
 export const MD_AUTOCOMPLETE_PANEL_OFFSET = 6;
@@ -20,14 +25,12 @@ export class MdAutocompleteTrigger implements OnDestroy {
   private _portal: TemplatePortal;
   private _panelOpen: boolean = false;
 
-  /** The subscription to events that close the autocomplete panel. */
-  private _closingActionsSubscription: Subscription;
-
   /* The autocomplete panel to be attached to this trigger. */
   @Input('mdAutocomplete') autocomplete: MdAutocomplete;
 
   constructor(private _element: ElementRef, private _overlay: Overlay,
-              private _viewContainerRef: ViewContainerRef) {}
+              private _viewContainerRef: ViewContainerRef,
+              @Optional() private _controlDir: NgControl) {}
 
   ngOnDestroy() { this._destroyPanel(); }
 
@@ -44,8 +47,7 @@ export class MdAutocompleteTrigger implements OnDestroy {
 
     if (!this._overlayRef.hasAttached()) {
       this._overlayRef.attach(this._portal);
-      this._closingActionsSubscription =
-          this.panelClosingActions.subscribe(() => this.closePanel());
+      this._subscribeToClosingActions();
     }
 
     this._panelOpen = true;
@@ -57,7 +59,6 @@ export class MdAutocompleteTrigger implements OnDestroy {
       this._overlayRef.detach();
     }
 
-    this._closingActionsSubscription.unsubscribe();
     this._panelOpen = false;
   }
 
@@ -75,6 +76,25 @@ export class MdAutocompleteTrigger implements OnDestroy {
     return this.autocomplete.options.map(option => option.onSelect);
   }
 
+
+  /**
+   * This method listens to a stream of panel closing actions and resets the
+   * stream every time the option list changes.
+   */
+  private _subscribeToClosingActions(): void {
+    // Every time the option list changes...
+    this.autocomplete.options.changes
+    // and also at initialization, before there are any option changes...
+        .startWith(null)
+        // create a new stream of panelClosingActions, replacing any previous streams
+        // that were created, and flatten it so our stream only emits closing events...
+        .switchMap(() => this.panelClosingActions)
+        // when the first closing event occurs...
+        .first()
+        // set the value, close the panel, and complete.
+        .subscribe(event => this._setValueAndClose(event));
+  }
+
   /** Destroys the autocomplete suggestion panel. */
   private _destroyPanel(): void {
     if (this._overlayRef) {
@@ -82,6 +102,22 @@ export class MdAutocompleteTrigger implements OnDestroy {
       this._overlayRef.dispose();
       this._overlayRef = null;
     }
+  }
+
+   /**
+   * This method closes the panel, and if a value is specified, also sets the associated
+   * control to that value. It will also mark the control as dirty if this interaction
+   * stemmed from the user.
+   */
+  private _setValueAndClose(event: MdOptionSelectEvent | null): void {
+    if (event) {
+      this._controlDir.control.setValue(event.source.value);
+      if (event.isUserInput) {
+        this._controlDir.control.markAsDirty();
+      }
+    }
+
+    this.closePanel();
   }
 
   private _createOverlay(): void {
