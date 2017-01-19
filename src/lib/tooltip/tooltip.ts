1ef64942d3e50e332cf2c25d5e33a8976759e300
Thomas Pink <thomas.pink@me.com>
2017-01-19 20:04:41 +0100

    changed tooltip detectChanges to markForCheck

--- src/lib/tooltip/tooltip.ts
+++ src/lib/tooltip/tooltip.ts
@@ -287,7 +287,7 @@ export class TooltipComponent {
   /** Subject for notifying that the tooltip has been hidden from the view */
   private _onHide: Subject<any> = new Subject();
 
-  constructor(@Optional() private _dir: Dir, private _ref: ChangeDetectorRef) {}
+  constructor(@Optional() private _dir: Dir, private _changeDetectorRef: ChangeDetectorRef) {}
 
   /**
    * Shows the tooltip with an animation originating from the provided origin
@@ -311,9 +311,9 @@ export class TooltipComponent {
       // trigger interaction and close the tooltip right after it was displayed.
       this._closeOnInteraction = false;
 
-      // Call change detection manually so tooltip will also work 
-      // if any parent component has set the ChangeDetectionStrategy to OnPush
-      this._ref.detectChanges();
+      // Mark for check so if any parent component has set the 
+      // ChangeDetectionStrategy to OnPush it will be checked anyways
+      this._changeDetectorRef.markForCheck();
       setTimeout(() => { this._closeOnInteraction = true; }, 0);
     }, delay);
   }
@@ -332,9 +332,9 @@ export class TooltipComponent {
       this._visibility = 'hidden';
       this._closeOnInteraction = false;
 
-      // Call change detection manually so tooltip will also work 
-      // if any parent component has set the ChangeDetectionStrategy to OnPush
-      this._ref.detectChanges();
+      // Mark for check so if any parent component has set the 
+      // ChangeDetectionStrategy to OnPush it will be checked anyways
+      this._changeDetectorRef.markForCheck();
     }, delay);
   }
 
