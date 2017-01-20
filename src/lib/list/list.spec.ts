734aad9b3f216ec4847191a63384222ee8e6fe27
Andrew Seguin <andrewjs@google.com>
2017-01-20 10:56:43 -0800

    final test fix

--- src/lib/list/list.spec.ts
+++ src/lib/list/list.spec.ts
@@ -98,7 +98,7 @@ describe('MdList', () => {
     fixture.detectChanges();
 
     let listItem = fixture.debugElement.children[0].query(By.css('md-list-item'));
-    expect(listItem.nativeElement.className).toBe('mat-2-line mat-list-item');
+    expect(listItem.nativeElement.className).toBe('mat-list-item mat-2-line');
 
     fixture.debugElement.componentInstance.showThirdLine = true;
     fixture.detectChanges();
