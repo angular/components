cdd4f43b6d0f3e36848b3d5929aa4780db6ba029
crisbeto <crisbeto@abv.bg>
2017-01-28 16:21:06 +0100

    fix: switch to using getComputedStyle

--- src/lib/core/theming/theme-check.ts
+++ src/lib/core/theming/theme-check.ts
@@ -1,39 +1,40 @@
 import {NgModule, isDevMode} from '@angular/core';
 
+/** Whether the theme presence has already been checked. */
+let hasBeenChecked = false;
+
 /**
  * Module that verifies that the user has loaded the core theming file,
  * without which most Material module won't work as expected.
+ *
+ * Note on testing methodology: A more efficient way to check for the theme
+ * would be to loop through the `document.styleSheets`, however most browsers
+ * don't expose the stylesheet rules, if the file was loaded from another domain.
+ * This would trigger false positives if the theme is being loaded from a CDN.
+ *
  * @docs-private
  */
 @NgModule()
 export class MdThemeCheckModule {
   constructor() {
-    if (!isDevMode() || typeof document === 'undefined') {
+    if (hasBeenChecked || typeof document === 'undefined' || !isDevMode()) {
       return;
     }
 
-    for (let i = 0; i < document.styleSheets.length; i++) {
-      // The try/catch is needed, because some browsers can throw a security
-      // error when accessing the `cssRules` from another domain.
-      try {
-        let rules = (document.styleSheets.item(i) as CSSStyleSheet).cssRules;
+    let testElement = document.createElement('div');
 
-        if (rules) {
-          for (let j = 0; j < rules.length; j++) {
-            let selector = (rules.item(j) as CSSStyleRule).selectorText;
+    testElement.classList.add('md-theme-loaded-marker');
+    document.body.appendChild(testElement);
 
-            if (selector && selector.includes('.md-theme-loaded-marker')) {
-              return;
-            }
-          }
-        }
-      } catch (e) { }
+    if (getComputedStyle(testElement).display !== 'none') {
+      console.warn(
+        'Could not find Angular Material core theme. Most Material ' +
+        'components may not work as expected. For more info refer ' +
+        'to the theming guide: https://github.com/angular/material2/blob/master/guides/theming.md'
+      );
     }
 
-    console.warn(
-      'Could not find Angular Material core theme. Most Material ' +
-      'components may not work as expected. For more info refer ' +
-      'to the theming guide: https://github.com/angular/material2/blob/master/guides/theming.md'
-    );
+    document.body.removeChild(testElement);
+    hasBeenChecked = true;
   }
 }
