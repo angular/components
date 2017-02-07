7adada1cb1791fff1bff531f94c0b76bee029b8e
mmalerba <mmalerba@google.com>
2017-02-07 13:59:25 -0800

    feat(datepicker): add month & year view (#2904)
    * date picker initial commit
    
    * month view
    
    * added month view functionality
    
    * more month view tweaking
    
    * started extracting common stuff to calendar-table.
    
    * base month view on calendar table
    
    * added year view
    
    * add disclaimers
    
    * addressed comments
    
    * fix lint
    
    * fixed aot and added comment
    
    * started on tests
    
    * calendar table tests
    
    * tests for month and year view
    
    * rebase on top of CalendarLocale & SimpleDate
    
    * add some additional functionality to SimpleDate
    
    * fix lint
    
    * addressed comments
    
    * add comment

--- src/lib/core/datetime/simple-date.ts
+++ src/lib/core/datetime/simple-date.ts
@@ -3,12 +3,63 @@
  * details and the time component of the native Date.
  */
 export class SimpleDate {
+  /**
+   * Create a SimpleDate from a native JS Date object.
+   * @param nativeDate The native JS Date object to convert.
+   */
   static fromNativeDate(nativeDate: Date) {
     return new SimpleDate(nativeDate.getFullYear(), nativeDate.getMonth(), nativeDate.getDate());
   }
 
-  constructor(public year: number, public month: number, public date: number) {}
+  /** Creates a SimpleDate object representing today. */
+  static today() {
+    return SimpleDate.fromNativeDate(new Date());
+  }
 
+  /** The native JS Date. */
+  private _date: Date;
+
+  constructor(year: number, month: number, date: number) {
+    this._date = new Date(year, month, date);
+    // We need to correct for the fact that JS native Date treats years in range [0, 99] as
+    // abbreviations for 19xx.
+    if (year >= 0 && year < 100) {
+      this._date = new Date(this._date.setFullYear(this.year - 1900));
+    }
+  }
+
+  /** The year component of this date. */
+  get year() {
+    return this._date.getFullYear();
+  }
+
+  /** The month component of this date. (0-indexed, 0 = January). */
+  get month() {
+    return this._date.getMonth();
+  }
+
+  /** The date component of this date. (1-indexed, 1 = 1st of month). */
+  get date() {
+    return this._date.getDate();
+  }
+
+  /** The day component of this date. (0-indexed, 0 = Sunday) */
+  get day() {
+    return this._date.getDay();
+  }
+
+  /**
+   * Adds an amount of time (in days, months, and years) to the date.
+   * @param amount The amount of time to add.
+   */
+  add(amount: {days: number, months: number, years: number}) {
+    return new SimpleDate(
+        this.year + amount.years || 0,
+        this.month + amount.months || 0,
+        this.date + amount.days || 0);
+  }
+
+  /** Converts the SimpleDate to a native JS Date object. */
   toNativeDate() {
     return new Date(this.year, this.month, this.date);
   }
