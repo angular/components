691fb2260e42d70d0104a957cd8f95bc54769beb
mmalerba <mmalerba@google.com>
2017-02-07 13:59:25 -0800

    feat(datepicker): add month & year view (#2904)
    * date picker initial commit
    
    * month view
    
    * added month view functionality
    
    * more month view tweaking
    
    * started extracting common stuff to calendar-table.
    
    * base month view on calendar table
    
    * added year view
    
    * add disclaimers
    
    * addressed comments
    
    * fix lint
    
    * fixed aot and added comment
    
    * started on tests
    
    * calendar table tests
    
    * tests for month and year view
    
    * rebase on top of CalendarLocale & SimpleDate
    
    * add some additional functionality to SimpleDate
    
    * fix lint
    
    * addressed comments
    
    * add comment

--- src/demo-app/demo-app-module.ts
+++ src/demo-app/demo-app-module.ts
@@ -38,6 +38,7 @@ import {PlatformDemo} from './platform/platform-demo';
 import {AutocompleteDemo} from './autocomplete/autocomplete-demo';
 import {InputContainerDemo} from './input/input-container-demo';
 import {StyleDemo} from './style/style-demo';
+import {DatepickerDemo} from './datepicker/datepicker-demo';
 
 @NgModule({
   imports: [
@@ -56,6 +57,7 @@ import {StyleDemo} from './style/style-demo';
     CardDemo,
     ChipsDemo,
     CheckboxDemo,
+    DatepickerDemo,
     DemoApp,
     DialogDemo,
     GesturesDemo,
