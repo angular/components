7e2d8a7faa76be56ebf4d6b5a9f19fc8cef7f2e4
mmalerba <mmalerba@google.com>
2017-02-14 14:20:07 -0800

    feat(datepicker): add calendar component that pulls together month and year views (#2994)
    * create a calendar component that pulls together month and year views
    
    styling still needs some love
    
    * fix typo

--- src/demo-app/datepicker/datepicker-demo.ts
+++ src/demo-app/datepicker/datepicker-demo.ts
@@ -5,9 +5,11 @@ import {SimpleDate} from '@angular/material';
 @Component({
   moduleId: module.id,
   selector: 'datepicker-demo',
-  templateUrl: 'datepicker-demo.html'
+  templateUrl: 'datepicker-demo.html',
+  styleUrls: ['datepicker-demo.css'],
 })
 export class DatepickerDemo {
+  startAt = new SimpleDate(2017, 0, 1);
   date = SimpleDate.today();
   selected: SimpleDate;
 }
