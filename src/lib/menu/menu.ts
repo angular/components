12e3e4425a1ed18704cfd9753372916117bb5714
nayfin <natjker@gmail.com>
2017-03-01 20:52:38 -0600

    feat(menu): allow use of input components in menu
    use attribute directive md-menu-input-item on input components (e.g.
    md-slider, md-input, etc) inside of menu to prevent menu from closing when
    components are focused via click or tab. this will allow users to use menu
    as a short form to input data without redirecting to another page or
    calling a dialog box.
    
    closes issue 2612

--- src/lib/menu/menu.ts
+++ src/lib/menu/menu.ts
@@ -3,10 +3,12 @@ import {CommonModule} from '@angular/common';
 import {OverlayModule, CompatibilityModule} from '../core';
 import {MdMenu} from './menu-directive';
 import {MdMenuItem} from './menu-item';
+import {MdMenuInputItem} from './menu-input-item';
 import {MdMenuTrigger} from './menu-trigger';
 import {MdRippleModule} from '../core/ripple/ripple';
 export {MdMenu} from './menu-directive';
 export {MdMenuItem} from './menu-item';
+export {MdMenuInputItem} from './menu-input-item';
 export {MdMenuTrigger} from './menu-trigger';
 export {MdMenuPanel} from './menu-panel';
 export {MenuPositionX, MenuPositionY} from './menu-positions';
@@ -14,8 +16,8 @@ export {MenuPositionX, MenuPositionY} from './menu-positions';
 
 @NgModule({
   imports: [OverlayModule, CommonModule, MdRippleModule, CompatibilityModule],
-  exports: [MdMenu, MdMenuItem, MdMenuTrigger, CompatibilityModule],
-  declarations: [MdMenu, MdMenuItem, MdMenuTrigger],
+  exports: [MdMenu, MdMenuItem, MdMenuInputItem, MdMenuTrigger, CompatibilityModule],
+  declarations: [MdMenu, MdMenuItem, MdMenuInputItem, MdMenuTrigger],
 })
 export class MdMenuModule {
   /** @deprecated */
