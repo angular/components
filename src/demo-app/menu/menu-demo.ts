58d004904743383adc2993610db33b1094c3a02e
nayfin <natjkern@gmail.com>
2017-03-01 23:17:39 -0600

    feat(menu): allow use of input components in menu
    use attribute directive md-menu-input-item on input components (e.g.
    md-slider, md-input, etc) inside of menu to prevent menu from closing when
    components are focused via click or tab. this will allow users to use menu
    as a short form to input data without redirecting to another page or
    calling a dialog box. Add styles to menu.scss to space and stretch
    components
    properly. Add additional card to menu-demo to demonstrate use with all
    input
    components.
    
    closes issue 2612

--- src/demo-app/menu/menu-demo.ts
+++ src/demo-app/menu/menu-demo.ts
@@ -21,6 +21,28 @@ export class MenuDemo {
     {text: 'Check voicemail', icon: 'voicemail', disabled: true},
     {text: 'Disable alerts', icon: 'notifications_off'}
   ];
-
+  // User data pulled from menu form
+  user = {
+    email: '',
+    subscribe: false,
+    favoriteSeason: 'Autumn',
+    favoriteDrink: 'Lemonade',
+    isVegetarian: false,
+    age: 0,
+  };
+  // Select box option in menu form
+  drinks = [
+    {value: 'coke-0', viewValue: 'Coke'},
+    {value: 'long-name-1', viewValue: 'Decaf Chocolate Brownie Vanilla Gingerbread Frappuccino'},
+    {value: 'water-2', viewValue: 'Water'},
+    {value: 'pepper-3', viewValue: 'Dr. Pepper'},
+  ];
+  // Radio option in menu form
+  seasonOptions = [
+    'Winter',
+    'Spring',
+    'Summer',
+    'Autumn',
+  ];
   select(text: string) { this.selected = text; }
 }
