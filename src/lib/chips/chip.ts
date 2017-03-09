profoundry-us/material2

feat(chips): Add remove functionality/styling.
Add events, styling and keyboard controls to allow removable chips.

 - Add basic styling for a user-provided remove icon.
 - Add keyboard controls for backspace/delete.
 - Add `(remove)` event which is emitted when the remove icon or
   one of the delete keys is pressed.
 - Add `md-chip-remove` directive which can be applied to `<md-icon>`
   (or others) to inform the chip of the remove request.

Add new directive `mdChipInput` for controlling:
 - `(chipAdded)` - Event fired when a chip should be added.
 - `[separatorKeys]` - The list of keycodes that will fire the
   `(chipAdded)` event.
 - `[addOnBlur]` - Whether or not to fire the `(chipAdded)` event
   when the input is blurred.

Additionally, fix some issues with dark theme and add styling/support
for usage inside the `md-input-container` and add styling for focused
chips.

BREAKING CHANGE - The `selectable` property of the `md-chip-list` has
now been moved to `md-chip` to maintain consistency with the new
`removable` option.

If you used the following code,

```html
<md-chip-list [selectable]="selectable">
  <md-chip>My Chip</md-chip>
</md-chip-list>
```

you should switch it to

```html
<md-chip-list>
  <md-chip [selectable]="selectable">My Chip</md-chip>
</md-chip-list>
```

References #120. Closes #3143.

Hat tip to @willshowell for suggestion to use secondary-text
for focus color :-)

src/lib/chips/chip.ts
@@ -11,6 +11,7 @@ import {
 
 import {Focusable} from '../core/a11y/focus-key-manager';
 import {coerceBooleanProperty} from '../core/coercion/boolean-property';
+import {SPACE, BACKSPACE, DELETE} from '../core/keyboard/keycodes';
 
 export interface MdChipEvent {
   chip: MdChip;
@@ -22,17 +23,19 @@ export interface MdChipEvent {
 @Component({
   selector: `md-basic-chip, [md-basic-chip], md-chip, [md-chip],
              mat-basic-chip, [mat-basic-chip], mat-chip, [mat-chip]`,
-  template: `<ng-content></ng-content>`,
+  template: `<ng-content></ng-content><div class="md-chip-focus-border"></div>`,
   host: {
     '[class.mat-chip]': 'true',
     'tabindex': '-1',
     'role': 'option',
 
     '[class.mat-chip-selected]': 'selected',
+    '[class.mat-chip-has-remove-icon]': '_hasRemoveIcon',
     '[attr.disabled]': 'disabled',
     '[attr.aria-disabled]': '_isAriaDisabled',
 
-    '(click)': '_handleClick($event)'
+    '(click)': '_handleClick($event)',
+    '(keydown)': '_handleKeydown($event)'
   }
 })
 export class MdChip implements Focusable, OnInit, OnDestroy {
@@ -40,14 +43,26 @@ export class MdChip implements Focusable, OnInit, OnDestroy {
   /** Whether or not the chip is disabled. Disabled chips cannot be focused. */
   protected _disabled: boolean = null;
 
+  /** Whether or not the chip is selectable. */
+  protected _selectable: boolean = true;
+
+  /** Whether or not the chip is removable. */
+  protected _removable: boolean = true;
+
   /** Whether or not the chip is selected. */
   protected _selected: boolean = false;
 
   /** The palette color of selected chips. */
   protected _color: string = 'primary';
 
+  /** Whether or not the chip is displaying the remove icon. */
+  _hasRemoveIcon: boolean = false;
+
   /** Emitted when the chip is focused. */
   onFocus = new EventEmitter<MdChipEvent>();
+
+  /** Emitted when the removable property changes. */
+  onRemovableChange = new EventEmitter<boolean>();
 
   /** Emitted when the chip is selected. */
   @Output() select = new EventEmitter<MdChipEvent>();
@@ -58,7 +73,11 @@ export class MdChip implements Focusable, OnInit, OnDestroy {
   /** Emitted when the chip is destroyed. */
   @Output() destroy = new EventEmitter<MdChipEvent>();
 
-  constructor(protected _renderer: Renderer, protected _elementRef: ElementRef) { }
+  /** Emitted when a chip is to be removed. */
+  @Output('remove') onRemove = new EventEmitter<MdChipEvent>();
+
+  constructor(protected _renderer: Renderer, protected _elementRef: ElementRef) {
+  }
 
   ngOnInit(): void {
     this._addDefaultCSSClass();
@@ -84,6 +103,30 @@ export class MdChip implements Focusable, OnInit, OnDestroy {
     return String(coerceBooleanProperty(this.disabled));
   }
 
+  /**
+   * Whether or not the chips are selectable. When a chip is not selectable,
+   * changes to it's selected state are always ignored.
+   */
+  @Input() get selectable(): boolean {
+    return this._selectable;
+  }
+
+  set selectable(value: boolean) {
+    this._selectable = coerceBooleanProperty(value);
+  }
+
+  /**
+   * Determines whether or not the chip displays the remove styling and emits (remove) events.
+   */
+  @Input() get removable(): boolean {
+    return this._removable;
+  }
+
+  set removable(value: boolean) {
+    this._removable = coerceBooleanProperty(value);
+    this.onRemovableChange.emit(this._removable);
+  }
+
   /** Whether or not this chip is selected. */
   @Input() get selected(): boolean {
     return this._selected;
@@ -99,10 +142,7 @@ export class MdChip implements Focusable, OnInit, OnDestroy {
     }
   }
 
-  /**
-   * Toggles the current selected state of this chip.
-   * @return Whether the chip is selected.
-   */
+  /** Toggles the current selected state of this chip. */
   toggleSelected(): boolean {
     this.selected = !this.selected;
     return this.selected;
@@ -123,15 +163,76 @@ export class MdChip implements Focusable, OnInit, OnDestroy {
     this.onFocus.emit({chip: this});
   }
 
+  /**
+   * Allows for programmatic removal of the chip. Called by the MdChipList when the DELETE or
+   * BACKSPACE keys are pressed.
+   *
+   * Note: This only informs any listeners of the removal request, it does **not** actually remove
+   * the chip from the DOM.
+   */
+  remove(): void {
+    if (this.removable) {
+      this.onRemove.emit({chip: this});
+    }
+  }
+
   /** Ensures events fire properly upon click. */
   _handleClick(event: Event) {
     // Check disabled
+    if (this._checkDisabled(event)) {
+      return;
+    }
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.focus();
+  }
+
+  /** Handle custom key presses. */
+  _handleKeydown(event: KeyboardEvent) {
+    if (this._checkDisabled(event)) {
+      return;
+    }
+
+    switch (event.keyCode) {
+      case DELETE:
+      case BACKSPACE:
+        // If we are removable, remove the focused chip
+        if (this.removable) {
+          this.onRemove.emit();
+        }
+
+        // Always prevent so page navigation does not occur
+        event.preventDefault();
+        break;
+      case SPACE:
+        // If we are selectable, toggle the focused chip
+        if (this.selectable) {
+          this.toggleSelected();
+        }
+
+        // Always prevent space from scrolling the page since the list has focus
+        event.preventDefault();
+        break;
+    }
+  }
+
+  /**
+   * Sets whether or not this chip is displaying a remove icon. Adds/removes the
+   * `md-chip-has-remove-icon` class.
+   */
+  _setHasRemoveIcon(value: boolean) {
+    this._hasRemoveIcon = value;
+  }
+
+  protected _checkDisabled(event: Event): boolean {
     if (this.disabled) {
       event.preventDefault();
       event.stopPropagation();
-    } else {
-      this.focus();
     }
+
+    return this.disabled;
   }
 
   /** Initializes the appropriate CSS classes based on the chip type (basic or standard). */
@@ -145,6 +246,7 @@ export class MdChip implements Focusable, OnInit, OnDestroy {
     if (el.nodeName.toLowerCase() == 'mat-basic-chip' || el.hasAttribute('mat-basic-chip') ||
         el.nodeName.toLowerCase() == 'md-basic-chip' || el.hasAttribute('md-basic-chip')) {
       el.classList.add('mat-basic-chip');
+      this._renderer.setElementClass(this._elementRef.nativeElement, `md-chip`, true);
     }
   }
 

angular/components

src/lib/chips/chip.ts
@@ -9,8 +9,10 @@ import {
   Renderer
 } from '@angular/core';
 
+import {Observable} from 'rxjs/Observable';
 import {Focusable} from '../core/a11y/focus-key-manager';
 import {coerceBooleanProperty} from '../core/coercion/boolean-property';
+import {SPACE, BACKSPACE, DELETE} from '../core/keyboard/keycodes';
 
 export interface MdChipEvent {
   chip: MdChip;
@@ -22,17 +24,19 @@ export interface MdChipEvent {
 @Component({
   selector: `md-basic-chip, [md-basic-chip], md-chip, [md-chip],
              mat-basic-chip, [mat-basic-chip], mat-chip, [mat-chip]`,
-  template: `<ng-content></ng-content>`,
+  template: `<ng-content></ng-content><div class="md-chip-focus-border"></div>`,
   host: {
     '[class.mat-chip]': 'true',
     'tabindex': '-1',
     'role': 'option',
 
     '[class.mat-chip-selected]': 'selected',
+    '[class.mat-chip-has-remove-icon]': '_hasRemoveIcon',
     '[attr.disabled]': 'disabled',
     '[attr.aria-disabled]': '_isAriaDisabled',
 
-    '(click)': '_handleClick($event)'
+    '(click)': '_handleClick($event)',
+    '(keydown)': '_handleKeydown($event)'
   }
 })
 export class MdChip implements Focusable, OnInit, OnDestroy {
@@ -40,11 +44,24 @@ export class MdChip implements Focusable, OnInit, OnDestroy {
   /** Whether or not the chip is disabled. Disabled chips cannot be focused. */
   protected _disabled: boolean = null;
 
+  /** Whether or not the chip is selectable. */
+  protected _selectable: boolean = true;
+
+  /** Whether or not the chip is removable. */
+  protected _removable: boolean = true;
+
   /** Whether or not the chip is selected. */
   protected _selected: boolean = false;
 
   /** The palette color of selected chips. */
   protected _color: string = 'primary';
+
+  /** Whether or not the chip is displaying the remove icon. */
+  _hasRemoveIcon: boolean = false;
+
+  /** Emitted when the removable property changes. */
+  private _onRemovableChange = new EventEmitter<boolean>();
+  onRemovableChange$: Observable<boolean> = this._onRemovableChange.asObservable();
 
   /** Emitted when the chip is focused. */
   onFocus = new EventEmitter<MdChipEvent>();
@@ -58,7 +75,11 @@ export class MdChip implements Focusable, OnInit, OnDestroy {
   /** Emitted when the chip is destroyed. */
   @Output() destroy = new EventEmitter<MdChipEvent>();
 
-  constructor(protected _renderer: Renderer, protected _elementRef: ElementRef) { }
+  /** Emitted when a chip is to be removed. */
+  @Output('remove') onRemove = new EventEmitter<MdChipEvent>();
+
+  constructor(protected _renderer: Renderer, protected _elementRef: ElementRef) {
+  }
 
   ngOnInit(): void {
     this._addDefaultCSSClass();
@@ -84,6 +105,30 @@ export class MdChip implements Focusable, OnInit, OnDestroy {
     return String(coerceBooleanProperty(this.disabled));
   }
 
+  /**
+   * Whether or not the chips are selectable. When a chip is not selectable,
+   * changes to it's selected state are always ignored.
+   */
+  @Input() get selectable(): boolean {
+    return this._selectable;
+  }
+
+  set selectable(value: boolean) {
+    this._selectable = coerceBooleanProperty(value);
+  }
+
+  /**
+   * Determines whether or not the chip displays the remove styling and emits (remove) events.
+   */
+  @Input() get removable(): boolean {
+    return this._removable;
+  }
+
+  set removable(value: boolean) {
+    this._removable = coerceBooleanProperty(value);
+    this._onRemovableChange.emit(this._removable);
+  }
+
   /** Whether or not this chip is selected. */
   @Input() get selected(): boolean {
     return this._selected;
@@ -99,10 +144,7 @@ export class MdChip implements Focusable, OnInit, OnDestroy {
     }
   }
 
-  /**
-   * Toggles the current selected state of this chip.
-   * @return Whether the chip is selected.
-   */
+  /** Toggles the current selected state of this chip. */
   toggleSelected(): boolean {
     this.selected = !this.selected;
     return this.selected;
@@ -123,23 +165,81 @@ export class MdChip implements Focusable, OnInit, OnDestroy {
     this.onFocus.emit({chip: this});
   }
 
+  /**
+   * Allows for programmatic removal of the chip. Called by the MdChipList when the DELETE or
+   * BACKSPACE keys are pressed.
+   *
+   * Note: This only informs any listeners of the removal request, it does **not** actually remove
+   * the chip from the DOM.
+   */
+  remove(): void {
+    if (this.removable) {
+      this.onRemove.emit({chip: this});
+    }
+  }
+
   /** Ensures events fire properly upon click. */
   _handleClick(event: Event) {
     // Check disabled
+    if (this._checkDisabled(event)) {
+      return;
+    }
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.focus();
+  }
+
+  /** Handle custom key presses. */
+  _handleKeydown(event: KeyboardEvent) {
+    if (this._checkDisabled(event)) {
+      return;
+    }
+
+    switch (event.keyCode) {
+      case DELETE:
+      case BACKSPACE:
+        // If we are removable, remove the focused chip
+        if (this.removable) {
+          this.onRemove.emit();
+        }
+
+        // Always prevent so page navigation does not occur
+        event.preventDefault();
+        break;
+      case SPACE:
+        // If we are selectable, toggle the focused chip
+        if (this.selectable) {
+          this.toggleSelected();
+        }
+
+        // Always prevent space from scrolling the page since the list has focus
+        event.preventDefault();
+        break;
+    }
+  }
+
+  /**
+   * Sets whether or not this chip is displaying a remove icon. Adds/removes the
+   * `md-chip-has-remove-icon` class.
+   */
+  _setHasRemoveIcon(value: boolean) {
+    this._hasRemoveIcon = value;
+  }
+
+  protected _checkDisabled(event: Event): boolean {
     if (this.disabled) {
       event.preventDefault();
       event.stopPropagation();
-    } else {
-      this.focus();
     }
+
+    return this.disabled;
   }
 
   /** Initializes the appropriate CSS classes based on the chip type (basic or standard). */
   private _addDefaultCSSClass() {
     let el: HTMLElement = this._elementRef.nativeElement;
-
-    // Always add the `mat-chip` class
-    el.classList.add('mat-chip');
 
     // If we are a basic chip, also add the `mat-basic-chip` class for :not() targeting
     if (el.nodeName.toLowerCase() == 'mat-basic-chip' || el.hasAttribute('mat-basic-chip') ||