e44b513aeb201ebb8ae20599fc07fa3985f134e4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-03-23 18:31:57 +0100

    feat(slide-toggle): add ripple focus indicator
    * Introduces a focus indiactor using the persistent ripples.

--- src/lib/slide-toggle/index.ts
+++ src/lib/slide-toggle/index.ts
@@ -1,16 +1,18 @@
 import {NgModule, ModuleWithProviders} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HAMMER_GESTURE_CONFIG} from '@angular/platform-browser';
-import {GestureConfig, CompatibilityModule} from '../core';
+import {GestureConfig, CompatibilityModule, MdRippleModule, FocusOriginMonitor} from '../core';
 import {MdSlideToggle} from './slide-toggle';
-import {MdRippleModule} from '../core/ripple/index';
 
 
 @NgModule({
   imports: [FormsModule, MdRippleModule, CompatibilityModule],
   exports: [MdSlideToggle, CompatibilityModule],
   declarations: [MdSlideToggle],
-  providers: [{provide: HAMMER_GESTURE_CONFIG, useClass: GestureConfig}],
+  providers: [
+    FocusOriginMonitor,
+    { provide: HAMMER_GESTURE_CONFIG, useClass: GestureConfig }
+  ],
 })
 export class MdSlideToggleModule {
   /** @deprecated */
