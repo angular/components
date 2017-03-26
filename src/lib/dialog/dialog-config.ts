4e0b44c86791f9f43af21a5f6d0099d72dc5aec9
Mac <Harlow44@hotmail.com>
2017-03-25 22:54:46 -0700

    Fixed alignment of *

--- src/lib/dialog/dialog-config.ts
+++ src/lib/dialog/dialog-config.ts
@@ -17,11 +17,11 @@ export interface DialogPosition {
 export class MdDialogConfig {
 
   /**
-  * Where the attached component should live in Angular's *logical* component tree.
-  * This affects what is available for injection and the change detection order for the
-  * component instantiated inside of the dialog. This does not affect where the dialog
-  * content will be rendered.
-  */
+   * Where the attached component should live in Angular's *logical* component tree.
+   * This affects what is available for injection and the change detection order for the
+   * component instantiated inside of the dialog. This does not affect where the dialog
+   * content will be rendered.
+   */
   viewContainerRef?: ViewContainerRef;
 
   /** The ARIA role of the dialog element. */
