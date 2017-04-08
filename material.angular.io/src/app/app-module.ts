65bdcce0e657c01f6b536f7e53f49cf5bd720d32
Jeremy Elbourn <jelbourn@gmail.com>
2017-04-08 10:01:43 -0600

    Update to Angular Material beta.3 (#143)

--- material.angular.io/src/app/app-module.ts
+++ material.angular.io/src/app/app-module.ts
@@ -1,4 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
 import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
 import {FormsModule} from '@angular/forms';
@@ -35,6 +36,7 @@ import {ComponentPageHeader} from './pages/component-page-header/component-page-
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     ExampleModule,
     SharedModule,
     FormsModule,
