b21d11162d2fb2f20c1e11a575be0547a22437d6
Jeremy Elbourn <jelbourn@gmail.com>
2017-04-10 09:20:56 -0600

    Fix plunkers for beta.3 release (#146)

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/assets/plunker/main.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/assets/plunker/main.ts
@@ -1,5 +1,6 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MaterialModule} from '@angular/material';
@@ -9,6 +10,7 @@ import {MaterialDocsExample} from './material-docs-example';
 
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
     MaterialModule,
