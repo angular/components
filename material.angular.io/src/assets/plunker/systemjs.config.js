d1e669f0d4c85b28538750180386a090d71ab854
Jeremy Elbourn <jelbourn@gmail.com>
2017-04-10 09:20:56 -0600

    Fix plunkers for beta.3 release (#146)

--- material.angular.io/src/assets/plunker/systemjs.config.js
+++ material.angular.io/src/assets/plunker/systemjs.config.js
@@ -20,6 +20,7 @@ System.config({
 
     // Angular specific mappings.
     '@angular/core': 'https://unpkg.com/@angular/core/bundles/core.umd.js',
+    '@angular/animations': 'https://unpkg.com/@angular/animations/bundles/animations.umd.js',
     '@angular/common': 'https://unpkg.com/@angular/common/bundles/common.umd.js',
     '@angular/compiler': 'https://unpkg.com/@angular/compiler/bundles/compiler.umd.js',
     '@angular/http': 'https://unpkg.com/@angular/http/bundles/http.umd.js',
@@ -27,6 +28,8 @@ System.config({
     '@angular/router': 'https://unpkg.com/@angular/router/bundles/router.umd.js',
     '@angular/platform-browser': 'https://unpkg.com/@angular/platform-browser/bundles/platform-browser.umd.js',
     '@angular/platform-browser-dynamic': 'https://unpkg.com/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+    '@angular/animations/browser': 'https://unpkg.com/@angular/animations/bundles/animations-browser.umd.js',
+    '@angular/platform-browser/animations': 'https://unpkg.com/@angular/platform-browser/bundles/platform-browser-animations.umd.js',
     '@angular/material': 'https://unpkg.com/@angular/material/bundles/material.umd.js',
 
     // Rxjs mapping
