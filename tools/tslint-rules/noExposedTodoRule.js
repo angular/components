0ccbb0e630552a333c28309872a3349baa39f465
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-04-12 11:52:37 -0700

    build: detect todos in public comments
    * Creates a naive rule that checks all multi-line comments and reports failures once it detects TODOs inside of it.
    
    * This means that TODOs always need to placed inside of single-line comments.
    
    Fixes #4026

---
+++ tools/tslint-rules/noExposedTodoRule.js
@@ -0,0 +1,32 @@
+const ts = require('typescript');
+const utils = require('tsutils');
+const Lint = require('tslint');
+
+const ERROR_MESSAGE = 'Todo inside of the comment might be published in the docs.';
+
+/**
+ * Rule that walks through all comments inside of the library and adds failures when it
+ * detects TODO's inside of multi-line comments. TODOs need to be placed inside of single-line
+ * comments.
+ */
+class Rule extends Lint.Rules.AbstractRule {
+
+  apply(sourceFile) {
+    return this.applyWithWalker(new NoExposedTodoWalker(sourceFile, this.getOptions()));
+  }
+}
+
+class NoExposedTodoWalker extends Lint.RuleWalker {
+
+  visitSourceFile(sourceFile) {
+    utils.forEachComment(sourceFile, (fullText, commentRange) => {
+      let isTodoComment = fullText.substring(commentRange.pos, commentRange.end).includes('TODO');
+
+      if (commentRange.kind === ts.SyntaxKind.MultiLineCommentTrivia && isTodoComment) {
+        this.addFailureAt(commentRange.pos, commentRange.end - commentRange.pos, ERROR_MESSAGE);
+      }
+    });
+  }
+}
+
+exports.Rule = Rule;
