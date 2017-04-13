d86ed16743cd476216f6702a74e7ffafaadb4d1b
Yuan Gao <tinagao@google.com>
2017-04-13 14:56:27 -0700

    fix lint

--- src/lib/checkbox/checkbox.ts
+++ src/lib/checkbox/checkbox.ts
@@ -142,7 +142,7 @@ export class MdCheckbox implements ControlValueAccessor, AfterViewInit, OnDestro
   /** Whether the checkbox is disabled. */
   @Input()
   get disabled(): boolean { return this._disabled; }
-  set disabled(value) { this._disabled = coerceBooleanProperty(value);}
+  set disabled(value) { this._disabled = coerceBooleanProperty(value); }
 
   /** Tabindex value that is passed to the underlying input element. */
   @Input() tabIndex: number = 0;
