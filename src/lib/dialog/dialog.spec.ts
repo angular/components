d959baeb60c6e5c229bbe7743920aabef6dd028b
crisbeto <crisbeto@abv.bg>
2017-04-17 14:44:48 +0200

    fix(dialog): provide default value for MD_DIALOG_DATA token
    Adds a default value for the `MD_DIALOG_DATA` injection token, instead of throwing an error when it's not defined.
    
    Fixes #4086.

--- src/lib/dialog/dialog.spec.ts
+++ src/lib/dialog/dialog.spec.ts
@@ -371,10 +371,11 @@ describe('MdDialog', () => {
       expect(instance.data.dateParam).toBe(config.data.dateParam);
     });
 
-    it('should throw if injected data is expected but none is passed', () => {
-      expect(() => {
-        dialog.open(DialogWithInjectedData);
-      }).toThrow();
+    it('should default to an empty object if no data is passed', () => {
+      let dialogRef: MdDialogRef<DialogWithInjectedData>;
+
+      expect(() => dialogRef = dialog.open(DialogWithInjectedData)).not.toThrow();
+      expect(dialogRef.componentInstance.data).toEqual({});
     });
   });
 
