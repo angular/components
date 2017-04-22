ec73bf8edb2dd9c6c0d7dca6ac95fed4c14b6b75
crisbeto <crisbeto@abv.bg>
2017-04-22 14:20:19 +0200

    fix(select): tab opening multiple select and space scrolling page
    * Fixes pressing tab (or any other key, except arrows) opening `md-select` in `multiple` mode.
    * Fixes the page getting scrolled down when opening a select by pressing space.
    * Adds missing test coverage for opening a multiple select with the arrow keys.

--- src/lib/core/testing/dispatch-events.ts
+++ src/lib/core/testing/dispatch-events.ts
@@ -6,15 +6,21 @@ import {
 
 /** Shorthand to dispatch a fake event on a specified node. */
 export function dispatchFakeEvent(node: Node | Window, type: string) {
-  node.dispatchEvent(createFakeEvent(type));
+  let event = createFakeEvent(type);
+  node.dispatchEvent(event);
+  return event;
 }
 
 /** Shorthand to dispatch a keyboard event with a specified key code. */
 export function dispatchKeyboardEvent(node: Node, type: string, keyCode: number) {
-  node.dispatchEvent(createKeyboardEvent(type, keyCode));
+  let event = createKeyboardEvent(type, keyCode);
+  node.dispatchEvent(event);
+  return event;
 }
 
 /** Shorthand to dispatch a mouse event on the specified coordinates. */
 export function dispatchMouseEvent(node: Node, type: string, x = 0, y = 0) {
-  node.dispatchEvent(createMouseEvent(type, x, y));
+  let event = createMouseEvent(type, x, y);
+  node.dispatchEvent(event);
+  return event;
 }
