ea40615f40bc110a8557a38b8e9d3f569d56a75b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-04-29 13:22:16 +0200

    build: script to build demo-app for firebase
    * Adds a new gulp task that can be used to build the devapp in "deploy"-mode. This allows us to deploy the demo-app on firebase.
    
    Fixes #3857

--- tools/gulp/tasks/development.ts
+++ tools/gulp/tasks/development.ts
@@ -1,13 +1,22 @@
 import {task, watch} from 'gulp';
-import {DIST_ROOT, SOURCE_ROOT} from '../constants';
+import {DIST_ROOT, SOURCE_ROOT, PROJECT_ROOT, DIST_BUNDLES, DIST_MATERIAL} from '../constants';
 import {
   sassBuildTask, tsBuildTask, copyTask, buildAppTask, sequenceTask, triggerLivereload,
   serverTask
 } from '../util/task_helpers';
 import {join} from 'path';
+import {copyFiles} from '../util/copy-files';
 
 const appDir = join(SOURCE_ROOT, 'demo-app');
 const outDir = join(DIST_ROOT, 'packages', 'demo-app');
+
+/** Array of vendors that are required to serve the demo-app. */
+const appVendors = [
+  '@angular', 'systemjs', 'zone.js', 'rxjs', 'hammerjs', 'core-js', 'web-animations-js'
+];
+
+/** Glob that matches all required vendors for the demo-app. */
+const vendorGlob = `+(${appVendors.join('|')})/**/*.+(html|css|js|map)`;
 
 task(':watch:devapp', () => {
   watch(join(appDir, '**/*.ts'), [':build:devapp:ts', triggerLivereload]);
@@ -28,3 +37,10 @@ task(':serve:devapp', serverTask(outDir, true));
 task('serve:devapp', ['build:devapp'], sequenceTask(
   [':serve:devapp', 'material:watch', ':watch:devapp']
 ));
+
+/** Task that copies all vendors into the demo-app package. Allows hosting the app on firebase. */
+task('build-deploy:devapp', ['build:devapp'], () => {
+  copyFiles(join(PROJECT_ROOT, 'node_modules'), vendorGlob, join(outDir, 'node_modules'));
+  copyFiles(DIST_BUNDLES, '*.+(js|map)', join(outDir, 'dist/bundles'));
+  copyFiles(DIST_MATERIAL, '**/prebuilt/*.+(css|map)', join(outDir, 'dist/packages/material'));
+});
