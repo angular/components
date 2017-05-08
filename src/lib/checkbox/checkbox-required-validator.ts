1ac862456fe483d2e396de7707b22dab5c04c24b
Yuan Gao <tinagao@google.com>
2017-05-08 11:20:07 -0700

    extends CheckboxRequiredValidator

--- src/lib/checkbox/checkbox-required-validator.ts
+++ src/lib/checkbox/checkbox-required-validator.ts
@@ -1,14 +1,10 @@
 import {
-  Input,
   Directive,
   forwardRef,
   Provider,
 } from '@angular/core';
 import {
-  AbstractControl,
-  ValidationErrors,
-  Validator,
-  Validators,
+  CheckboxRequiredValidator,
   NG_VALIDATORS,
 } from '@angular/forms';
 
@@ -25,24 +21,4 @@ export const MD_CHECKBOX_REQUIRED_VALIDATOR: Provider = {
   providers: [MD_CHECKBOX_REQUIRED_VALIDATOR],
   host: {'[attr.required]': 'required ? "" : null'}
 })
-export class MdCheckboxRequiredValidator implements Validator {
-  validate(c: AbstractControl): ValidationErrors|null {
-    return this.required ? Validators.requiredTrue(c) : null;
-  }
-
-  private _required: boolean;
-  private _onChange: () => void;
-
-  @Input()
-  get required(): boolean|string { return this._required; }
-
-  set required(value: boolean|string) {
-    this._required = value != null && value !== false && `${value}` !== 'false';
-    if (this._onChange) {
-      this._onChange();
-    }
-  }
-
-  registerOnValidatorChange(fn: () => void): void { this._onChange = fn; }
-}
-
+export class MdCheckboxRequiredValidator extends CheckboxRequiredValidator {}
