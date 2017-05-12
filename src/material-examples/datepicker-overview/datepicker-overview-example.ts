8f71009b091bbe6e4648965fa105c4d41a4e279c
Miles Malerba <mmalerba@google.com>
2017-05-12 14:37:06 -0700

    add style reference

--- src/material-examples/datepicker-overview/datepicker-overview-example.ts
+++ src/material-examples/datepicker-overview/datepicker-overview-example.ts
@@ -4,5 +4,6 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'datepicker-overview-example',
   templateUrl: './datepicker-overview-example.html',
+  styleUrls: ['./datepicker-overview-example.css'],
 })
 export class DatepickerOverviewExample {}
