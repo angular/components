3a492f27a4f39f7dab6a6ee100f5269db8aa32c5
Miles Malerba <mmalerba@google.com>
2017-05-12 14:27:36 -0700

    Add datepicker overview example

---
+++ src/material-examples/datepicker-overview/datepicker-overview-example.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'datepicker-overview-example',
+  templateUrl: './datepicker-overview-example.html',
+})
+export class DatepickerOverviewExample {}
