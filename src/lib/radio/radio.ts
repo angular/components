be84b29f13be7ee8ee534501ad25adb1781c0438
Youssef Taghlabi <ytaghlabi@gmail.com>
2017-05-17 22:10:18 -0400

    chore(focus-origin-monitor): remove unused method parameter
    * Remove the unused `renderer` parameter in the `focusvia` method.

--- src/lib/radio/radio.ts
+++ src/lib/radio/radio.ts
@@ -424,7 +424,7 @@ export class MdRadioButton implements OnInit, AfterViewInit, OnDestroy {
 
   /** Focuses the radio button. */
   focus(): void {
-    this._focusOriginMonitor.focusVia(this._inputElement.nativeElement, this._renderer, 'keyboard');
+    this._focusOriginMonitor.focusVia(this._inputElement.nativeElement, 'keyboard');
   }
 
   ngOnInit() {
