090750921b87310c90ce6c322160f30af2a28bbe
Jeremy Elbourn <jelbourn@google.com>
2017-06-17 12:40:49 -0700

    Remove run-time SVG theming to pure SCSS

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/shared/svg-viewer/svg-viewer.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/shared/svg-viewer/svg-viewer.ts
@@ -1,80 +1,40 @@
-import {ElementRef, Component, Input} from '@angular/core';
+import {Component, ElementRef, Input, OnInit} from '@angular/core';
 import {Http} from '@angular/http';
-import {Observable} from 'rxjs/Observable';
-import {Subscription} from 'rxjs/Subscription';
-
-import {ThemeColors, SvgBuilder} from './svg-builder';
-import {ThemeStorage, DocsSiteTheme} from '../theme-picker/theme-storage/theme-storage';
 
 
 @Component({
   selector: 'docs-svg-viewer',
   template: '<div class="docs-svg-viewer"></div>',
 })
-
-export class SvgViewer {
-  @Input() svgHref: string;
+export class SvgViewer implements OnInit {
+  @Input() src: string;
   @Input() scaleToContainer: boolean;
 
-  private _subscription: Subscription;
-  private currTheme: DocsSiteTheme;
-  private _previousThemeColors: ThemeColors = SvgBuilder.DEFAULT_THEME;
+  constructor(private elementRef: ElementRef, private http: Http) { }
 
-  constructor(
-    public themeStorage: ThemeStorage,
-    public svgBuilder: SvgBuilder,
-    public el: ElementRef,
-    public http: Http
-  ) {
-    this.currTheme = this.themeStorage.getStoredTheme();
-    this._subscription = this.themeStorage.onThemeUpdate
-      .subscribe(theme => this.swapTheme(theme));
+  ngOnInit() {
+    this.fetchAndInlineSvgContent(this.src);
   }
 
-  public ngAfterViewInit() {
-    if (this.currTheme) {
-      setTimeout(() => this.swapTheme(this.currTheme));
-    } else {
-      setTimeout(() => this.svgBuilder.getSvgAsString(this.svgHref)
-        .then(svgString => this.injectSvg(svgString)));
-    }
-  }
-
-  public swapTheme(theme) {
-    if (this._getSvgFromDom()) {
-      this._convertToTheme(theme, this._getSvgFromDom());
-    } else {
-      this.svgBuilder.getSvgAsString(this.svgHref)
-        .then(svgString => this._convertToTheme(theme, svgString));
-    }
-  }
-
-  private _convertToTheme(theme, template) {
-    const {newTemplate, colors} = this.svgBuilder.buildSvg(
-      theme, template, this._previousThemeColors);
-    this.injectSvg(newTemplate);
-    this._previousThemeColors = colors;
-  }
-
-  public ngOnDestroy() {
-    if (this._subscription) {
-      this._subscription.unsubscribe();
-    }
-  }
-
-  public injectSvg(template) {
-    this.el.nativeElement.innerHTML = template;
+  private inlineSvgContent(template) {
+    this.elementRef.nativeElement.innerHTML = template;
 
     if (this.scaleToContainer) {
-      let svg = this.el.nativeElement.querySelector('svg');
+      let svg = this.elementRef.nativeElement.querySelector('svg');
       svg.setAttribute('width', '100%');
       svg.setAttribute('height', '100%');
       svg.setAttribute('preserveAspectRatio', 'xMidYMid meet');
     }
   }
 
-  private _getSvgFromDom() {
-    const svg = this.el.nativeElement.querySelector('svg');
-    return svg ? this.el.nativeElement.innerHTML : null;
+  private fetchAndInlineSvgContent(path: string): void {
+    const svgAbsPath = getAbsolutePathFromSrc(path);
+    this.http.get(svgAbsPath).subscribe(svgResponse => {
+      this.inlineSvgContent(svgResponse.text())
+    });
   }
+}
+
+function getAbsolutePathFromSrc(src: string) {
+  return src.slice(src.indexOf('assets/') - 1);
 }
