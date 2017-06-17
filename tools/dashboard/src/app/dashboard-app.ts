c0e9ebadf69165cfa660d2946502c1410fe55693
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-06-17 14:27:31 +0200

    build: firebase integration for dashboard
    * As part of the initial dashboard setup, this installs Angular Fire and Angular Material in the dashboard.
    * For testing purposes the list of all payload results will be printed right now.

--- tools/dashboard/src/app/dashboard-app.ts
+++ tools/dashboard/src/app/dashboard-app.ts
@@ -1,8 +1,32 @@
 import {Component} from '@angular/core';
+import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
 
 @Component({
   selector: 'dashboard-app',
   templateUrl: './dashboard-app.html',
   styleUrls: ['./dashboard-app.css']
 })
-export class DashboardApp {}
+export class DashboardApp {
+
+  /** Observable that emits all payload results from Firebase. */
+  payloads: FirebaseListObservable<PayloadResult[]>;
+
+  constructor(database: AngularFireDatabase) {
+    this.payloads = database.list(`payloads`);
+  }
+}
+
+/** Interface that describes the payload results from the Firebase database. */
+interface PayloadResult {
+  timestamp: number;
+  // Material bundles
+  material_umd: string;
+  material_umd_minified_uglify: string;
+  material_fesm_2015: string;
+  material_fesm_2014: string;
+  // CDK bundles
+  cdk_umd: string;
+  cdk_umd_minified_uglify: string;
+  cdk_fesm_2015: string;
+  cdk_fesm_2014: string;
+}
