5e27bf9c218adbbda932b2453440f979513324d9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-06-17 15:27:05 +0200

    Address feedback

--- tools/dashboard/src/app/dashboard-app.ts
+++ tools/dashboard/src/app/dashboard-app.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import {PayloadResult} from './data-definitions';
 
 @Component({
   selector: 'dashboard-app',
@@ -16,17 +17,3 @@ export class DashboardApp {
   }
 }
 
-/** Interface that describes the payload results from the Firebase database. */
-interface PayloadResult {
-  timestamp: number;
-  // Material bundles
-  material_umd: string;
-  material_umd_minified_uglify: string;
-  material_fesm_2015: string;
-  material_fesm_2014: string;
-  // CDK bundles
-  cdk_umd: string;
-  cdk_umd_minified_uglify: string;
-  cdk_fesm_2015: string;
-  cdk_fesm_2014: string;
-}
