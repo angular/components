345ecf54075f741d88010288d857e0bb1719dee3
Jeremy Elbourn <jelbourn@google.com>
2017-06-18 12:13:26 -0700

    Simplify application routes

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/routes.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/routes.ts
@@ -10,12 +10,21 @@ import {GuideViewer} from './pages/guide-viewer/guide-viewer';
 export const MATERIAL_DOCS_ROUTES: Routes = [
   {path: '', component: Homepage, pathMatch: 'full'},
   {
+    path: 'categories',
+    component: ComponentSidenav,
+    children: [
+      {path: '', component: ComponentCategoryList},
+      {path: ':id', component: ComponentList},
+    ],
+  },
+  {
     path: 'components',
     component: ComponentSidenav,
     children: [
       {path: '', component: ComponentCategoryList},
-      {path: 'category/:id', component: ComponentList},
-      {path: 'component/:id', component: ComponentViewer},
+      {path: 'component/:id', redirectTo: ':id', pathMatch: 'full'},
+      {path: 'category/:id', redirectTo: '/categories/:id', pathMatch: 'full'},
+      {path: ':id', component: ComponentViewer},
     ],
   },
   {path: 'guides', component: GuideList},
