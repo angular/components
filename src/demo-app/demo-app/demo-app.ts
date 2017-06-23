52d138105b1c509590646266bddb7fb8058925b2
Lin Sun <2808411862@qq.com>
2017-06-22 17:21:28 -0700

    revert demo-app.ts

--- src/demo-app/demo-app/demo-app.ts
+++ src/demo-app/demo-app/demo-app.ts
@@ -67,7 +67,7 @@ export class DemoApp {
     {name: 'Tooltip', route: 'tooltip'},
     {name: 'Platform', route: 'platform'},
     {name: 'Style', route: 'style'},
-    {name: 'Typography', route: 'typography'},
+    {name: 'Typography', route: 'typography'}
   ];
 
   constructor(private _element: ElementRef) {
