33c62998951d45a325869ed18e5d97da38366597
Ji Won Shin <jwshin@google.com>
2017-06-29 16:37:48 -0700

    Prototyping

---
+++ src/lib/stepper/step.ts
@@ -0,0 +1,23 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {Component} from '@angular/core';
+import {TemplatePortal} from '../core';
+@Component({
+    moduleId: module.id,
+    selector: 'mat-step',
+    templateUrl: ''
+})
+export class MdStep {
+    active: boolean = false;
+    isLast: boolean = false;
+
+    /** The portal that will be the hosted content of the step */
+    private _contentPortal: TemplatePortal | null = null;
+    get content(): TemplatePortal | null { return this._contentPortal; };
+}
