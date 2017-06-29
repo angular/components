2f50325195ec37356ab983a55c24be08d61b952b
Austin McDaniel <amcdaniel2@gmail.com>
2017-06-29 17:46:30 -0500

    chore(lint): fix lint errors

--- src/material-examples/datepicker-filter/datepicker-filter-example.ts
+++ src/material-examples/datepicker-filter/datepicker-filter-example.ts
@@ -8,8 +8,7 @@ import {Component} from '@angular/core';
 export class DatepickerFilterExample {
   myFilter = (d: Date): boolean => {
     const day = d.getDay();
-    
     // Prevent Saturday and Sunday from being selected.
     return day !== 0 && day !== 6;
-  };
+  }
 }
