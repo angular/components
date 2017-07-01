5d7eca93b472cac44b34d211cd817d293486446f
Robin Dijkhof <tardijkhof@gmail.com>
2017-07-01 16:05:38 +0200

    test(sidenav): add e2e tests

---
+++ e2e/components/sidenav-e2e.spec.ts
@@ -0,0 +1,28 @@
+import {browser, by, element, protractor} from 'protractor';
+const EC = protractor.ExpectedConditions;
+
+
+describe('sidenav', () => {
+    describe('opening and closing', () => {
+        beforeEach(() => browser.get('/sidenav'));
+
+        let input = element(by.tagName('md-sidenav'));
+
+
+        it('should be closed', () => {
+            expect(input.isDisplayed()).toBeFalsy();
+        });
+
+        it('should open', () => {
+            element(by.buttonText('Open sidenav')).click();
+            expect(input.isDisplayed()).toBeTruthy();
+        });
+
+        it('should close again', () => {
+            element(by.buttonText('Open sidenav')).click();
+            element(by.buttonText('Open sidenav')).click();
+            browser.wait(EC.presenceOf(element(by.className('mat-sidenav-closed'))), 1000);
+            expect(input.isDisplayed()).toBeFalsy();
+        });
+    });
+});
---
+++ src/e2e-app/sidenav/sidenav-e2e.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'sidenav-e2e',
+    templateUrl: 'sidenav-e2e.html',
+})
+export class SidenavE2E {}
