de89a081b5677fd00fcd804fcea46824533c6a81
Andrew Seguin <andrewjs@google.com>
2017-07-05 15:52:16 -0700

    lint

--- src/material-examples/table-filtering/table-filtering-example.ts
+++ src/material-examples/table-filtering/table-filtering-example.ts
@@ -114,8 +114,3 @@ export class ExampleDataSource extends DataSource<any> {
 
   disconnect() {}
 }
-
-
-/**  Copyright 2017 Google Inc. All Rights Reserved.
- Use of this source code is governed by an MIT-style license that
- can be found in the LICENSE file at http://angular.io/license */
