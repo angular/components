31e0a5cc7e6f263f9e73567440815fca55318e7d
Jeremy Elbourn <jelbourn@gmail.com>
2017-07-07 09:26:16 -0700

    Fix broken plunkers (#212)

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/assets/plunker/main.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/assets/plunker/main.ts
@@ -3,9 +3,80 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MaterialModule, MdNativeDateModule} from '@angular/material';
+import {
+  MdAutocompleteModule,
+  MdButtonModule,
+  MdButtonToggleModule,
+  MdCardModule,
+  MdCheckboxModule,
+  MdChipsModule,
+  MdCoreModule,
+  MdDatepickerModule,
+  MdDialogModule,
+  MdExpansionModule,
+  MdGridListModule,
+  MdIconModule,
+  MdInputModule,
+  MdListModule,
+  MdMenuModule,
+  MdNativeDateModule,
+  MdPaginatorModule,
+  MdProgressBarModule,
+  MdProgressSpinnerModule,
+  MdRadioModule,
+  MdRippleModule,
+  MdSelectModule,
+  MdSidenavModule,
+  MdSliderModule,
+  MdSlideToggleModule,
+  MdSnackBarModule,
+  MdSortModule,
+  MdTableModule,
+  MdTabsModule,
+  MdToolbarModule,
+  MdTooltipModule,
+} from '@angular/material';
 import {MaterialDocsExample} from './material-docs-example';
 import {HttpModule} from '@angular/http';
+import {CdkTableModule} from '@angular/cdk';
+
+@NgModule({
+  exports: [
+    CdkTableModule,
+    MdAutocompleteModule,
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdChipsModule,
+    MdCoreModule,
+    MdDatepickerModule,
+    MdDialogModule,
+    MdExpansionModule,
+    MdGridListModule,
+    MdIconModule,
+    MdInputModule,
+    MdListModule,
+    MdMenuModule,
+    MdNativeDateModule,
+    MdPaginatorModule,
+    MdProgressBarModule,
+    MdProgressSpinnerModule,
+    MdRadioModule,
+    MdRippleModule,
+    MdSelectModule,
+    MdSidenavModule,
+    MdSliderModule,
+    MdSlideToggleModule,
+    MdSnackBarModule,
+    MdSortModule,
+    MdTableModule,
+    MdTabsModule,
+    MdToolbarModule,
+    MdTooltipModule,
+  ]
+})
+export class PlunkerMaterialModule {}
 
 @NgModule({
 
@@ -14,7 +85,7 @@ import {HttpModule} from '@angular/http';
     BrowserAnimationsModule,
     FormsModule,
     HttpModule,
-    MaterialModule,
+    PlunkerMaterialModule,
     MdNativeDateModule,
     ReactiveFormsModule,
   ],
