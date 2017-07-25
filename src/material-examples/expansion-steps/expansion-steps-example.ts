9c4912f35e07f8d2f7c7a8d6c543533743478cee
Juliano <juliano.brasil@gmail.com>
2017-07-25 16:06:08 -0300

    add styleUrls in steps example @Component decorator

--- src/material-examples/expansion-steps/expansion-steps-example.ts
+++ src/material-examples/expansion-steps/expansion-steps-example.ts
@@ -4,6 +4,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'expansion-overview-example',
   templateUrl: 'expansion-overview-example.html',
+  styleUrls: ['expansion-steps-example.css']
 })
 export class ExpansionStepsExample {
   step = 0;
