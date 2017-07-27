fbd08b68f23a361cb1a0d2b6c898cb6ea962911a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-07-27 18:52:12 +0200

    feat: expose version object in releases (#4962)
    * In releases there will be a constant called `VERSION` that holds the current version of the installed package (material or cdk)
    * This is similar as for every @angular package like core, forms, compiler.
    
    Add accessibility demo page for grid list
    
    .

---
+++ src/demo-app/a11y/grid-list/grid-list-a11y.ts
@@ -0,0 +1,35 @@
+import {Component} from '@angular/core';
+
+interface Dog {
+  name: string;
+  human: string;
+}
+@Component({
+  moduleId: module.id,
+  selector: 'grid-list-a11y',
+  templateUrl: 'grid-list-a11y.html',
+  styleUrls: ['grid-list-a11y.css'],
+})
+export class GridListAccessibilityDemo {
+  dogs: Dog[] = [
+    { name: 'Porter', human: 'Kara' },
+    { name: 'Mal', human: 'Jeremy' },
+    { name: 'Koby', human: 'Igor' },
+    { name: 'Razzle', human: 'Ward' },
+    { name: 'Molly', human: 'Rob' },
+    { name: 'Husi', human: 'Matias' },
+  ];
+
+  tiles = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
+
+  fixedCols = 4;
+  fixedRowHeight = 100;
+  ratioGutter = 1;
+  fitListHeight = '400px';
+  ratio = '4:1';
+}
