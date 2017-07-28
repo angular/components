9993db16c9faa5bc4e0a4f9a3e7c883d9bad2439
Jeremy Elbourn <jelbourn@google.com>
2017-07-28 13:09:38 -0700

    fix: add importAs to generated metadata re-export
    Fixes #6046

--- tools/package-tools/metadata-reexport.ts
+++ tools/package-tools/metadata-reexport.ts
@@ -2,12 +2,14 @@ import {writeFileSync} from 'fs';
 import {join} from 'path';
 
 /** Creates a metadata file that re-exports the metadata bundle inside of the typings. */
-export function createMetadataReexportFile(destDir: string, from: string, name: string) {
+export function createMetadataReexportFile(
+    destDir: string, from: string, name: string, importAs: string) {
   const metadataReExport = `{
     "__symbolic":"module",
     "version":3,"metadata":{},
     "exports":[{"from":"${from}"}],
-    "flatModuleIndexRedirect": true
+    "flatModuleIndexRedirect": true,
+    "importAs": "@angular/${importAs}"
   }`;
   writeFileSync(join(destDir, `${name}.metadata.json`), metadataReExport, 'utf-8');
 }
