0abc5ee2eec35034a7fa6ef65a3964c37b4cf650
Lin Sun <2808411862@qq.com>
2017-08-01 11:21:47 -0700

    Added test for sticky-header without StickyRegion

--- src/lib/sticky-header/sticky-header.spec.ts
+++ src/lib/sticky-header/sticky-header.spec.ts
@@ -120,6 +120,38 @@ describe('sticky-header with positioning supported', () => {
   });
 });
 
+describe('test sticky-header without StickyRegion', () => {
+  let fixture: ComponentFixture<StickyHeaderTestNoStickyRegion>;
+  let testComponent: StickyHeaderTestNoStickyRegion;
+  let stickyElement: DebugElement;
+  let stickyHeader: CdkStickyHeader;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ OverlayModule, PlatformModule, StickyHeaderModule ],
+      declarations: [StickyHeaderTestNoStickyRegion],
+      providers: [
+        {provide: STICKY_HEADER_SUPPORT_STRATEGY, useValue: false},
+      ],
+    });
+    TestBed.compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StickyHeaderTestNoStickyRegion);
+    fixture.detectChanges();
+    testComponent = fixture.debugElement.componentInstance;
+    stickyElement = fixture.debugElement.query(By.directive(CdkStickyHeader));
+    stickyHeader = stickyElement.injector.get<CdkStickyHeader>(CdkStickyHeader);
+  });
+
+  it('should be able to find stickyParent', () => {
+    let p = stickyHeader.stickyParent;
+    expect(p).not.toBe(null);
+    expect(p!.id).toBe('default-region');
+  });
+});
+
 @Component({
   // Use styles to define the style of scrollable container and header,
   // which help test to make sure whether the header is stuck at the right position.
@@ -177,4 +209,45 @@ class StickyHeaderTest {
     // Emit a scroll event from the scrolling element in our component.
     dispatchFakeEvent(scrollingContainerEl, 'scroll');
   }
+}
+
+@Component({
+  // Use styles to define the style of scrollable container and header,
+  // which help test to make sure whether the header is stuck at the right position.
+  styles:[`
+    .scrollable-style {
+      text-align: center;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      height: 300px;
+      overflow: auto;
+    }
+    .heading-style {
+      background: whitesmoke;
+      padding: 5px;
+    }
+  `],
+  template: `
+    <div cdk-scrollable class="scrollable-style">
+      <p *ngFor="let item of items"> {{item.name}} : {{item.message}}</p>
+      <div id="default-region">
+        <div cdkStickyHeader class="heading-style">
+          <h2>Heading 1</h2>
+        </div>
+        <p *ngFor="let item of items"> {{item.name}} : {{item.message}}</p>
+        <p *ngFor="let item of items"> {{item.name}} : {{item.message}}</p>
+        <p *ngFor="let item of items"> {{item.name}} : {{item.message}}</p>
+      </div>
+    </div>
+  `})
+class StickyHeaderTestNoStickyRegion {
+  items: any[] = [
+    {'name': 'Forrest', 'message': 'Life was like a box of chocolates'},
+    {'name': 'Gump', 'message': 'you never know what you are gonna get'},
+    {'name': 'Lion King', 'message': 'Everything you see exists together'},
+    {'name': 'Jack', 'message': 'in a delicate balance'},
+    {'name': 'Garfield', 'message': 'Save Water'},
+    {'name': 'Shawshank', 'message': 'There is something inside'},
+    {'name': 'Jone', 'message': 'Enough movies?'},
+  ];
 }
