t2hm/material2

feat(select): allow focusing items by typing
Allows for users to skip to a select item by typing, similar to the native select.

Fixes #2668.

src/lib/core/option/option.ts
@@ -130,6 +130,11 @@ export class MdOption {
     Promise.resolve(null).then(() => this._active = false);
   }
 
+  /** Fetches the label to be used when determining whether the option should be focused. */
+  getFocusableLabel(): string {
+    return this.viewValue;
+  }
+
   /** Ensures the option is selected when activated from the keyboard. */
   _handleKeydown(event: KeyboardEvent): void {
     if (event.keyCode === ENTER || event.keyCode === SPACE) {

angular/components

src/lib/core/option/option.ts
@@ -174,6 +174,11 @@ export class MdOption {
     }
   }
 
+  /** Fetches the label to be used when determining whether the option should be focused. */
+  getLabel(): string {
+    return this.viewValue;
+  }
+
   /** Ensures the option is selected when activated from the keyboard. */
   _handleKeydown(event: KeyboardEvent): void {
     if (event.keyCode === ENTER || event.keyCode === SPACE) {