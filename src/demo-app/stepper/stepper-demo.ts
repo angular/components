92636df1e13401a392937bc2810d4dddd6f32423
Ji Won Shin <jwshin@google.com>
2017-08-08 14:16:53 -0700

    feat(stepper): Add support for linear stepper (#6116)
    * Add form controls and custom error state matcher
    
    * Modify form controls for stepper-demo and add custom validator
    
    * Move custom step validation function so that users can simply import and use
    
    * Implement @Input() stepControl for each step
    
    * Add linear attribute to stepper
    
    * Add enabling/disabling linear state of demo

--- src/demo-app/stepper/stepper-demo.ts
+++ src/demo-app/stepper/stepper-demo.ts
@@ -1,16 +1,39 @@
 import {Component} from '@angular/core';
+import {Validators, FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   moduleId: module.id,
   selector: 'stepper-demo',
   templateUrl: 'stepper-demo.html',
-  styleUrls: ['stepper-demo.scss'],
+  styleUrls: ['stepper-demo.scss']
 })
 export class StepperDemo {
+  formGroup: FormGroup;
+  isNonLinear = false;
+
   steps = [
     {label: 'Confirm your name', content: 'Last name, First name.'},
     {label: 'Confirm your contact information', content: '123-456-7890'},
     {label: 'Confirm your address', content: '1600 Amphitheater Pkwy MTV'},
     {label: 'You are now done', content: 'Finished!'}
   ];
+
+  /** Returns a FormArray with the name 'formArray'. */
+  get formArray() { return this.formGroup.get('formArray'); }
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.formGroup = this._formBuilder.group({
+      formArray: this._formBuilder.array([
+        this._formBuilder.group({
+          firstNameFormCtrl: ['', Validators.required],
+          lastNameFormCtrl: ['', Validators.required],
+        }),
+        this._formBuilder.group({
+          phoneFormCtrl: [''],
+        })
+      ])
+    });
+  }
 }
