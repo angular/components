344392425441e36fba9f8a9efa370f88d20bceea
Andrew Seguin <andrewjs@google.com>
2017-08-09 17:09:21 -0700

    compat(datepicker): add mat compatible selectors

--- src/lib/datepicker/datepicker.ts
+++ src/lib/datepicker/datepicker.ts
@@ -78,7 +78,7 @@ export const MD_DATEPICKER_SCROLL_STRATEGY_PROVIDER = {
  */
 @Component({
   moduleId: module.id,
-  selector: 'md-datepicker-content',
+  selector: 'md-datepicker-content, mat-datepicker-content',
   templateUrl: 'datepicker-content.html',
   styleUrls: ['datepicker-content.css'],
   host: {
