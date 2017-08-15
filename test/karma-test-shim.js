39a36498f1c981010222dcf506f9526ee828b9c9
Ji Won Shin <jwshin@google.com>
2017-08-15 12:55:35 -0700

    feat(stepper): Add unit tests for stepper (#6428)
    * Add unit tests for stepper
    
    * Changes made based on review
    
    * More changes based on review

--- test/karma-test-shim.js
+++ test/karma-test-shim.js
@@ -58,6 +58,7 @@ System.config({
     '@angular/cdk/platform': 'dist/packages/cdk/platform/index.js',
     '@angular/cdk/portal': 'dist/packages/cdk/portal/index.js',
     '@angular/cdk/rxjs': 'dist/packages/cdk/rxjs/index.js',
+    '@angular/cdk/stepper': 'dist/packages/cdk/stepper/index.js',
     '@angular/cdk/table': 'dist/packages/cdk/table/index.js',
     '@angular/cdk/testing': 'dist/packages/cdk/testing/index.js',
   },
