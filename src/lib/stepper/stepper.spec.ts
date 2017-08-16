619226072a17267e7deeaa947b17cd0504528d48
Ji Won Shin <jwshin@google.com>
2017-08-15 17:27:20 -0700

    Add tests and style changes

--- src/lib/stepper/stepper.spec.ts
+++ src/lib/stepper/stepper.spec.ts
@@ -75,6 +75,12 @@ describe('MdHorizontalStepper', () => {
       let stepHeaders = fixture.debugElement.queryAll(By.css('.mat-horizontal-stepper-header'));
       checkKeyboardEvent(stepperComponent, fixture, stepHeaders);
     });
+
+    it('should not set focus on header of selected step if header is not clicked', () => {
+      let stepHeaderEl = fixture.debugElement
+          .queryAll(By.css('.mat-horizontal-stepper-header'))[1].nativeElement;
+      checkStepHeaderFocusNotCalled(stepHeaderEl, stepperComponent, fixture);
+    });
   });
 
   describe('linear horizontal stepper', () => {
@@ -104,6 +110,12 @@ describe('MdHorizontalStepper', () => {
       let stepHeaderEl = fixture.debugElement
           .queryAll(By.css('.mat-horizontal-stepper-header'))[1].nativeElement;
       checkLinearStepperValidity(stepHeaderEl, stepperComponent, testComponent, fixture);
+    });
+
+    it('should not focus step header upon click if it is not able to be selected', () => {
+      let stepHeaderEl = fixture.debugElement
+          .queryAll(By.css('.mat-horizontal-stepper-header'))[1].nativeElement;
+      checkStepHeaderBlur(stepHeaderEl, fixture);
     });
   });
 });
@@ -173,6 +185,12 @@ describe('MdVerticalStepper', () => {
       let stepHeaders = fixture.debugElement.queryAll(By.css('.mat-vertical-stepper-header'));
       checkKeyboardEvent(stepperComponent, fixture, stepHeaders);
     });
+
+    it('should not set focus on header of selected step if header is not clicked', () => {
+      let stepHeaderEl = fixture.debugElement
+          .queryAll(By.css('.mat-vertical-stepper-header'))[1].nativeElement;
+      checkStepHeaderFocusNotCalled(stepHeaderEl, stepperComponent, fixture);
+    });
   });
 
   describe('linear vertical stepper', () => {
@@ -203,6 +221,12 @@ describe('MdVerticalStepper', () => {
           .queryAll(By.css('.mat-vertical-stepper-header'))[1].nativeElement;
 
       checkLinearStepperValidity(stepHeaderEl, stepperComponent, testComponent, fixture);
+    });
+
+    it('should not focus step header upon click if it is not able to be selected', () => {
+      let stepHeaderEl = fixture.debugElement
+          .queryAll(By.css('.mat-vertical-stepper-header'))[1].nativeElement;
+      checkStepHeaderBlur(stepHeaderEl, fixture);
     });
   });
 });
@@ -387,6 +411,19 @@ function checkKeyboardEvent(stepperComponent: MdStepper,
           'Expected index of selected step to change to index of focused step after SPACE event.');
 }
 
+function checkStepHeaderFocusNotCalled(stepHeaderEl: HTMLElement,
+                                       stepperComponent: MdStepper,
+                                       fixture: ComponentFixture<any>) {
+  let nextButtonNativeEl = fixture.debugElement
+      .queryAll(By.directive(MdStepperNext))[0].nativeElement;
+  spyOn(stepHeaderEl, 'focus');
+  nextButtonNativeEl.click();
+  fixture.detectChanges();
+
+  expect(stepperComponent.selectedIndex).toBe(1);
+  expect(stepHeaderEl.focus).not.toHaveBeenCalled();
+}
+
 function checkLinearStepperValidity(stepHeaderEl: HTMLElement,
                                     stepperComponent: MdStepper,
                                     testComponent:
@@ -410,6 +447,14 @@ function checkLinearStepperValidity(stepHeaderEl: HTMLElement,
 
   expect(testComponent.oneGroup.valid).toBe(true);
   expect(stepperComponent.selectedIndex).toBe(1);
+}
+
+function checkStepHeaderBlur(stepHeaderEl: HTMLElement, fixture: ComponentFixture<any>) {
+  spyOn(stepHeaderEl, 'blur');
+  stepHeaderEl.click();
+  fixture.detectChanges();
+
+  expect(stepHeaderEl.blur).toHaveBeenCalled();
 }
 
 @Component({
