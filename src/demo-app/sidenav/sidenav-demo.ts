40d760ee62b121dd1c777b5d95bb735d873971cc
Miles Malerba <mmalerba@google.com>
2017-08-29 15:07:00 -0700

    create sidenav demo

---
+++ src/demo-app/sidenav/sidenav-demo.ts
@@ -0,0 +1,14 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'sidenav-demo',
+  templateUrl: 'sidenav-demo.html',
+  styleUrls: ['sidenav-demo.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class SidenavDemo {
+  isLaunched = false;
+  fillerContent = Array(30);
+}
