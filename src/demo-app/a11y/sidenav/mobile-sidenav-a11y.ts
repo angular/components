53a573c8da53e525977ed26d0b77aed3af9401f7
Miles Malerba <mmalerba@google.com>
2017-09-07 10:15:40 -0700

    mobile example

--- src/demo-app/a11y/sidenav/mobile-sidenav-a11y.ts
+++ src/demo-app/a11y/sidenav/mobile-sidenav-a11y.ts
@@ -1,12 +1,25 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, ViewEncapsulation} from '@angular/core';
 
 
 @Component({
   moduleId: module.id,
   selector: 'mobile-sidenav-a11y',
   templateUrl: 'mobile-sidenav-a11y.html',
-  styleUrls: ['shared.css'],
+  styleUrls: ['shared.css', 'mobile-sidenav-a11y.css'],
   host: {'class': 'a11y-demo-sidenav-app'},
   encapsulation: ViewEncapsulation.None,
 })
-export class SidenavMobileAccessibilityDemo {}
+export class SidenavMobileAccessibilityDemo implements OnDestroy {
+  mobileQuery = matchMedia('(max-width: 600px)');
+
+  _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef) {
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+}
