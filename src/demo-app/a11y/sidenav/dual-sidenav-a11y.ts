b0ecb4b7fef58319826a8ce05c6bd122f2b708bf
Miles Malerba <mmalerba@google.com>
2017-09-08 14:04:55 -0700

    dual demo

--- src/demo-app/a11y/sidenav/dual-sidenav-a11y.ts
+++ src/demo-app/a11y/sidenav/dual-sidenav-a11y.ts
@@ -1,12 +1,19 @@
 import {Component, ViewEncapsulation} from '@angular/core';
+import {MdSnackBar} from '@angular/material';
 
 
 @Component({
   moduleId: module.id,
   selector: 'dual-sidenav-a11y',
   templateUrl: 'dual-sidenav-a11y.html',
-  styleUrls: ['shared.css'],
+  styleUrls: ['shared.css', 'dual-sidenav-a11y.css'],
   host: {'class': 'a11y-demo-sidenav-app'},
   encapsulation: ViewEncapsulation.None,
 })
-export class SidenavDualAccessibilityDemo {}
+export class SidenavDualAccessibilityDemo {
+  constructor(private _snackbar: MdSnackBar) {}
+
+  play(list: string) {
+    this._snackbar.open(`Playing "${list}"`, '', {duration: 1000});
+  }
+}
