angular/angular

commit f6d1078e5440531a57fd8e187ccb6720b725a6f6

fix(tooltip): ensure tooltip never passes undefined message to (#7018)
AriaDescriber

src/lib/tooltip/tooltip.ts
@@ -142,17 +142,15 @@ export class MdTooltip implements OnDestroy {
   /** The default delay in ms before hiding the tooltip after hide is called */
   @Input('mdTooltipHideDelay') hideDelay = 0;
 
-  private _message: string;
+  private _message = '';
 
   /** The message to be displayed in the tooltip */
   @Input('mdTooltip') get message() { return this._message; }
   set message(value: string) {
-    if (this._message) {
-      this._ariaDescriber.removeDescription(this._elementRef.nativeElement, this._message);
-    }
+    this._ariaDescriber.removeDescription(this._elementRef.nativeElement, this._message);
 
     // If the message is not a string (e.g. number), convert it to a string and trim it.
-    this._message = value ? `${value}`.trim() : '';
+    this._message = value ? value.trim() : '';
     this._updateTooltipMessage();
     this._ariaDescriber.describe(this._elementRef.nativeElement, this.message);
   }

angular/components

commit b42015ed7fbe2b312807da48e616481018a60dd6

chore(tooltip): regression when trimming non-string messages
Fixes a regression from f6d1078e5440531a57fd8e187ccb6720b725a6f6.

src/lib/tooltip/tooltip.ts
@@ -150,7 +150,7 @@ export class MdTooltip implements OnDestroy {
     this._ariaDescriber.removeDescription(this._elementRef.nativeElement, this._message);
 
     // If the message is not a string (e.g. number), convert it to a string and trim it.
-    this._message = value ? value.trim() : '';
+    this._message = value != null ? `${value}`.trim() : '';
     this._updateTooltipMessage();
     this._ariaDescriber.describe(this._elementRef.nativeElement, this.message);
   }