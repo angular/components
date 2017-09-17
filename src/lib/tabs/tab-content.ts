17ef473f16b0c17e212e1566573533d2aec7aa9b
Austin <amcdaniel2@gmail.com>
2017-09-17 11:55:07 -0500

    chore(merge): fix merge

--- src/lib/tabs/tab-content.ts
+++ src/lib/tabs/tab-content.ts
@@ -1,4 +1,12 @@
-import {Directive, TemplateRef} from '@angular/core';
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+ import {Directive, TemplateRef} from '@angular/core';
 
 @Directive({selector: '[mdTabContent]'})
 export class MdTabContent {
