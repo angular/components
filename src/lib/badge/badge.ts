4a454fe618e8f881d6b3956229a83159b32e1519
Austin <amcdaniel2@gmail.com>
2017-10-03 16:30:32 -0500

    chore(nit): address PR feedback

--- src/lib/badge/badge.ts
+++ src/lib/badge/badge.ts
@@ -6,7 +6,9 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
- import {Component, Input, ViewEncapsulation, Renderer2, ElementRef} from '@angular/core';
+ import {
+   Component, Input, ViewEncapsulation, Renderer2, ElementRef, ChangeDetectionStrategy
+} from '@angular/core';
  import {coerceBooleanProperty} from '@angular/cdk/coercion';
  import {CanColor, mixinColor} from '@angular/material/core';
 
@@ -25,12 +27,13 @@ export const _MatBadgeMixinBase = mixinColor(MatBadgeBase);
     'class': 'mat-badge',
     '[class.mat-badge-overlap]': 'overlap',
     '[class.mat-badge-above]': '_isAbove',
-    '[class.mat-badge-below]': '!_isAbove',
+    '[class.mat-badge-below]': '_isBelow',
     '[class.mat-badge-before]': '_isBefore',
-    '[class.mat-badge-after]': '!_isBefore',
+    '[class.mat-badge-after]': '_isAfter',
   },
   encapsulation: ViewEncapsulation.None,
   preserveWhitespaces: false,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MatBadge extends _MatBadgeMixinBase implements CanColor {
 
@@ -44,15 +47,38 @@ export class MatBadge extends _MatBadgeMixinBase implements CanColor {
   }
   private _overlap: boolean = true;
 
-  /** Content of the badget */
+  /** Content of the badge */
   @Input() content: string = '';
 
-  /** Direction the badge should reside; 'before|after above|below' */
-  @Input() direction: string = 'above after';
+  /** Direction the badge should reside; 'above|below before|after' */
+  @Input()
+  set direction(val: string) {
+    this._direction = val;
+    if (val.indexOf('above') > -1) {
+      this._isAbove = true;
+      this._isBelow = false;
+    } else if (val.indexOf('below') > -1) {
+      this._isAbove = false;
+      this._isBelow = true;
+    }
 
-  get _isAbove(): boolean { return this.direction.indexOf('above') > -1; }
+    if (val.indexOf('before') > -1) {
+      this._isBefore = true;
+      this._isAfter = false;
+    } else if (val.indexOf('after') > -1) {
+      this._isAfter = true;
+      this._isBefore = false;
+    }
+  }
+  get direction(): string {
+    return this._direction;
+  }
+  private _direction: string = 'above after';
 
-  get _isBefore(): boolean { return this.direction.indexOf('before') > -1; }
+  _isAbove: boolean = true;
+  _isBelow: boolean = false;
+  _isBefore: boolean = false;
+  _isAfter: boolean = true;
 
   constructor(_renderer: Renderer2, _elementRef: ElementRef) {
     super(_renderer, _elementRef);
