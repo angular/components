3b4036146e97827ed687190f08cbadebd1e247d9
Emilio Martinez-Cordero <e.marticor@gmail.com>
2017-10-05 11:12:39 -0700

    chore(dialog): allow number|string for min/max widths and heights

--- src/lib/dialog/dialog-config.ts
+++ src/lib/dialog/dialog-config.ts
@@ -57,17 +57,17 @@ export class MatDialogConfig {
   /** Height of the dialog. */
   height?: string = '';
 
-  /** Min-width of the dialog. */
-  minWidth?: string;
+  /** Min-width of the dialog. If a number is provided, pixel units are assumed. */
+  minWidth?: number | string;
 
-  /** Min-height of the dialog */
-  minHeight?: string;
+  /** Min-height of the dialog. If a number is provided, pixel units are assumed. */
+  minHeight?: number | string;
 
-  /** Max-width of the dialog. */
-  maxWidth?: string = '80vw';
+  /** Max-width of the dialog. If a number is provided, pixel units are assumed. Defaults to 80vw */
+  maxWidth?: number | string = '80vw';
 
-  /** Max-height of the dialog */
-  maxHeight?: string;
+  /** Max-height of the dialog. If a number is provided, pixel units are assumed. */
+  maxHeight?: number | string;
 
   /** Position overrides. */
   position?: DialogPosition;
