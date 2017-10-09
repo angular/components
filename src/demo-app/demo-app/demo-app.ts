pallapo/material2

change sidenav demo to drawer

src/demo-app/demo-app/demo-app.ts
@@ -53,6 +53,7 @@ export class DemoApp {
     {name: 'Checkbox', route: '/checkbox'},
     {name: 'Datepicker', route: '/datepicker'},
     {name: 'Dialog', route: '/dialog'},
+    {name: 'Drawer', route: '/drawer'},
     {name: 'Expansion Panel', route: '/expansion'},
     {name: 'Gestures', route: '/gestures'},
     {name: 'Grid List', route: '/grid-list'},
@@ -68,7 +69,6 @@ export class DemoApp {
     {name: 'Radio', route: '/radio'},
     {name: 'Ripple', route: '/ripple'},
     {name: 'Select', route: '/select'},
-    {name: 'Sidenav', route: '/sidenav'},
     {name: 'Slider', route: '/slider'},
     {name: 'Slide Toggle', route: '/slide-toggle'},
     {name: 'Snack Bar', route: '/snack-bar'},

angular/components

src/demo-app/demo-app/demo-app.ts
@@ -70,6 +70,7 @@ export class DemoApp {
     {name: 'Screen Type', route: '/screen-type'},
     {name: 'Select', route: '/select'},
     {name: 'Sidenav', route: '/sidenav'},
+    {name: 'Slider', route: '/slider'},
     {name: 'Slide Toggle', route: '/slide-toggle'},
     {name: 'Slider', route: '/slider'},
     {name: 'Snack Bar', route: '/snack-bar'},