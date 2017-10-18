10271d984bd0d31110e7a0693c5d7d238e3d5056
Miles Malerba <mmalerba@google.com>
2017-10-18 08:46:24 -0700

    fix test

--- src/lib/datepicker/datepicker.spec.ts
+++ src/lib/datepicker/datepicker.spec.ts
@@ -30,6 +30,8 @@ import {MatDatepickerIntl, MatDatepickerModule} from './index';
 
 
 describe('MatDatepicker', () => {
+  const SUPPORTS_INTL = typeof Intl != 'undefined';
+
   afterEach(inject([OverlayContainer], (container: OverlayContainer) => {
     container.getContainerElement().parentNode!.removeChild(container.getContainerElement());
   }));
@@ -83,7 +85,9 @@ describe('MatDatepicker', () => {
       }));
 
       it('should initialize with correct value shown in input', () => {
-        expect(fixture.nativeElement.querySelector('input').value).toBe('1/1/2020');
+        if (SUPPORTS_INTL) {
+          expect(fixture.nativeElement.querySelector('input').value).toBe('1/1/2020');
+        }
       });
 
       it('open non-touch should open popup', () => {
