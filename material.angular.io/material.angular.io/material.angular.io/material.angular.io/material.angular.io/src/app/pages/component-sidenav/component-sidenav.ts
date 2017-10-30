fb6860dea95904ac2cb2ff465a41bf3e9819e9cd
Jeremy Elbourn <jelbourn@google.com>
2017-10-30 10:29:03 -0700

    Revert "feat(nav): new sidenav UX (#300)"
    This reverts commit 4f07a4e24e4c5ab4e5d8f184bc503638135273ee.

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/pages/component-sidenav/component-sidenav.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/pages/component-sidenav/component-sidenav.ts
@@ -1,18 +1,12 @@
-import {
-  Component, Input, NgZone, ViewEncapsulation, ViewChild, OnInit, NgModule, trigger, state,
-  animate, transition, style, OnDestroy
-} from '@angular/core';
+import {Component, NgZone, ViewEncapsulation, ViewChild, OnInit, NgModule} from '@angular/core';
 import {DocumentationItems} from '../../shared/documentation-items/documentation-items';
-import {MatSidenav, MatSidenavModule, MatIconModule} from '@angular/material';
+import {MatSidenav, MatSidenavModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ActivatedRoute, Params, Router, RouterModule} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {ComponentHeaderModule} from '../component-page-header/component-page-header';
 import {FooterModule} from '../../shared/footer/footer';
 import {Observable} from 'rxjs/Observable';
-import {Subject} from 'rxjs/Subject';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/takeUntil';
 
 const SMALL_WIDTH_BREAKPOINT = 840;
 
@@ -43,85 +37,15 @@ export class ComponentSidenav implements OnInit {
         this.sidenav.close();
       }
     });
-
     // Combine params from all of the path into a single object.
     this.params = Observable.combineLatest(
-        this._route.pathFromRoot.map(route => route.params),
-        Object.assign);
+      this._route.pathFromRoot.map(route => route.params),
+      Object.assign);
   }
 
   isScreenSmall(): boolean {
     return this.mediaMatcher.matches;
   }
-}
-
-@Component({
-  selector: 'app-component-nav',
-  templateUrl: './component-nav.html',
-  animations: [
-    trigger('bodyExpansion', [
-      state('collapsed', style({height: '0px', visibility: 'hidden'})),
-      state('expanded', style({height: '*', visibility: 'visible'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4,0.0,0.2,1)')),
-    ]),
-  ],
-})
-export class ComponentNav implements OnInit, OnDestroy {
-
-  @Input() params: Observable<Params>;
-  expansions = {};
-  private _onDestroy = new Subject<void>();
-
-  constructor(public docItems: DocumentationItems,
-              private _router: Router) { }
-
-  ngOnInit() {
-    this._router.events
-        .startWith(null)
-        .switchMap(() => this.params)
-        .takeUntil(this._onDestroy)
-        .subscribe(p => this.setExpansions(p));
-  }
-
-  ngOnDestroy() {
-    this._onDestroy.next();
-    this._onDestroy.complete();
-  }
-
-  /** Set the expansions based on the route url */
-  setExpansions(params: Params) {
-    const categories = this.docItems.getCategories(params.section);
-    for (const category of categories) {
-      if (this.expansions[category.id] === true) {
-        continue;
-      }
-
-      let match = false;
-      for (const item of category.items) {
-        if (this._router.url.indexOf(item.id) > -1) {
-          match = true;
-          break;
-        }
-      }
-      this.expansions[category.id] = match;
-    }
-  }
-
-  /** Gets the expanded state */
-  _getExpandedState(category: string) {
-    return this.getExpanded(category) ? 'expanded' : 'collapsed';
-  }
-
-  /** Toggles the expanded state */
-  toggleExpand(category: string) {
-    this.expansions[category] = !this.expansions[category];
-  }
-
-  /** Gets whether expanded or not */
-  getExpanded(category: string): boolean {
-    return this.expansions[category];
-  }
-
 }
 
 
@@ -132,11 +56,10 @@ export class ComponentNav implements OnInit, OnDestroy {
     CommonModule,
     ComponentHeaderModule,
     FooterModule,
-    BrowserAnimationsModule,
-    MatIconModule,
+    BrowserAnimationsModule
   ],
   exports: [ComponentSidenav],
-  declarations: [ComponentSidenav, ComponentNav],
+  declarations: [ComponentSidenav],
   providers: [DocumentationItems],
 })
 export class ComponentSidenavModule {}
