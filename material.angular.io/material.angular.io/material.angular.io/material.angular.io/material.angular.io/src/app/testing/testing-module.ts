bc4dd16917f3aa490e9e3eafe51889b684e51e9b
Andrew Seguin <andrewseguin@users.noreply.github.com>
2017-11-01 13:01:11 -0700

    chore: update to 5.0.0 (#312)

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/testing/testing-module.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/testing/testing-module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {HttpModule, XHRBackend} from '@angular/http';
 import {MockBackend} from '@angular/http/testing';
 import {RouterTestingModule} from '@angular/router/testing';
-import {MATERIAL_SANITY_CHECKS, MATERIAL_COMPATIBILITY_MODE} from '@angular/material';
+import {MATERIAL_SANITY_CHECKS} from '@angular/material';
 
 @NgModule({
   imports: [RouterTestingModule, HttpModule],
@@ -11,7 +11,6 @@ import {MATERIAL_SANITY_CHECKS, MATERIAL_COMPATIBILITY_MODE} from '@angular/mate
     MockBackend,
     {provide: XHRBackend, useExisting: MockBackend},
     {provide: MATERIAL_SANITY_CHECKS, useValue: false},
-    {provide: MATERIAL_COMPATIBILITY_MODE, useValue: true},
   ],
 })
 export class DocsAppTestingModule {}
