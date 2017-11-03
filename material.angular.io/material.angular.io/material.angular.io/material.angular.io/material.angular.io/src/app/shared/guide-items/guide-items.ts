fc952997e3d5f4fa1e38891a3b18480ee55588d9
Andrew Seguin <andrewseguin@users.noreply.github.com>
2017-11-03 12:18:23 -0700

    fix guides references (#320)
    * fix guides references
    
    * revert elevation guide

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/shared/guide-items/guide-items.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/shared/guide-items/guide-items.ts
@@ -10,37 +10,37 @@ const GUIDES = [
   {
     id: 'getting-started',
     name: 'Getting started',
-    document: '/assets/documents/guides/getting-started.html',
+    document: '/assets/documents/guides/material-getting-started.html',
   },
   {
     id: 'theming',
     name: 'Theming Angular Material',
-    document: '/assets/documents/guides/theming.html',
+    document: '/assets/documents/guides/material-theming.html',
   },
   {
     id: 'theming-your-components',
     name: 'Theming your own components',
-    document: '/assets/documents/guides/theming-your-components.html',
+    document: '/assets/documents/guides/material-theming-your-components.html',
   },
   {
     id: 'typography',
     name: `Using Angular Material's Typography`,
-    document: '/assets/documents/guides/typography.html',
+    document: '/assets/documents/guides/material-typography.html',
   },
   {
     id: 'cdk-table',
     name: `CDK data-table`,
-    document: '/assets/documents/guides/cdk-table.html',
+    document: '/assets/documents/guides/material-cdk-table.html',
   },
   {
     id: 'customizing-component-styles',
     name: 'Customizing component styles',
-    document: '/assets/documents/guides/customizing-component-styles.html'
+    document: '/assets/documents/guides/material-customizing-component-styles.html'
   },
   {
     id: 'creating-a-custom-form-field-control',
     name: 'Creating a custom form field control',
-    document: '/assets/documents/guides/creating-a-custom-form-field-control.html'
+    document: '/assets/documents/guides/material-creating-a-custom-form-field-control.html'
   },
   {
     id: 'elevation',
