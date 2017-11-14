4b5f8351bf83080b495bdd12863452488c9901f4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-11-14 19:15:06 +0100

    Address feedback

--- tools/dgeni/common/dgeni-definitions.ts
+++ tools/dgeni/common/dgeni-definitions.ts
@@ -3,12 +3,14 @@ import {ClassLikeExportDoc} from 'dgeni-packages/typescript/api-doc-types/ClassL
 import {PropertyMemberDoc} from 'dgeni-packages/typescript/api-doc-types/PropertyMemberDoc';
 import {NormalizedMethodMemberDoc} from './normalize-method-parameters';
 
+/** Extended Dgeni class-like document that includes separated class members. */
 export interface CategorizedClassLikeDoc extends ClassLikeExportDoc {
   methods: CategorizedMethodMemberDoc[];
   properties: CategorizedPropertyMemberDoc[];
   isDeprecated: boolean;
 }
 
+/** Extended Dgeni class document that includes extracted Angular metadata. */
 export interface CategorizedClassDoc extends ClassExportDoc, CategorizedClassLikeDoc {
   isDirective: boolean;
   isService: boolean;
@@ -18,6 +20,7 @@ export interface CategorizedClassDoc extends ClassExportDoc, CategorizedClassLik
   extendedDoc: ClassLikeExportDoc | null;
 }
 
+/** Extended Dgeni property-member document that includes extracted Angular metadata. */
 export interface CategorizedPropertyMemberDoc extends PropertyMemberDoc {
   description: string;
   isDeprecated: boolean;
@@ -27,6 +30,7 @@ export interface CategorizedPropertyMemberDoc extends PropertyMemberDoc {
   directiveOutputAlias: string;
 }
 
+/** Extended Dgeni method-member document that simplifies logic for the Dgeni template. */
 export interface CategorizedMethodMemberDoc extends NormalizedMethodMemberDoc {
   showReturns: boolean;
   isDeprecated: boolean;
