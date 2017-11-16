e0914a96d927e6bbe9d3fa3971caa1e19ee2faad
Miles Malerba <mmalerba@google.com>
2017-11-16 08:34:40 -0800

    universal: dump out the prerendered file so we can examine it

--- src/universal-app/prerender.ts
+++ src/universal-app/prerender.ts
@@ -4,7 +4,7 @@ import 'zone.js';
 import {enableProdMode} from '@angular/core';
 import {renderModuleFactory} from '@angular/platform-server';
 import {join} from 'path';
-import {readFileSync} from 'fs-extra';
+import {readFileSync, writeFileSync} from 'fs-extra';
 import {log} from 'gulp-util';
 import {KitchenSinkServerModuleNgFactory} from './kitchen-sink/kitchen-sink.ngfactory';
 
@@ -15,7 +15,10 @@ const result = renderModuleFactory(KitchenSinkServerModuleNgFactory, {
 });
 
 result
-  .then(() => log('Prerender done.'))
+  .then(content => {
+    writeFileSync(join(__dirname, 'index-prerendered.html'), content, 'utf-8');
+    log('Prerender done.');
+  })
   // If rendering the module factory fails, exit the process with an error code because otherwise
   // the CI task will not recognize the failure and will show as "success". The error message
   // will be printed automatically by the `renderModuleFactory` method.
