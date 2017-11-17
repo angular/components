7fa850221c4a584d6bf2f4445eab696c8b36555b
Gabriel Schuster <actra-gschuster@users.noreply.github.com>
2017-11-17 07:16:56 +0100

    Update stepper.ts

--- src/lib/stepper/stepper.ts
+++ src/lib/stepper/stepper.ts
@@ -27,7 +27,6 @@ import {FormControl, FormGroupDirective, NgForm} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
 import {MatStepHeader} from './step-header';
 import {MatStepLabel} from './step-label';
-import {Subject} from 'rxjs/Subject';
 import {takeUntil} from 'rxjs/operators/takeUntil';
 
 /** Workaround for https://github.com/angular/angular/issues/17849 */
