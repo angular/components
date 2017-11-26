b0ea290a709ab9732638f6bd518ed11735e83d9a
Elad Bezalel <elad.bezalel@gmail.com>
2017-11-26 16:40:41 +0200

    docs(button): enhance button docs and examples

---
+++ src/material-examples/button-toolbar/button-toolbar-example.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Icon button usage in toolbar
+ */
+@Component({
+  selector: 'button-toolbar-example',
+  templateUrl: 'button-toolbar-example.html',
+  styleUrls: ['button-toolbar-example.css'],
+})
+export class ButtonToolbarExample {}
