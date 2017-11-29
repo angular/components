pallapo/material2

demo(list): Add accessibility demo page for list

src/lib/list/list.ts
@@ -46,35 +46,30 @@ export class MdListDivider {}
 /** A Material Design list component. */
 @Component({
   moduleId: module.id,
-  selector: 'md-list, mat-list, md-nav-list, mat-nav-list',
-  host: {'role': 'list'},
+  selector: 'md-nav-list, mat-nav-list',
+  host: {
+    'role': 'navigation',
+    'class': 'mat-nav-list'
+  },
   template: '<ng-content></ng-content>',
   styleUrls: ['list.css'],
   inputs: ['disableRipple'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MdList extends _MdListMixinBase implements CanDisableRipple {}
+export class MdNavList extends _MdListMixinBase implements CanDisableRipple {}
 
-/**
- * Directive whose purpose is to add the mat- CSS styling to this selector.
- * @docs-private
- */
-@Directive({
+@Component({
+  moduleId: module.id,
   selector: 'md-list, mat-list',
-  host: {'class': 'mat-list'}
+  template: '<ng-content></ng-content>',
+  host: {'class': 'mat-list'},
+  styleUrls: ['list.css'],
+  inputs: ['disableRipple'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MdListCssMatStyler {}
-
-/**
- * Directive whose purpose is to add the mat- CSS styling to this selector.
- * @docs-private
- */
-@Directive({
-  selector: 'md-nav-list, mat-nav-list',
-  host: {'class': 'mat-nav-list'}
-})
-export class MdNavListCssMatStyler {}
+export class MdList extends _MdListMixinBase implements CanDisableRipple {}
 
 /**
  * Directive whose purpose is to add the mat- CSS styling to this selector.
@@ -149,9 +144,9 @@ export class MdListItem extends _MdListItemMixinBase implements AfterContentInit
   constructor(private _renderer: Renderer2,
               private _element: ElementRef,
               @Optional() private _list: MdList,
-              @Optional() navList: MdNavListCssMatStyler) {
+              @Optional() private _navList: MdNavList) {
     super();
-    this._isNavList = !!navList;
+    this._isNavList = !!_navList;
   }
 
   ngAfterContentInit() {

angular/components

src/lib/list/list.ts
@@ -44,9 +44,12 @@ export class MatListDivider {}
 /** A Material Design list component. */
 @Component({
   moduleId: module.id,
-  selector: 'mat-list, mat-nav-list',
-  exportAs: 'matList, matNavList',
-  host: {'role': 'list'},
+  selector: 'mat-nav-list',
+  exportAs: 'matNavList',
+  host: {
+    'role': 'navigation',
+    'class': 'mat-nav-list'
+  },
   template: '<ng-content></ng-content>',
   styleUrls: ['list.css'],
   inputs: ['disableRipple'],
@@ -54,27 +57,20 @@ export class MatListDivider {}
   preserveWhitespaces: false,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MatList extends _MatListMixinBase implements CanDisableRipple {}
+export class MatNavList extends _MatListMixinBase implements CanDisableRipple {}
 
-/**
- * Directive whose purpose is to add the mat- CSS styling to this selector.
- * @docs-private
- */
-@Directive({
+@Component({
+  moduleId: module.id,
   selector: 'mat-list',
-  host: {'class': 'mat-list'}
+  exportAs: 'matList',
+  template: '<ng-content></ng-content>',
+  host: {'class': 'mat-list'},
+  styleUrls: ['list.css'],
+  inputs: ['disableRipple'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MatListCssMatStyler {}
-
-/**
- * Directive whose purpose is to add the mat- CSS styling to this selector.
- * @docs-private
- */
-@Directive({
-  selector: 'mat-nav-list',
-  host: {'class': 'mat-nav-list'}
-})
-export class MatNavListCssMatStyler {}
+export class MatList extends _MatListMixinBase implements CanDisableRipple {}
 
 /**
  * Directive whose purpose is to add the mat- CSS styling to this selector.
@@ -149,11 +145,18 @@ export class MatListItem extends _MatListItemMixinBase implements AfterContentIn
     }
   }
 
+<<<<<<< HEAD
   constructor(private _element: ElementRef,
               @Optional() private _list: MatList,
               @Optional() navList: MatNavListCssMatStyler) {
+=======
+  constructor(private _renderer: Renderer2,
+              private _element: ElementRef,
+              @Optional() private _list: MdList,
+              @Optional() private _navList: MdNavList) {
+>>>>>>> demo(list): Add accessibility demo page for list
     super();
-    this._isNavList = !!navList;
+    this._isNavList = !!_navList;
   }
 
   ngAfterContentInit() {