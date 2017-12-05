2115d8ddfe2fcfff073f837785d4352899b2a639
Joey Perrott <josephperrott@gmail.com>
2017-12-05 09:12:48 -0800

    fix(table): use combineLatest as mapper for FakeDataSource

--- src/cdk/table/table.spec.ts
+++ src/cdk/table/table.spec.ts
@@ -639,7 +639,7 @@ class FakeDataSource extends DataSource<TestData> {
   connect(collectionViewer: CollectionViewer) {
     this.isConnected = true;
     const streams = [this._dataChange, collectionViewer.viewChange];
-    return combineLatest<TestData[]>(streams).pipe(map(([data]) => data));
+    return combineLatest(streams, (data, _) => data);
   }
 
   disconnect() {
