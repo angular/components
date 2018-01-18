41f84bee7420b697933bac6385ca6e39ca8f93b1
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-01-18 21:01:18 +0100

    chore: bump year (#363)

--- material.angular.io/src/app/shared/stackblitz/stackblitz-writer.ts
+++ material.angular.io/src/app/shared/stackblitz/stackblitz-writer.ts
@@ -6,7 +6,7 @@ import {VERSION} from '@angular/material';
 const STACKBLITZ_URL = 'https://run.stackblitz.com/api/angular/v1/';
 
 const COPYRIGHT =
-  `Copyright 2017 Google Inc. All Rights Reserved.
+  `Copyright 2018 Google Inc. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
     can be found in the LICENSE file at http://angular.io/license`;
 
