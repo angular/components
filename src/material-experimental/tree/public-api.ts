5bc5b16a52ed03826b4190aba74871c79a05e8df
tinayuangao <tinagao@google.com>
2018-01-23 10:54:50 -0800

    feat(tree): add data sources for tree (#9036)

--- src/material-experimental/tree/public-api.ts
+++ src/material-experimental/tree/public-api.ts
@@ -12,3 +12,5 @@ export * from './padding';
 export * from './tree';
 export * from './tree-module';
 export * from './toggle';
+export * from './data-source/flat-data-source';
+export * from './data-source/nested-data-source';
