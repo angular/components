e0ded4bc0264c11dbe7e4c2868f4518f4d2ea1b2
Miles Malerba <mmalerba@google.com>
2018-02-15 14:59:37 -0800

    move virtual-scroll to cdk-experimental

--- src/cdk/scrolling/scrolling-module.ts
+++ src/cdk/scrolling/scrolling-module.ts
@@ -8,26 +8,13 @@
 
 import {PlatformModule} from '@angular/cdk/platform';
 import {NgModule} from '@angular/core';
-import {CdkVirtualForOf} from './virtual-for-of';
 import {SCROLL_DISPATCHER_PROVIDER} from './scroll-dispatcher';
 import {CdkScrollable} from './scrollable';
-import {CdkVirtualScrollFixedSize} from './virtual-scroll-fixed-size';
-import {CdkVirtualScrollViewport} from './virtual-scroll-viewport';
 
 @NgModule({
   imports: [PlatformModule],
-  exports: [
-    CdkVirtualForOf,
-    CdkScrollable,
-    CdkVirtualScrollFixedSize,
-    CdkVirtualScrollViewport,
-  ],
-  declarations: [
-    CdkVirtualForOf,
-    CdkScrollable,
-    CdkVirtualScrollFixedSize,
-    CdkVirtualScrollViewport,
-  ],
+  exports: [CdkScrollable],
+  declarations: [CdkScrollable],
   providers: [SCROLL_DISPATCHER_PROVIDER],
 })
 export class ScrollDispatchModule {}
