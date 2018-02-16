edeff71548a2cfa6815680a858bd252c98ee2746
Jeremy Elbourn <jelbourn@google.com>
2018-02-15 16:00:37 -0800

    build: create sass_bundle bazel rule
    This defines a bazel rule to bundle sass partials into a single file
    using the "scss-bundle" npm package. The rule definition is commented so
    that it can serve as an example for creating other rules in the future.
    
    I've also added the missing build file for badge so that its theme can
    be included in the bundle.

---
+++ tools/sass_bundle.ts
@@ -0,0 +1,29 @@
+import {dirname, join} from 'path';
+import {readFileSync, writeFileSync, readdirSync, lstatSync} from 'fs';
+
+const Bundler = require('scss-bundle').Bundler;
+const minimist = require('minimist');
+
+/** The root of the bazel workspace (sandbox). */
+const workspaceRoot = process.cwd();
+
+export async function main(args: string[]): Promise<number> {
+  const parsedArgs: {srcs: string, output: string, entry: string} = minimist(args);
+  const inputFiles = parsedArgs.srcs.split(',');
+
+  return new Bundler().Bundle(join(workspaceRoot, parsedArgs.entry), inputFiles).then(result => {
+    writeFileSync(parsedArgs.output, result.bundledContent);
+    return 0;
+  }).catch(error => {
+    console.error('Sass bundling failed');
+    console.dir(error);
+    return 1;
+  });
+}
+
+if (require.main === module) {
+  const args = process.argv.slice(2);
+  main(args).then(exitCode => {
+    process.exitCode = exitCode;
+  });
+}
