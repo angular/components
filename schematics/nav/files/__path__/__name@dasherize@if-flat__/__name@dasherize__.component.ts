c94bf6471ea0d3620339b5b93d58822f35fdefdd
Austin <amcdaniel2@gmail.com>
2018-02-26 18:41:20 -0600

    chore: nit

--- schematics/nav/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.component.ts
+++ schematics/nav/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.component.ts
@@ -1,39 +1,40 @@
 import { Component, <% if(!!viewEncapsulation) { %>, ViewEncapsulation<% }%><% if(changeDetection !== 'Default') { %>, ChangeDetectionStrategy<% }%> } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: '<%= selector %>',<% if(inlineTemplate) { %>
   template: `
-      <mat-drawer-container>
-      <mat-drawer mode="side" #drawer [opened]="!isHandset">
+    <mat-sidenav-container>
+      <mat-sidenav fixedInViewport="true" [mode]="isHandset ? 'over' : 'side'" #drawer [opened]="!(_isHandset | async)!.matches">
         <mat-toolbar color="primary">Menu</mat-toolbar>
         <mat-nav-list>
           <a mat-list-item href="#">Link 1</a>
           <a mat-list-item href="#">Link 2</a>
           <a mat-list-item href="#">Link 3</a>
         </mat-nav-list>
-      </mat-drawer>
-      <mat-drawer-content>
+      </mat-sidenav>
+      <mat-sidenav-content>
         <mat-toolbar color="primary">
-          <button type="button" mat-icon-button (click)="drawer.toggle()" *ngIf="isHandset">
+          <button type="button" mat-icon-button (click)="drawer.toggle()" *ngIf="(_isHandset | async)!.matches">
             <mat-icon aria-label="Side nav toggle icon">menu</mat-icon>
           </button>
           <span>Application Title</span>
         </mat-toolbar>
-      </mat-drawer-content>
-    </mat-drawer-container>
+      </mat-sidenav-content>
+    </mat-sidenav-container>
   `,<% } else { %>
   templateUrl: './<%= dasherize(name) %>.component.html',<% } if(inlineStyle) { %>
   styles: [
     `
-      mat-drawer-container {
-        height: 100%;
-      }
-      
-      mat-drawer {
-        width: 200px;
-        box-shadow: 3px 0 6px rgba(0,0,0,.24);
-      }
+    mat-sidenav-container {
+      height: 100%;
+    }
+    
+    mat-sidenav {
+      width: 200px;
+      box-shadow: 3px 0 6px rgba(0,0,0,.24);
+    }
   `
   ]<% } else { %>
   styleUrls: ['./<%= dasherize(name) %>.component.<%= styleext %>']<% } %><% if(!!viewEncapsulation) { %>,
@@ -41,8 +42,6 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
   changeDetection: ChangeDetectionStrategy.<%= changeDetection %><% } %>
 })
 export class <%= classify(name) %>Component {
-  isHandset: boolean;
-  constructor(breakpointObserver: BreakpointObserver) {
-    this.isHandset = breakpointObserver.isMatched(Breakpoints.Handset);
-  }
+  _isHandset: Observable<BreakpointState> = this._breakpointObserver.observe(Breakpoints.Handset);
+  constructor(private _breakpointObserver: BreakpointObserver) {}
 }
