a7181401eabf8beac51298bcff5bd88a1759d17f
Miles Malerba <mmalerba@google.com>
2018-02-27 14:18:03 -0800

    virtual-scroll: switch throttleTime to sampleTime

--- src/cdk-experimental/scrolling/virtual-scroll-viewport.ts
+++ src/cdk-experimental/scrolling/virtual-scroll-viewport.ts
@@ -22,8 +22,8 @@ import {
 } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {fromEvent} from 'rxjs/observable/fromEvent';
+import {sampleTime} from 'rxjs/operators/sampleTime';
 import {takeUntil} from 'rxjs/operators/takeUntil';
-import {throttleTime} from 'rxjs/operators/throttleTime';
 import {animationFrame} from 'rxjs/scheduler/animationFrame';
 import {Subject} from 'rxjs/Subject';
 import {CdkVirtualForOf} from './virtual-for-of';
@@ -177,7 +177,7 @@ export class CdkVirtualScrollViewport implements OnInit, OnDestroy {
 
       this._ngZone.runOutsideAngular(() => {
         fromEvent(this.elementRef.nativeElement, 'scroll')
-            .pipe(throttleTime(0, animationFrame))
+            .pipe(sampleTime(0, animationFrame))
             .subscribe(() => this._scrollStrategy.onContentScrolled());
       });
     });
