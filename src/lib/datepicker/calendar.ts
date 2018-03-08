57bd12ccf89ef5a3140b237dffddddf7c3404c84
Tobias Schweizer <t.schweizer@unibas.ch>
2018-03-08 18:32:43 +0100

    refactor (MatCalendar): fix indentation

--- src/lib/datepicker/calendar.ts
+++ src/lib/datepicker/calendar.ts
@@ -42,14 +42,14 @@ import {ComponentPortal, ComponentType, Portal} from '@angular/cdk/portal';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MatCalendarHeader {
-    constructor(@Host() public calendar: MatCalendar<any>,
-                private _intl: MatDatepickerIntl,
-                changeDetectorRef: ChangeDetectorRef) {
+  constructor(@Host() public calendar: MatCalendar<any>,
+              private _intl: MatDatepickerIntl,
+              changeDetectorRef: ChangeDetectorRef) {
 
-      this._intlChanges = _intl.changes.subscribe(() => changeDetectorRef.markForCheck());
-    }
+    this._intlChanges = _intl.changes.subscribe(() => changeDetectorRef.markForCheck());
+  }
 
-    private _intlChanges: Subscription;
+  private _intlChanges: Subscription;
 }
 
 /**
