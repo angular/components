54e067b896e72301379ffd2cc439f846be4a25b0
Austin <amcdaniel2@gmail.com>
2018-03-07 18:44:34 -0600

    chore: pr feedback

--- schematics/dashboard/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.component.ts
+++ schematics/dashboard/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.component.ts
@@ -7,11 +7,11 @@ import { Component, <% if(!!viewEncapsulation) { %>, ViewEncapsulation<% }%><% i
       <h1 class="mat-h1">Dashboard</h1>
       <mat-grid-list cols="2" rowHeight="350px">
         <mat-grid-tile *ngFor="let card of cards" [colspan]="card.cols" [rowspan]="card.rows">
-          <mat-card>
+          <mat-card class="dashboard-card">
             <mat-card-header>
               <mat-card-title>
                 {{card.title}}
-                <button mat-icon-button [matMenuTriggerFor]="menu" aria-label="Toggle menu">
+                <button mat-icon-button class="more-button" [matMenuTriggerFor]="menu" aria-label="Toggle menu">
                   <mat-icon>more_vert</mat-icon>
                 </button>
                 <mat-menu #menu="matMenu" xPosition="before">
@@ -20,7 +20,7 @@ import { Component, <% if(!!viewEncapsulation) { %>, ViewEncapsulation<% }%><% i
                 </mat-menu>
               </mat-card-title>
             </mat-card-header>
-            <mat-card-content>
+            <mat-card-content class="dashboard-card-content">
               <div>Card Content Here</div>
             </mat-card-content>
           </mat-card>
@@ -35,7 +35,7 @@ import { Component, <% if(!!viewEncapsulation) { %>, ViewEncapsulation<% }%><% i
         margin: 20px;
       }
       
-      mat-card {
+      .dashboard-card {
         position: absolute;
         top: 15px;
         left: 15px;
@@ -43,13 +43,13 @@ import { Component, <% if(!!viewEncapsulation) { %>, ViewEncapsulation<% }%><% i
         bottom: 15px;
       }
       
-      [mat-icon-button] {
+      .more-button {
         position: absolute;
         top: 5px;
         right: 10px;
       }
 
-      mat-card-content {
+      .dashboard-card-content {
         text-align: center;
       }
   `
@@ -60,25 +60,9 @@ import { Component, <% if(!!viewEncapsulation) { %>, ViewEncapsulation<% }%><% i
 })
 export class <%= classify(name) %>Component {
   cards = [
-    {
-      title: 'Card 1',
-      cols: 2,
-      rows: 1
-    },
-    {
-      title: 'Card 2',
-      cols: 1,
-      rows: 1
-    },
-    {
-      title: 'Card 3',
-      cols: 1,
-      rows: 2
-    },
-    {
-      title: 'Card 4',
-      cols: 1,
-      rows: 1
-    }
+    { title: 'Card 1', cols: 2, rows: 1 },
+    { title: 'Card 2', cols: 1, rows: 1 },
+    { title: 'Card 3', cols: 1, rows: 2 },
+    { title: 'Card 4', cols: 1, rows: 1 }
   ];
 }
