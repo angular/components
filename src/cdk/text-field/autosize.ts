98a302a194e3349d443205fd7488b52d8fb68ca0
Aleš <foxxcz@gmail.com>
2018-03-19 09:18:34 +0100

    feat(cdkTextareaAutosize): emit event when textarea height changes
    fixes https://github.com/angular/material2/issues/10472
    
    emit an event when height of the area changes

--- src/cdk/text-field/autosize.ts
+++ src/cdk/text-field/autosize.ts
@@ -14,6 +14,8 @@ import {
   DoCheck,
   OnDestroy,
   NgZone,
+  EventEmitter,
+  Output
 } from '@angular/core';
 import {Platform} from '@angular/cdk/platform';
 import {fromEvent} from 'rxjs/observable/fromEvent';
@@ -41,6 +43,7 @@ export class CdkTextareaAutosize implements AfterViewInit, DoCheck, OnDestroy {
 
   private _minRows: number;
   private _maxRows: number;
+  private _maxHeight: number;
 
   /** Minimum amount of rows in the textarea. */
   @Input('cdkAutosizeMinRows')
@@ -57,6 +60,10 @@ export class CdkTextareaAutosize implements AfterViewInit, DoCheck, OnDestroy {
     this._maxRows = value;
     this._setMaxHeight();
   }
+                                            
+  /** Emit event when textarea height changes */                                            
+  @Output('cdkAutosizeSizeChanged')
+  sizeChanged = new EventEmitter<number>(false);                                            
 
   /** Cached height of a textarea with a single row. */
   private _cachedLineHeight: number;
@@ -80,11 +87,11 @@ export class CdkTextareaAutosize implements AfterViewInit, DoCheck, OnDestroy {
 
   /** Sets the maximum height of the textarea as determined by maxRows. */
   _setMaxHeight(): void {
-    const maxHeight = this.maxRows && this._cachedLineHeight ?
-        `${this.maxRows * this._cachedLineHeight}px` : null;
+    this._maxHeight = this.maxRows && this._cachedLineHeight ?
+        this.maxRows * this._cachedLineHeight : null;
 
     if (maxHeight) {
-      this._setTextareaStyle('maxHeight', maxHeight);
+      this._setTextareaStyle('maxHeight', `${maxHeight}px`);
     }
   }
 
@@ -203,6 +210,12 @@ export class CdkTextareaAutosize implements AfterViewInit, DoCheck, OnDestroy {
     textarea.placeholder = placeholderText;
 
     this._previousValue = value;
+
+    if ((textarea.scrollHeight !== this._previousHeight && !this._maxHeight) 
+        || textarea.scrollHeight !== this._previousHeight && textarea.scrollHeight < this._maxHeight) {
+      this._previousHeight = textarea.scrollHeight;
+      this.sizeChanged.emit(this._previousHeight);
+    }
   }
 
   _noopInputHandler() {
