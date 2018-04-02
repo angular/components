23f7333eccb7e6e784768a29179e8a1612c459fa
Ray Gervais <raygervais@hotmail.ca>
2018-04-02 13:02:36 -0400

    docs(autocomplete): Fixes option group example
    Changes code style and logic to expected logic and styleguide

--- src/material-examples/autocomplete-optgroup/autocomplete-optgroup-example.ts
+++ src/material-examples/autocomplete-optgroup/autocomplete-optgroup-example.ts
@@ -1,63 +1,111 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import {Observable} from 'rxjs/Observable';
-import {startWith} from 'rxjs/operators/startWith';
-import {map} from 'rxjs/operators/map';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {startWith, map} from 'rxjs/operators';
 
+export interface StateGroup {
+  letter: string;
+  names: string[];
+}
+
+/**
+ * @title Option groups autocomplete
+ */
 @Component({
-    selector: 'my-app',
-    templateUrl: './autocomplete-optgroup-example.html',
-    styleUrls: [ './autocomplete-optgroup-example.component.css' ],
-    providers: []
+  templateUrl: './autocomplete-optgroup-example.html',
+  styleUrls: ['./autocomplete-optgroup-example.css'],
 })
 
 export class AutocompleteOptionGroupExample implements OnInit {
-    animalForm: FormGroup;
-    
-    speciesGroup: SpeciesGroup[] = [{
-        letter : 'C', name : [ 'Castor', 'Cat']
-    }, {
-        letter : 'D',  name : [ 'Dog', 'Dragon']
-    }];
-    
-    speciesGroupeOptions: Observable<SpeciesGroup[]>;
-    
-    constructor(private fb: FormBuilder) {
-        this.buildForm();
-    }
-    
-    ngOnInit() {
-        this.speciesGroupeOptions = this.animalForm.get('speciesGroup').valueChanges
-        .pipe(
-            startWith(''),
-            map(val => this.filterGroup(val))
-        );
-    }
-    
-    filterGroup(val: string): SpeciesGroup[] {
-        if (val) {
-            return this.speciesGroup
-            .map(group => ({ letter: group.letter, name: this._filter(group.name, val) }))
-            .filter(group => group.name.length > 0);
-        }
-        
-        return this.speciesGroup;
-    }
-    
-    private _filter(opt: string[], val: string) {
-        const filterValue = val.toLowerCase();
-        return opt.filter(item => item.toLowerCase().startsWith(filterValue));
-    }
-    
-    buildForm() {
-        this.animalForm = this.fb.group({
-            species: ['', [Validators.required]],
-            speciesGroup: ['', [Validators.required]]
-        })
-    };
-}
+  stateForm: FormGroup = this.fb.group({
+    stateGroup: '',
+  });
 
-class SpeciesGroup {
-    letter:string;
-    name: string[];
+  stateGroups: StateGroup[] = [{
+    letter: 'A',
+    names: ['Alabama', 'Alaska', 'Arizona', 'Arkansas']
+  }, {
+    letter: 'C',
+    names: ['California', 'Colorado', 'Connecticut']
+  }, {
+    letter: 'D',
+    names: ['Delaware']
+  }, {
+    letter: 'F',
+    names: ['Florida']
+  }, {
+    letter: 'G',
+    names: ['Georgia']
+  }, {
+    letter: 'H',
+    names: ['Hawaii']
+  }, {
+    letter: 'I',
+    names: ['Idaho', 'Illinois', 'Indiana', 'Iowa']
+  }, {
+    letter: 'K',
+    names: ['Kansas', 'Kentucky']
+  }, {
+    letter: 'L',
+    names: ['Louisiana']
+  }, {
+    letter: 'M',
+    names: ['Maine', 'Maryland', 'Massachusetts', 'Michigan',
+      'Minnesota', 'Mississippi', 'Missouri', 'Montana']
+  }, {
+    letter: 'N',
+    names: ['Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
+      'New Mexico', 'New York', 'North Carolina', 'North Dakota']
+  }, {
+    letter: 'O',
+    names: ['Ohio', 'Oklahoma', 'Oregon']
+  }, {
+    letter: 'P',
+    names: ['Pennsylvania']
+  }, {
+    letter: 'R',
+    names: ['Rhode Island']
+  }, {
+    letter: 'S',
+    names: ['South Carolina', 'South Dakota']
+  }, {
+    letter: 'T',
+    names: ['Tennessee', 'Texas']
+  }, {
+    letter: 'U',
+    names: ['Utah']
+  }, {
+    letter: 'V',
+    names: ['Vermont', 'Virginia']
+  }, {
+    letter: 'W',
+    names: ['Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+  }];
+
+  stateGroupOptions: Observable<StateGroup[]>;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.stateGroupOptions = this.stateForm.get('stateGroup')!.valueChanges
+      .pipe(
+        startWith(''),
+        map(val => this.filterGroup(val))
+      );
+  }
+
+  filterGroup(val: string): StateGroup[] {
+    if (val) {
+      return this.stateGroups
+        .map(group => ({ letter: group.letter, names: this._filter(group.names, val) }))
+        .filter(group => group.names.length > 0);
+    }
+
+    return this.stateGroups;
+  }
+
+  private _filter(opt: string[], val: string) {
+    const filterValue = val.toLowerCase();
+    return opt.filter(item => item.toLowerCase().startsWith(filterValue));
+  }
 }
