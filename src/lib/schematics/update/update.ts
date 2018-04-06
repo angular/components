c7b12a324d6ec8f4b2e79acaf0543ce86268b55c
Miles Malerba <mmalerba@google.com>
2018-04-06 10:35:23 -0700

    address comments

--- src/lib/schematics/update/update.ts
+++ src/lib/schematics/update/update.ts
@@ -2,6 +2,7 @@ import {Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
 import {TslintFixTask} from '@angular-devkit/schematics/tasks';
 import * as path from 'path';
 
+/** Entry point for `ng update` from Angular CLI. */
 export default function(): Rule {
   return (_: Tree, context: SchematicContext) => {
     context.addTask(new TslintFixTask({
