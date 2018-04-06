2fdc9bc01616c5cb4c837de0045c47d85ac57cea
Miles Malerba <mmalerba@google.com>
2018-04-06 15:22:31 -0700

    Add additional rules

--- src/lib/schematics/update/update.ts
+++ src/lib/schematics/update/update.ts
@@ -9,6 +9,22 @@ export default function(): Rule {
       rulesDirectory: path.join(__dirname, 'rules'),
       rules: {
         "switch-identifiers": true,
+        "switch-property-names": true,
+        "switch-string-literal-attribute-selectors": true,
+        "switch-string-literal-css-names": true,
+        "switch-string-literal-element-selectors": true,
+        // TODO(mmalerba): These require an extra CLI param, figure out how to handle.
+        /*"switch-stylesheet-attribute-selectors": true,
+        "switch-stylesheet-css-names": true,
+        "switch-stylesheet-element-selectors": true,
+        "switch-stylesheet-input-names": true,
+        "switch-stylesheet-output-names": true,*/
+        "switch-template-attribute-selectors": true,
+        "switch-template-css-names": true,
+        "switch-template-element-selectors": true,
+        "switch-template-export-as-names": true,
+        "switch-template-input-names": true,
+        "switch-template-output-names": true,
       }
     }, {
       silent: false,
