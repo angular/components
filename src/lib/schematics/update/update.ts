fa6ec090748351bfe189624dfe27af8ac708ca33
Miles Malerba <mmalerba@google.com>
2018-04-09 10:05:23 -0700

    wip

--- src/lib/schematics/update/update.ts
+++ src/lib/schematics/update/update.ts
@@ -28,35 +28,48 @@ import * as literal from './typescript/literal';
 /** Entry point for `ng update` from Angular CLI. */
 export default function(): Rule {
   return chain([
-      (_: Tree, context: SchematicContext) => {
-        context.addTask(new NodePackageInstallTask());
-      },
-      (_: Tree, context: SchematicContext) => {
-        context.addTask(new TslintFixTask({
-          rulesDirectory: path.join(__dirname, 'rules'),
-          rules: {
-            "switch-identifiers": true,
-            "switch-property-names": true,
-            "switch-string-literal-attribute-selectors": true,
-            "switch-string-literal-css-names": true,
-            "switch-string-literal-element-selectors": true,
-            // TODO(mmalerba): These require an extra CLI param, figure out how to handle.
-            /*"switch-stylesheet-attribute-selectors": true,
-            "switch-stylesheet-css-names": true,
-            "switch-stylesheet-element-selectors": true,
-            "switch-stylesheet-input-names": true,
-            "switch-stylesheet-output-names": true,*/
-            "switch-template-attribute-selectors": true,
-            "switch-template-css-names": true,
-            "switch-template-element-selectors": true,
-            "switch-template-export-as-names": true,
-            "switch-template-input-names": true,
-            "switch-template-output-names": true,
-          }
-        }, {
-          silent: false,
-          tsConfigPath: './tsconfig.json',
-        }));
-      }
+    (_: Tree, context: SchematicContext) => {
+      context.addTask(new NodePackageInstallTask({
+        packageName: '@angular/cdk@">=5 <6"'
+      }));
+      context.addTask(new NodePackageInstallTask({
+        packageName: '@angular/material@">=5 <6"'
+      }));
+    },
+    (_: Tree, context: SchematicContext) => {
+      context.addTask(new TslintFixTask({
+        rulesDirectory: path.join(__dirname, 'rules'),
+        rules: {
+          "switch-identifiers": true,
+          "switch-property-names": true,
+          "switch-string-literal-attribute-selectors": true,
+          "switch-string-literal-css-names": true,
+          "switch-string-literal-element-selectors": true,
+          // TODO(mmalerba): These require an extra CLI param, figure out how to handle.
+          /*"switch-stylesheet-attribute-selectors": true,
+          "switch-stylesheet-css-names": true,
+          "switch-stylesheet-element-selectors": true,
+          "switch-stylesheet-input-names": true,
+          "switch-stylesheet-output-names": true,*/
+          "switch-template-attribute-selectors": true,
+          "switch-template-css-names": true,
+          "switch-template-element-selectors": true,
+          "switch-template-export-as-names": true,
+          "switch-template-input-names": true,
+          "switch-template-output-names": true,
+        }
+      }, {
+        silent: false,
+        tsConfigPath: './tsconfig.json',
+      }));
+    },
+    (_: Tree, context: SchematicContext) => {
+      context.addTask(new NodePackageInstallTask({
+        packageName: '@angular/cdk@next'
+      }));
+      context.addTask(new NodePackageInstallTask({
+        packageName: '@angular/material@next'
+      }));
+    },
   ]);
 }
