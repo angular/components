1b66ef525e75b9f50580d7382dc669ec0e706d27
Miles Malerba <mmalerba@google.com>
2018-04-09 17:13:40 -0700

    yay, working

--- src/lib/schematics/update/update.ts
+++ src/lib/schematics/update/update.ts
@@ -1,75 +1,84 @@
-import {chain, Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
-import {NodePackageInstallTask, TslintFixTask} from '@angular-devkit/schematics/tasks';
+import {FileEntry, Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
+import {
+  NodePackageInstallTask,
+  RunSchematicTask,
+  TslintFixTask
+} from '@angular-devkit/schematics/tasks';
 import * as path from 'path';
 
-// Import everything that the `TslintFixTask` will need to cache it in memory.
-import * as color from './material/color';
-import * as componentData from  './material/component-data';
-import * as extraStylesheets from './material/extra-stylsheets';
-import * as typescriptSpecifiers from './material/typescript-specifiers';
-import * as switchIdentifiersRule from './rules/switchIdentifiersRule';
-import * as switchPropertyNamesRule from './rules/switchPropertyNamesRule';
-import * as switchStringLiteralAttributeSelecotrsRule from './rules/switchStringLiteralAttributeSelectorsRule';
-import * as switchStringLiteralCssNamesRule from './rules/switchStringLiteralCssNamesRule';
-import * as switchStringLiteralElementSelectorsRule from './rules/switchStringLiteralElementSelectorsRule';
-import * as switchTemplateAttributeSelectorRule from './rules/switchTemplateAttributeSelectorsRule';
-import * as switchTemplateCssNamesRule from './rules/switchTemplateCssNamesRule';
-import * as switchTemplateElementSelectorsRule from './rules/switchTemplateElementSelectorsRule';
-import * as switchTemplateExportAsNamesRule from './rules/switchTemplateExportAsNamesRule';
-import * as switchTemplateInputNamesRule from './rules/switchTemplateInputNamesRule';
-import * as switchTemplateOutpitNamesRule from './rules/switchTemplateOutputNamesRule';
-import * as componentFile from './tslint/component-file';
-import * as componentWalker from './tslint/component-walker';
-import * as findTslintBinary from './tslint/find-tslint-binary';
-import * as identifiers from './typescript/identifiers';
-import * as imports from './typescript/imports';
-import * as literal from './typescript/literal';
+const schematicsSrcPath = 'node_modules/@angular/material/schematics';
+const schematicsTmpPath = 'node_modules/_tmp_angular_material_schematics';
 
 /** Entry point for `ng update` from Angular CLI. */
 export default function(): Rule {
-  return chain([
-    (_: Tree, context: SchematicContext) => {
-      context.addTask(new NodePackageInstallTask({
-        packageName: '@angular/cdk@">=5 <6"'
-      }));
-      context.addTask(new NodePackageInstallTask({
-        packageName: '@angular/material@">=5 <6"'
-      }));
-    },
-    (_: Tree, context: SchematicContext) => {
-      context.addTask(new TslintFixTask({
-        rulesDirectory: path.join(__dirname, 'rules'),
-        rules: {
-          "switch-identifiers": true,
-          "switch-property-names": true,
-          "switch-string-literal-attribute-selectors": true,
-          "switch-string-literal-css-names": true,
-          "switch-string-literal-element-selectors": true,
-          // TODO(mmalerba): These require an extra CLI param, figure out how to handle.
-          /*"switch-stylesheet-attribute-selectors": true,
-          "switch-stylesheet-css-names": true,
-          "switch-stylesheet-element-selectors": true,
-          "switch-stylesheet-input-names": true,
-          "switch-stylesheet-output-names": true,*/
-          "switch-template-attribute-selectors": true,
-          "switch-template-css-names": true,
-          "switch-template-element-selectors": true,
-          "switch-template-export-as-names": true,
-          "switch-template-input-names": true,
-          "switch-template-output-names": true,
-        }
-      }, {
-        silent: false,
-        tsConfigPath: './tsconfig.json',
-      }));
-    },
-    (_: Tree, context: SchematicContext) => {
-      context.addTask(new NodePackageInstallTask({
-        packageName: '@angular/cdk@next'
-      }));
-      context.addTask(new NodePackageInstallTask({
-        packageName: '@angular/material@next'
-      }));
-    },
-  ]);
+  return (tree: Tree, context: SchematicContext) => {
+    // Copy the update schematics to a temporary directory.
+    const updateSrcs: FileEntry[] = [];
+    tree.getDir(schematicsSrcPath).visit((_, entry) => updateSrcs.push(entry));
+    for (let src of updateSrcs) {
+      tree.create(src.path.replace(schematicsSrcPath, schematicsTmpPath), src.content);
+    }
+
+    // Downgrade @angular/material to 5.x. This allows us to use the 5.x type information in the
+    // update script.
+    const downgradeCdkTask = context.addTask(new NodePackageInstallTask({
+      packageName: '@angular/cdk@">=5 <6"'
+    }));
+    const downgradeMaterialTask = context.addTask(new NodePackageInstallTask({
+      packageName: '@angular/material@">=5 <6"'
+    }));
+
+    // Run the update tslint rules.
+    const updateTask = context.addTask(new TslintFixTask({
+      rulesDirectory: path.join(schematicsTmpPath, 'update/rules'),
+      rules: {
+        "switch-identifiers": true,
+        "switch-property-names": true,
+        "switch-string-literal-attribute-selectors": true,
+        "switch-string-literal-css-names": true,
+        "switch-string-literal-element-selectors": true,
+        // TODO(mmalerba): These require an extra CLI param, figure out how to handle.
+        /*"switch-stylesheet-attribute-selectors": true,
+        "switch-stylesheet-css-names": true,
+        "switch-stylesheet-element-selectors": true,
+        "switch-stylesheet-input-names": true,
+        "switch-stylesheet-output-names": true,*/
+        "switch-template-attribute-selectors": true,
+        "switch-template-css-names": true,
+        "switch-template-element-selectors": true,
+        "switch-template-export-as-names": true,
+        "switch-template-input-names": true,
+        "switch-template-output-names": true,
+      }
+    }, {
+      silent: false,
+      tsConfigPath: './tsconfig.json',
+    }), [downgradeCdkTask, downgradeMaterialTask]);
+
+    // Upgrade @angular/material back to 6.x.
+    const upgradeCdkTask = context.addTask(new NodePackageInstallTask({
+      // TODO(mmalerba): Change "next" to ">=6 <7".
+      packageName: '@angular/cdk@next'
+    }), [updateTask]);
+    const upgradeMaterialTask = context.addTask(new NodePackageInstallTask({
+      // TODO(mmalerba): Fix before submitting.
+      packageName: '/usr/local/google/home/mmalerba/material2/dist/releases/material/angular-material-6.0.0-rc.1.tgz'
+    }), [updateTask]);
+
+    // Delete the temporary schematics directory.
+    context.addTask(
+        new RunSchematicTask(path.join(__dirname, '../collection.json'), 'ng-post-update', {
+          deleteFiles: updateSrcs
+              .map(entry => entry.path.replace(schematicsSrcPath, schematicsTmpPath))
+        }), [upgradeCdkTask, upgradeMaterialTask]);
+  };
+}
+
+/** Post-update schematic to be called when ng update is finished. */
+export function postUpdate(options: {deleteFiles: string[]}): Rule {
+  return (tree: Tree) => {
+    for (let file of options.deleteFiles) {
+      tree.delete(file);
+    }
+  }
 }
