34977c06b7f1a01396cefdd74d4952cb06051dec
Austin <amcdaniel2@gmail.com>
2018-04-11 19:09:12 -0500

    chore: fix test?

--- src/lib/schematics/shell/theming.ts
+++ src/lib/schematics/shell/theming.ts
@@ -13,7 +13,7 @@ import {Schema} from './schema';
 
 /** Add pre-built styles to the main project style file. */
 export function addThemeToAppStyles(options: Schema) {
-  return (host: Tree) => {
+  return function(host: Tree) {
     const workspace = getWorkspace(host);
     const project = getProjectFromWorkspace(workspace, options.project);
 
