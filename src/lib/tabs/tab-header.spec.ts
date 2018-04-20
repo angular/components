nandhini-ramanujam/material2

fix(scrolling): rename ScrollDispatchModule to ScrollingModule
Renames the `ScrollDispatchModule` to `ScrollingModule` in order to match the `@angular/cdk/scrolling` entry point.

src/lib/tabs/tab-header.spec.ts
@@ -13,7 +13,7 @@ import {MatRippleModule} from '@angular/material/core';
 import {MatInkBar} from './ink-bar';
 import {MatTabLabelWrapper} from './tab-label-wrapper';
 import {Subject} from 'rxjs/Subject';
-import {VIEWPORT_RULER_PROVIDER, ScrollDispatchModule} from '@angular/cdk/scrolling';
+import {VIEWPORT_RULER_PROVIDER, ScrollingModule} from '@angular/cdk/scrolling';
 
 
 describe('MatTabHeader', () => {
@@ -25,7 +25,7 @@ describe('MatTabHeader', () => {
   beforeEach(async(() => {
     dir = 'ltr';
     TestBed.configureTestingModule({
-      imports: [CommonModule, PortalModule, MatRippleModule, ScrollDispatchModule],
+      imports: [CommonModule, PortalModule, MatRippleModule, ScrollingModule],
       declarations: [
         MatTabHeader,
         MatInkBar,

angular/components

src/lib/tabs/tab-header.spec.ts
@@ -1,7 +1,7 @@
 import {Direction, Directionality} from '@angular/cdk/bidi';
 import {END, ENTER, HOME, LEFT_ARROW, RIGHT_ARROW, SPACE} from '@angular/cdk/keycodes';
 import {PortalModule} from '@angular/cdk/portal';
-import {ScrollDispatchModule, ViewportRuler} from '@angular/cdk/scrolling';
+import {ScrollingModule, ViewportRuler} from '@angular/cdk/scrolling';
 import {dispatchFakeEvent, dispatchKeyboardEvent} from '@angular/cdk/testing';
 import {CommonModule} from '@angular/common';
 import {Component, ViewChild} from '@angular/core';
@@ -30,7 +30,7 @@ describe('MatTabHeader', () => {
   beforeEach(async(() => {
     dir = 'ltr';
     TestBed.configureTestingModule({
-      imports: [CommonModule, PortalModule, MatRippleModule, ScrollDispatchModule],
+      imports: [CommonModule, PortalModule, MatRippleModule, ScrollingModule],
       declarations: [
         MatTabHeader,
         MatInkBar,