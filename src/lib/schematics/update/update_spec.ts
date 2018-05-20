5432d5a8ea7cf04d60461f22a7f98c62e6aa1be6
Austin <amcdaniel2@gmail.com>
2018-05-20 10:44:07 -0500

    fix(schematics): remove temp path #11198

---
+++ src/lib/schematics/update/update_spec.ts
@@ -0,0 +1,32 @@
+import {SchematicTestRunner} from '@angular-devkit/schematics/testing';
+import {join} from 'path';
+import {Tree} from '@angular-devkit/schematics';
+import {createTestApp} from '../utils/testing';
+import {getFileContent} from '@schematics/angular/utility/test';
+
+const collectionPath = join(__dirname, '../collection.json');
+
+describe('material-nav-schematic', () => {
+  let runner: SchematicTestRunner;
+
+  beforeEach(() => {
+    runner = new SchematicTestRunner('schematics', collectionPath);
+  });
+
+  it('should remove the temp directory', () => {
+    console.log('!!!!!!!!!!!!!')
+    const tree = runner.runSchematic('update', {}, createTestApp());
+    const files = tree.files;
+
+    let has = false;
+    for (const file of files) {
+      if (file.indexOf('angular_material_schematics-')) {
+        has = true;
+        break;
+      }
+    }
+
+    expect(has).toBe(false);
+  });
+
+});
