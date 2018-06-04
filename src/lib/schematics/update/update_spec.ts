9506e64259d5e268df1274f9935e2aa2871de97f
Austin <amcdaniel2@gmail.com>
2018-06-04 10:09:35 -0500

    chore: pr nit

--- src/lib/schematics/update/update_spec.ts
+++ src/lib/schematics/update/update_spec.ts
@@ -16,16 +16,7 @@ describe('material-nav-schematic', () => {
   it('should remove the temp directory', () => {
     const tree = runner.runSchematic('update', {}, createTestApp());
     const files = tree.files;
-
-    let has = false;
-    for (const file of files) {
-      if (file.indexOf('angular_material_schematics-')) {
-        has = true;
-        break;
-      }
-    }
-
-    expect(has).toBe(false);
+    expect(files.find(file => file.includes('angular_material_schematics-'))).toBeTruthy();
   });
 
 });
