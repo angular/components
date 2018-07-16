7ae7c8183ccfc5e07949a3479c06f308d0d2badd
Mark Arranz <mark.arranz@gmail.com>
2018-07-16 08:52:23 -0700

    Revert whitespace changes made by auto-format on save.

--- src/material-examples/stepper-vertical/stepper-vertical-example.ts
+++ src/material-examples/stepper-vertical/stepper-vertical-example.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 /**
  * @title Stepper vertical
