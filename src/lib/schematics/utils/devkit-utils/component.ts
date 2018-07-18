2032ae08a3e713b0f5614a296005064d97f934fc
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-07-18 20:21:57 +0200

    Add valid license headers for devkit-utils

--- src/lib/schematics/utils/devkit-utils/component.ts
+++ src/lib/schematics/utils/devkit-utils/component.ts
@@ -1,5 +1,13 @@
 /* tslint:disable */
 
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
 import {strings} from '@angular-devkit/core';
 import {
   apply,
