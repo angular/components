f7da96c5f90913d684ed178c25d227ea86795de4
Victoria Wei <vwei@google.com>
2018-07-24 15:50:52 -0700

    Separated MAT_DATE_FORMATS and CDK_DATE FORMATS

--- src/lib/core/datetime/native-date-formats.ts
+++ src/lib/core/datetime/native-date-formats.ts
@@ -10,11 +10,7 @@ import {MatDateFormats} from './date-formats';
 
 
 export const MAT_NATIVE_DATE_FORMATS: MatDateFormats = {
-  parse: {
-    dateInput: null,
-  },
   display: {
-    dateInput: {year: 'numeric', month: 'numeric', day: 'numeric'},
     monthYearLabel: {year: 'numeric', month: 'short'},
     dateA11yLabel: {year: 'numeric', month: 'long', day: 'numeric'},
     monthYearA11yLabel: {year: 'numeric', month: 'long'},
