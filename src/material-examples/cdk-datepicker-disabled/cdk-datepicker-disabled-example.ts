39a3e971f4f902b15dc4a9413a3d7fdc65c5433e
Victoria Wei <vwei@google.com>
2018-07-26 18:33:39 -0700

    deleted unneccessary material-examples for cdk

--- src/material-examples/cdk-datepicker-disabled/cdk-datepicker-disabled-example.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-/** @title Disabled CDK datepicker */
-@Component({
-  selector: 'cdk-datepicker-disabled-example',
-  templateUrl: 'cdk-datepicker-disabled-example.html',
-  styleUrls: ['cdk-datepicker-disabled-example.css'],
-})
-export class CdkDatepickerDisabledExample {}
