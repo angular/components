9969815f8312e687d37ad24432a1bfb566e3e53d
Miles Malerba <mmalerba@google.com>
2018-08-02 09:12:12 -0700

    switch spacer to use transform instead of width/height

--- src/cdk/scrolling/virtual-scroll-viewport.ts
+++ src/cdk/scrolling/virtual-scroll-viewport.ts
@@ -78,9 +78,15 @@ export class CdkVirtualScrollViewport implements OnInit, OnDestroy {
   renderedRangeStream: Observable<ListRange> = this._renderedRangeSubject.asObservable();
 
   /**
+   * The transform used to scale the spacer to the same size as all content, including content that
+   * is not currently rendered.
+   */
+  _totalContentSizeTransform = '';
+
+  /**
    * The total size of all content (in pixels), including content that is not currently rendered.
    */
-  _totalContentSize = 0;
+  private _totalContentSize = 0;
 
   /**
    * The CSS transform applied to the rendered subset of items so that they appear within the bounds
@@ -211,6 +217,8 @@ export class CdkVirtualScrollViewport implements OnInit, OnDestroy {
   setTotalContentSize(size: number) {
     if (this._totalContentSize !== size) {
       this._totalContentSize = size;
+      const axis = this.orientation == 'horizontal' ? 'X' : 'Y';
+      this._totalContentSizeTransform = `scale${axis}(${this._totalContentSize})`;
       this._markChangeDetectionNeeded();
     }
   }
@@ -236,7 +244,7 @@ export class CdkVirtualScrollViewport implements OnInit, OnDestroy {
    */
   setRenderedContentOffset(offset: number, to: 'to-start' | 'to-end' = 'to-start') {
     const axis = this.orientation === 'horizontal' ? 'X' : 'Y';
-    let transform = `translateZ(0) translate${axis}(${Number(offset)}px)`;
+    let transform = `translate${axis}(${Number(offset)}px)`;
     this._renderedContentOffset = offset;
     if (to === 'to-end') {
       transform += ` translate${axis}(-100%)`;
