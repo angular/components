9ca18d54d4b4ad9a7f908cf58565c89f6bd78e8a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-08-07 10:43:34 +0200

    fix(radio): ng-touched incorrectly being set on click
    * The `ControlValueAccessor` `onTouched` should be just called if a radio-button blurred. Right now, the `ng-touched` state will be immediately set if someone presses SPACE or clicks on a radio-button.

--- src/lib/radio/radio.ts
+++ src/lib/radio/radio.ts
@@ -566,7 +566,6 @@ export class MatRadioButton extends _MatRadioButtonMixinBase
 
     if (this.radioGroup) {
       this.radioGroup._controlValueAccessorChangeFn(this.value);
-      this.radioGroup._touch();
       if (groupValueChanged) {
         this.radioGroup._emitChangeEvent();
       }
