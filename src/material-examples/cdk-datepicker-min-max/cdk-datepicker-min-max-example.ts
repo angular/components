08604b4c3057ac6c11845fbf74c5f58d9209970b
Victoria Wei <vwei@google.com>
2018-08-08 00:05:06 -0700

    Edited examples

--- src/material-examples/cdk-datepicker-min-max/cdk-datepicker-min-max-example.ts
+++ src/material-examples/cdk-datepicker-min-max/cdk-datepicker-min-max-example.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {CalendarView} from '@angular/cdk/datetime';
 
 /** @title CDK Datepicker with min & max validation */
 @Component({
@@ -9,4 +10,20 @@ import {Component} from '@angular/core';
 export class CdkDatepickerMinMaxExample {
   minDate = new Date(2000, 0, 1);
   maxDate = new Date(2020, 0, 1);
+}
+
+
+@Component({
+  selector: 'my-min-max-calendar',
+  template: `
+    <div>Min date: {{this.minDate}}</div>
+    <div>Max date: {{this.maxDate}}</div>
+  `,
+  providers: [{provide: CalendarView, useExisting: MyMinMaxCalendar}],
+})
+export class MyMinMaxCalendar<Date> extends CalendarView<Date> {
+  activeDate = null;
+  minDate = null;
+  maxDate = null;
+  selected = null;
 }
---
+++ src/material-examples/cdk-datepicker-start-date/cdk-datepicker-start-date-example.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {CalendarView} from '@angular/cdk/datetime';
+
+/** @title CDK Datepicker start date */
+@Component({
+  selector: 'cdk-datepicker-start-date-example',
+  templateUrl: 'cdk-datepicker-start-date-example.html',
+  styleUrls: ['cdk-datepicker-start-date-example.css'],
+})
+export class CdkDatepickerStartDateExample {
+  startDate = new Date(1990, 0, 1);
+}
+
+
+@Component({
+  selector: 'my-start-date-calendar',
+  template: `
+    <div>Start date: {{this.activeDate}}</div>
+  `,
+  providers: [{provide: CalendarView, useExisting: MyStartDateCalendar}],
+})
+export class MyStartDateCalendar<Date> extends CalendarView<Date> {
+  activeDate = null;
+  minDate = null;
+  maxDate = null;
+  selected = null;
+}
