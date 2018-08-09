76730098fdab49a3f2f3d5663fed71a0c4557b16
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-08-09 22:14:19 +0200

    build: test against angular snapshots
    * Runs the whole CI test suite with the latest Angular snapshot from GitHub in the CI nightly cronjob.

---
+++ scripts/install-angular-snapshot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script that modifies the project `package.json` by setting the version for all Angular
+# dependencies to the Github snapshot builds. Afterwards, if the `--only-save` flag is not set,
+# the script will also run NPM to install the new versions.
+
+set -e
+
+# Go to the project root directory
+cd $(dirname $0)/../
+
+searchRegex='(@angular\/(.*))":\s+".*"'
+searchReplace='\1": "github:angular\/\2-builds"'
+
+sed -i -r "s/${searchRegex}/${searchReplace}/g" package.json
+
+if [[ ${*} != *--only-save* ]]; then
+  npm install
+fi
