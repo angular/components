3db0c9df3900f27d110177937114cad3fb7a5f47
Victoria Wei <vwei@google.com>
2018-08-09 10:59:05 -0700

    Edited examples

--- src/material-examples/cdk-datepicker-disabled/cdk-datepicker-disabled-example.ts
+++ src/material-examples/cdk-datepicker-disabled/cdk-datepicker-disabled-example.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {CalendarView} from '@angular/cdk/datetime';
+import {Component, Input} from '@angular/core';
+import {CalendarView, DateAdapter} from '@angular/cdk/datetime';
 
 /** @title CDK Disabled datepicker */
 @Component({
@@ -7,18 +7,48 @@ import {CalendarView} from '@angular/cdk/datetime';
   templateUrl: 'cdk-datepicker-disabled-example.html',
   styleUrls: ['cdk-datepicker-disabled-example.css'],
 })
-export class CdkDatepickerDisabledExample {}
+export class CdkDatepickerDisabledExample<D> {
+  dates: D[] = [];
+  messages: string[] = [];
+
+  constructor(private _dateAdapter: DateAdapter<D>) {
+    this.dates.push(this._dateAdapter.addCalendarDays(this._dateAdapter.today(), 5));
+    this.dates.push(this._dateAdapter.addCalendarDays(this._dateAdapter.today(), 10));
+    this.dates.push(this._dateAdapter.addCalendarDays(this._dateAdapter.today(), 15));
+  }
+  _dateSelected() {
+    this.messages.push('Date has changed. ');
+  }
+}
 
 
 @Component({
   selector: 'my-disabled-calendar',
-  template: '',
+  outputs: ['selectedChange'],
+  template: `
+    <div *ngFor="let date of dates">
+      <button (click)="_selected(date)">{{date}}</button>
+    </div>
+    <div>Date: {{this.selected}}</div>
+  `,
   providers: [{provide: CalendarView, useExisting: MyDisabledCalendar}],
 })
-export class MyDisabledCalendar<Date> extends CalendarView<Date> {
-  activeDate = null;
+export class MyDisabledCalendar<D> extends CalendarView<D> {
+  @Input() dates: D[];
+
+  activeDate: D;
   minDate = null;
   maxDate = null;
-  selected = null;
+  selected: D | null = null;
   dateFilter = () => true;
+
+  constructor(private _dateAdapter: DateAdapter<D>) {
+    super();
+    this.activeDate = this._dateAdapter.today();
+  }
+
+  _selected(date: D) {
+    this.selected = date;
+    this.selectedChange.emit(date);
+  }
 }
--- src/material-examples/cdk-datepicker-events/cdk-datepicker-events-example.ts
+++ src/material-examples/cdk-datepicker-events/cdk-datepicker-events-example.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {DatepickerInputEvent, CalendarView} from '@angular/cdk/datetime';
+import {Component, Input} from '@angular/core';
+import {DatepickerInputEvent, CalendarView, DateAdapter} from '@angular/cdk/datetime';
 
 /** @title CDK Datepicker input and change events */
 @Component({
@@ -7,24 +7,53 @@ import {DatepickerInputEvent, CalendarView} from '@angular/cdk/datetime';
   templateUrl: 'cdk-datepicker-events-example.html',
   styleUrls: ['cdk-datepicker-events-example.css'],
 })
-export class CdkDatepickerEventsExample {
+export class CdkDatepickerEventsExample<D> {
   events: string[] = [];
+  dates: D[] = [];
+  messages: string[] = [];
 
   addEvent(type: string, event: DatepickerInputEvent<Date>) {
     this.events.push(`${type}: ${event.value}`);
+  }
+
+  constructor(private _dateAdapter: DateAdapter<D>) {
+    this.dates.push(this._dateAdapter.addCalendarDays(this._dateAdapter.today(), 5));
+    this.dates.push(this._dateAdapter.addCalendarDays(this._dateAdapter.today(), 10));
+    this.dates.push(this._dateAdapter.addCalendarDays(this._dateAdapter.today(), 15));
+  }
+  _dateSelected() {
+    this.messages.push('Date has changed. ');
   }
 }
 
 
 @Component({
-  selector: 'my-events-calendar',
-  template: '',
-  providers: [{provide: CalendarView, useExisting: MyEventsCalendar}],
+    selector: 'my-events-calendar',
+    outputs: ['selectedChange'],
+    template: `
+    <div *ngFor="let date of dates">
+      <button (click)="_selected(date)">{{date}}</button>
+    </div>
+    <div>Date: {{this.selected}}</div>
+  `,
+    providers: [{provide: CalendarView, useExisting: MyEventsCalendar}],
 })
-export class MyEventsCalendar<Date> extends CalendarView<Date> {
-  activeDate = null;
+export class MyEventsCalendar<D> extends CalendarView<D> {
+  @Input() dates: D[];
+
+  activeDate: D;
   minDate = null;
   maxDate = null;
-  selected = null;
+  selected: D | null = null;
   dateFilter = () => true;
+
+  constructor(private _dateAdapter: DateAdapter<D>) {
+    super();
+    this.activeDate = this._dateAdapter.today();
+  }
+
+  _selected(date: D) {
+    this.selected = date;
+    this.selectedChange.emit(date);
+  }
 }
