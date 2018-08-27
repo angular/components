dc9a46cac038f9b0e6dd3ddbd34b48b378a57a07
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-08-27 17:39:12 +0200

    Address feedback

--- src/lib/schematics/utils/package-json.ts
+++ src/lib/schematics/utils/package-json.ts
@@ -8,9 +8,13 @@
 
 import {Tree} from '@angular-devkit/schematics';
 
-/** Function that sorts the keys of the specified object and returns the result as a new object. */
-const sortObjectByKeys = (obj: any) => Object.keys(obj).sort()
-    .reduce((result, key) => (result[key] = obj[key]) && result, {});
+/**
+ * Sorts the keys of the given object.
+ * @returns A new object instance with sorted keys
+ */
+function sortObjectByKeys(obj: object) {
+  return Object.keys(obj).sort().reduce((result, key) => (result[key] = obj[key]) && result, {});
+}
 
 /** Adds a package to the package.json in the given host tree. */
 export function addPackageToPackageJson(host: Tree, pkg: string, version: string): Tree {
