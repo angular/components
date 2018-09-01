c66d608caac685541d4ef46126090561ead7c9cb
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-09-01 17:49:33 +0200

    fix(sort): indicator hint not showing up on mobile
    * Due to the fact that the sort module uses the `(longpress)` event, the custom HammerJS gesture config needs to be provided.
    
    Related to #12940

--- src/material/sort/sort-module.ts
+++ src/material/sort/sort-module.ts
@@ -7,6 +7,8 @@
  */
 
 import {NgModule} from '@angular/core';
+import {GestureConfig} from '@angular/material/core';
+import {HAMMER_GESTURE_CONFIG} from '@angular/platform-browser';
 import {MatSortHeader} from './sort-header';
 import {MatSort} from './sort';
 import {MAT_SORT_HEADER_INTL_PROVIDER} from './sort-header-intl';
@@ -17,6 +19,9 @@ import {CommonModule} from '@angular/common';
   imports: [CommonModule],
   exports: [MatSort, MatSortHeader],
   declarations: [MatSort, MatSortHeader],
-  providers: [MAT_SORT_HEADER_INTL_PROVIDER]
+  providers: [
+    MAT_SORT_HEADER_INTL_PROVIDER,
+    {provide: HAMMER_GESTURE_CONFIG, useClass: GestureConfig},
+  ]
 })
 export class MatSortModule {}
