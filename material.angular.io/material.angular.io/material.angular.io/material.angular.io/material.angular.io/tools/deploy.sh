a9182a92ba10d4c64fad3fcb1a39349a12bf945e
Joey Perrott <josephperrott.github@gmail.com>
2018-10-17 17:19:54 -0400

    chore: add v6 firebase project and update deploy script (#534)

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/tools/deploy.sh
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/tools/deploy.sh
@@ -9,6 +9,8 @@ PROJECT_ID["stable", "dev"]="material2-docs-dev"
 PROJECT_ID["stable", "prod"]="material-angular-io"
 PROJECT_ID["v5", "dev"]="material2-docs-5"
 PROJECT_ID["v5", "prod"]="v5-material-angular-io"
+PROJECT_ID["v6", "dev"]="material2-docs-6"
+PROJECT_ID["v6", "prod"]="v6-material-angular-io"
 
 version=${1:-stable}
 mode=${2:-dev}
