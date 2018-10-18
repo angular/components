e592ba19e066d16be5f5df2ba6a4a79dfd94f3cd
Joey Perrott <josephperrott.github@gmail.com>
2018-10-18 14:29:16 -0700

    feat: create a version.json file for all version of the docs sites to load version info from (#539)

--- material.angular.io/src/app/shared/version-picker/version-picker.ts
+++ material.angular.io/src/app/shared/version-picker/version-picker.ts
@@ -1,7 +1,10 @@
 import {Component, NgModule} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import {CommonModule} from '@angular/common';
 import {MatButtonModule, MatIconModule, MatMenuModule} from '@angular/material';
-import {docVersions, materialVersion, VersionInfo} from '../version/version';
+import {materialVersion, VersionInfo} from '../version/version';
+
+const versionUrl = 'https://material.angular.io/assets/versions.json';
 
 @Component({
   selector: 'version-picker',
@@ -11,7 +14,9 @@ export class VersionPicker {
   /** The currently running versin of Material. */
   materialVersion = materialVersion;
   /** The possible versions of the doc site. */
-  docVersions = docVersions;
+  docVersions = this.http.get(versionUrl);
+
+  constructor(private http: HttpClient) {}
 
   /** Updates the window location if the selected version is a different version. */
   onVersionChanged(version: VersionInfo) {
