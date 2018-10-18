7814568259f261fd3de971d0836f3ec6b0a4ca7d
Joey Perrott <josephperrott.github@gmail.com>
2018-10-18 14:29:16 -0700

    feat: create a version.json file for all version of the docs sites to load version info from (#539)

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/shared/version/version.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/shared/version/version.ts
@@ -8,19 +8,3 @@ export interface VersionInfo {
   url: string;
   title: string;
 }
-
-/** Doc site versions. We update the urls and titles manually */
-export const docVersions: VersionInfo[] = [
-  {
-    url: 'https://v5.material.angular.io/',
-    title: '5.2.5'
-  },
-  {
-    url: 'https://v6.material.angular.io/',
-    title: '6.4.7'
-  },
-  {
-    url: `https://material.angular.io/`,
-    title: materialVersion
-  }
-];
