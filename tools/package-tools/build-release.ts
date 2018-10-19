4b481b404c5bd9b93b3a94fabc70dc11cac33529
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-19 18:56:47 +0200

    build(schematics): version placeholders not replaced
    * Since we copy the schematics after the version placeholders have been inlined, the version range that uses the NG version placeholder is not replaced properly.
    
    This is not problematic because by default we determine the NG version based on the installed `@angular/core` version.
    
    ```
     addPackageToPackageJson(host, '@angular/animations',
            ngCoreVersionTag || requiredAngularVersionRange);
    ```
    ilease enter the commit message for your changes. Lines starting

--- tools/package-tools/build-release.ts
+++ tools/package-tools/build-release.ts
@@ -48,15 +48,15 @@ export function composeRelease(buildPackage: BuildPackage) {
   copyFiles(packagesDir, 'README.md', releasePath);
   copyFiles(sourceDir, 'package.json', releasePath);
 
+  if (buildPackage.hasSchematics) {
+    copyFiles(join(packageOut, 'schematics'), '**/*', join(releasePath, 'schematics'));
+  }
+
   replaceVersionPlaceholders(releasePath);
   insertPackageJsonVersionStamp(join(releasePath, 'package.json'));
 
   createTypingsReexportFile(releasePath, './typings/index', name);
   createMetadataReexportFile(releasePath, './typings/index', name, importAsName);
-
-  if (buildPackage.hasSchematics) {
-    copyFiles(join(packageOut, 'schematics'), '**/*', join(releasePath, 'schematics'));
-  }
 
   if (buildPackage.secondaryEntryPoints.length) {
     createFilesForSecondaryEntryPoint(buildPackage, releasePath);
