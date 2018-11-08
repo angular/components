0d2a51577af3c72591cb6cc94056a5be32b73e77
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-08 12:16:50 +0000

    build: switch from saucelabs to browserstack

--- test/browser-providers.js
+++ test/browser-providers.js
@@ -4,10 +4,7 @@
  * Browser Configuration for the different jobs in the CI.
  *
  *   - local: Launches the browser locally on the current operating system.
- *   - BS: Launches the browser within BrowserStack
- *   - SL: Launches the browser within Saucelabs
- *
- * TODO(devversion): rename this to "browserstack" and "saucelabs".
+ *   - browserstack: Launches the browser within BrowserStack
  */
 const browserConfig = {
   'ChromeHeadlessCI':  { unitTest: {target: 'local', }},
@@ -19,7 +16,7 @@ const browserConfig = {
   'IE9':               { unitTest: {target: null, }},
   'IE10':              { unitTest: {target: null, }},
   'IE11':              { unitTest: {target: null, }},
-  'Edge':              { unitTest: {target: 'BS', }},
+  'Edge':              { unitTest: {target: 'browserstack', }},
   'Android4.1':        { unitTest: {target: null, }},
   'Android4.2':        { unitTest: {target: null, }},
   'Android4.3':        { unitTest: {target: null, }},
@@ -28,14 +25,12 @@ const browserConfig = {
   'Safari7':           { unitTest: {target: null, }},
   'Safari8':           { unitTest: {target: null, }},
   'Safari9':           { unitTest: {target: null, }},
-  'Safari10':          { unitTest: {target: 'BS', }},
+  'Safari10':          { unitTest: {target: 'browserstack', }},
   'iOS7':              { unitTest: {target: null, }},
   'iOS8':              { unitTest: {target: null, }},
   'iOS9':              { unitTest: {target: null, }},
   'iOS10':             { unitTest: {target: null, }},
-  // Don't use Browserstack until our open-source license includes automate testing on
-  // mobile devices. For now, we need to use Saucelabs to keep our coverage.
-  'iOS11':             { unitTest: {target: 'SL', }},
+  'iOS11':             { unitTest: {target: 'browserstack', }},
   'WindowsPhone':      { unitTest: {target: null, }}
 };
 
@@ -44,8 +39,7 @@ exports.customLaunchers = require('./remote_browsers.json');
 
 /** Exports a map of configured browsers, which should run in the given platform. */
 exports.platformMap = {
-  'saucelabs': buildConfiguration('unitTest', 'SL'),
-  'browserstack': buildConfiguration('unitTest', 'BS'),
+  'browserstack': buildConfiguration('unitTest', 'browserstack'),
   'local': buildConfiguration('unitTest', 'local'),
 };
 
@@ -63,16 +57,7 @@ function buildConfiguration(type, target) {
     return targetBrowsers;
   }
 
-  return targetBrowsers.map(browserName => `${target}_${browserName.toUpperCase()}`);
-}
-
-/** Decode the token for Travis to use. */
-function decodeToken(token) {
-  return (token || '').split('').reverse().join('');
-}
-
-/** Ensures that the Saucelabs and Browserstack access keys work properly. */
-if (process.env.TRAVIS) {
-  process.env.SAUCE_ACCESS_KEY = decodeToken(process.env.SAUCE_ACCESS_KEY);
-  process.env.BROWSER_STACK_ACCESS_KEY = decodeToken(process.env.BROWSER_STACK_ACCESS_KEY);
+  return targetBrowsers.map(browserName => {
+    return `${target.toUpperCase()}_${browserName.toUpperCase()}`
+  });
 }
