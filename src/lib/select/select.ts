8eddf294a7634ed93c6ae7e54f0505029df8dd92
crisbeto <crisbeto@abv.bg>
2019-02-12 15:57:19 +0100

    fix(select): error when navigating via keyboard to reset option on a closed select
    Fixes `mat-select` throwing an error when the user reaches a reset option using the arrow keys on a closed select. Seems to be due to a missing null check in the changes from #14540.
    
    Fixes #15159.

--- src/lib/select/select.ts
+++ src/lib/select/select.ts
@@ -705,7 +705,7 @@ export class MatSelect extends _MatSelectMixinBase implements AfterContentInit, 
       event.preventDefault(); // prevents the page from scrolling down when pressing space
       this.open();
     } else if (!this.multiple) {
-      const selectedOption = this.selected;
+      const previouslySelectedOption = this.selected;
 
       if (keyCode === HOME || keyCode === END) {
         keyCode === HOME ? manager.setFirstItemActive() : manager.setLastItemActive();
@@ -714,10 +714,12 @@ export class MatSelect extends _MatSelectMixinBase implements AfterContentInit, 
         manager.onKeydown(event);
       }
 
+      const selectedOption = this.selected;
+
       // Since the value has changed, we need to announce it ourselves.
       // @breaking-change 8.0.0 remove null check for _liveAnnouncer.
-      if (this._liveAnnouncer && selectedOption !== this.selected) {
-        this._liveAnnouncer.announce((this.selected as MatOption).viewValue);
+      if (this._liveAnnouncer && selectedOption && previouslySelectedOption !== selectedOption) {
+        this._liveAnnouncer.announce((selectedOption as MatOption).viewValue);
       }
     }
   }
