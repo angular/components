9a844db5f8f2410dd4a1321ee1d3054937b70116
ME <32621250+erdom@users.noreply.github.com>
2019-02-16 00:39:14 +0100

    Update year references to 2019 (#579)

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/shared/stackblitz/stackblitz-writer.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/shared/stackblitz/stackblitz-writer.ts
@@ -8,7 +8,7 @@ import {materialVersion} from '../version/version';
 const STACKBLITZ_URL = 'https://run.stackblitz.com/api/angular/v1';
 
 const COPYRIGHT =
-  `Copyright 2018 Google Inc. All Rights Reserved.
+  `Copyright 2019 Google Inc. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
     can be found in the LICENSE file at http://angular.io/license`;
 
