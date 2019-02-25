c3bc40b06142234d13634af076c077f769ae7b98
Aleksandar Kamenov <aleksandyr.kamenov@gmail.com>
2019-02-25 13:35:57 +0200

    docs(chips): fix space indentations and apply animation
    drag&drop fix spaces indentation and apply animation
    
    Closes #15276

--- src/material-examples/chips-drag-and-drop/chips-drag-drop-example.ts
+++ src/material-examples/chips-drag-and-drop/chips-drag-drop-example.ts
@@ -2,30 +2,26 @@ import {Component} from '@angular/core';
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 export interface Vegetable {
-    name: string;
+  name: string;
 }
 
-/**
- * @title Chips Drag and Drop
- */
-
 @Component({
-    selector: 'chips-drag-drop-example',
-    templateUrl: 'chips-drag-drop-example.html',
-    styleUrls: ['chips-drag-drop-example.css']
+  selector: 'chips-drag-drop-example',
+  templateUrl: 'chips-drag-drop-example.html',
+  styleUrls: ['chips-drag-drop-example.css']
 })
 export class ChipsDragDropExample {
 
-    vegetables: Vegetable[] = [
-        { name: 'apple' },
-        { name: 'banana' },
-        { name: 'strawberry' },
-        { name: 'orange' },
-        { name: 'kiwi' },
-        { name: 'cherry' },
-    ];
+  vegetables: Vegetable[] = [
+    { name: 'apple' },
+    { name: 'banana' },
+    { name: 'strawberry' },
+    { name: 'orange' },
+    { name: 'kiwi' },
+    { name: 'cherry' },
+  ];
 
-    drop(event: CdkDragDrop<Vegetable[]>) {
-        moveItemInArray(this.vegetables, event.previousIndex, event.currentIndex);
-    }
+  drop(event: CdkDragDrop<Vegetable[]>) {
+    moveItemInArray(this.vegetables, event.previousIndex, event.currentIndex);
+  }
 }
