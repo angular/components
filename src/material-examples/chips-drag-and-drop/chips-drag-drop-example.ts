133cdce06b9d0d910d4de9be3e6fa4c77de03ebc
Aleksandar Kamenov <aleksandyr.kamenov@gmail.com>
2019-03-07 15:17:42 +0200

    fix(chips-drag-drop): fix lint errors
    Closes #13215

--- src/material-examples/chips-drag-and-drop/chips-drag-drop-example.ts
+++ src/material-examples/chips-drag-and-drop/chips-drag-drop-example.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import {Component} from '@angular/core';
 
 export interface Vegetable {
   name: string;
@@ -14,14 +14,13 @@ export interface Vegetable {
   styleUrls: ['chips-drag-drop-example.css']
 })
 export class ChipsDragDropExample {
-
   vegetables: Vegetable[] = [
-    { name: 'apple' },
-    { name: 'banana' },
-    { name: 'strawberry' },
-    { name: 'orange' },
-    { name: 'kiwi' },
-    { name: 'cherry' },
+    {name: 'apple'},
+    {name: 'banana'},
+    {name: 'strawberry'},
+    {name: 'orange'},
+    {name: 'kiwi'},
+    {name: 'cherry'},
   ];
 
   drop(event: CdkDragDrop<Vegetable[]>) {
