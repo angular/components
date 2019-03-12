641e6765954a4cd57d2667165deaed3a12b3509e
Marc Laval <marc.laval.opensource@gmail.com>
2019-03-12 18:32:55 +0100

    test(table): update static query test (#15450)
    7 tests are impacted

--- src/lib/table/table.spec.ts
+++ src/lib/table/table.spec.ts
@@ -538,7 +538,7 @@ class MatTableApp {
   columnsToRender = ['column_a', 'column_b', 'column_c'];
   isFourthRow = (i: number, _rowData: TestData) => i == 3;
 
-  @ViewChild(MatTable) table: MatTable<TestData>;
+  @ViewChild(MatTable, {static: true}) table: MatTable<TestData>;
 }
 
 @Component({
@@ -568,7 +568,7 @@ class NativeHtmlTableApp {
   dataSource: FakeDataSource | null = new FakeDataSource();
   columnsToRender = ['column_a', 'column_b', 'column_c'];
 
-  @ViewChild(MatTable) table: MatTable<TestData>;
+  @ViewChild(MatTable, {static: true}) table: MatTable<TestData>;
 }
 
 @Component({
@@ -588,7 +588,7 @@ class StickyTableApp {
   dataSource = new FakeDataSource();
   columnsToRender = ['column_a'];
 
-  @ViewChild(MatTable) table: MatTable<TestData>;
+  @ViewChild(MatTable, {static: true}) table: MatTable<TestData>;
 }
 
 
@@ -655,9 +655,9 @@ class ArrayDataSourceMatTableApp implements OnInit {
   dataSource = new MatTableDataSource<TestData>();
   columnsToRender = ['column_a', 'column_b', 'column_c'];
 
-  @ViewChild(MatTable) table: MatTable<TestData>;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTable, {static: true}) table: MatTable<TestData>;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
   @ViewChild(MatSortHeader) sortHeader: MatSortHeader;
 
   constructor() {
@@ -708,8 +708,8 @@ class MatTableWithSortApp implements OnInit {
   dataSource = new MatTableDataSource<TestData>();
   columnsToRender = ['column_a', 'column_b', 'column_c'];
 
-  @ViewChild(MatTable) table: MatTable<TestData>;
-  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTable, {static: true}) table: MatTable<TestData>;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor() {
     this.underlyingDataSource.data = [];
@@ -759,8 +759,8 @@ class MatTableWithPaginatorApp implements OnInit {
   dataSource = new MatTableDataSource<TestData>();
   columnsToRender = ['column_a', 'column_b', 'column_c'];
 
-  @ViewChild(MatTable) table: MatTable<TestData>;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatTable, {static: true}) table: MatTable<TestData>;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor() {
     this.underlyingDataSource.data = [];
