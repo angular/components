012581f80ee17cfdbb7fee93f1c933bfaa577ddf
Miles Malerba <mmalerba@google.com>
2019-03-18 13:52:59 -0700

    dev-app: add lazy loading

---
+++ src/dev-app/main-module.ts
@@ -0,0 +1,39 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {FullscreenOverlayContainer, OverlayContainer} from '@angular/cdk/overlay';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {MAT_RIPPLE_GLOBAL_OPTIONS} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+import {DevAppComponent} from './dev-app';
+import {DevAppModule} from './dev-app/dev-app-module';
+import {DEV_APP_ROUTES} from './dev-app/routes';
+import {DevAppRippleOptions} from './ripple/ripple-options';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    DevAppModule,
+    HttpClientModule,
+    RouterModule.forRoot(DEV_APP_ROUTES),
+  ],
+  declarations: [
+    DevAppComponent,
+  ],
+  providers: [
+    {provide: OverlayContainer, useClass: FullscreenOverlayContainer},
+    {provide: MAT_RIPPLE_GLOBAL_OPTIONS, useExisting: DevAppRippleOptions},
+  ],
+  bootstrap: [DevAppComponent],
+})
+export class MainModule {
+}
