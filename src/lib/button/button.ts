c18e37722e08250d9bae195530604a7c333efd6f
Olcay Usta <olcayustaweb@yandex.com>
2019-03-29 05:59:34 +0300

    fix maximum-line-length

--- src/lib/button/button.ts
+++ src/lib/button/button.ts
@@ -84,7 +84,8 @@ export class MatButton extends _MatButtonMixinBase
     implements OnDestroy, CanDisable, CanColor, CanDisableRipple {
 
   /** Whether the button is round. */
-  readonly isRoundButton: boolean = this._hasHostAttributes('mat-fab', 'mat-mini-fab', 'mat-extended-fab');
+  readonly isRoundButton: boolean = this._hasHostAttributes('mat-fab', 'mat-mini-fab', 
+                                                            'mat-extended-fab');
 
   /** Whether the button is icon button. */
   readonly isIconButton: boolean = this._hasHostAttributes('mat-icon-button');
