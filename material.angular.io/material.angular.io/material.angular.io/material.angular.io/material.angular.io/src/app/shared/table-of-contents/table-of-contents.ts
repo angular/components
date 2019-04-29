02d1cb8bdd1f3de4ae2949da78b9fa59e4d030dc
mmalerba <mmalerba@google.com>
2019-04-29 10:31:35 -0700

    update deps for 8.0.0-rc.0 (#593)

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/shared/table-of-contents/table-of-contents.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/shared/table-of-contents/table-of-contents.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef, Inject, Input, OnInit} from '@angular/core';
-import {DOCUMENT} from '@angular/platform-browser';
+import {DOCUMENT} from '@angular/common';
 import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 import {Subject, fromEvent} from 'rxjs';
 import {debounceTime, takeUntil} from 'rxjs/operators';
