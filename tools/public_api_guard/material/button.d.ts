f2dc7b337cdabe269b68bfd8b31c6017d7952050
Jonathan Rinciari <jrinciari@greencheckverified.com>
2019-05-10 10:36:58 -0400

    refactor(Button): Updated typings to include default options (#15989)

--- tools/public_api_guard/material/button.d.ts
+++ tools/public_api_guard/material/button.d.ts
@@ -1,6 +1,12 @@
+export declare const MAT_BUTTON_DEFAULT_OPTIONS: InjectionToken<MatButtonDefaultOptions>;
+
+export declare function MAT_BUTTON_DEFAULT_OPTIONS_FACTORY(): MatButtonDefaultOptions;
+
 export declare class MatAnchor extends MatButton {
     tabIndex: number;
-    constructor(focusMonitor: FocusMonitor, elementRef: ElementRef, animationMode: string);
+
+    constructor(focusMonitor: FocusMonitor, elementRef: ElementRef, defaults: MatButtonDefaultOptions, animationMode: string);
+
     _haltDisabledEvents(event: Event): void;
 }
 
@@ -9,12 +15,24 @@ export declare class MatButton extends _MatButtonMixinBase implements OnDestroy,
     readonly isIconButton: boolean;
     readonly isRoundButton: boolean;
     ripple: MatRipple;
-    constructor(elementRef: ElementRef, _focusMonitor: FocusMonitor, _animationMode: string);
+
+    constructor(elementRef: ElementRef, _focusMonitor: FocusMonitor, defaults: MatButtonDefaultOptions, _animationMode: string);
+
     _getHostElement(): any;
+
     _hasHostAttributes(...attributes: string[]): boolean;
+
     _isRippleDisabled(): boolean;
+
     focus(): void;
+
     ngOnDestroy(): void;
+}
+
+export interface MatButtonDefaultOptions {
+    type: 'submit' | 'reset' | 'button';
+
+    [key: string]: string;
 }
 
 export declare class MatButtonModule {
