2d5614c52be0d11baad566a2dd1c91357bb18266
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-06-07 17:57:49 +0200

    fix: remove imports from top-level package (#604)
    Removes the imports from `@angular/material` since they end up breaking some of the examples once they're forked to Stackblitz.

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/assets/stackblitz/main.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/assets/stackblitz/main.ts
@@ -3,7 +3,7 @@ import './polyfills';
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material';
+import {MatNativeDateModule} from '@angular/material/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
