df045385ed2d5ef01762605116018ba0fe31de36
Miles Malerba <mmalerba@google.com>
2019-06-20 10:03:22 -0700

    fix BUILD setup

--- src/cdk-experimental/testing/testbed/public-api.ts
+++ src/cdk-experimental/testing/testbed/public-api.ts
@@ -6,4 +6,5 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-export * from './public-api';
+export * from './testbed-harness-environment';
+export * from './unit-test-element';
