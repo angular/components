d215f1f34ae437e60b88f0e2fab8665cef417738
Kyrylo Seliukov <kys@ciklum.com>
2019-07-03 18:27:02 +0300

    sitemap * fir linter issues

--- src/material/sitemap/public-api.ts
+++ src/material/sitemap/public-api.ts
@@ -3,7 +3,7 @@
  * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at [path to license file]
+ * found in the LICENSE file at https://angular.io/license
  */
 
 export * from './sitemap';
