f38330b985f46852d2e3da22cc642088e63f69e9
Andrew Seguin <andrewjs@google.com>
2019-07-26 14:19:45 -0700

    feat(material-experimental): add progress spinner test harness skeleton

---
+++ src/material-experimental/mdc-progress-spinner/harness/progress-spinner-harness.ts
@@ -0,0 +1,38 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {ComponentHarness} from '@angular/cdk-experimental/testing';
+import {ProgressSpinnerMode} from '@angular/material/progress-spinner';
+
+/**
+ * Harness for interacting with a standard mat-progress-spinner in tests.
+ * @dynamic
+ */
+export class MatProgressSpinnerHarness extends ComponentHarness {
+  static hostSelector = 'mat-progress-spinner';
+
+  /** Gets a promise for the progress spinner's value. */
+  async getValue(): Promise<number|null> {
+    const host = await this.host();
+    const ariaValue = await host.getAttribute('aria-valuenow');
+
+    if (ariaValue) {
+      return Number(ariaValue);
+    }
+
+    return null;
+  }
+
+  /** Gets a promise for the progress spinner's mode. */
+  async getMode(): Promise<ProgressSpinnerMode> {
+    const modeAttr = (await this.host()).getAttribute('mode');
+    return await modeAttr as ProgressSpinnerMode;
+  }
+
+  // TODO: Add getters for width, height, value min, value max
+}
