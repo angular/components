5afee8f2b26522e1233d1e9a060be486010a95b3
Tobias Schweizer <t.schweizer@unibas.ch>
2019-08-05 14:45:58 +0200

    feature (datepicker): make _initWeekdays a private method

--- src/material/datepicker/month-view.ts
+++ src/material/datepicker/month-view.ts
@@ -151,19 +151,6 @@ export class MatMonthView<D> implements AfterContentInit {
     this._init();
   }
 
-  /** Initializes the weekdays. */
-  _initWeekdays() {
-    const firstDayOfWeek = this._dateAdapter.getFirstDayOfWeek();
-    const narrowWeekdays = this._dateAdapter.getDayOfWeekNames('narrow');
-    const longWeekdays = this._dateAdapter.getDayOfWeekNames('long');
-
-    // Rotate the labels for days of the week based on the configured first day of the week.
-    let weekdays = longWeekdays.map((long, i) => {
-      return {long, narrow: narrowWeekdays[i]};
-    });
-    this._weekdays = weekdays.slice(firstDayOfWeek).concat(weekdays.slice(0, firstDayOfWeek));
-  }
-
   /** Handles when a new date is selected. */
   _dateSelected(date: number) {
     if (this._selectedDate != date) {
@@ -256,7 +243,6 @@ export class MatMonthView<D> implements AfterContentInit {
          this._dateAdapter.getFirstDayOfWeek()) % DAYS_PER_WEEK;
 
     this._initWeekdays();
-
     this._createWeekCells();
     this._changeDetectorRef.markForCheck();
   }
@@ -264,6 +250,19 @@ export class MatMonthView<D> implements AfterContentInit {
   /** Focuses the active cell after the microtask queue is empty. */
   _focusActiveCell() {
     this._matCalendarBody._focusActiveCell();
+  }
+
+  /** Initializes the weekdays. */
+  private _initWeekdays() {
+    const firstDayOfWeek = this._dateAdapter.getFirstDayOfWeek();
+    const narrowWeekdays = this._dateAdapter.getDayOfWeekNames('narrow');
+    const longWeekdays = this._dateAdapter.getDayOfWeekNames('long');
+
+    // Rotate the labels for days of the week based on the configured first day of the week.
+    let weekdays = longWeekdays.map((long, i) => {
+        return {long, narrow: narrowWeekdays[i]};
+    });
+    this._weekdays = weekdays.slice(firstDayOfWeek).concat(weekdays.slice(0, firstDayOfWeek));
   }
 
   /** Creates MatCalendarCells for the dates in this month. */
