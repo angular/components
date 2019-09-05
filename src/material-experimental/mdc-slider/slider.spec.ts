425f3250a362a0a68d322609f06d114a7fca1865
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-09-05 18:55:55 +0200

    Add debugging

--- src/material-experimental/mdc-slider/slider.spec.ts
+++ src/material-experimental/mdc-slider/slider.spec.ts
@@ -538,10 +538,11 @@ describe('MatMdcSlider', () => {
       flushRequestAnimationFrame();
     }));
 
-    it('should initialize based on bound value', () => {
+    it('should initialize based on bound value', async () => {
+      await new Promise(res => setTimeout(res, 1000000));
       expect(sliderInstance.value).toBe(50);
       expect(thumbContainerEl.style.transform).toContain('translateX(50px)');
-    });
+    }, 1000000);
 
     it('should update when bound value changes', fakeAsync(() => {
       testComponent.val = 75;
