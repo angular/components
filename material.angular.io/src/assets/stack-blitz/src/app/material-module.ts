91f6ddebb5b430693625aeebdb4002a2dbc3b74a
Michael Prentice <splaktar@gmail.com>
2019-11-04 11:47:35 -0500

    build: Angular CLI update to 9.0.0-rc.0 (#672)
    - update versions in StackBlitz and version picker
    - add `ClipboardModule` to StackBlitz
    - improve build margins in StackBlitz
    - @angular/core@next
    - @angular/material@next and @angular/material-experimental@next
    - @angular/cdk@next and @angular/cdk-experimental@next
    - @angular/cli@next
    - @angular/localize@next

--- material.angular.io/src/assets/stack-blitz/src/app/material-module.ts
+++ material.angular.io/src/assets/stack-blitz/src/app/material-module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {A11yModule} from '@angular/cdk/a11y';
+import {ClipboardModule} from '@angular/cdk/clipboard';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {PortalModule} from '@angular/cdk/portal';
 import {ScrollingModule} from '@angular/cdk/scrolling';
@@ -44,6 +45,7 @@ import {MatTreeModule} from '@angular/material/tree';
 @NgModule({
   exports: [
     A11yModule,
+    ClipboardModule,
     CdkStepperModule,
     CdkTableModule,
     CdkTreeModule,
