72daa7b3d24443255fbd7bb4b63de2065426d15b
JanMalch <janmalch@gmail.com>
2019-12-09 20:59:52 +0100

    refactor(coercion): improve coerceStringArray implementation
    The function now only requires one loop over the array instead of two.
    Improve documentation by adding parameter description and behaviour for special cases.
    Minor style improvements.

--- src/cdk/coercion/string-array.ts
+++ src/cdk/coercion/string-array.ts
@@ -8,17 +8,32 @@
 
 /**
  * Coerces a value to an array of trimmed non-empty strings.
+ * Anything that is not an array or null-ish will be turned into a string via a template literal.
+ * Null-ish values will result in an empty array.
+ * This results in the following outcomes:
+ * - `null` -&gt; `[]`
+ * - `[null]` -&gt; `["null"]`
+ * - `[1, [2, 3]]` -&gt; `["1", "2,3"]`
+ * - `[{ a: 0 }]` -&gt; `["[object Object]"]`
+ * - `{ a: 0 }` -&gt; `["[object", "Object]"]`
+ *
  * Useful for defining CSS classes or table columns.
+ * @param value the value to coerce into an array of strings
+ * @param separator split-separator if value isn't an array
  */
-export function coerceStringArray(value: any, separator: string | RegExp = ' '): string[] {
-    if (value == null) {
-        return [];
+export function coerceStringArray(value: any, separator: string | RegExp = /\s+/): string[] {
+  const result = [];
+
+  if (value != null) {
+    const sourceValues = Array.isArray(value) ? value : `${value}`.split(separator);
+    for (const sourceValue of sourceValues) {
+      const trimmedString = `${sourceValue}`.trim();
+      if (!!trimmedString) {
+        result.push(trimmedString);
+      }
     }
-    const stringArray = Array.isArray(value) ?
-        value.map(item => `${item}`) :
-        value.toString().split(separator);
-    return stringArray
-        .map((item: string) => item.trim())
-        .filter((item: string) => item.length > 0);
+  }
+
+  return result;
 }
 
