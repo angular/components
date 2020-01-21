1f0c3cf9dab528546dac403406616f13239a9d26
Aakash06 <aakash.jain@knoldus.in>
2020-01-21 11:26:27 +0530

    refactor(keyboard-event): Added new key module and added new keys constants

---
+++ src/cdk/key/key.ts
@@ -0,0 +1,61 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+export const BACKSPACE = 'Backspace';
+export const TAB = 'Tab';
+export const ENTER = 'Enter';
+export const SHIFT = 'Shift';
+export const ALT = 'Alt';
+export const ESCAPE = 'Escape';
+// ' ' is standard, 'Space' was used by IE9 and Firefox < 37
+export const SPACE = ' ' || 'Space';
+export const PAGE_UP = 'PageUp';
+export const PAGE_DOWN = 'PageDown';
+export const END = 'End';
+export const HOME = 'Home';
+export const LEFT_ARROW = 'ArrowLeft';
+export const UP_ARROW = 'ArrowUp';
+export const RIGHT_ARROW = 'ArrowRight';
+export const DOWN_ARROW = 'ArrowDown';
+export const DELETE = 'Delete';
+export const ZERO = '0';
+export const ONE = '1';
+export const TWO = '2';
+export const THREE = '3';
+export const FOUR = '4';
+export const FIVE = '5';
+export const SIX = '6';
+export const SEVEN = '7';
+export const EIGHT = '8';
+export const NINE = '9';
+export const QUESTION_MARK = '?';
+export const A = 'A';
+export const B = 'B';
+export const C = 'C';
+export const D = 'D';
+export const E = 'E';
+export const F = 'F';
+export const G = 'G';
+export const H = 'H';
+export const I = 'I';
+export const J = 'J';
+export const K = 'K';
+export const L = 'L';
+export const M = 'M';
+export const N = 'N';
+export const O = 'O';
+export const P = 'P';
+export const Q = 'Q';
+export const R = 'R';
+export const S = 'S';
+export const T = 'T';
+export const U = 'U';
+export const V = 'V';
+export const W = 'W';
+export const X = 'X';
+export const Y = 'Y';
+export const Z = 'Z';
---
+++ src/cdk/key/modifiers.spec.ts
@@ -0,0 +1,42 @@
+import {createKeyboardEvent} from '@angular/cdk/testing/private';
+import {hasModifierKey} from './modifiers';
+describe('keyboard modifiers', () => {
+    it('should check whether the alt key is pressed', () => {
+        const event = createKeyboardEvent('keydown', 'alt');
+        const altEvent = createKeyboardEvent('keydown', 'alt', 0, undefined, {alt: true});
+        expect(hasModifierKey(event)).toBe(false);
+        expect(hasModifierKey(altEvent)).toBe(true);
+    });
+    it('should check whether the shift key is pressed', () => {
+        const event = createKeyboardEvent('keydown', 'shift');
+        const shiftEvent = createKeyboardEvent('keydown', '', 0, undefined, {shift: true});
+        expect(hasModifierKey(event)).toBe(false);
+        expect(hasModifierKey(shiftEvent)).toBe(true);
+    });
+    it('should check whether the meta key is pressed', () => {
+        const event = createKeyboardEvent('keydown', '~');
+        const metaEvent = createKeyboardEvent('keydown', '', 0, undefined, {meta: true});
+        expect(hasModifierKey(event)).toBe(false);
+        expect(hasModifierKey(metaEvent)).toBe(true);
+    });
+    it('should check whether the ctrl key is pressed', () => {
+        const event = createKeyboardEvent('keydown', 'Control');
+        const ctrlEvent = createKeyboardEvent('keydown', '', 0, undefined, {control: true});
+        expect(hasModifierKey(event)).toBe(false);
+        expect(hasModifierKey(ctrlEvent)).toBe(true);
+    });
+    it('should check if a particular modifier key is pressed', () => {
+        const ctrlEvent = createKeyboardEvent('keydown', '', 0, undefined, {control: true});
+        const ctrlAltEvent = createKeyboardEvent(
+            'keydown', '', 0, undefined, {control: true, alt: true});
+        expect(hasModifierKey(ctrlEvent, 'altKey')).toBe(false);
+        expect(hasModifierKey(ctrlAltEvent, 'altKey')).toBe(true);
+    });
+    it('should check if multiple specific modifier keys are pressed', () => {
+        const ctrlEvent = createKeyboardEvent('keydown',  '', 0, undefined, {control: true});
+        const ctrlAltShiftEvent = createKeyboardEvent(
+            'keydown', '', 0, undefined, {control: true, alt: true, shift: true});
+        expect(hasModifierKey(ctrlEvent, 'altKey', 'shiftKey')).toBe(false);
+        expect(hasModifierKey(ctrlAltShiftEvent, 'altKey', 'shiftKey')).toBe(true);
+    });
+});
