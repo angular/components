28b9970eaba420e46f1684d7835ca716e62ff075
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-29 21:06:30 +0100

    refactor(datepicker): set up date range picker classes (#18292)
    Moves some things around to set up the date range picker. Includes:
    * Moves all of the datepicker logic into a base class so that it can be reused between the single and range selection.
    * Makes the input being passed into a datepicker generic so that we can strongly type `MatDatepicker` to `MatDatepickerInput` and `MatDateRangePicker` to `MatDateRangeInput`.
    * Sets up min, max and date filter validation for the date range picker.
    * Hooks up the date range picker to the date range input.
    * Makes it so that a generic datepicker can be passed to a datepicker toggle.
    * Adds a `DateFilterFn` type so we don't have to repeat the signature for the filter function everywhere.
    
    Note that while picking a date range somewhat works now, it's still a bit broken. The primary goal of these changes was to have all of components in place and wired up so that we can start working on the UI in follow-up PRs.

---
+++ src/material/datepicker/date-range-picker.ts
@@ -0,0 +1,27 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {MAT_RANGE_DATE_SELECTION_MODEL_PROVIDER, DateRange} from '@angular/material/core';
+import {MatDatepickerBase} from './datepicker-base';
+import {MatDateRangeInput} from './date-range-input';
+
+// TODO(mmalerba): We use a component instead of a directive here so the user can use implicit
+// template reference variables (e.g. #d vs #d="matDateRangePicker"). We can change this to a
+// directive if angular adds support for `exportAs: '$implicit'` on directives.
+/** Component responsible for managing the date range picker popup/dialog. */
+@Component({
+  selector: 'mat-date-range-picker',
+  template: '',
+  exportAs: 'matDateRangePicker',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+  providers: [MAT_RANGE_DATE_SELECTION_MODEL_PROVIDER]
+})
+export class MatDateRangePicker<D>
+  extends MatDatepickerBase<MatDateRangeInput<D>, DateRange<D>, D> {}
