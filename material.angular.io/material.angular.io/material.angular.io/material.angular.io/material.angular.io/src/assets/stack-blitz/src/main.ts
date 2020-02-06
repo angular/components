a5a301a1fb393910a00392bf807bf0b33cb35dc3
Michael Prentice <splaktar@gmail.com>
2020-02-06 15:46:07 -0500

    fix(stack-blitz): default to form-field fill appearance (#712)

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/assets/stack-blitz/src/main.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/assets/stack-blitz/src/main.ts
@@ -8,9 +8,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {DemoMaterialModule} from './app/material-module';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
 
 import {MaterialDocsExample} from './app/material-docs-example';
 
+// Default MatFormField appearance to 'fill' as that is the new recommended approach and the
+// `legacy` and `standard` appearances are scheduled for deprecation in version 10.
+// This makes the examples that use MatFormField render the same in StackBlitz as on the docs site.
 @NgModule({
   imports: [
     BrowserModule,
@@ -24,7 +28,9 @@ import {MaterialDocsExample} from './app/material-docs-example';
   entryComponents: [MaterialDocsExample],
   declarations: [MaterialDocsExample],
   bootstrap: [MaterialDocsExample],
-  providers: []
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  ]
 })
 export class AppModule {}
 
