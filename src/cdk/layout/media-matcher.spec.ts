863f37ec36c6d63448fcd7a42c0db1b28c638901
CaerusKaru <caerus.karu@gmail.com>
2020-02-09 19:00:31 -0600

    fixup! test(layout): remove unneeded LayoutModule

--- src/cdk/layout/media-matcher.spec.ts
+++ src/cdk/layout/media-matcher.spec.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import {MediaMatcher} from './media-matcher';
-import {TestBed, inject} from '@angular/core/testing';
+import {inject} from '@angular/core/testing';
 import {Platform} from '@angular/cdk/platform';
 
 describe('MediaMatcher', () => {
