ddcaf81ab40d2ce3f735cf8059c285e591705e4b
Michael Prentice <splaktar@gmail.com>
2020-02-13 15:41:02 -0500

    fix(guide): add ReactiveFormsModule to GuideViewerModule (#719)
    Fixes #718

--- material.angular.io/src/app/pages/guide-viewer/guide-viewer.ts
+++ material.angular.io/src/app/pages/guide-viewer/guide-viewer.ts
@@ -5,6 +5,7 @@ import {FooterModule} from '../../shared/footer/footer';
 import {DocViewerModule} from '../../shared/doc-viewer/doc-viewer-module';
 import {TableOfContentsModule} from '../../shared/table-of-contents/table-of-contents.module';
 import {ComponentPageTitle} from '../page-title/page-title';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'guide-viewer',
@@ -39,8 +40,14 @@ export class GuideViewer implements OnInit {
 
 const routes: Routes = [ {path : '', component : GuideViewer} ];
 
+// This module needs to include all of the modules required by the examples in the guides.
+// For example, the custom form-field guide requires the ReactiveFormsModule.
+// These imports may need to be updated when adding examples to new or existing guides.
 @NgModule({
-  imports: [DocViewerModule, FooterModule, TableOfContentsModule, RouterModule.forChild(routes)],
+  imports: [
+    DocViewerModule, FooterModule, TableOfContentsModule, ReactiveFormsModule,
+    RouterModule.forChild(routes),
+  ],
   exports: [GuideViewer],
   declarations: [GuideViewer],
   providers: [GuideItems],
