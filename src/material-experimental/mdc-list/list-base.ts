f38f0e64ece5983d59d64b47dfedd52736bed6e0
Miles Malerba <mmalerba@google.com>
2020-02-26 15:30:56 -0800

    fix(material-experimental/mdc-list): fix styles for normal lists
    Fixes up various styles for the lists under the "normal lists" section
    of the demo. There are still some issues around the avatars and dividers
    on the last list

--- src/material-experimental/mdc-list/list-base.ts
+++ src/material-experimental/mdc-list/list-base.ts
@@ -6,6 +6,34 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import {AfterContentInit, ElementRef, NgZone, OnDestroy, QueryList} from '@angular/core';
+import {setLines} from '@angular/material/core';
+import {Subscription} from 'rxjs';
+import {startWith} from 'rxjs/operators';
+
 export class MatListBase {}
 
-export class MatListItemBase {}
+export class MatListItemBase implements AfterContentInit, OnDestroy {
+  lines: QueryList<ElementRef<Element>>;
+
+  private _subscriptions = new Subscription();
+
+  constructor(protected _element: ElementRef, protected _ngZone: NgZone) {}
+
+  ngAfterContentInit() {
+    this._ngZone.runOutsideAngular(() => {
+      this._subscriptions.add(this.lines.changes.pipe(startWith(this.lines))
+          .subscribe((lines: QueryList<ElementRef<Element>>) => {
+            lines.forEach((line: ElementRef<Element>, index: number) => {
+              line.nativeElement.classList.toggle('mdc-list-item__primary-text', index === 0);
+              line.nativeElement.classList.toggle('mdc-list-item__secondary-text', index !== 0);
+            });
+            setLines(lines, this._element, 'mat-mdc');
+          }));
+    });
+  }
+
+  ngOnDestroy() {
+    this._subscriptions.unsubscribe();
+  }
+}
--- src/material-experimental/mdc-list/list.ts
+++ src/material-experimental/mdc-list/list.ts
@@ -6,7 +6,15 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {ChangeDetectionStrategy, Component, Directive, ViewEncapsulation} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ContentChildren,
+  Directive, ElementRef,
+  QueryList,
+  ViewEncapsulation
+} from '@angular/core';
+import {MatLine} from '@angular/material/core';
 import {MatListBase, MatListItemBase} from './list-base';
 
 /**
@@ -15,7 +23,7 @@ import {MatListBase, MatListItemBase} from './list-base';
  */
 @Directive({
   selector: '[mat-list-avatar], [matListAvatar]',
-  host: {'class': 'mat-mdc-list-avatar'}
+  host: {'class': 'mat-mdc-list-avatar mdc-list-item__graphic'}
 })
 export class MatListAvatarCssMatStyler {}
 
@@ -25,7 +33,7 @@ export class MatListAvatarCssMatStyler {}
  */
 @Directive({
   selector: '[mat-list-icon], [matListIcon]',
-  host: {'class': 'mat-mdc-list-icon'}
+  host: {'class': 'mat-mdc-list-icon mdc-list-item__graphic'}
 })
 export class MatListIconCssMatStyler {}
 
@@ -35,7 +43,9 @@ export class MatListIconCssMatStyler {}
  */
 @Directive({
   selector: '[mat-subheader], [matSubheader]',
-  host: {'class': 'mat-mdc-subheader'}
+  // TODO(mmalerba): MDC's subheader font looks identical to the list item font, figure out why and
+  //  make a change in one of the repos to visually distinguish.
+  host: {'class': 'mat-mdc-subheader mdc-list-group__subheader'}
 })
 export class MatListSubheaderCssMatStyler {}
 
@@ -62,4 +72,7 @@ export class MatList extends MatListBase {}
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MatListItem extends MatListItemBase {}
+export class MatListItem extends MatListItemBase {
+  @ContentChildren(MatLine, {read: ElementRef, descendants: true}) lines:
+      QueryList<ElementRef<Element>>;
+}
