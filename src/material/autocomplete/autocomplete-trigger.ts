0129a4857bbde7b4b4b61c19afa3a2ad693db3ff
crisbeto <crisbeto@abv.bg>
2020-03-04 17:15:11 +0100

    fix(autocomplete): restore focus after emitting option selected event
    Currently we restore focus to the input and then we emit the change event, but we have a report that it may be making some use cases more difficult. From what I can tell, this shouldn't have much of an impact on existing users so these changes swap the order so that the focus event is last.
    
    Fixes #18650.

--- src/material/autocomplete/autocomplete-trigger.ts
+++ src/material/autocomplete/autocomplete-trigger.ts
@@ -595,12 +595,14 @@ export class MatAutocompleteTrigger implements ControlValueAccessor, AfterViewIn
    * stemmed from the user.
    */
   private _setValueAndClose(event: MatOptionSelectionChange | null): void {
-    if (event && event.source) {
-      this._clearPreviousSelectedOption(event.source);
-      this._setTriggerValue(event.source.value);
-      this._onChange(event.source.value);
+    const source = event && event.source;
+
+    if (source) {
+      this._clearPreviousSelectedOption(source);
+      this._setTriggerValue(source.value);
+      this._onChange(source.value);
+      this.autocomplete._emitSelectEvent(source);
       this._element.nativeElement.focus();
-      this.autocomplete._emitSelectEvent(event.source);
     }
 
     this.closePanel();
