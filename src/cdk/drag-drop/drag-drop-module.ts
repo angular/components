07c79b1dc010440a70e70c3ca6e505c7f683df3e
Andrea Canciani <ranma42@gmail.com>
2020-04-01 00:50:28 +0200

    fix(cdk/drag-drop): make DragDrop tree-shakable
    In order to be tree-shakable, services must be marked as `providedIn`
    and they must not be explicitly referenced in the `providers` of an
    `NgModule`.

--- src/cdk/drag-drop/drag-drop-module.ts
+++ src/cdk/drag-drop/drag-drop-module.ts
@@ -14,7 +14,6 @@ import {CdkDrag} from './directives/drag';
 import {CdkDragHandle} from './directives/drag-handle';
 import {CdkDragPreview} from './directives/drag-preview';
 import {CdkDragPlaceholder} from './directives/drag-placeholder';
-import {DragDrop} from './drag-drop';
 
 const DRAG_DROP_DIRECTIVES = [
   CdkDropList,
@@ -28,6 +27,5 @@ const DRAG_DROP_DIRECTIVES = [
 @NgModule({
   imports: DRAG_DROP_DIRECTIVES,
   exports: [CdkScrollableModule, ...DRAG_DROP_DIRECTIVES],
-  providers: [DragDrop],
 })
 export class DragDropModule {}
