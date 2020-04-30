f6f1fd467caa72d8b9c0e9473ce611186b3de52a
Miles Malerba <mmalerba@google.com>
2020-04-30 14:02:04 -0700

    clarifying comment

--- src/cdk/testing/protractor/protractor-element.ts
+++ src/cdk/testing/protractor/protractor-element.ts
@@ -74,7 +74,11 @@ export class ProtractorElement implements TestElement {
   }
 
   async click(...args: number[]): Promise<void> {
+    // Omitting the offset argument to mouseMove results in clicking the center.
+    // This is the default behavior we want, so we use an empty array of offsetArgs if no args are
+    // passed to this method.
     const offsetArgs = args.length ? [{x: args[0], y: args[1]}] : [];
+
     await browser.actions()
       .mouseMove(await this.element.getWebElement(), ...offsetArgs)
       .click()
