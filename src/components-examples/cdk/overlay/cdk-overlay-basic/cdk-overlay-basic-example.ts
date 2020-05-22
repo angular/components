7f577248ee7d43829cbdee096a3a96fd3d073ef3
Andy Chrzaszcz <andymchr@google.com>
2020-05-22 12:24:07 -0400

    docs(examples/cdk/overlay): remove long comment
    Remove long component comment in order to prevent it from rendering
    fully on the examples page as the title

--- src/components-examples/cdk/overlay/cdk-overlay-basic/cdk-overlay-basic-example.ts
+++ src/components-examples/cdk/overlay/cdk-overlay-basic/cdk-overlay-basic-example.ts
@@ -2,10 +2,6 @@ import {Component} from '@angular/core';
 
 /**
  * @title Overlay basic example
- *
- * CdkOverlayBasicExample demonstrates how it is possible to trigger and
- * connect a component to a set origin (in this case a button).
- *
  */
 @Component({
   selector: 'cdk-overlay-basic-example',
