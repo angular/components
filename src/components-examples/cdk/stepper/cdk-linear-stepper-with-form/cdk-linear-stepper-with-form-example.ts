bd6a4740a542e606363b22d53937d16da300e04a
niels <nielsr@google.com>
2020-06-02 15:32:11 -0400

    Created cdk linear stepper component that has two steps and a form at each step which acts as the StepControl. There is a toggle button to enable or disable the linear feature.

---
+++ src/components-examples/cdk/stepper/cdk-linear-stepper-with-form/cdk-linear-stepper-with-form-example.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import {CdkStepper} from '@angular/cdk/stepper';
+import {FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+/** @title A custom CDK linear stepper with forms */
+@Component({
+    selector: 'cdk-linear-stepper-with-form',
+    templateUrl: './cdk-linear-stepper-with-form-example.html',
+    styleUrls: ['./cdk-linear-stepper-with-form-example.css']
+})
+export class CdkLinearStepperWithFormExample {
+    isLinear = true;
+    firstFormGroup: FormGroup;
+    secondFormGroup: FormGroup;
+
+    constructor(private _formBuilder: FormBuilder) {}
+
+    ngOnInit() {
+        this.firstFormGroup = this._formBuilder.group({
+            firstControl: ['', Validators.required]
+        });
+        this.secondFormGroup = this._formBuilder.group({
+            secondControl: ['', Validators.required]
+        });
+    }
+}
+
+/** Custom CDK linear stepper component */
+@Component({
+    selector: 'example-custom-linear-stepper',
+    templateUrl: './example-custom-linear-stepper.html',
+    styleUrls: ['./example-custom-linear-stepper.css'],
+    providers: [{ provide: CdkStepper, useExisting: CustomLinearStepper}]
+})
+export class CustomLinearStepper extends CdkStepper {
+    onClick(index: number): void {
+        this.selectedIndex = index;
+    }
+}
+
+
+
+
