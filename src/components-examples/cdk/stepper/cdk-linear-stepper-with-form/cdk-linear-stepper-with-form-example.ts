09978e10a59998d61f231540e94eef443dcf005d
niels <nielsr@google.com>
2020-06-03 13:38:58 -0400

    Changed all tabs to be default 2 spaces instead of 4.

--- src/components-examples/cdk/stepper/cdk-linear-stepper-with-form/cdk-linear-stepper-with-form-example.ts
+++ src/components-examples/cdk/stepper/cdk-linear-stepper-with-form/cdk-linear-stepper-with-form-example.ts
@@ -4,38 +4,38 @@ import {FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule} fr
 
 /** @title A custom CDK linear stepper with forms */
 @Component({
-    selector: 'cdk-linear-stepper-with-form',
-    templateUrl: './cdk-linear-stepper-with-form-example.html',
-    styleUrls: ['./cdk-linear-stepper-with-form-example.css']
+  selector: 'cdk-linear-stepper-with-form',
+  templateUrl: './cdk-linear-stepper-with-form-example.html',
+  styleUrls: ['./cdk-linear-stepper-with-form-example.css']
 })
 export class CdkLinearStepperWithFormExample {
-    isLinear = true;
-    firstFormGroup: FormGroup;
-    secondFormGroup: FormGroup;
+  isLinear = true;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
 
-    constructor(private _formBuilder: FormBuilder) {}
+  constructor(private _formBuilder: FormBuilder) {}
 
-    ngOnInit() {
-        this.firstFormGroup = this._formBuilder.group({
-            firstControl: ['', Validators.required]
-        });
-        this.secondFormGroup = this._formBuilder.group({
-            secondControl: ['', Validators.required]
-        });
-    }
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstControl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondControl: ['', Validators.required]
+    });
+  }
 }
 
 /** Custom CDK linear stepper component */
 @Component({
-    selector: 'example-custom-linear-stepper',
-    templateUrl: './example-custom-linear-stepper.html',
-    styleUrls: ['./example-custom-linear-stepper.css'],
-    providers: [{provide: CdkStepper, useExisting: CustomLinearStepper}]
+  selector: 'example-custom-linear-stepper',
+  templateUrl: './example-custom-linear-stepper.html',
+  styleUrls: ['./example-custom-linear-stepper.css'],
+  providers: [{provide: CdkStepper, useExisting: CustomLinearStepper}]
 })
 export class CustomLinearStepper extends CdkStepper {
-    onClick(index: number): void {
-        this.selectedIndex = index;
-    }
+  onClick(index: number): void {
+    this.selectedIndex = index;
+  }
 }
 
 
