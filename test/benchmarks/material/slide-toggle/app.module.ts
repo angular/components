c57b8c5f4b94444318a24b5129d44639ece3cdb1
Wagner Maciel <wagnermaciel@google.com>
2020-06-08 17:08:47 -0700

    test(slide-toggle): add performance tests for mat-slide-toggle

---
+++ test/benchmarks/material/slide-toggle/app.module.ts
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import {A11yModule} from '@angular/cdk/a11y';
+import {Component, NgModule, ViewEncapsulation} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <button id="show" (click)="show()">Show</button>
+    <button id="hide" (click)="hide()">Hide</button>
+
+    <ng-container *ngIf="isVisible">
+      <mat-slide-toggle>Slide me!</mat-slide-toggle>
+    </ng-container>
+  `,
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['//src/material/core/theming/prebuilt/indigo-pink.css'],
+})
+export class SlideToggleBenchmarkApp {
+  isVisible = false;
+  show() { this.isVisible = true; }
+  hide() { this.isVisible = false; }
+}
+
+
+@NgModule({
+  declarations: [SlideToggleBenchmarkApp],
+  imports: [
+    A11yModule,
+    BrowserModule,
+    MatSlideToggleModule,
+  ],
+  providers: [],
+  bootstrap: [SlideToggleBenchmarkApp]
+})
+export class AppModule {}
