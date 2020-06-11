cb69b20fb4dc8b20746f33969b42edd1509e14ce
Annie Wang <annieyw@google.com>
2020-06-10 17:03:46 -0700

    add button-toggle scene (#777)

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/scenes/src/app/app-routing.module.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/scenes/src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { SceneViewer } from './scene-viewer/scene-viewer';
 import { RipplesScene } from './scenes/ripples/ripples-scene';
 import { InputScene } from './scenes/input/input-scene';
+import { ButtonToggleScene } from './scenes/button-toggle/button-toggle-scene';
+import {SliderScene} from './scenes/slider/slider-scene';
 
 
 const routes: Routes = [
   {path: 'ripples', component: SceneViewer, data: {hueRotate: 120, scene: RipplesScene}},
-  {path: 'slider', component: SceneViewer, data: {hueRotate: 135, scene: RipplesScene}},
+  {path: 'slider', component: SceneViewer, data: {hueRotate: 135, scene: SliderScene}},
   {path: 'input', component: SceneViewer, data: {hueRotate: 45, scale: 0.7, scene: InputScene}},
+  {
+    path: 'button-toggle',
+    component: SceneViewer,
+    data: {hueRotate: 135, scale: 1.5, scene: ButtonToggleScene}
+  },
 ];
 
 @NgModule({
