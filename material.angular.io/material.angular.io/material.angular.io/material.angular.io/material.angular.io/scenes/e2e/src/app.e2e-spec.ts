d482b0531cd75bcf2eab28e8b421ff72d06e2865
Annie Wang <annieyw@google.com>
2020-06-11 10:37:43 -0700

    add slide-toggle scene (#773)

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/scenes/e2e/src/app.e2e-spec.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/scenes/e2e/src/app.e2e-spec.ts
@@ -5,7 +5,7 @@ describe('screenshot scenes for each component', () => {
   // These tests simply serve as a convenient way to take snapshots of different pages,
   // they are not actually testing anything
   let page: AppPage;
-  const components = ['input', 'ripples', 'slider', 'button-toggle'];
+  const components = ['input', 'ripples', 'slider', 'button-toggle', 'slide-toggle'];
 
   beforeEach(() => {
     page = new AppPage();
--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/scenes/src/app/app-routing.module.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/scenes/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RipplesScene } from './scenes/ripples/ripples-scene';
 import { InputScene } from './scenes/input/input-scene';
 import { ButtonToggleScene } from './scenes/button-toggle/button-toggle-scene';
 import {SliderScene} from './scenes/slider/slider-scene';
+import {SlideToggleScene} from './scenes/slide-toggle/slide-toggle-scene';
 
 
 const routes: Routes = [
@@ -16,6 +17,8 @@ const routes: Routes = [
     component: SceneViewer,
     data: {hueRotate: 135, scale: 1.5, scene: ButtonToggleScene}
   },
+  {path: 'slide-toggle', component: SceneViewer, data: {hueRotate: 90, scene: SlideToggleScene}},
+
 ];
 
 @NgModule({
