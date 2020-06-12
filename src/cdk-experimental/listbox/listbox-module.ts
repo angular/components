7298164c5c7eaa91a7ded78ef7a9aebf18629617
niels <nielsr@google.com>
2020-06-12 00:10:29 -0400

    fix: filled out BUILD.bazel and other required listbox files.

---
+++ src/cdk-experimental/listbox/listbox-module.ts
@@ -0,0 +1,17 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {NgModule} from '@angular/core';
+import {CdkListbox, CdkListboxOption} from "./listbox";
+
+const EXPORTED_DECLARATIONS = [CdkListbox, CdkListboxOption];
+@NgModule({
+    exports: EXPORTED_DECLARATIONS,
+    declarations: EXPORTED_DECLARATIONS,
+})
+export class CdkListboxModule {}
