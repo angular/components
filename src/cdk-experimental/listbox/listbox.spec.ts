a3a69409d1812f1caa656b7737ca64cdddd63021
niels <nielsr@google.com>
2020-06-18 11:55:43 -0400

    feat(cdk-experimental/listbox): added tests for listbox and options, and included click listener.

---
+++ src/cdk-experimental/listbox/listbox.spec.ts
@@ -0,0 +1,91 @@
+import {
+  ComponentFixture,
+  async,
+  TestBed,
+} from '@angular/core/testing';
+import {Component} from '@angular/core';
+import {By} from '@angular/platform-browser';
+import {
+  CdkOption,
+  CdkListbox,
+  CdkListboxModule
+} from './index';
+import {dispatchMouseEvent} from '@angular/cdk/testing/private';
+
+describe('CdkOption', () => {
+
+  describe('selection state change', () => {
+    let fixture: ComponentFixture<CdkListboxWithCdkOptions>;
+    let listboxInstance: CdkListbox;
+    let options: Array<CdkOption>;
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [CdkListboxModule],
+        declarations: [CdkListboxWithCdkOptions],
+      }).compileComponents();
+
+      fixture = TestBed.createComponent(CdkListboxWithCdkOptions);
+      fixture.detectChanges();
+      listboxInstance = fixture.debugElement.query(By.directive(CdkListbox)).injector.get(CdkListbox);
+      options = listboxInstance._options.toArray();
+    }));
+
+    it('should generate a unique optionid for each option', () => {
+      options.forEach(option => {
+        expect(option.getOptionId()).toMatch(/cdk-option-\d+/);
+      });
+    });
+
+    it('should have set the selected input of the options to null by default', () => {
+      options.forEach(option => {
+        expect(option.selected).toBe(null);
+      });
+    });
+
+    it('should update aria-selected when selected is changed programmatically', () => {
+      expect(options[1].getElementRef().nativeElement.getAttribute('aria-selected')).toBe(null);
+      options[1].selected = true;
+      fixture.detectChanges();
+
+      expect(options[1].getElementRef().nativeElement.getAttribute('aria-selected')).toBe('true');
+    });
+
+    it('should be able to return the currently selected options', () => {
+      expect(listboxInstance.getSelectedOptions().length).toBe(0);
+      options[0].selected = true;
+      fixture.detectChanges();
+      const selectedOptions = listboxInstance.getSelectedOptions();
+
+      expect(selectedOptions.length).toBe(1);
+      expect(selectedOptions[0].selected).toBeTruthy();
+    });
+
+    it('should update selected option on click event', () => {
+      expect(listboxInstance.getSelectedOptions().length).toBe(0);
+      expect(options[0].getElementRef().nativeElement.getAttribute('aria-selected')).toBe(null);
+      expect(options[0].selected).toBeFalsy();
+
+      dispatchMouseEvent(options[0].getElementRef().nativeElement, 'click');
+      fixture.detectChanges();
+
+      expect(listboxInstance.getSelectedOptions().length).toBe(1);
+      expect(options[0].getElementRef().nativeElement.getAttribute('aria-selected')).toBe('true');
+      expect(options[0].selected).toBeTruthy();
+    });
+  });
+
+});
+
+@Component({
+    template:`
+    <div cdkListbox>
+      <div cdkOption></div>
+      <div cdkOption></div>
+      <div cdkOption></div>
+      <div cdkOption></div> 
+    </div>`
+})
+class CdkListboxWithCdkOptions {
+
+}
--- src/cdk-experimental/listbox/listbox.ts
+++ src/cdk-experimental/listbox/listbox.ts
@@ -6,19 +6,68 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {Directive} from '@angular/core';
+import {
+  ContentChildren,
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  QueryList
+} from '@angular/core';
 
+/**
+ * Directive that applies interaction patterns to an element following the aria role of option.
+ * Typically meant to be placed inside a listbox. Logic handling selection, disabled state, and
+ * value is built in.
+ */
 @Directive({
   selector: '[cdkOption]',
   exportAs: 'cdkOption',
   host: {
     role: 'option',
+    '[attr.aria-selected]': '_selected',
+    '[attr.data-optionid]': '_optionId',
   }
 })
 export class CdkOption {
+  private _selected: boolean | null = null;
+  private _optionId: string;
 
+  /** Whether the option is selected or not */
+  @Input()
+  get selected(): boolean | null {
+    return this._selected;
+  }
+  set selected(value: boolean | null) {
+    this._selected = value;
+  }
+
+  constructor(private el: ElementRef) {
+  }
+
+  /** Sets the optionId to the given id */
+  setOptionId(id: string): void {
+    this._optionId = id;
+  }
+
+  /** Returns the optionId of this option */
+  getOptionId(): string {
+    return this._optionId;
+  }
+
+  /** Returns an ElementRef of this option */
+  getElementRef(): ElementRef {
+    return this.el;
+  }
 }
 
+let _uniqueIdCounter = 0;
+
+/**
+ * Directive that applies interaction patterns to an element following the aria role of listbox.
+ * Typically CdkOption elements are placed inside the listbox. Logic to handle keyboard navigation,
+ * selection of options, active options, and disabled states is built in.
+ */
 @Directive({
   selector: '[cdkListbox]',
   exportAs: 'cdkListbox',
@@ -28,4 +77,57 @@ export class CdkOption {
 })
 export class CdkListbox {
 
+  constructor(private el: ElementRef) {
+  }
+
+  /** A query list containing all CdkOption elements within this listbox */
+  @ContentChildren(CdkOption) _options: QueryList<CdkOption>;
+
+  /** Listener activated on click of this listbox */
+  @HostListener('click', ['$event']) onClickUpdateSelectedOption($event: MouseEvent) {
+    console.log('in listbox click event');
+    console.log($event.target);
+    this._options.toArray().forEach(option => {
+      const optionId = option.getOptionId();
+      if ($event.target instanceof Element && optionId === $event.target?.getAttribute('data-optionid')) {
+        this.updateSelectedOption(option);
+      }
+    });
+  }
+
+  ngAfterContentInit() {
+    this._options.forEach(option => {
+      option.setOptionId(`cdk-option-${_uniqueIdCounter++}`);
+    });
+  }
+
+  private updateSelectedOption(option: CdkOption): void {
+    if (option.selected) {
+      this.deselectOption(option);
+    } else {
+      this.selectOption(option);
+    }
+  }
+
+  /** Sets the given option's selected state to true */
+  selectOption(option: CdkOption): void {
+    option.selected = true;
+  }
+
+  /** Sets the given option's selected state to null. Null is preferable to false for screen readers */
+  deselectOption(option: CdkOption): void {
+    option.selected = null;
+  }
+
+  /** Returns an array of all options that are currently selected */
+  getSelectedOptions(): Array<CdkOption> {
+    const selectedOptions = new Array<CdkOption>();
+    this._options.toArray().forEach(option => {
+      if (option.selected) {
+        selectedOptions.push(option);
+      }
+    });
+
+    return selectedOptions;
+  }
 }
