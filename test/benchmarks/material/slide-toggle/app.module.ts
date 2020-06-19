42528d04d65a33312e7de7e96cd07beca19e5385
Wagner Maciel <wagnermaciel@google.com>
2020-06-19 14:59:30 -0700

    fixup! test(slide-toggle): add performance tests for mat-slide-toggle

--- test/benchmarks/material/slide-toggle/app.module.ts
+++ test/benchmarks/material/slide-toggle/app.module.ts
@@ -1,10 +1,11 @@
 /**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+
 import {Component, NgModule, ViewEncapsulation} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
