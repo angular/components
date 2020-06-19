3decf7f210e11069cd288b6553f1c2e27c7f111e
Wagner Maciel <wagnermaciel@google.com>
2020-06-19 13:55:55 -0700

    fixup! test(radio): add performance tests for mat-radio-button

--- test/benchmarks/material/radio/app.module.ts
+++ test/benchmarks/material/radio/app.module.ts
@@ -1,10 +1,11 @@
 /**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+
 import {Component, NgModule, ViewEncapsulation} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {MatRadioModule} from '@angular/material/radio';
@@ -18,25 +19,23 @@ import {MatRadioModule} from '@angular/material/radio';
     <button id="show-ten" (click)="showTen()">Show Ten</button>
     <button id="hide-ten" (click)="hideTen()">Hide Ten</button>
 
-    <ng-container *ngIf="isTwoVisible">
+    <mat-radio-group aria-label="Select an option" *ngIf="isTwoVisible">
       <mat-radio-button value="1">Option 1</mat-radio-button>
       <mat-radio-button value="2">Option 2</mat-radio-button>
-    </ng-container>
+    </mat-radio-group>
 
-    <ng-container *ngIf="isTenVisible">
-      <mat-radio-group aria-label="Select an option">
-        <mat-radio-button value="1">Option 1</mat-radio-button>
-        <mat-radio-button value="2">Option 2</mat-radio-button>
-        <mat-radio-button value="3">Option 3</mat-radio-button>
-        <mat-radio-button value="4">Option 4</mat-radio-button>
-        <mat-radio-button value="5">Option 5</mat-radio-button>
-        <mat-radio-button value="6">Option 6</mat-radio-button>
-        <mat-radio-button value="7">Option 7</mat-radio-button>
-        <mat-radio-button value="8">Option 8</mat-radio-button>
-        <mat-radio-button value="9">Option 9</mat-radio-button>
-        <mat-radio-button value="10">Option 10</mat-radio-button>
-      </mat-radio-group>
-    </ng-container>
+    <mat-radio-group aria-label="Select an option" *ngIf="isTenVisible">
+      <mat-radio-button value="1">Option 1</mat-radio-button>
+      <mat-radio-button value="2">Option 2</mat-radio-button>
+      <mat-radio-button value="3">Option 3</mat-radio-button>
+      <mat-radio-button value="4">Option 4</mat-radio-button>
+      <mat-radio-button value="5">Option 5</mat-radio-button>
+      <mat-radio-button value="6">Option 6</mat-radio-button>
+      <mat-radio-button value="7">Option 7</mat-radio-button>
+      <mat-radio-button value="8">Option 8</mat-radio-button>
+      <mat-radio-button value="9">Option 9</mat-radio-button>
+      <mat-radio-button value="10">Option 10</mat-radio-button>
+    </mat-radio-group>
   `,
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['//src/material/core/theming/prebuilt/indigo-pink.css'],
@@ -60,6 +59,6 @@ export class RadioBenchmarkApp {
     MatRadioModule,
   ],
   providers: [],
-  bootstrap: [RadioBenchmarkApp]
+  bootstrap: [RadioBenchmarkApp],
 })
 export class AppModule {}
