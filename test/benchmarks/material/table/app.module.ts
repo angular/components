caba7251328d4bec2b8772ee7c30bf56858b85e3
Wagner Maciel <wagnermaciel@google.com>
2020-06-23 15:06:59 -0700

    fixup! test(benchmarks): Add performance tests for mat-table

--- test/benchmarks/material/table/app.module.ts
+++ test/benchmarks/material/table/app.module.ts
@@ -1,96 +1,99 @@
 /**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import {A11yModule} from '@angular/cdk/a11y';
+
 import {Component, NgModule, ViewEncapsulation} from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
 import {BrowserModule} from '@angular/platform-browser';
 import {BasicTable, GenericObject} from './basic-table';
-import {MatTableModule} from '@angular/material/table';
 
-/**
- * @title Checkbox benchmark component.
- */
+function createRows(numRows: number, cols: string[]): GenericObject[] {
+  const rows: GenericObject[] = new Array(numRows);
+  for (let i = 0; i < rows.length; i++) {
+    rows[i] = createRow(cols, (i + 1).toString());
+  }
+  return rows;
+}
+
+function createRow(cols: string[], value: string): GenericObject {
+  const row: GenericObject = {};
+  for (let i = 0; i < cols.length; i++) {
+    const col = cols[i];
+    row[col] = value;
+  }
+  return row;
+}
+
+const fiveCols = ['a', 'b', 'c', 'd', 'e'];
+const tenCols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+const twentyCols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'];
+
+const tenRows = createRows(10, twentyCols);
+const oneHundredRows = createRows(100, twentyCols);
+const oneThousandRows = createRows(1000, twentyCols);
+
 @Component({
   selector: 'app-root',
   template: `
-    <button id="show" (click)="show()">Show</button>
     <button id="hide" (click)="hide()">Hide</button>
+    <button id="show-10-rows-5-cols" (click)="showTenRowsFiveCols()">Show 10 Rows 5 Cols</button>
+    <button id="show-100-rows-5-cols" (click)="showOneHundredRowsFiveCols()">Show 100 Rows 5 Cols</button>
+    <button id="show-1000-rows-5-cols" (click)="showOneThousandRowsFiveCols()">Show 1000 Rows 5 Cols</button>
+    <button id="show-10-rows-10-cols" (click)="showTenRowsTenCols()">Show 10 Rows 10 Cols</button>
+    <button id="show-10-rows-20-cols" (click)="showTenRowsTwentyCols()">Show 10 Rows 20 Cols</button>
 
-    <button id="ten-rows" (click)="updateTable({ numRows: 10 })">10 Rows</button>
-    <button id="one-hundred-rows" (click)="updateTable({ numRows: 100 })">100 Rows</button>
-    <button id="one-thousand-rows" (click)="updateTable({ numRows: 1000 })">1000 Rows</button>
+    <basic-table [rows]="tenRows" [cols]="fiveCols" *ngIf="isTenRowsFiveColsVisible"></basic-table>
+    <basic-table [rows]="oneHundredRows" [cols]="fiveCols" *ngIf="isOneHundredRowsFiveColsVisible"></basic-table>
+    <basic-table [rows]="oneThousandRows" [cols]="fiveCols" *ngIf="isOneThousandRowsFiveColsVisible"></basic-table>
 
-    <button id="five-cols" (click)="updateTable({ numCols: 5 })">5 Cols</button>
-    <button id="ten-cols" (click)="updateTable({ numCols: 10 })">10 Cols</button>
-    <button id="twenty-cols" (click)="updateTable({ numCols: 20 })">20 Cols</button>
-
-    <ng-container *ngIf="isVisible">
-      <basic-table [rows]="createRows()" [cols]="createCols()"></basic-table>
-    </ng-container>
+    <basic-table [rows]="tenRows" [cols]="tenCols" *ngIf="isTenRowsTenColsVisible"></basic-table>
+    <basic-table [rows]="tenRows" [cols]="twentyCols" *ngIf="isTenRowsTwentyColsVisible"></basic-table>
   `,
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['//src/material/core/theming/prebuilt/indigo-pink.css'],
 })
 export class TableBenchmarkApp {
-  isVisible = false;
-  numRows = 10;
-  numCols = 5;
+  fiveCols = fiveCols;
+  tenCols = tenCols;
+  twentyCols = twentyCols;
 
-  show() { this.isVisible = true; }
-  hide() { this.isVisible = false; }
+  tenRows = tenRows;
+  oneHundredRows = oneHundredRows;
+  oneThousandRows = oneThousandRows;
 
-  /**
-   * The cols array is just a string array of size numCols whose values are 0 to numCols.
-   */
-  createCols() {
-    const cols = new Array(this.numCols);
-    for (let i = 0; i < this.numCols; i++) {
-      cols[i] = `${i}`;
-    }
-    return cols;
+  isTenRowsFiveColsVisible = false;
+  isOneHundredRowsFiveColsVisible = false;
+  isOneThousandRowsFiveColsVisible = false;
+  isTenRowsTenColsVisible = false;
+  isTenRowsTwentyColsVisible = false;
+
+  hide() {
+    this.isTenRowsFiveColsVisible = false;
+    this.isOneHundredRowsFiveColsVisible = false;
+    this.isOneThousandRowsFiveColsVisible = false;
+    this.isTenRowsTenColsVisible = false;
+    this.isTenRowsTwentyColsVisible = false;
   }
 
-  /**
-   * The rows array is an object array.
-   * Each object's keys are the values in the cols array, and the values are "ROW - COL".
-   */
-  createRows() {
-    const rows = new Array(this.numRows);
-    for (let i = 0; i < this.numRows; i++) {
-      const row: GenericObject = {};
-      for (let j = 0; j < this.numCols; j++) {
-        row[j] = `${i} - ${j}`;
-      }
-      rows[i] = row;
-    }
-    return rows;
-  }
-
-  /**
-   * Sets the number of rows and cols to display in our table.
-   *
-   * @param param0.numRows The new number of rows to display.
-   * @param param0.numCols The new number of cols to display.
-   */
-  updateTable({ numRows, numCols }: { numRows?: number, numCols?: number}) {
-    if (numRows !== undefined) { this.numRows = numRows; }
-    if (numCols !== undefined) { this.numCols = numCols; }
-  }
+  showTenRowsFiveCols() { this.isTenRowsFiveColsVisible = true; }
+  showOneHundredRowsFiveCols() { this.isOneHundredRowsFiveColsVisible = true; }
+  showOneThousandRowsFiveCols() { this.isOneThousandRowsFiveColsVisible = true; }
+  showTenRowsTenCols() { this.isTenRowsTenColsVisible = true; }
+  showTenRowsTwentyCols() { this.isTenRowsTwentyColsVisible = true; }
 }
 
 
 @NgModule({
   declarations: [BasicTable, TableBenchmarkApp],
   imports: [
-    A11yModule,
     BrowserModule,
     MatTableModule,
   ],
   providers: [],
-  bootstrap: [TableBenchmarkApp]
+  bootstrap: [TableBenchmarkApp],
 })
 export class AppModule {}
--- test/benchmarks/material/table/table.perf-spec.ts
+++ test/benchmarks/material/table/table.perf-spec.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
@@ -9,83 +9,39 @@
 import {$, browser} from 'protractor';
 import {runBenchmark} from '@angular/dev-infra-private/benchmark/driver-utilities';
 
-describe('checkbox overview performance benchmarks', () => {
+function runTableRenderBenchmark(testId: string, buttonId: string) {
+  return runBenchmark({
+    id: testId,
+    url: '',
+    ignoreBrowserSynchronization: true,
+    params: [],
+    prepare: async () => await $('#hide').click(),
+    work: async () => await $(buttonId).click(),
+  });
+}
+
+describe('table performance benchmarks', () => {
   beforeAll(() => {
     browser.rootEl = '#root';
   });
 
-  describe('increasing rows', () => {
-    it('renders 10 rows with 5 cols', async() => {
-      await runBenchmark({
-        id: 'basic-table-render-10-rows-5-cols',
-        url: '',
-        ignoreBrowserSynchronization: true,
-        params: [],
-        prepare: async () => await $('#hide').click(),
-        work: async () => await $('#show').click()
-      });
-    });
-
-    it('renders 100 rows with 5 cols', async() => {
-      await runBenchmark({
-        id: 'basic-table-render-100-rows-5-cols',
-        url: '',
-        ignoreBrowserSynchronization: true,
-        params: [],
-        setup: async () => await $('#one-hundred-rows').click(),
-        prepare: async () => await $('#hide').click(),
-        work: async () => await $('#show').click()
-      });
-    });
-
-    it('renders 1000 rows with 5 cols', async() => {
-      await runBenchmark({
-        id: 'basic-table-render-1000-rows-5-cols',
-        url: '',
-        ignoreBrowserSynchronization: true,
-        params: [],
-        setup: async () => await $('#one-thousand-rows').click(),
-        prepare: async () => await $('#hide').click(),
-        work: async () => await $('#show').click()
-      });
-    });
+  it('renders 10 rows with 5 cols', async() => {
+    await runTableRenderBenchmark('table-render-10-rows-5-cols', '#show-10-rows-5-cols');
   });
 
-  describe('increasing cols', () => {
-    it('renders 10 rows with 5 cols', async() => {
-      await runBenchmark({
-        id: 'basic-table-render-10-rows-5-cols',
-        url: '',
-        ignoreBrowserSynchronization: true,
-        params: [],
-        setup: async () => await $('#show').click(),
-        prepare: async () => await $('#hide').click(),
-        work: async () => await $('#show').click()
-      });
-    });
+  it('renders 100 rows with 5 cols', async() => {
+    await runTableRenderBenchmark('table-render-100-rows-5-cols', '#show-100-rows-5-cols');
+  });
 
-    it('renders 10 rows with 10 cols', async() => {
-      await runBenchmark({
-        id: 'basic-table-render-10-rows-10-cols',
-        url: '',
-        ignoreBrowserSynchronization: true,
-        params: [],
-        setup: async () => await $('#ten-cols').click(),
-        prepare: async () => await $('#hide').click(),
-        work: async () => await $('#show').click()
-      });
-    });
+  it('renders 1000 rows with 5 cols', async() => {
+    await runTableRenderBenchmark('table-render-1000-rows-5-cols', '#show-1000-rows-5-cols');
+  });
 
-    it('renders 10 rows with 20 cols', async() => {
-      await runBenchmark({
-        id: 'basic-table-render-10-rows-20-cols',
-        url: '',
-        ignoreBrowserSynchronization: true,
-        params: [],
-        setup: async () => await $('#twenty-cols').click(),
-        prepare: async () => await $('#hide').click(),
-        work: async () => await $('#show').click()
-      });
-    });
+  it('renders 10 rows with 10 cols', async() => {
+    await runTableRenderBenchmark('table-render-10-rows-10-cols', '#show-10-rows-10-cols');
+  });
+
+  it('renders 10 rows with 20 cols', async() => {
+    await runTableRenderBenchmark('table-render-10-rows-20-cols', '#show-10-rows-20-cols');
   });
 });
