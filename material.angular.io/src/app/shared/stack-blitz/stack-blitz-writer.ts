5118df5d711349f807023b5354edfd64b7a6ff44
Jeremy Elbourn <jelbourn@google.com>
2020-06-24 16:13:18 -0700

    Update to Angular 10.0.0 (#799)

--- material.angular.io/src/app/shared/stack-blitz/stack-blitz-writer.ts
+++ material.angular.io/src/app/shared/stack-blitz/stack-blitz-writer.ts
@@ -38,7 +38,7 @@ const TEMPLATE_FILES = [
 ];
 
 const TAGS: string[] = ['angular', 'material', 'example'];
-const angularVersion = '^9.0.0';
+const angularVersion = '^10.0.0';
 
 const dependencies = {
   '@angular/cdk': materialVersion,
