3c39387e05a553c1b0669ea16209fe7f1b60aa9d
andrewseguin <andrewjs@google.com>
2020-06-30 06:54:16 -0700

    remove docs private from TextOnlySnackBar

--- src/material/snack-bar/simple-snack-bar.ts
+++ src/material/snack-bar/simple-snack-bar.ts
@@ -13,7 +13,6 @@ import {MatSnackBarRef} from './snack-bar-ref';
 
 /**
  * Interface for a simple snack bar component that has a message and a single action.
- * @docs-private
  */
 export interface TextOnlySnackBar {
   data: {message: string, action: string};
