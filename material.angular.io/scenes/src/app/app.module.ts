2367306f9ee48601778fe522222d465fd24d0150
Jeremy Elbourn <jelbourn@google.com>
2020-07-01 14:45:46 -0700

    Add scene for snackbar (#808)

--- material.angular.io/scenes/src/app/app.module.ts
+++ material.angular.io/scenes/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {DOCUMENT} from '@angular/common';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {BottomSheetSceneModule} from './scenes/bottom-sheet/bottom-sheet-scene';
+import {SnackBarSceneModule} from './scenes/snack-bar/snack-bar-scene';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import {BottomSheetSceneModule} from './scenes/bottom-sheet/bottom-sheet-scene';
     MatMenuModule,
     MatPaginatorModule,
     BottomSheetSceneModule,
+    SnackBarSceneModule,
   ],
   bootstrap: [AppComponent],
   providers: [{
