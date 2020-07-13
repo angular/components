9f79b66e149b1804979c4b321deff36acf51770f
Annie Wang <annieyw@google.com>
2020-07-13 15:02:35 -0700

    landing page redesign (#780)

--- material.angular.io/src/app/pages/homepage/homepage.ts
+++ material.angular.io/src/app/pages/homepage/homepage.ts
@@ -5,6 +5,14 @@ import {FooterModule} from '../../shared/footer/footer';
 import {RouterModule, Routes} from '@angular/router';
 import {ComponentPageTitle} from '../page-title/page-title';
 import {NavigationFocusModule} from '../../shared/navigation-focus/navigation-focus';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+import {GuideItems} from '../../shared/guide-items/guide-items';
+import {CommonModule} from '@angular/common';
+import {CarouselModule} from '../../shared/carousel/carousel-module';
+
+const TOP_COMPONENTS = ['datepicker', 'input', 'slide-toggle', 'slider', 'button'];
 
 @Component({
   selector: 'app-homepage',
@@ -14,23 +22,30 @@ import {NavigationFocusModule} from '../../shared/navigation-focus/navigation-fo
 export class Homepage implements OnInit {
   isNextVersion = location.hostname.startsWith('next.material.angular.io');
 
-  constructor(public _componentPageTitle: ComponentPageTitle) {}
+  constructor(public _componentPageTitle: ComponentPageTitle, public guideItems: GuideItems) {
+  }
 
   ngOnInit(): void {
     this._componentPageTitle.title = '';
   }
+
+  getTopComponents(): string[] {
+    return TOP_COMPONENTS;
+  }
 }
 
-const routes: Routes = [ {path: '', component: Homepage}];
+const routes: Routes = [{path: '', component: Homepage}];
 
 @NgModule({
   imports: [SvgViewerModule,
             MatButtonModule,
             FooterModule,
             RouterModule.forChild(routes),
-            NavigationFocusModule],
+            NavigationFocusModule, MatIconModule, MatDividerModule, MatCardModule, CommonModule,
+            CarouselModule],
   exports: [Homepage],
   declarations: [Homepage],
+  providers: [GuideItems]
 })
 export class HomepageModule {
 }
