de8e7ebefaf8238e1a42d50770fa8230616b9194
Wagner Maciel <wagnermaciel@google.com>
2020-08-03 12:37:23 -0700

    fixup! test(menu): add performance tests for mat-menu

--- test/benchmarks/material/menu/menu.perf-spec.ts
+++ test/benchmarks/material/menu/menu.perf-spec.ts
@@ -26,9 +26,48 @@ describe('menu performance benchmarks', () => {
       url: '',
       ignoreBrowserSynchronization: true,
       params: [],
-      setup: async () => menu = await loader.getHarness(MatMenuHarness),
+      setup: async () => {
+        menu = await loader.getHarness(MatMenuHarness.with({ triggerText: 'Basic Menu' }));
+      },
       prepare: async () => await menu.close(),
       work: async () => await menu.open(),
+    });
+  });
+
+  it('opens a nested menu', async () => {
+    let menu: MatMenuHarness;
+    await runBenchmark({
+      id: 'nested-menu-open',
+      url: '',
+      ignoreBrowserSynchronization: true,
+      params: [],
+      setup: async () => {
+        menu = await loader.getHarness(MatMenuHarness.with({ triggerText: 'Nested Menu' }));
+      },
+      prepare: async () => await menu.close(),
+      work: async () => await menu.open(),
+    });
+  });
+
+  // NOTE: This test seems very slow at the moment. This IS NOT because opening nested menus is
+  // slow. This IS because calls to the loader are expensive.
+
+  it('fully opens a nested menu', async () => {
+    let menu: MatMenuHarness;
+    await runBenchmark({
+      id: 'nested-menu-open',
+      url: '',
+      ignoreBrowserSynchronization: true,
+      params: [],
+      setup: async () => {
+        menu = await loader.getHarness(MatMenuHarness.with({ triggerText: 'Nested Menu' }));
+      },
+      prepare: async () => await menu.close(),
+      work: async () => {
+        await menu.open();
+        await (await loader.getHarness(MatMenuHarness.with({ triggerText: 'Sub Menu 1' }))).open();
+        await (await loader.getHarness(MatMenuHarness.with({ triggerText: 'Sub Menu 2' }))).open();
+      },
     });
   });
 });
