c28352b0642969a8ba881b212e89c0040681a7db
niels <nielsr@google.com>
2020-08-04 11:31:18 -0400

    feat(combobox): created combobox file skeleton, added to codeowners.

---
+++ src/cdk-experimental/combobox/combobox-panel.ts
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {Directive} from "@angular/core";
+
+@Directive({
+  selector: 'ng-template[cdkComboboxPanel]',
+  exportAs: 'cdkComboboxPanel',
+  host: {
+    'role': 'combobox'
+  }
+})
+export class CdkComboboxPanel<V = unknown> {
+
+}
