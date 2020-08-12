ba873a04a6b2431c04e3a9ee9f9c00e84a735cc4
niels <nielsr@google.com>
2020-08-12 12:09:15 -0400

    feat(listbox): added functions and panel injection to make listbox automatically compatible with being inside a combobox.

--- src/cdk-experimental/listbox/listbox.ts
+++ src/cdk-experimental/listbox/listbox.ts
@@ -12,7 +12,7 @@ import {
   Directive,
   ElementRef, EventEmitter, forwardRef,
   Inject,
-  Input, OnDestroy, OnInit, Output,
+  Input, OnDestroy, OnInit, Optional, Output,
   QueryList
 } from '@angular/core';
 import {ActiveDescendantKeyManager, Highlightable, ListKeyManagerOption} from '@angular/cdk/a11y';
@@ -22,8 +22,10 @@ import {SelectionChange, SelectionModel} from '@angular/cdk/collections';
 import {defer, merge, Observable, Subject} from 'rxjs';
 import {startWith, switchMap, takeUntil} from 'rxjs/operators';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {CdkComboboxPanel} from "@angular/cdk-experimental/combobox";
 
 let nextId = 0;
+let listboxId = 0;
 
 export const CDK_LISTBOX_VALUE_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
@@ -191,6 +193,7 @@ export class CdkOption<T = unknown> implements ListKeyManagerOption, Highlightab
   exportAs: 'cdkListbox',
   host: {
     'role': 'listbox',
+    '[id]': 'id',
     '(keydown)': '_keydown($event)',
     '[attr.tabindex]': '_tabIndex',
     '[attr.aria-disabled]': 'disabled',
@@ -231,6 +234,8 @@ export class CdkListbox<T> implements AfterContentInit, OnDestroy, OnInit, Contr
   @Output() readonly selectionChange: EventEmitter<ListboxSelectionChangeEvent<T>> =
       new EventEmitter<ListboxSelectionChangeEvent<T>>();
 
+  @Input() id = `cdk-option-${listboxId++}`;
+
   /**
    * Whether the listbox allows multiple options to be selected.
    * If `multiple` switches from `true` to `false`, all options are deselected.
@@ -263,6 +268,12 @@ export class CdkListbox<T> implements AfterContentInit, OnDestroy, OnInit, Contr
 
   @Input() compareWith: (o1: T, o2: T) => boolean = (a1, a2) => a1 === a2;
 
+  @Input('parentPanel') private readonly _explicitPanel: CdkComboboxPanel;
+
+  constructor(
+      @Optional() readonly _parentPanel?: CdkComboboxPanel<T>,
+  ) { }
+
   ngOnInit() {
     this._selectionModel = new SelectionModel<CdkOption<T>>(this.multiple);
   }
@@ -270,11 +281,13 @@ export class CdkListbox<T> implements AfterContentInit, OnDestroy, OnInit, Contr
   ngAfterContentInit() {
     this._initKeyManager();
     this._initSelectionModel();
+    this.registerWithPanel();
 
     this.optionSelectionChanges.subscribe(event => {
-      this._emitChangeEvent(event.source);
       this._updateSelectionModel(event.source);
       this.setActiveOption(event.source);
+      this._emitChangeEvent(event.source);
+      this._updatePanel(event.source);
     });
   }
 
@@ -282,6 +295,16 @@ export class CdkListbox<T> implements AfterContentInit, OnDestroy, OnInit, Contr
     this._listKeyManager.change.complete();
     this._destroyed.next();
     this._destroyed.complete();
+  }
+
+  private registerWithPanel(): void {
+    if (this._parentPanel === null || this._parentPanel === undefined) {
+      if (this._explicitPanel !== null && this._explicitPanel !== undefined) {
+        this._explicitPanel._registerContent(this.id, 'listbox');
+      }
+    } else {
+      this._parentPanel._registerContent(this.id, 'listbox');
+    }
   }
 
   private _initKeyManager() {
@@ -356,6 +379,19 @@ export class CdkListbox<T> implements AfterContentInit, OnDestroy, OnInit, Contr
 
     option.selected ? this._selectionModel.select(option) :
                       this._selectionModel.deselect(option);
+  }
+
+  _updatePanel(option: CdkOption<T>) {
+    const data = option.selected ? option.value : null;
+    if (!this.multiple) {
+      if (this._parentPanel === null || this._parentPanel === undefined) {
+        if (this._explicitPanel !== null && this._explicitPanel !== undefined) {
+          this._explicitPanel.closePanel(data);
+        }
+      } else {
+        option.selected ? this._parentPanel.closePanel(option.value) : this._parentPanel.closePanel();
+      }
+    }
   }
 
   /** Toggles the selected state of the active option if not disabled. */
