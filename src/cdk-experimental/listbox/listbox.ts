52a46337c124412cfc91c651c93618ef6fc163dc
niels <nielsr@google.com>
2020-08-17 02:53:41 -0400

    refactor(listbox): prevented listbox from auto closing when in multiple mode in a combobox.

--- src/cdk-experimental/listbox/listbox.ts
+++ src/cdk-experimental/listbox/listbox.ts
@@ -411,8 +411,6 @@ export class CdkListbox<T> implements AfterContentInit, OnDestroy, OnInit, Contr
     const panel = this._parentPanel || this._explicitPanel;
     if (!this.multiple) {
       option.selected ? panel?.closePanel(option.value) : panel?.closePanel();
-    } else {
-      panel?.closePanel(this.getSelectedValues());
     }
   }
 
