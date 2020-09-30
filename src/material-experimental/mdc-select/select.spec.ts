angular/components

commit 88d8416b5f9ef9f4b1dd755a87f9b87526d8f976

fix(select): indicating programmatic value change as user interaction in some cases (#19970)
We were using `ListKeyManager.setActiveItem` to change the active item when a value is assigned through a `FormControl`, in order to have it be highlighted if the user opens the panel. The problem is that `setActiveItem` emits a change event as if it's coming from the user, causing us to dispatch an event object with `isUserInput: true`.

These changes use `updateActiveItem` instead which only updates the item, but doesn't emit any events.

Fixes #19967.

(cherry picked from commit 316141ff0ff685d0ad2f570b6e5c4b74dda755ec)

src/material/select/select.ts
@@ -903,11 +903,11 @@ export class MatSelect extends _MatSelectMixinBase implements AfterContentInit, 
       // Shift focus to the active item. Note that we shouldn't do this in multiple
       // mode, because we don't know what option the user interacted with last.
       if (correspondingOption) {
-        this._keyManager.setActiveItem(correspondingOption);
+        this._keyManager.updateActiveItem(correspondingOption);
       } else if (!this.panelOpen) {
         // Otherwise reset the highlighted option. Note that we only want to do this while
         // closed, because doing it while open can shift the user's focus unnecessarily.
-        this._keyManager.setActiveItem(-1);
+        this._keyManager.updateActiveItem(-1);
       }
     }
 

src/material/select/select.spec.ts
@@ -1653,6 +1653,19 @@ describe('MatSelect', () => {
           subscription!.unsubscribe();
         }));
 
+        it('should not indicate programmatic value changes as user interactions', () => {
+          const events: MatOptionSelectionChange[] = [];
+          const subscription = fixture.componentInstance.select.optionSelectionChanges
+            .subscribe((event: MatOptionSelectionChange) => events.push(event));
+
+          fixture.componentInstance.control.setValue('eggs-5');
+          fixture.detectChanges();
+
+          expect(events.map(event => event.isUserInput)).toEqual([false]);
+
+          subscription.unsubscribe();
+        });
+
     });
 
     describe('forms integration', () => {

commit 72be295ba3493055ebee352ae2a72224648a5f89

fix(material/select): option active styling not removed when value is changed programmatically
If an option is selected by the user and the value is changed programmatically afterwards, we weren't removing the
active state from the previous option. This seems to be a regression from #19970.

These changes resolve the issue and preserve the fix from #19970 by resetting the
active styling manually.

src/material/select/select.ts
@@ -813,16 +813,17 @@ export abstract class _MatSelectBase<C> extends _MatSelectMixinBase implements A
    * found with the designated value, the select trigger is cleared.
    */
   private _setSelectionByValue(value: any | any[]): void {
+    this._selectionModel.selected.forEach(option => option.setInactiveStyles());
+    this._selectionModel.clear();
+
     if (this.multiple && value) {
       if (!Array.isArray(value) && (typeof ngDevMode === 'undefined' || ngDevMode)) {
         throw getMatSelectNonArrayValueError();
       }
 
-      this._selectionModel.clear();
       value.forEach((currentValue: any) => this._selectValue(currentValue));
       this._sortValues();
     } else {
-      this._selectionModel.clear();
       const correspondingOption = this._selectValue(value);
 
       // Shift focus to the active item. Note that we shouldn't do this in multiple

src/material-experimental/mdc-select/select.spec.ts
@@ -1098,6 +1098,39 @@ describe('MDC-based MatSelect', () => {
           expect(options[1].getAttribute('aria-disabled')).toEqual('false');
           expect(options[2].getAttribute('aria-disabled')).toEqual('false');
         }));
+
+        it('should remove the active state from options that have been deselected while closed',
+          fakeAsync(() => {
+            let activeOptions = options.filter(option => {
+              return option.classList.contains('mat-mdc-option-active');
+            });
+            expect(activeOptions).toEqual([options[0]],
+                'Expected first option to have active styles.');
+
+            options[1].click();
+            fixture.detectChanges();
+            trigger.click();
+            fixture.detectChanges();
+            flush();
+
+            activeOptions = options.filter(option => {
+              return option.classList.contains('mat-mdc-option-active');
+            });
+            expect(activeOptions).toEqual([options[1]],
+              'Expected only selected option to be marked as active after it is clicked.');
+
+            fixture.componentInstance.control.setValue(fixture.componentInstance.foods[7].value);
+            fixture.detectChanges();
+            trigger.click();
+            fixture.detectChanges();
+            flush();
+
+            activeOptions = options.filter(option => {
+              return option.classList.contains('mat-mdc-option-active');
+            });
+            expect(activeOptions).toEqual([options[7]],
+              'Expected only selected option to be marked as active after the value has changed.');
+          }));
       });
 
       describe('for option groups', () => {

src/material/select/select.spec.ts
@@ -1066,6 +1066,33 @@ describe('MatSelect', () => {
           expect(options[1].getAttribute('aria-disabled')).toEqual('false');
           expect(options[2].getAttribute('aria-disabled')).toEqual('false');
         }));
+
+        it('should remove the active state from options that have been deselected while closed',
+          fakeAsync(() => {
+            let activeOptions = options.filter(option => option.classList.contains('mat-active'));
+            expect(activeOptions).toEqual([options[0]],
+                'Expected first option to have active styles.');
+
+            options[1].click();
+            fixture.detectChanges();
+            trigger.click();
+            fixture.detectChanges();
+            flush();
+
+            activeOptions = options.filter(option => option.classList.contains('mat-active'));
+            expect(activeOptions).toEqual([options[1]],
+              'Expected only selected option to be marked as active after it is clicked.');
+
+            fixture.componentInstance.control.setValue(fixture.componentInstance.foods[7].value);
+            fixture.detectChanges();
+            trigger.click();
+            fixture.detectChanges();
+            flush();
+
+            activeOptions = options.filter(option => option.classList.contains('mat-active'));
+            expect(activeOptions).toEqual([options[7]],
+              'Expected only selected option to be marked as active after the value has changed.');
+          }));
       });
 
       describe('for option groups', () => {