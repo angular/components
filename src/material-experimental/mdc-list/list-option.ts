22bd6935a61ccf4904af2cb463fc972fed5128c7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-09 08:45:44 +0200

    fix(material-experimental/mdc-list): make internal input non-interactive (#21438)
    Due to how MDC's checkbox styles are set up, we always have to keep an `input` inside
    the `mat-list-option`, however our current setup where we only set `tabindex="-1"` on it
    is invalid, because the input is still interactive either through clicking on it, or by using
    a screen reader's forms mode.
    
    These changes completely remove the input from the accessibility model by setting it
    to `display: none`.
    
    (cherry picked from commit c268246c05409df3f9df8496305f8bf57c624eae)

--- src/material-experimental/mdc-list/list-option.ts
+++ src/material-experimental/mdc-list/list-option.ts
@@ -50,9 +50,6 @@ export interface SelectionList extends MatListBase {
   _onTouched: () => void;
 }
 
-/** Unique id for created list options. */
-let uniqueId = 0;
-
 @Component({
   selector: 'mat-list-option',
   exportAs: 'matListOption',
@@ -85,9 +82,6 @@ export class MatListOption extends MatListItemBase implements OnInit, OnDestroy 
   @ViewChild('text') _itemText: ElementRef<HTMLElement>;
   @ContentChildren(MatLine, {read: ElementRef, descendants: true}) lines:
     QueryList<ElementRef<Element>>;
-
-  /** Unique id for the text. Used for describing the underlying checkbox input. */
-  _optionTextId: string = `mat-mdc-list-option-text-${uniqueId++}`;
 
   /** Whether the label should appear before or after the checkbox. Defaults to 'after' */
   @Input() checkboxPosition: 'before' | 'after' = 'after';
