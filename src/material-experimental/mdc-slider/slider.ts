5cbe03cf8d7f60930c2604c131d14be1c2d49ac5
wagnermaciel <wagnermaciel@google.com>
2021-01-20 08:13:05 -0800

    feat(material-experimental/mdc-slider): add skeleton code for MatSliderAdapter
    * create slider-adapter.ts
    * add method stubs for MDCSliderAdapter implementation
    * add MDCSliderFoundation class variable to MatSlider

--- src/material-experimental/mdc-slider/slider.ts
+++ src/material-experimental/mdc-slider/slider.ts
@@ -6,8 +6,18 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import {MDCSliderFoundation} from '@material/slider';
+import {SliderAdapter} from './slider-adapter';
 
+/**
+ * Allows users to select from a range of values by moving the slider thumb. It is similar in
+ * behavior to the native `<input type="range">` element.
+ */
 @Component({
   selector: 'mat-slider',
   templateUrl: 'slider.html',
@@ -15,4 +25,10 @@ import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/co
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class MatSlider {}
+export class MatSlider {
+
+  /** Instance of the MDC slider foundation for this slider. */
+
+  // tslint:disable-next-line:no-unused-variable
+  private _foundation = new MDCSliderFoundation(new SliderAdapter());
+}
