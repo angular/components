a2ed2a7f6a5fe33e235bb7b66d7540ce7fa13a28
Michael Prentice <splaktar@gmail.com>
2021-01-29 15:54:00 -0500

    feat: add CSP and recommended security headers to site
    - update cache ages for different types of assets
      - assets 6 months, CDN 1 year
      - js/css that is hashed per build, 1 year
      - webmanifest/ico 7 days, CDN 14 days
      - CLI auto-inlined SVG in root dir 1 year
    
    Relates to https://github.com/angular/angular/issues/37631

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/tools/audit-docs.js
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/tools/audit-docs.js
@@ -32,7 +32,7 @@ const MIN_SCORES_PER_PAGE = [
     url: '',
     minScores: {
       'pwa': 70,
-      'performance': 20,
+      'performance': 30,
       'seo': 98,
       'best-practices': 100,
       'accessibility': 100
