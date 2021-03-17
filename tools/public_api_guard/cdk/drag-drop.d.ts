3a8e5f7c783a2132bdbf98624527704284215512
Rafael Santana <rafaelsantana3095@gmail.com>
2021-03-17 20:12:45 -0300

    build: enforce consistent readonly array type

--- tools/public_api_guard/cdk/drag-drop.d.ts
+++ tools/public_api_guard/cdk/drag-drop.d.ts
@@ -395,7 +395,7 @@ export declare class DropListRef<T = any> {
     enter(item: DragRef, pointerX: number, pointerY: number, index?: number): void;
     exit(item: DragRef): void;
     getItemIndex(item: DragRef): number;
-    getScrollableParents(): ReadonlyArray<HTMLElement>;
+    getScrollableParents(): readonly HTMLElement[];
     isDragging(): boolean;
     isReceiving(): boolean;
     start(): void;
