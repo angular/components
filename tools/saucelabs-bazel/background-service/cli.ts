945cc36fc52eea10a6b58e95b611ee679d0e2d2b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-04-28 20:27:01 +0200

    fixup! Improve debugging of acquired browser tests
    ds

--- tools/saucelabs-bazel/background-service/cli.ts
+++ tools/saucelabs-bazel/background-service/cli.ts
@@ -7,6 +7,7 @@ const {platformMap, customLaunchers} = require('../../../test/browser-providers'
 const username = process.env.SAUCE_USERNAME;
 const accessKey = process.env.SAUCE_ACCESS_KEY;
 const tunnelIdentifier = process.env.SAUCE_TUNNEL_IDENTIFIER;
+const buildName = process.env.CIRCLE_BUILD_NUM;
 
 if (!username || !accessKey) {
   throw Error('Please set the `SAUCE_USERNAME` and `SAUCE_ACCESS_KEY` variables.');
@@ -14,6 +15,10 @@ if (!username || !accessKey) {
 
 if (!tunnelIdentifier) {
   throw Error('No tunnel set up. Please set the `SAUCE_TUNNEL_IDENTIFIER` variable.');
+}
+
+if (!buildName) {
+  throw Error('No build name specified. Set the `CIRCLE_BUILD_NUM` variable.');
 }
 
 const saucelabsBrowsers: Browser[] = platformMap.saucelabs
@@ -25,5 +30,5 @@ for (let i = 0; i < parallelExecutions; i++) {
 }
 
 // Start the daemon and launch the given browser.
-new SaucelabsDaemon(username, accessKey, {tunnelIdentifier})
+new SaucelabsDaemon(username, accessKey, process.env.CIRCLE_BUILD_NUM!, {tunnelIdentifier})
   .launchBrowsers(browserInstances);
