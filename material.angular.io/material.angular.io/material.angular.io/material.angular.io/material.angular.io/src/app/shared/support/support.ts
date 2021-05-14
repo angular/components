8738eecc37b29cb7f8047e83adb5dc6915d00b05
kian <kian23kpt@gmail.com>
2021-05-14 05:27:29 +0430

    feat: add help/support section (#915)
    - add links for additional help/support resources in homePage
      - add twitter, angular event and angular blog
    
    Fixes #133
    
    Co-authored-by: Michael Prentice <splaktar@gmail.com>

---
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/shared/support/support.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-support',
+  templateUrl: './support.html',
+  styleUrls: ['./support.scss']
+})
+export class Support {
+}
