0bba6eabd7b5ea4cc27ac3b1ac39baedf687a44e
ByzantineFailure <byzanitnefailure@gmail.com>
2021-06-07 14:02:19 -0700

    fix(material/input): Do not set `aria-invalid` on required empty `matInput`s (#22802)
    Updates the logic for setting `aria-invalid` on `matInput` to not set
    the attribute at all if the input is `required` and has no value.
    
    Prior to this PR `matInput` sets `aria-invalid="false"` for any empty
    `matInput`, including `required` ones.  This suppresses screen readers'
    announcement to users that such inputs are in an invalid state.
    
    Fixes #22777
    
    (cherry picked from commit d1852940d2597a45785da5581ac652f170e8b4d8)

--- src/material/input/input.ts
+++ src/material/input/input.ts
@@ -86,7 +86,7 @@ const _MatInputMixinBase: CanUpdateErrorStateCtor & typeof MatInputBase =
     '[attr.readonly]': 'readonly && !_isNativeSelect || null',
     // Only mark the input as invalid for assistive technology if it has a value since the
     // state usually overlaps with `aria-required` when the input is empty and can be redundant.
-    '[attr.aria-invalid]': 'errorState && !empty',
+    '[attr.aria-invalid]': '(empty && required) ? null : errorState',
     '[attr.aria-required]': 'required',
   },
   providers: [{provide: MatFormFieldControl, useExisting: MatInput}],
