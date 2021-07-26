0da7125e0285c696066f295ec52c71ebc2dd56c2
rameshworsht <rameshworsht@gmail.com>
2021-07-26 18:56:03 -0400

    docs(material/card): add more examples
    Introduce more examples with actions, footer, image size, and subtitles.

--- src/components-examples/material/card/index.ts
+++ src/components-examples/material/card/index.ts
@@ -4,17 +4,29 @@ import {MatCardModule} from '@angular/material/card';
 import {CardFancyExample} from './card-fancy/card-fancy-example';
 import {CardOverviewExample} from './card-overview/card-overview-example';
 import {CardHarnessExample} from './card-harness/card-harness-example';
+import {CardActionsExample} from './card-actions/card-actions-example';
+import {CardMediaSizeExample} from './card-media-size/card-media-size-example';
+import {CardSubtitleExample} from './card-subtitle/card-subtitle-example';
+import {CardFooterExample} from './card-footer/card-footer-example';
 
 export {
   CardFancyExample,
   CardOverviewExample,
   CardHarnessExample,
+  CardActionsExample,
+  CardMediaSizeExample,
+  CardSubtitleExample,
+  CardFooterExample
 };
 
 const EXAMPLES = [
   CardFancyExample,
   CardOverviewExample,
   CardHarnessExample,
+  CardActionsExample,
+  CardMediaSizeExample,
+  CardSubtitleExample,
+  CardFooterExample
 ];
 
 @NgModule({
