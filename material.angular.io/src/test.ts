73a5c978dfee97a52c650a2809b63f3e43142970
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-08-31 12:26:49 +0200

    build: enable unit test teardown (#1059)
    Opts the docs site into the new test module teardown behavior.

--- material.angular.io/src/test.ts
+++ material.angular.io/src/test.ts
@@ -12,7 +12,8 @@ declare const require: any;
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+  platformBrowserDynamicTesting(),
+  {teardown: {destroyAfterEach: true}}
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
