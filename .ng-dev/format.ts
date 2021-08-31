e6267fca191cb605ed764e705ebcd4b137d1238d
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-08-31 13:33:33 +0200

    build: enable clang-format in ng-dev format command
    Enables `clang-format` in the `ng-dev format` command.

--- .ng-dev/format.ts
+++ .ng-dev/format.ts
@@ -6,4 +6,19 @@ import {FormatConfig} from '@angular/dev-infra-private/ng-dev/format/config';
  */
 export const format: FormatConfig = {
   buildifier: true,
+  'clang-format': {
+    matchers: [
+      '**/*.{js,ts}',
+      // These schematic ng-generate files are template files that cannot be formatted.
+      '!src/material/schematics/ng-generate/*/files/**/*',
+      '!src/cdk/schematics/ng-generate/*/files/**/*',
+
+      // Schematic ng-update test cases should not be formatted as that could break the
+      // expected output tests. Generated code does is not formatted according to prettier.
+      '!src/cdk/schematics/ng-update/test-cases/**/*_input.ts',
+      '!src/cdk/schematics/ng-update/test-cases/**/*_expected_output.ts',
+      '!src/material/schematics/ng-update/test-cases/**/*_input.ts',
+      '!src/material/schematics/ng-update/test-cases/**/*_expected_output.ts',
+    ],
+  },
 };
