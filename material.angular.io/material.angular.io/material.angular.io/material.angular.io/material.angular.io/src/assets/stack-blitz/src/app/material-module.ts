b66fb6f4eef170a64d31998e8044c79fc8a1a915
Charles Barnes <charles@charlesabarnes.com>
2021-09-04 14:20:47 -0500

    fix: include cdkAccordionModule in material-module for accordion stackblitz demo (#1060)

--- material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/assets/stack-blitz/src/app/material-module.ts
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/assets/stack-blitz/src/app/material-module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {A11yModule} from '@angular/cdk/a11y';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
 import {ClipboardModule} from '@angular/cdk/clipboard';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {PortalModule} from '@angular/cdk/portal';
@@ -46,6 +47,7 @@ import {OverlayModule} from '@angular/cdk/overlay';
 @NgModule({
   exports: [
     A11yModule,
+    CdkAccordionModule,
     ClipboardModule,
     CdkStepperModule,
     CdkTableModule,
