905438fa3a88557c554369816244a13dc799f249
Kristiyan Kostadinov <crisbeto@abv.bg>
2021-09-04 11:02:40 +0200

    refactor(material/core): remove deprecated APIs for v13
    Cleans up the APIs that were marked for removal in v13.
    
    BREAKING CHANGE:
    * `CanColorCtor` is no longer necessary and has been removed.
    * `CanDisableRippleCtor` is no longer necessary and has been removed.
    * `CanDisableCtor` is no longer necessary and has been removed.
    * `CanUpdateErrorStateCtor` is no longer necessary and has been removed.
    * `HasInitializedCtor` is no longer necessary and has been removed.
    * `HasTabIndexCtor` is no longer necessary and has been removed.

--- src/material-experimental/mdc-chips/chip-grid.ts
+++ src/material-experimental/mdc-chips/chip-grid.ts
@@ -30,7 +30,6 @@ import {
 import {ControlValueAccessor, FormGroupDirective, NgControl, NgForm} from '@angular/forms';
 import {
   CanUpdateErrorState,
-  CanUpdateErrorStateCtor,
   ErrorStateMatcher,
   mixinErrorState,
 } from '@angular/material-experimental/mdc-core';
@@ -68,8 +67,7 @@ class MatChipGridBase extends MatChipSet {
     super(_elementRef, _changeDetectorRef, _dir);
   }
 }
-const _MatChipGridMixinBase: CanUpdateErrorStateCtor & typeof MatChipGridBase =
-    mixinErrorState(MatChipGridBase);
+const _MatChipGridMixinBase = mixinErrorState(MatChipGridBase);
 
 /**
  * An extension of the MatChipSet component used with MatChipRow chips and
