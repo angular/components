5f889b63d610aa204875749934b2e56c8dc7ec36
Brandon Yanofsky <byanofsky@me.com>
2021-09-24 19:58:24 +0000

    Enable defiintg custom label in list label

--- src/material/tabs/tab-list.ts
+++ src/material/tabs/tab-list.ts
@@ -11,6 +11,7 @@ import {BooleanInput, NumberInput} from '@angular/cdk/coercion';
 import {
   ChangeDetectionStrategy,
   Component,
+  ContentChild,
   ContentChildren,
   Directive,
   ElementRef,
@@ -30,12 +31,13 @@ import {
   mixinDisableRipple
 } from '@angular/material/core';
 
-import {MatTabLabel} from './tab-label';
+import {MAT_TAB_LABEL, MatTabLabel} from './tab-label';
 
-// Boilerplate for applying mixins to MatTab.
+// Boilerplate for applying mixins to MatTabListLabel.
 /** @docs-private */
 const _MatTabListLabelMixinBase = mixinDisabled(class {});
 
+/** Directive for passing tab labels to tab list. */
 @Directive({
   selector: 'mat-tab-list-label, matTabListLabel',
   inputs: ['disabled'],
@@ -49,11 +51,39 @@ export class MatTabListLabel extends _MatTabListLabelMixinBase {
    */
   @Input() ariaLabelledby: string;
 
+  // TODO: Deduplicate with tab
   /** Content for the tab label given by `<ng-template mat-tab-label>`. */
-  @Input() templateLabel: MatTabLabel;
+  @ContentChild(MAT_TAB_LABEL)
+  get templateLabel(): MatTabLabel {
+    return this._templateLabelInput || this._templateLabel;
+  }
+  set templateLabel(value: MatTabLabel) {
+    this._setTemplateLabelInput(value);
+  }
+  protected _templateLabel: MatTabLabel;
+
+  /** Content for tab label passed as input. */
+  @Input('templateLabel') _templateLabelInput?: MatTabLabel;
 
   /** Plain text label for the tab label, used when there is no template label. */
   @Input() textLabel: string = '';
+
+  // TODO: Dedpulicate with tab
+  /**
+   * This has been extracted to a util because of TS 4 and VE.
+   * View Engine doesn't support property rename inheritance.
+   * TS 4.0 doesn't allow properties to override accessors or vice-versa.
+   * @docs-private
+   */
+  protected _setTemplateLabelInput(value: MatTabLabel) {
+    // Only update the templateLabel via query if there is actually
+    // a MatTabLabel found. This works around an issue where a user may have
+    // manually set `templateLabel` during creation mode, which would then get clobbered
+    // by `undefined` when this query resolves.
+    if (value) {
+      this._templateLabel = value;
+    }
+  }
 }
 
 // Boilerplate for applying mixins to MatTabList.
