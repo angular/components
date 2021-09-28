0c16fbfce35b0398943cd6a973fab64bc2a88683
Vasya Yuhas <drakka@meta.ua>
2021-09-29 00:25:00 +0300

    fix(cdk/drag-drop): error when preview is destroyed before animation completes

--- src/cdk/drag-drop/drag-ref.ts
+++ src/cdk/drag-drop/drag-ref.ts
@@ -1097,7 +1097,9 @@ export class DragRef<T = any> {
         const handler = ((event: TransitionEvent) => {
           if (!event || (_getEventTarget(event) === this._preview &&
               event.propertyName === 'transform')) {
-            this._preview.removeEventListener('transitionend', handler);
+            if (this._preview) {
+              this._preview.removeEventListener('transitionend', handler);
+            }
             resolve();
             clearTimeout(timeout);
           }
