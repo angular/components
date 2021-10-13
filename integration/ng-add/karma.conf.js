7fc2307d2d5954e207d8a788de0d5db7d0303082
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-10-13 19:30:37 +0200

    test: setup integration test for running `ng add`
    Sets up the integration test for running `ng add` to run
    with Bazel. The test is also run with NodeJS v12 which does
    not support ES2020 output.

--- integration/ng-add/karma.conf.js
+++ integration/ng-add/karma.conf.js
@@ -1,6 +1,11 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+// This code runs within Bazel where the environment does not have access to
+// the system Chrome browser. To workaround this we use Puppeteer to provide
+// a local version of Chromium that can run within Bazel.
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
