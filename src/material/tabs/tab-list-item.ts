05b2acdccf5706aa427d57db28277583a14c21fd
Brandon Yanofsky <byanofsky@me.com>
2021-10-20 20:42:55 +0000

    refactor(material/tabs): remove content proj
    MatTabListItem is directive attached to ng-template, not component.

--- src/material/tabs/tab-list-item.ts
+++ src/material/tabs/tab-list-item.ts
@@ -6,14 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  TemplateRef,
-  ViewChild,
-  ViewEncapsulation,
-} from '@angular/core';
+import {Directive, Input, TemplateRef} from '@angular/core';
 import {mixinDisabled} from '@angular/material/core';
 
 // Boilerplate for applying mixins to MatTabListItem.
@@ -21,17 +14,11 @@ import {mixinDisabled} from '@angular/material/core';
 const _MatTabListItemMixinBase = mixinDisabled(class {});
 
 /** Directive for passing tab list items to tab list. */
-@Component({
-  selector: 'mat-tab-list-item',
-  templateUrl: 'tab-list-item.html',
+@Directive({
+  selector: '[mat-tab-list-item], [matTabListItem]',
   inputs: ['disabled'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None,
 })
 export class MatTabListItem extends _MatTabListItemMixinBase {
-  /** Template inside the MatTabListItem view that contains an `<ng-content>`. */
-  @ViewChild(TemplateRef, {static: true}) _template: TemplateRef<void>;
-
   /** Aria label for the tab list item. */
   @Input() ariaLabel: string;
 
@@ -39,4 +26,8 @@ export class MatTabListItem extends _MatTabListItemMixinBase {
    * Reference to the element that the tab list item is labelled by.
    */
   @Input() ariaLabelledby: string;
+
+  constructor(public _template: TemplateRef<void>) {
+    super();
+  }
 }
