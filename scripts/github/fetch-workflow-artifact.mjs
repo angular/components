b29a33d9a27e2f6b06455321e308324c4010548f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-10-25 15:02:39 +0200

    fixup! build: setup preview builds for dev-app
    Address feedback

--- scripts/github/fetch-workflow-artifact.mjs
+++ scripts/github/fetch-workflow-artifact.mjs
@@ -8,21 +8,21 @@
  *   ./fetch-workflow-artifact.js <gh-token> <workflow-id> <artifact-name>
  */
 
-const {Octokit} = require('@octokit/rest');
+import octokit from '@octokit/rest';
 
 async function main() {
   const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/', 2);
   const [token, workflowId, artifactName] = process.argv.slice(2);
-  const github = new Octokit({auth: token});
+  const github = new octokit.Octokit({auth: token});
   const artifacts = await github.actions.listWorkflowRunArtifacts({
     owner,
     repo,
     run_id: workflowId,
   });
 
-  const matchArtifact = artifacts.data.artifacts.filter(
+  const matchArtifact = artifacts.data.artifacts.find(
     artifact => artifact.name === artifactName,
-  )[0];
+  );
 
   const download = await github.actions.downloadArtifact({
     owner,
@@ -34,7 +34,4 @@ async function main() {
   process.stdout.write(Buffer.from(download.data));
 }
 
-main().catch(e => {
-  console.error(e);
-  process.exitCode = 1;
-});
+await main();
