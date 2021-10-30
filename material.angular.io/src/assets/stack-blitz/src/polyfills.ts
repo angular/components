f06098acf4762e3cb6c90f76ea3f2204640b563f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-10-30 17:28:53 +0200

    refactor: update stackblitz CLI asset to match with CLI rc.2 (#1094)

--- material.angular.io/src/assets/stack-blitz/src/polyfills.ts
+++ material.angular.io/src/assets/stack-blitz/src/polyfills.ts
@@ -8,8 +8,8 @@
  *      file.
  *
  * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
- * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
- * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
+ * automatically update themselves. This includes recent versions of Safari, Chrome (including
+ * Opera), Edge on the desktop, and iOS and Chrome on mobile.
  *
  * Learn more in https://angular.io/guide/browser-support
  */
