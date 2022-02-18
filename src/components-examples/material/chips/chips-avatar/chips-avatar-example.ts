f05b469565b07a7d23adc6afcdb8ddba7ab507c7
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2022-02-18 21:37:18 +0330

    docs(material/chips): add chips avatar sample
    resolves angular#22905

---
+++ src/components-examples/material/chips/chips-avatar/chips-avatar-example.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Chips avatar
+ */
+@Component({
+  selector: 'chips-avatar-example',
+  templateUrl: 'chips-avatar-example.html',
+  styleUrls: ['chips-avatar-example.css'],
+})
+export class ChipsAvatarExample {}
