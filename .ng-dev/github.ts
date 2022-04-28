9a6f32705173b2ff22307bdba409f7d0127c41e1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-04-28 19:48:28 +0200

    build: changes for renaming primary branch to `main` [12.2.x] (#24830)
    Changes of the `DIRECT` phase for the "renaming master to
    main" migration/planning doc.

--- .ng-dev/github.ts
+++ .ng-dev/github.ts
@@ -7,5 +7,5 @@ import {GithubConfig} from '@angular/dev-infra-private/ng-dev/utils/config';
 export const github: GithubConfig = {
   owner: 'angular',
   name: 'components',
-  mainBranchName: 'master',
+  mainBranchName: 'main',
 };
