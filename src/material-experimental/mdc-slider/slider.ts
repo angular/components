angular/components

commit 43bfbac51514dcb64fc227512adfdadf84567f31

fix(material-experimental/mdc-slider): thumb incorrectly positioned when inside an overlay (#25288)
Fixes an issue where the thumb of an MDC slider wasn't positioned correctly when it has an initial value inside an overlay that is being animated.

There are multiple ways to resolve this, but I ended up doing it by removing our logic that skips the first `ResizeObserver` emission since we can get around the performance issues with the `layout` call by reusing the dimensions provided in the callback.

Fixes #25286.

(cherry picked from commit fb4d7a06de0ee884f1d2e204f9da5f5417160075)

src/material-experimental/mdc-slider/slider.ts
@@ -702,6 +702,9 @@ export class MatSlider
   /** Timeout used to debounce resize listeners. */
   private _resizeTimer: number;
 
+  /** Cached dimensions of the host element. */
+  private _cachedHostRect: DOMRect | null;
+
   constructor(
     readonly _ngZone: NgZone,
     readonly _cdr: ChangeDetectorRef,
@@ -941,6 +944,11 @@ export class MatSlider
     return this.disabled || this.disableRipple || !!this._globalRippleOptions?.disabled;
   }
 
+  /** Gets the dimensions of the host element. */
+  _getHostDimensions() {
+    return this._cachedHostRect || this._elementRef.nativeElement.getBoundingClientRect();
+  }
+
   /** Starts observing and updating the slider if the host changes its size. */
   private _observeHostResize() {
     if (typeof ResizeObserver === 'undefined' || !ResizeObserver) {
@@ -949,18 +957,18 @@ export class MatSlider
 
     // MDC only updates the slider when the window is resized which
     // doesn't capture changes of the container itself. We use a resize
-    // observer to ensure that the layout is correct (see #24590).
+    // observer to ensure that the layout is correct (see #24590 and #25286).
     this._ngZone.runOutsideAngular(() => {
-      // The callback will fire as soon as an element is observed and
-      // we only want to know after the initial layout.
-      let hasResized = false;
-      this._resizeObserver = new ResizeObserver(() => {
-        if (hasResized) {
-          // Debounce the layouts since they can happen frequently.
-          clearTimeout(this._resizeTimer);
-          this._resizeTimer = setTimeout(this._layout, 50);
-        }
-        hasResized = true;
+      this._resizeObserver = new ResizeObserver(entries => {
+        clearTimeout(this._resizeTimer);
+        this._resizeTimer = setTimeout(() => {
+          // The `layout` call is going to call `getBoundingClientRect` to update the dimensions
+          // of the host. Since the `ResizeObserver` already calculated them, we can save some
+          // work by returning them instead of having to check the DOM again.
+          this._cachedHostRect = entries[0]?.contentRect;
+          this._layout();
+          this._cachedHostRect = null;
+        }, 50);
       });
       this._resizeObserver.observe(this._elementRef.nativeElement);
     });
@@ -1130,7 +1138,7 @@ class SliderAdapter implements MDCSliderAdapter {
     return this._delegate._getThumbElement(thumbPosition).getBoundingClientRect();
   };
   getBoundingClientRect = (): DOMRect => {
-    return this._delegate._elementRef.nativeElement.getBoundingClientRect();
+    return this._delegate._getHostDimensions();
   };
   getValueIndicatorContainerWidth = (thumbPosition: Thumb): number => {
     return this._delegate._getValueIndicatorContainerElement(thumbPosition).getBoundingClientRect()

commit 4cb5c4752a2992708da03d9b4ef4715ae5d0e703

fix(material-experimental/mdc-slider): skip resizing while the user is dragging
Fixes an internal bug caused by #25288 where a slider was resizing while the user is dragging, causing the thumb to jump around.

src/material-experimental/mdc-slider/slider.ts
@@ -127,7 +127,7 @@ export class MatSliderVisualThumb implements AfterViewInit, OnDestroy {
   private _activeRippleRef: RippleRef | undefined;
 
   /** Whether the slider thumb is currently being pressed. */
-  private _isActive: boolean = false;
+  readonly _isActive = false;
 
   /** Whether the slider thumb is currently being hovered. */
   private _isHovered: boolean = false;
@@ -201,14 +201,14 @@ export class MatSliderVisualThumb implements AfterViewInit, OnDestroy {
 
   private _onDragStart(event: MatSliderDragEvent): void {
     if (event.source._thumbPosition === this.thumbPosition) {
-      this._isActive = true;
+      (this as {_isActive: boolean})._isActive = true;
       this._showActiveRipple();
     }
   }
 
   private _onDragEnd(event: MatSliderDragEvent): void {
     if (event.source._thumbPosition === this.thumbPosition) {
-      this._isActive = false;
+      (this as {_isActive: boolean})._isActive = false;
       this._activeRippleRef?.fadeOut();
       // Happens when the user starts dragging a thumb, tabs away, and then stops dragging.
       if (!this._sliderInput._isFocused()) {
@@ -960,18 +960,30 @@ export class MatSlider
     // observer to ensure that the layout is correct (see #24590 and #25286).
     this._ngZone.runOutsideAngular(() => {
       this._resizeObserver = new ResizeObserver(entries => {
+        // Triggering a layout while the user is dragging can throw off the alignment.
+        if (this._isActive()) {
+          return;
+        }
+
         clearTimeout(this._resizeTimer);
         this._resizeTimer = setTimeout(() => {
           // The `layout` call is going to call `getBoundingClientRect` to update the dimensions
           // of the host. Since the `ResizeObserver` already calculated them, we can save some
           // work by returning them instead of having to check the DOM again.
-          this._cachedHostRect = entries[0]?.contentRect;
-          this._layout();
-          this._cachedHostRect = null;
+          if (!this._isActive()) {
+            this._cachedHostRect = entries[0]?.contentRect;
+            this._layout();
+            this._cachedHostRect = null;
+          }
         }, 50);
       });
       this._resizeObserver.observe(this._elementRef.nativeElement);
     });
+  }
+
+  /** Whether any of the thumbs are currently active. */
+  private _isActive(): boolean {
+    return this._getThumb(Thumb.START)._isActive || this._getThumb(Thumb.END)._isActive;
   }
 }
 