4a7296b67440b1f09724f914748341f566b7c6f8
Wagner Maciel <wagnermaciel@google.com>
2022-08-03 23:13:20 +0000

    feat(material/schematics): v15 ng-update scss migration

---
+++ src/material/schematics/ng-update/migrations/legacy-components-v15/constants.ts
@@ -0,0 +1,28 @@
+export const COMPONENTS = [
+  'autocomplete',
+  'button',
+  'card',
+  'checkbox',
+  'chips',
+  'dialog',
+  'form-field',
+  'input',
+  'menu',
+  'paginator',
+  'progress-bar',
+  'progress-spinner',
+  'radio',
+  'select',
+  'slide-toggle',
+  'snack-bar',
+  'table',
+  'tabs',
+  'tooltip',
+];
+
+export const MIXINS = COMPONENTS.flatMap(component => [
+  `${component}-theme`,
+  `${component}-color`,
+  `${component}-density`,
+  `${component}-typography`,
+]);
