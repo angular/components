4ab4dbfbf889a212d64769ff56cb892a7f49ee36
Wagner Maciel <wagnermaciel@google.com>
2022-08-19 21:30:10 +0000

    fixup! fix(material/schematics): make ts import replacements more specific

--- src/material/schematics/ng-update/migrations/legacy-components-v15/constants.ts
+++ src/material/schematics/ng-update/migrations/legacy-components-v15/constants.ts
@@ -9,6 +9,7 @@
 export const COMPONENTS = [
   'autocomplete',
   'button',
+  'core',
   'card',
   'checkbox',
   'chips',
@@ -40,7 +41,7 @@ export const MDC_IMPORT_CHANGES = COMPONENTS.map(component => ({
   new: `@angular/material/${component}`,
 }));
 
-export const MIXINS = COMPONENTS.concat(['option', 'optgroup', 'core']).flatMap(component => [
+export const MIXINS = COMPONENTS.concat(['option', 'optgroup']).flatMap(component => [
   `${component}-theme`,
   `${component}-color`,
   `${component}-density`,
