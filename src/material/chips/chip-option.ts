8643c9ccb56bc6c2a4306ffb8ecb5db25adac1d5
Zach Arend <zarend@users.noreply.github.com>
2022-11-03 08:01:33 -0700

    fix(material/chips): use checkmark graphic for single-selection (#25890)
    For mat-chip-listbox, use the checkmark component for single-selection.
    Fixes issue where selected state is communicated visually only using
    color (#25886).
    
    (cherry picked from commit a999a52bb967608b4ec59729cf720ade9db274d8)

--- src/material/chips/chip-option.ts
+++ src/material/chips/chip-option.ts
@@ -164,8 +164,10 @@ export class MatChipOption extends MatChip implements OnInit {
   }
 
   _hasLeadingGraphic() {
-    // The checkmark graphic is built in for multi-select chip lists.
-    return this.leadingIcon || (this._chipListMultiple && this.selectable);
+    // The checkmark graphic communicates selected state for both single-select and multi-select.
+    // Include checkmark in single-select to fix a11y issue where selected state is communicated
+    // visually only using color (#25886).
+    return this.leadingIcon || this.selectable;
   }
 
   _setSelectedState(isSelected: boolean, isUserInput: boolean, emitEvent: boolean) {
