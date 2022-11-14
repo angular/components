06ddcc2c5fcbea2b8a9a36b9a7f013683789800a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-11-14 18:57:13 +0000

    docs: add example for variants of mdc list

---
+++ src/components-examples/material/list/list-variants/list-variants-example.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title List variants
+ */
+@Component({
+  selector: 'list-variants-example',
+  templateUrl: 'list-variants-example.html',
+  styleUrls: ['./list-variants-example.css'],
+})
+export class ListVariantsExample {}
