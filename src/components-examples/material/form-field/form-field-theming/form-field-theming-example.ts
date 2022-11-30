906f8cd9803d3fb92d8c0cd49c3f22bad1970fdb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2022-11-30 11:34:18 +0100

    docs(material/form-field): remove font size example (#26129)
    We don't support having the form field inherit the font size with the MDC version. These changes remove the example from the docs.
    
    Fixes #26091.

--- src/components-examples/material/form-field/form-field-theming/form-field-theming-example.ts
+++ src/components-examples/material/form-field/form-field-theming/form-field-theming-example.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {NonNullableFormBuilder, Validators} from '@angular/forms';
+import {FormControl} from '@angular/forms';
 import {ThemePalette} from '@angular/material/core';
 
 /** @title Form field theming */
@@ -9,14 +9,5 @@ import {ThemePalette} from '@angular/material/core';
   styleUrls: ['form-field-theming-example.css'],
 })
 export class FormFieldThemingExample {
-  options = this._formBuilder.group({
-    color: this._formBuilder.control('primary' as ThemePalette),
-    fontSize: this._formBuilder.control(16, Validators.min(10)),
-  });
-
-  constructor(private _formBuilder: NonNullableFormBuilder) {}
-
-  getFontSize() {
-    return Math.max(10, this.options.value.fontSize || 0);
-  }
+  colorControl = new FormControl('primary' as ThemePalette);
 }
