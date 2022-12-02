2477539d92a9cf2f4903d1c05d8ce403ff540fcc
Wagner Maciel <wagnermaciel@google.com>
2022-12-02 15:18:41 +0000

    refactor(material/progress-spinner): deprecate MatSpinner (#26108)
    * refactor(material/progress-spinner): deprecate MatSpinner
    
    * fixup! refactor(material/progress-spinner): deprecate MatSpinner

--- src/material/progress-spinner/progress-spinner.ts
+++ src/material/progress-spinner/progress-spinner.ts
@@ -198,10 +198,9 @@ export class MatProgressSpinner extends _MatProgressSpinnerBase implements CanCo
 }
 
 /**
- * `<mat-spinner>` component.
- *
- * This is a component definition to be used as a convenience reference to create an
- * indeterminate `<mat-progress-spinner>` instance.
+ * @deprecated Import Progress Spinner instead. Note that the
+ *    `mat-spinner` selector isn't deprecated.
+ * @breaking-change 16.0.0
  */
 // tslint:disable-next-line:variable-name
 export const MatSpinner = MatProgressSpinner;
