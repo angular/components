9113309c2c2d809c7a65c954604d80dd44383c00
Wagner Maciel <wagnermaciel@google.com>
2022-12-12 17:03:38 -0500

    fixup! fix(material/slider): avoid manual fixing values on pointer up

--- src/material/slider/slider.spec.ts
+++ src/material/slider/slider.spec.ts
@@ -1686,7 +1686,7 @@ function setValueByClick(
   input.focus();
   dispatchPointerEvent(inputElement, 'pointerup', x, y);
   dispatchEvent(input._hostElement, new Event('change'));
-  tick(10);
+  tick();
 }
 
 /** Slides the MatSlider's thumb to the given value. */
@@ -1702,7 +1702,7 @@ function slideToValue(slider: MatSlider, input: MatSliderThumb, value: number) {
   dispatchEvent(input._hostElement, new Event('input'));
   dispatchPointerEvent(sliderElement, 'pointerup', endX, endY);
   dispatchEvent(input._hostElement, new Event('change'));
-  tick(10);
+  tick();
 }
 
 /** Returns the x and y coordinates for the given slider value. */
