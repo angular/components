15648802d00341da251e8b8b9da7de8094737696
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2023-01-28 09:47:27 +0100

    fix(google-maps): error if API is initialized incorrectly (#26520)
    When the Google Maps API fails to initialize (e.g. when the API key is invalid), it returns dummy objects which return `undefined` from `addListener`, causing an error when the `MapEventManager` is destroyed.
    
    These changes add a check to guard against those errors.
    
    Fixes #26514.

--- src/google-maps/map-event-manager.spec.ts
+++ src/google-maps/map-event-manager.spec.ts
@@ -141,6 +141,20 @@ describe('MapEventManager', () => {
     alternateTarget.triggerListeners('click');
     expect(spy).toHaveBeenCalledTimes(2);
   });
+
+  it('should not throw with an invalid target', () => {
+    manager.setTarget({
+      addListener: () => undefined,
+    });
+    const stream = manager.getLazyEmitter('click');
+    const completeSpy = jasmine.createSpy('completeSpy');
+    const errorSpy = jasmine.createSpy('errorSpy');
+    stream.subscribe({complete: completeSpy, error: errorSpy});
+
+    expect(() => manager.destroy()).not.toThrow();
+    expect(completeSpy).toHaveBeenCalled();
+    expect(errorSpy).not.toHaveBeenCalled();
+  });
 });
 
 /** Imitates a Google Maps event target and keeps track of the registered events. */
