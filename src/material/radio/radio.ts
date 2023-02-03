75dccfc72fb9e7b27bd9db3bfd69b9b66fa5b7a7
Kristiyan Kostadinov <crisbeto@abv.bg>
2023-02-03 10:58:16 +0100

    fix(material/radio): input not focused when clicking on touch target
    Fixes that the radio button's internal `input` wasn't receiving focus when the touch target is clicked.
    
    Fixes #26487.

--- src/material/radio/radio.ts
+++ src/material/radio/radio.ts
@@ -611,6 +611,17 @@ export abstract class _MatRadioButtonBase
     }
   }
 
+  /** Triggered when the user clicks on the touch target. */
+  _onTouchTargetClick(event: Event) {
+    this._onInputInteraction(event);
+
+    if (!this.disabled) {
+      // Normally the input should be focused already, but if the click
+      // comes from the touch target, then we might have to focus it ourselves.
+      this._inputElement.nativeElement.focus();
+    }
+  }
+
   /** Sets the disabled state and marks for check if a change occurred. */
   protected _setDisabled(value: boolean) {
     if (this._disabled !== value) {
