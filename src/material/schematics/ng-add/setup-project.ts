11697b1d4f35191337b9d29142c3c90d3085a357
Henrique <32647398+HenriqueOliveiraFerraz@users.noreply.github.com>
2023-02-12 19:29:36 -0300

    Update setup.project.ts
    For some reason, when I use the ng new with the --collection argument, the getWorkspace is unable to find the angular.json file and throws an error with the following message:
    
    - Exception has occurred: Error: Path "/angular.json" does not exist.

--- src/material/schematics/ng-add/setup-project.ts
+++ src/material/schematics/ng-add/setup-project.ts
@@ -39,7 +39,7 @@ const noopAnimationsModuleName = 'NoopAnimationsModule';
  */
 export default function (options: Schema): Rule {
   return async (host: Tree, context: SchematicContext) => {
-    const workspace = await getWorkspace(host);
+    const workspace = await getWorkspace(host, options.project);
     const project = getProjectFromWorkspace(workspace, options.project);
 
     if (project.extensions.projectType === ProjectType.Application) {
