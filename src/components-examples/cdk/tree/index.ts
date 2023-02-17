7c4c7dcf05220337e15fc184e5f1fbfd31cee1ea
Cassandra Choi <cassc@google.com>
2023-02-17 16:47:16 -0500

    feat(cdk/tree): add demos to the dev-app

--- src/components-examples/cdk/tree/index.ts
+++ src/components-examples/cdk/tree/index.ts
@@ -3,11 +3,12 @@ import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {CdkTreeFlatExample} from './cdk-tree-flat/cdk-tree-flat-example';
+import {CdkTreeFlatLevelAccessorExample} from './cdk-tree-flat-level-accessor/cdk-tree-flat-level-accessor-example';
 import {CdkTreeNestedExample} from './cdk-tree-nested/cdk-tree-nested-example';
 
-export {CdkTreeFlatExample, CdkTreeNestedExample};
+export {CdkTreeFlatExample, CdkTreeNestedExample, CdkTreeFlatLevelAccessorExample};
 
-const EXAMPLES = [CdkTreeFlatExample, CdkTreeNestedExample];
+const EXAMPLES = [CdkTreeFlatExample, CdkTreeNestedExample, CdkTreeFlatLevelAccessorExample];
 
 @NgModule({
   imports: [CdkTreeModule, MatButtonModule, MatIconModule],
