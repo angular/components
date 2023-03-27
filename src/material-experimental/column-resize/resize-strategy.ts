e78d05889d3bb9b3c70b6def14d0ecfca29f3dcc
Kristiyan Kostadinov <crisbeto@abv.bg>
2023-03-27 11:22:53 +0200

    fix(cdk-experimental/column-resize): avoid CSP errors
    Uses the new `CSP_NONCE` token to avoid errors when inserting `style` tags dynamically in the column resize package.

--- src/material-experimental/column-resize/resize-strategy.ts
+++ src/material-experimental/column-resize/resize-strategy.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {Inject, Injectable, Provider} from '@angular/core';
+import {CSP_NONCE, Inject, Injectable, Optional, Provider} from '@angular/core';
 import {DOCUMENT} from '@angular/common';
 import {CdkTable, _CoalescedStyleScheduler, _COALESCED_STYLE_SCHEDULER} from '@angular/cdk/table';
 
@@ -29,8 +29,9 @@ export class MatFlexTableResizeStrategy extends CdkFlexTableResizeStrategy {
     @Inject(_COALESCED_STYLE_SCHEDULER) styleScheduler: _CoalescedStyleScheduler,
     table: CdkTable<unknown>,
     @Inject(DOCUMENT) document: any,
+    @Inject(CSP_NONCE) @Optional() nonce?: string | null,
   ) {
-    super(columnResize, styleScheduler, table, document);
+    super(columnResize, styleScheduler, table, document, nonce);
   }
 
   protected override getColumnCssClass(cssFriendlyColumnName: string): string {
