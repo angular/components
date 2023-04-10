3adfa9a2c8879f1970e3cb2d6a41b93d1b36d33d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2023-04-10 23:33:04 +0200

    build: update ng add standalone integration test (#26894)
    The layout for a standalone project has changed since we added our integration test.

--- integration/ng-add-standalone/src/main.ts
+++ integration/ng-add-standalone/src/main.ts
@@ -1,14 +1,5 @@
-import {enableProdMode, importProvidersFrom} from '@angular/core';
-import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from './app/app-routing.module';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {appConfig} from './app/app.config';
 import {AppComponent} from './app/app.component';
 
-import {environment} from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, AppRoutingModule)],
-});
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
