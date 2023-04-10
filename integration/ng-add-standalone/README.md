254cc4766312d4174d2ee53611fa10e39e17e639
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2023-04-10 23:33:04 +0200

    build: update ng add standalone integration test (#26894)
    The layout for a standalone project has changed since we added our integration test.

--- integration/ng-add-standalone/README.md
+++ integration/ng-add-standalone/README.md
@@ -1,10 +1,10 @@
 # NgAddStandalone
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.0-next.7.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.0-next.7.
 
 ## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
 ## Code scaffolding
 
--- integration/ng-add-standalone/src/app/app.component.spec.ts
+++ integration/ng-add-standalone/src/app/app.component.spec.ts
@@ -1,13 +1,12 @@
 import {TestBed} from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-    }).compileComponents();
-  });
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }),
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -15,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-add-standalone'`, () => {
+  it(`should have the 'ng-add-standalone' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ng-add-standalone');
