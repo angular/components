2b2daf6293393792f84d6f19cdb8a636e41def67
Kristiyan Kostadinov <crisbeto@abv.bg>
2023-04-20 16:24:48 +0200

    build: remove axe checks from e2e tests
    Removes the code for running Axe against the e2e tests since it wasn't catching many failures. Automated accessibility checks have been set up in g3 which should be a bit closer to what we're looking for during GAR.

--- src/e2e-app/protractor.conf.js
+++ src/e2e-app/protractor.conf.js
@@ -6,32 +6,6 @@ exports.config = {
     defaultTimeoutInterval: 120000,
   },
 
-  plugins: [
-    {
-      // Runs the axe-core accessibility checks each time the e2e page changes and
-      // Angular is ready.
-      path: require.resolve('angular_material/tools/axe-protractor'),
-
-      rules: [
-        // Exclude mat-menu elements because those are empty if not active.
-        {id: 'aria-required-children', selector: '*:not(mat-menu)'},
-
-        // Disable color contrast checks since the final colors will vary based on the theme.
-        {id: 'color-contrast', enabled: false},
-
-        // Don't require all content to be inside landmarks since some tests depend on the
-        // page layout and moving this around can break them.
-        {id: 'region', enabled: false},
-
-        // Don't require at least one `<h1>` since we don't have any content.
-        {id: 'page-has-heading-one', enabled: false},
-
-        // Axe incorrectly picks up that `aria-required` is not allowed on the MDC slide toggle.
-        {id: 'aria-allowed-attr', selector: '*:not(.mdc-switch)'},
-      ],
-    },
-  ],
-
   // Since we want to use async/await we don't want to mix up with selenium's promise
   // manager. In order to enforce this, we disable the promise manager.
   SELENIUM_PROMISE_MANAGER: false,
--- src/e2e-app/test_suite.bzl
+++ src/e2e-app/test_suite.bzl
@@ -4,10 +4,7 @@ def e2e_test_suite(name, data = [], tags = ["e2e"], deps = []):
     protractor_web_test_suite(
         name = name,
         configuration = "//src/e2e-app:protractor.conf.js",
-        data = [
-            "//tools/axe-protractor",
-            "@npm//@axe-core/webdriverjs",
-        ] + data,
+        data = data,
         on_prepare = "//src/e2e-app:start-devserver.js",
         server = "//src/e2e-app:server",
         tags = tags,
