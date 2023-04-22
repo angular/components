9c32902d00bbe6985f8432506fb21f8c19ab998c
Kristiyan Kostadinov <crisbeto@abv.bg>
2023-04-22 09:52:01 +0200

    fix(material/schematics): support standalone projects in table schematic
    Updates the `ng generate table` schematic to support standalone projects.

--- src/material/schematics/ng-generate/table/index.spec.ts
+++ src/material/schematics/ng-generate/table/index.spec.ts
@@ -70,6 +70,38 @@ describe('material-table-schematic', () => {
     ).toBeRejectedWithError(/required property 'name'/);
   });
 
+  describe('standalone option', () => {
+    it('should generate a standalone component', async () => {
+      const app = await createTestApp(runner);
+      const tree = await runner.runSchematic('table', {...baseOptions, standalone: true}, app);
+      const module = getFileContent(tree, '/projects/material/src/app/app.module.ts');
+      const component = getFileContent(tree, '/projects/material/src/app/foo/foo.component.ts');
+      const requiredModules = ['MatTableModule', 'MatPaginatorModule', 'MatSortModule'];
+
+      requiredModules.forEach(name => {
+        expect(module).withContext('Module should not import dependencies').not.toContain(name);
+        expect(component).withContext('Component should import dependencies').toContain(name);
+      });
+
+      expect(module).not.toContain('FooComponent');
+      expect(component).toContain('standalone: true');
+      expect(component).toContain('imports: [');
+    });
+
+    it('should infer the standalone option from the project structure', async () => {
+      const app = await createTestApp(runner, {standalone: true});
+      const tree = await runner.runSchematic('table', baseOptions, app);
+      const componentContent = getFileContent(
+        tree,
+        '/projects/material/src/app/foo/foo.component.ts',
+      );
+
+      expect(tree.exists('/projects/material/src/app/app.module.ts')).toBe(false);
+      expect(componentContent).toContain('standalone: true');
+      expect(componentContent).toContain('imports: [');
+    });
+  });
+
   describe('style option', () => {
     it('should respect the option value', async () => {
       const tree = await runner.runSchematic(
