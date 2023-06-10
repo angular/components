cfe2f2b6cc6ca271690b2dc4cda3a543128e80dc
aanchal <aanchal.agarwal@knoldus.com>
2023-06-10 11:58:32 +0530

    update

--- integration/mdc-migration/golden/src/app/components/button/button.component.ts
+++ integration/mdc-migration/golden/src/app/components/button/button.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'button-example',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent implements OnInit {}
+export class ButtonComponent {}
