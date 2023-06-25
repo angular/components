0d499a3322a3df1145084d0b34c73687fdb1c4a9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2023-06-25 15:36:12 +0200

    fix(cdk/schematics): clean up deep imports of devkit APIs (#27363)
    Removes deep imports for some devkit APIs from the CDK schematics code to avoid potential build issues.
    
    Fixes #27344.

--- src/cdk/schematics/utils/ast.ts
+++ src/cdk/schematics/utils/ast.ts
@@ -13,7 +13,7 @@ import {getWorkspace} from '@schematics/angular/utility/workspace';
 import {findModuleFromOptions as internalFindModule} from '@schematics/angular/utility/find-module';
 import {addImportToModule} from '@schematics/angular/utility/ast-utils';
 import {getAppModulePath} from '@schematics/angular/utility/ng-ast-utils';
-import {ProjectDefinition} from '@angular-devkit/core/src/workspace';
+import {workspaces} from '@angular-devkit/core';
 import * as ts from 'typescript';
 import {getProjectMainFile} from './project-main-file';
 
@@ -31,7 +31,7 @@ export function addModuleImportToRootModule(
   host: Tree,
   moduleName: string,
   src: string,
-  project: ProjectDefinition,
+  project: workspaces.ProjectDefinition,
 ) {
   const modulePath = getAppModulePath(host, getProjectMainFile(project));
   addModuleImportToModule(host, modulePath, moduleName, src);
