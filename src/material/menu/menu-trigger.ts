1fddc6be05e85cfc4bc9299daf25fe20d269d4bb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2023-07-06 18:00:24 +0200

    refactor(material/menu): switch to tokens API (#27377)
    Switches the menu to use the tokens API for theming.
    
    (cherry picked from commit 73754451d99f43e0901423c987d497fb270364f2)

--- src/material/menu/menu-trigger.ts
+++ src/material/menu/menu-trigger.ts
@@ -96,7 +96,6 @@ export abstract class _MatMenuTriggerBase implements AfterContentInit, OnDestroy
   private _menuCloseSubscription = Subscription.EMPTY;
   private _scrollStrategy: () => ScrollStrategy;
   private _changeDetectorRef = inject(ChangeDetectorRef);
-  protected _panelClass: string | null;
 
   /**
    * We're specifically looking for a `MatMenu` here since the generic `MatMenuPanel`
@@ -329,10 +328,6 @@ export abstract class _MatMenuTriggerBase implements AfterContentInit, OnDestroy
 
     this._closingActionsSubscription = this._menuClosingActions().subscribe(() => this.closeMenu());
     this._initMenu(menu);
-
-    if (this._panelClass) {
-      overlayRef.overlayElement.classList.add(this._panelClass);
-    }
 
     if (menu instanceof _MatMenuBase) {
       menu._startAnimation();
@@ -691,6 +686,4 @@ export abstract class _MatMenuTriggerBase implements AfterContentInit, OnDestroy
   },
   exportAs: 'matMenuTrigger',
 })
-export class MatMenuTrigger extends _MatMenuTriggerBase {
-  protected override _panelClass = 'mat-mdc-menu-panel-wrapper';
-}
+export class MatMenuTrigger extends _MatMenuTriggerBase {}
