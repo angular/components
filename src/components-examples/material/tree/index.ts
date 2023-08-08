c26c4d017049d2fd06a05703ceee52d87e345899
Zach Arend <zachzach@google.com>
2023-08-08 22:30:20 +0000

    docs(material/tree): delete loadmore example
    Delete the loadmore example from the tree demos.

--- src/components-examples/material/tree/index.ts
+++ src/components-examples/material/tree/index.ts
@@ -2,5 +2,4 @@ export {TreeChecklistExample} from './tree-checklist/tree-checklist-example';
 export {TreeDynamicExample} from './tree-dynamic/tree-dynamic-example';
 export {TreeFlatOverviewExample} from './tree-flat-overview/tree-flat-overview-example';
 export {TreeHarnessExample} from './tree-harness/tree-harness-example';
-export {TreeLoadmoreExample} from './tree-loadmore/tree-loadmore-example';
 export {TreeNestedOverviewExample} from './tree-nested-overview/tree-nested-overview-example';
