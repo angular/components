1dc30e8eacae1f318b53a9d22a462571b9de8ddb
Aanchal Agarwal <76724203+aanchal88@users.noreply.github.com>
2023-08-28 17:18:20 +0530

    docs: remove duplicate words (#27712)

--- src/material/datepicker/multi-year-view.ts
+++ src/material/datepicker/multi-year-view.ts
@@ -212,7 +212,7 @@ export class MatMultiYearView<D> implements AfterContentInit, OnDestroy {
   }
 
   /**
-   * Takes the index of a calendar body cell wrapped in in an event as argument. For the date that
+   * Takes the index of a calendar body cell wrapped in an event as argument. For the date that
    * corresponds to the given cell, set `activeDate` to that date and fire `activeDateChange` with
    * that date.
    *
