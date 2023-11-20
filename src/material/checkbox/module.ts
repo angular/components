54677b07214f4db2aca38c7065c35251ee4ddfcb
Kristiyan Kostadinov <crisbeto@abv.bg>
2023-11-20 10:28:22 +0100

    refactor(material/checkbox): convert to standalone
    Converts `material/checkbox` to standalone.

--- src/material/checkbox/module.ts
+++ src/material/checkbox/module.ts
@@ -13,14 +13,13 @@ import {MatCheckboxRequiredValidator} from './checkbox-required-validator';
 
 /** This module is used by both original and MDC-based checkbox implementations. */
 @NgModule({
+  imports: [MatCheckboxRequiredValidator],
   exports: [MatCheckboxRequiredValidator],
-  declarations: [MatCheckboxRequiredValidator],
 })
 export class _MatCheckboxRequiredValidatorModule {}
 
 @NgModule({
-  imports: [MatCommonModule, MatRippleModule, _MatCheckboxRequiredValidatorModule],
+  imports: [MatCommonModule, MatRippleModule, _MatCheckboxRequiredValidatorModule, MatCheckbox],
   exports: [MatCheckbox, MatCommonModule, _MatCheckboxRequiredValidatorModule],
-  declarations: [MatCheckbox],
 })
 export class MatCheckboxModule {}
