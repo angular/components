f22c4092b1b4471dd0c569660fbc06fcc5a9101f
Kristiyan Kostadinov <crisbeto@abv.bg>
2023-11-28 09:59:31 +0100

    refactor(material-experimental/menubar): switch to standalone
    Reworks `material-experimental/menubar` to support standalone.

--- src/material-experimental/menubar/menubar-item.ts
+++ src/material-experimental/menubar/menubar-item.ts
@@ -36,6 +36,7 @@ function removeIcons(element: Element) {
     '[attr.aria-disabled]': 'disabled || null',
   },
   providers: [{provide: CdkMenuItem, useExisting: MatMenuBarItem}],
+  standalone: true,
 })
 export class MatMenuBarItem extends CdkMenuItem {
   override getLabel(): string {
