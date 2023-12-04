6c8102632797b558f684364471a3a87c4410f48e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2023-12-04 12:08:15 +0100

    build: set up hydration in universal app (#28223)
    Reworks the Universal app so that it's runnable and that it serves an SSR-generated `index.html`, allowing us to test hydration locally.
    
    (cherry picked from commit 0762d69b394343555488b5aaada9444dd1a5083a)

---
+++ src/universal-app/main.ts
@@ -0,0 +1,14 @@
+import {bootstrapApplication, provideClientHydration} from '@angular/platform-browser';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {AUTOMATED_KITCHEN_SINK, KitchenSink} from './kitchen-sink/kitchen-sink';
+
+bootstrapApplication(KitchenSink, {
+  providers: [
+    provideAnimations(),
+    provideClientHydration(),
+    {
+      provide: AUTOMATED_KITCHEN_SINK,
+      useValue: false,
+    },
+  ],
+});
