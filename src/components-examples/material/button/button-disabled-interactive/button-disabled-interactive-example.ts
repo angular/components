5c261246da750372e0599109aff01ac245a2f12d
Kristiyan Kostadinov <crisbeto@abv.bg>
2023-12-11 22:20:13 +0100

    feat(material/button): add the ability to interact with disabled buttons
    Native disabled buttons don't allow focus and prevent the button from dispatching events. In some cases this can be problematic, because it prevents the app from showing to the user why the button is disabled.
    
    These changes introduce a new opt-in input that will style buttons as disabled and set `aria-disabled="true"`, but not set the native `disabled` attribute, allowing them to be interactive.

---
+++ src/components-examples/material/button/button-disabled-interactive/button-disabled-interactive-example.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {MatButton} from '@angular/material/button';
+import {MatTooltip} from '@angular/material/tooltip';
+
+/**
+ * @title Interactive disabled buttons
+ */
+@Component({
+  selector: 'button-disabled-interactive-example',
+  templateUrl: 'button-disabled-interactive-example.html',
+  styleUrls: ['button-disabled-interactive-example.css'],
+  standalone: true,
+  imports: [MatButton, MatTooltip],
+})
+export class ButtonDisabledInteractiveExample {}
--- src/dev-app/button/button-demo.ts
+++ src/dev-app/button/button-demo.ts
@@ -7,18 +7,45 @@
  */
 
 import {Component} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from '@angular/forms';
+import {
+  MatButton,
+  MatAnchor,
+  MatFabButton,
+  MatFabAnchor,
+  MatIconButton,
+  MatIconAnchor,
+  MatMiniFabButton,
+  MatMiniFabAnchor,
+} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+import {MatTooltip} from '@angular/material/tooltip';
+import {MatCheckbox} from '@angular/material/checkbox';
 
 @Component({
   selector: 'button-demo',
   templateUrl: 'button-demo.html',
   styleUrls: ['button-demo.css'],
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [
+    MatButton,
+    MatAnchor,
+    MatFabButton,
+    MatFabAnchor,
+    MatMiniFabButton,
+    MatMiniFabAnchor,
+    MatIconButton,
+    MatIconAnchor,
+    MatIcon,
+    MatTooltip,
+    MatCheckbox,
+    FormsModule,
+  ],
 })
 export class ButtonDemo {
-  isDisabled: boolean = false;
-  clickCounter: number = 0;
-  toggleDisable: boolean = false;
+  isDisabled = false;
+  clickCounter = 0;
+  toggleDisable = false;
+  tooltipText = 'This is a button tooltip!';
+  disabledInteractive = false;
 }
