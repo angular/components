angular/components

commit 5dd614aa100e28f0815fdd83e5b68f4239ef01f9

fix(material/tabs): nav bar not navigating on enter presses (#27862)
Fixes that the tab nav bar wasn't navigating when pressing the enter key.

Fixes #27843.

src/material/tabs/tab-nav-bar/tab-nav-bar.ts
@@ -384,10 +384,12 @@ export class MatTabLink
   }
 
   _handleKeydown(event: KeyboardEvent) {
-    if (this.disabled && (event.keyCode === SPACE || event.keyCode === ENTER)) {
-      event.preventDefault();
-    } else if (this._tabNavBar.tabPanel && event.keyCode === SPACE) {
-      this.elementRef.nativeElement.click();
+    if (event.keyCode === SPACE || event.keyCode === ENTER) {
+      if (this.disabled) {
+        event.preventDefault();
+      } else if (this._tabNavBar.tabPanel) {
+        this.elementRef.nativeElement.click();
+      }
     }
   }
 

src/material/tabs/tab-nav-bar/tab-nav-bar.spec.ts
@@ -327,6 +327,19 @@ describe('MDC-based MatTabNavBar', () => {
     expect(tabLinks[1].classList.contains('mdc-tab--active')).toBe(true);
   });
 
+  it('should activate a link when enter is pressed', () => {
+    const fixture = TestBed.createComponent(SimpleTabNavBarTestApp);
+    fixture.detectChanges();
+
+    const tabLinks = fixture.nativeElement.querySelectorAll('.mat-mdc-tab-link');
+    expect(tabLinks[1].classList.contains('mdc-tab--active')).toBe(false);
+
+    dispatchKeyboardEvent(tabLinks[1], 'keydown', ENTER);
+    fixture.detectChanges();
+
+    expect(tabLinks[1].classList.contains('mdc-tab--active')).toBe(true);
+  });
+
   describe('ripples', () => {
     let fixture: ComponentFixture<SimpleTabNavBarTestApp>;
 

commit 4037817f760ba9b69caba325b9b4c6695e341fbe

fix(material/tabs): prevent page scroll on space press
This appears to be a regression from #27862 where the `preventDefault` was removed both from space and enter presses. We still need it on space so that the page doesn't scroll.

Fixes #28392.

src/material/tabs/tab-nav-bar/tab-nav-bar.ts
@@ -372,6 +372,12 @@ export class MatTabLink
       if (this.disabled) {
         event.preventDefault();
       } else if (this._tabNavBar.tabPanel) {
+        // Only prevent the default action on space since it can scroll the page.
+        // Don't prevent enter since it can break link navigation.
+        if (event.keyCode === SPACE) {
+          event.preventDefault();
+        }
+
         this.elementRef.nativeElement.click();
       }
     }