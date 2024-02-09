4742a175ab3b971aae9b6f6ad87a2cafd3257eb9
Miles Malerba <mmalerba@google.com>
2024-02-09 08:57:24 -0800

    fix(material-experimental/theming): Add more tests for M3 theme tokens (#28561)
    Additional tests that help ensure our M3 theme conforms to the following
    rules:
    - Does not add any additional selector specificity
    - Emits only CSS variable definitions
    - Emits each token for one and only one theme dimension
    
    Also fixes several existing bugs that were discovered by the new tests.
    
    (cherry picked from commit acf72e1b468216fa771366eaf1819ff0e323efdb)

--- src/material/core/theming/tests/m3-theme.spec.ts
+++ src/material/core/theming/tests/m3-theme.spec.ts
@@ -44,9 +44,27 @@ function transpile(content: string) {
   ).css.toString();
 }
 
+function union<T>(...sets: Set<T>[]): Set<T> {
+  return new Set(sets.flatMap(s => [...s]));
+}
+
+function intersection<T>(set: Set<T>, ...sets: Set<T>[]): Set<T> {
+  return new Set([...set].filter(i => sets.every(s => s.has(i))));
+}
+
 describe('M3 theme', () => {
   it('should emit all styles under the given selector', () => {
-    const root = parse(transpile(`html { @include mat.all-component-themes($theme); }`));
+    const root = parse(
+      transpile(`
+      html {
+        @include mat.all-component-themes($theme);
+        @include mat.all-component-bases($theme);
+        @include mat.all-component-colors($theme);
+        @include mat.all-component-typographies($theme);
+        @include mat.all-component-densities($theme);
+      }
+    `),
+    );
     const selectors: string[] = [];
     root.walkRules(rule => {
       selectors.push(rule.selector);
@@ -63,5 +81,42 @@ describe('M3 theme', () => {
       }
     });
     expect(nonVarProps).toEqual([]);
+  });
+
+  it('should not have overlapping tokens between theme dimensions', () => {
+    const css = transpile(`
+        $theme: matx.define-theme();
+        base {
+          @include mat.all-component-bases($theme);
+        }
+        color {
+          @include mat.all-component-colors($theme);
+        }
+        typography {
+          @include mat.all-component-typographies($theme);
+        }
+        density {
+          @include mat.all-component-densities($theme);
+        }
+    `);
+    const root = parse(css);
+    const propSets: {[key: string]: Set<string>} = {};
+    root.walkRules(rule => {
+      rule.walkDecls(decl => {
+        propSets[rule.selector] = propSets[rule.selector] || new Set();
+        propSets[rule.selector].add(decl.prop);
+      });
+    });
+    let overlap = new Set();
+    for (const [dimension1, props1] of Object.entries(propSets)) {
+      for (const [dimension2, props2] of Object.entries(propSets)) {
+        if (dimension1 !== dimension2) {
+          overlap = union(overlap, intersection(props1, props2));
+        }
+      }
+    }
+    expect([...overlap])
+      .withContext('Did you forget to wrap these in `_hardcode()`?')
+      .toEqual([]);
   });
 });
