9b2f11ab7fb71c1625aa997bccf3bd66a913ac20
Miles Malerba <mmalerba@google.com>
2024-02-28 21:54:47 +0000

    refactor: Remove use of zone onStable in autosize textarea example

--- src/components-examples/cdk/text-field/text-field-autosize-textarea/text-field-autosize-textarea-example.ts
+++ src/components-examples/cdk/text-field/text-field-autosize-textarea/text-field-autosize-textarea-example.ts
@@ -1,6 +1,5 @@
 import {CdkTextareaAutosize, TextFieldModule} from '@angular/cdk/text-field';
-import {Component, NgZone, ViewChild} from '@angular/core';
-import {take} from 'rxjs/operators';
+import {afterNextRender, Component, inject, Injector, ViewChild} from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -14,12 +13,19 @@ import {MatFormFieldModule} from '@angular/material/form-field';
   imports: [MatFormFieldModule, MatSelectModule, MatInputModule, TextFieldModule],
 })
 export class TextFieldAutosizeTextareaExample {
-  constructor(private _ngZone: NgZone) {}
+  private _injector = inject(Injector);
 
   @ViewChild('autosize') autosize: CdkTextareaAutosize;
 
   triggerResize() {
-    // Wait for changes to be applied, then trigger textarea resize.
-    this._ngZone.onStable.pipe(take(1)).subscribe(() => this.autosize.resizeToFitContent(true));
+    // Wait for content to render, then trigger textarea resize.
+    afterNextRender(
+      () => {
+        this.autosize.resizeToFitContent(true);
+      },
+      {
+        injector: this._injector,
+      },
+    );
   }
 }
