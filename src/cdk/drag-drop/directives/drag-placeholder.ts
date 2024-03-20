angular/components

commit ef68e3215c992393934cfe15e358185b4ba8ce8b

fix(cdk/drag-drop): resolve helper directives with DI for proper hostDirectives support (#28633)
Currently `CdkDrag` resolve its helper directives (e.g. handle or preview) using a content query, but that doesn't work when it's applied as a host directive, because no content is being projected.

These changes switch to having the helper directives inject the closest drag directive and register themselves manually.

Fixes #28614.

src/cdk/drag-drop/directives/drag-placeholder.ts
@@ -6,7 +6,8 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {Directive, TemplateRef, Input, InjectionToken} from '@angular/core';
+import {Directive, TemplateRef, Input, InjectionToken, inject, OnDestroy} from '@angular/core';
+import {CDK_DRAG_PARENT} from '../drag-parent';
 
 /**
  * Injection token that can be used to reference instances of `CdkDragPlaceholder`. It serves as
@@ -24,8 +25,17 @@ export const CDK_DRAG_PLACEHOLDER = new InjectionToken<CdkDragPlaceholder>('CdkD
   standalone: true,
   providers: [{provide: CDK_DRAG_PLACEHOLDER, useExisting: CdkDragPlaceholder}],
 })
-export class CdkDragPlaceholder<T = any> {
+export class CdkDragPlaceholder<T = any> implements OnDestroy {
+  private _drag = inject(CDK_DRAG_PARENT);
+
   /** Context data to be added to the placeholder template instance. */
   @Input() data: T;
-  constructor(public templateRef: TemplateRef<T>) {}
+
+  constructor(public templateRef: TemplateRef<T>) {
+    this._drag._setPlaceholderTemplate(this);
+  }
+
+  ngOnDestroy(): void {
+    this._drag._resetPlaceholderTemplate(this);
+  }
 }

src/cdk/drag-drop/directives/drag-preview.ts
@@ -6,7 +6,16 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {Directive, InjectionToken, Input, TemplateRef, booleanAttribute} from '@angular/core';
+import {
+  Directive,
+  InjectionToken,
+  Input,
+  OnDestroy,
+  TemplateRef,
+  booleanAttribute,
+  inject,
+} from '@angular/core';
+import {CDK_DRAG_PARENT} from '../drag-parent';
 
 /**
  * Injection token that can be used to reference instances of `CdkDragPreview`. It serves as
@@ -24,12 +33,20 @@ export const CDK_DRAG_PREVIEW = new InjectionToken<CdkDragPreview>('CdkDragPrevi
   standalone: true,
   providers: [{provide: CDK_DRAG_PREVIEW, useExisting: CdkDragPreview}],
 })
-export class CdkDragPreview<T = any> {
+export class CdkDragPreview<T = any> implements OnDestroy {
+  private _drag = inject(CDK_DRAG_PARENT);
+
   /** Context data to be added to the preview template instance. */
   @Input() data: T;
 
   /** Whether the preview should preserve the same size as the item that is being dragged. */
   @Input({transform: booleanAttribute}) matchSize: boolean = false;
 
-  constructor(public templateRef: TemplateRef<T>) {}
+  constructor(public templateRef: TemplateRef<T>) {
+    this._drag._setPreviewTemplate(this);
+  }
+
+  ngOnDestroy(): void {
+    this._drag._resetPreviewTemplate(this);
+  }
 }

src/cdk/drag-drop/directives/drag-handle.ts
@@ -18,6 +18,7 @@ import {
   booleanAttribute,
 } from '@angular/core';
 import {Subject} from 'rxjs';
+import type {CdkDrag} from './drag';
 import {CDK_DRAG_PARENT} from '../drag-parent';
 import {assertElementNode} from './assertions';
 
@@ -38,9 +39,6 @@ export const CDK_DRAG_HANDLE = new InjectionToken<CdkDragHandle>('CdkDragHandle'
   providers: [{provide: CDK_DRAG_HANDLE, useExisting: CdkDragHandle}],
 })
 export class CdkDragHandle implements OnDestroy {
-  /** Closest parent draggable instance. */
-  _parentDrag: {} | undefined;
-
   /** Emits when the state of the handle has changed. */
   readonly _stateChanges = new Subject<CdkDragHandle>();
 
@@ -57,16 +55,17 @@ export class CdkDragHandle implements OnDestroy {
 
   constructor(
     public element: ElementRef<HTMLElement>,
-    @Inject(CDK_DRAG_PARENT) @Optional() @SkipSelf() parentDrag?: any,
+    @Inject(CDK_DRAG_PARENT) @Optional() @SkipSelf() private _parentDrag?: CdkDrag,
   ) {
     if (typeof ngDevMode === 'undefined' || ngDevMode) {
       assertElementNode(element.nativeElement, 'cdkDragHandle');
     }
 
-    this._parentDrag = parentDrag;
+    _parentDrag?._addHandle(this);
   }
 
   ngOnDestroy() {
+    this._parentDrag?._removeHandle(this);
     this._stateChanges.complete();
   }
 }

src/cdk/drag-drop/directives/drag.ts
@@ -10,8 +10,6 @@ import {Directionality} from '@angular/cdk/bidi';
 import {DOCUMENT} from '@angular/common';
 import {
   AfterViewInit,
-  ContentChild,
-  ContentChildren,
   Directive,
   ElementRef,
   EventEmitter,
@@ -21,7 +19,6 @@ import {
   OnDestroy,
   Optional,
   Output,
-  QueryList,
   SkipSelf,
   ViewContainerRef,
   OnChanges,
@@ -32,7 +29,7 @@ import {
   booleanAttribute,
 } from '@angular/core';
 import {coerceElement, coerceNumberProperty} from '@angular/cdk/coercion';
-import {Observable, Observer, Subject, merge} from 'rxjs';
+import {BehaviorSubject, Observable, Observer, Subject, merge} from 'rxjs';
 import {startWith, take, map, takeUntil, switchMap, tap} from 'rxjs/operators';
 import type {
   CdkDragDrop,
@@ -44,8 +41,8 @@ import type {
   CdkDragRelease,
 } from '../drag-events';
 import {CDK_DRAG_HANDLE, CdkDragHandle} from './drag-handle';
-import {CDK_DRAG_PLACEHOLDER, CdkDragPlaceholder} from './drag-placeholder';
-import {CDK_DRAG_PREVIEW, CdkDragPreview} from './drag-preview';
+import {CdkDragPlaceholder} from './drag-placeholder';
+import {CdkDragPreview} from './drag-preview';
 import {CDK_DRAG_PARENT} from '../drag-parent';
 import {DragRef, Point, PreviewContainer} from '../drag-ref';
 import type {CdkDropList} from './drop-list';
@@ -77,18 +74,12 @@ export const CDK_DROP_LIST = new InjectionToken<CdkDropList>('CdkDropList');
 export class CdkDrag<T = any> implements AfterViewInit, OnChanges, OnDestroy {
   private readonly _destroyed = new Subject<void>();
   private static _dragInstances: CdkDrag[] = [];
+  private _handles = new BehaviorSubject<CdkDragHandle[]>([]);
+  private _previewTemplate: CdkDragPreview | null;
+  private _placeholderTemplate: CdkDragPlaceholder | null;
 
   /** Reference to the underlying drag instance. */
   _dragRef: DragRef<CdkDrag<T>>;
-
-  /** Elements that can be used to drag the draggable item. */
-  @ContentChildren(CDK_DRAG_HANDLE, {descendants: true}) _handles: QueryList<CdkDragHandle>;
-
-  /** Element that will be used as a template to create the draggable item's preview. */
-  @ContentChild(CDK_DRAG_PREVIEW) _previewTemplate: CdkDragPreview;
-
-  /** Template for placeholder element rendered to show where a draggable would be dropped. */
-  @ContentChild(CDK_DRAG_PLACEHOLDER) _placeholderTemplate: CdkDragPlaceholder;
 
   /** Arbitrary data to attach to this drag instance. */
   @Input('cdkDragData') data: T;
@@ -351,10 +342,47 @@ export class CdkDrag<T = any> implements AfterViewInit, OnChanges, OnDestroy {
 
     // Unnecessary in most cases, but used to avoid extra change detections with `zone-paths-rxjs`.
     this._ngZone.runOutsideAngular(() => {
+      this._handles.complete();
       this._destroyed.next();
       this._destroyed.complete();
       this._dragRef.dispose();
     });
+  }
+
+  _addHandle(handle: CdkDragHandle) {
+    const handles = this._handles.getValue();
+    handles.push(handle);
+    this._handles.next(handles);
+  }
+
+  _removeHandle(handle: CdkDragHandle) {
+    const handles = this._handles.getValue();
+    const index = handles.indexOf(handle);
+
+    if (index > -1) {
+      handles.splice(index, 1);
+      this._handles.next(handles);
+    }
+  }
+
+  _setPreviewTemplate(preview: CdkDragPreview) {
+    this._previewTemplate = preview;
+  }
+
+  _resetPreviewTemplate(preview: CdkDragPreview) {
+    if (preview === this._previewTemplate) {
+      this._previewTemplate = null;
+    }
+  }
+
+  _setPlaceholderTemplate(placeholder: CdkDragPlaceholder) {
+    this._placeholderTemplate = placeholder;
+  }
+
+  _resetPlaceholderTemplate(placeholder: CdkDragPlaceholder) {
+    if (placeholder === this._placeholderTemplate) {
+      this._placeholderTemplate = null;
+    }
   }
 
   /** Syncs the root element with the `DragRef`. */
@@ -559,30 +587,25 @@ export class CdkDrag<T = any> implements AfterViewInit, OnChanges, OnDestroy {
   /** Sets up the listener that syncs the handles with the drag ref. */
   private _setupHandlesListener() {
     // Listen for any newly-added handles.
-    this._handles.changes
+    this._handles
       .pipe(
-        startWith(this._handles),
         // Sync the new handles with the DragRef.
-        tap((handles: QueryList<CdkDragHandle>) => {
-          const childHandleElements = handles
-            .filter(handle => handle._parentDrag === this)
-            .map(handle => handle.element);
+        tap(handles => {
+          const handleElements = handles.map(handle => handle.element);
 
           // Usually handles are only allowed to be a descendant of the drag element, but if
           // the consumer defined a different drag root, we should allow the drag element
           // itself to be a handle too.
           if (this._selfHandle && this.rootElementSelector) {
-            childHandleElements.push(this.element);
+            handleElements.push(this.element);
           }
 
-          this._dragRef.withHandles(childHandleElements);
+          this._dragRef.withHandles(handleElements);
         }),
         // Listen if the state of any of the handles changes.
-        switchMap((handles: QueryList<CdkDragHandle>) => {
+        switchMap((handles: CdkDragHandle[]) => {
           return merge(
-            ...handles.map(item => {
-              return item._stateChanges.pipe(startWith(item));
-            }),
+            ...handles.map(item => item._stateChanges.pipe(startWith(item))),
           ) as Observable<CdkDragHandle>;
         }),
         takeUntil(this._destroyed),

src/cdk/drag-drop/drag-parent.ts
@@ -7,6 +7,7 @@
  */
 
 import {InjectionToken} from '@angular/core';
+import type {CdkDrag} from './directives/drag';
 
 /**
  * Injection token that can be used for a `CdkDrag` to provide itself as a parent to the
@@ -14,4 +15,4 @@ import {InjectionToken} from '@angular/core';
  * to avoid circular imports.
  * @docs-private
  */
-export const CDK_DRAG_PARENT = new InjectionToken<{}>('CDK_DRAG_PARENT');
+export const CDK_DRAG_PARENT = new InjectionToken<CdkDrag>('CDK_DRAG_PARENT');

commit 7b25c654e4e640dfb9e438fc0beaef952e6453a2

fix(cdk/drag-drop): optionally inject parent drag in preview and placeholder
Fixes a regression from #28633 where using `cdkDragPlaceholder` or `cdkDragPreview` without a `cdkDrag` would throw. Technically this is a no-op, but it appears that folks started depending on the old behavior so these changes bring it back.

Fixes #28744.

src/cdk/drag-drop/directives/drag-placeholder.ts
@@ -26,16 +26,16 @@ export const CDK_DRAG_PLACEHOLDER = new InjectionToken<CdkDragPlaceholder>('CdkD
   providers: [{provide: CDK_DRAG_PLACEHOLDER, useExisting: CdkDragPlaceholder}],
 })
 export class CdkDragPlaceholder<T = any> implements OnDestroy {
-  private _drag = inject(CDK_DRAG_PARENT);
+  private _drag = inject(CDK_DRAG_PARENT, {optional: true});
 
   /** Context data to be added to the placeholder template instance. */
   @Input() data: T;
 
   constructor(public templateRef: TemplateRef<T>) {
-    this._drag._setPlaceholderTemplate(this);
+    this._drag?._setPlaceholderTemplate(this);
   }
 
   ngOnDestroy(): void {
-    this._drag._resetPlaceholderTemplate(this);
+    this._drag?._resetPlaceholderTemplate(this);
   }
 }

src/cdk/drag-drop/directives/drag-preview.ts
@@ -34,7 +34,7 @@ export const CDK_DRAG_PREVIEW = new InjectionToken<CdkDragPreview>('CdkDragPrevi
   providers: [{provide: CDK_DRAG_PREVIEW, useExisting: CdkDragPreview}],
 })
 export class CdkDragPreview<T = any> implements OnDestroy {
-  private _drag = inject(CDK_DRAG_PARENT);
+  private _drag = inject(CDK_DRAG_PARENT, {optional: true});
 
   /** Context data to be added to the preview template instance. */
   @Input() data: T;
@@ -43,10 +43,10 @@ export class CdkDragPreview<T = any> implements OnDestroy {
   @Input({transform: booleanAttribute}) matchSize: boolean = false;
 
   constructor(public templateRef: TemplateRef<T>) {
-    this._drag._setPreviewTemplate(this);
+    this._drag?._setPreviewTemplate(this);
   }
 
   ngOnDestroy(): void {
-    this._drag._resetPreviewTemplate(this);
+    this._drag?._resetPreviewTemplate(this);
   }
 }