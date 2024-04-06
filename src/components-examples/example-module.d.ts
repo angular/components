de093b45d8f618c506c4d083ae839d25de486b17
Miles Malerba <mmalerba@google.com>
2024-04-06 10:02:01 -0700

    build: fix errors due to missing examples

---
+++ src/components-examples/example-module.d.ts
@@ -0,0 +1,20 @@
+interface LiveExample {
+  /** Title of the example. */
+  title: string;
+  /** Name of the example component. */
+  componentName: string;
+  /** Selector to match the component of this example. */
+  selector: string;
+  /** Name of the primary file of this example. */
+  primaryFile: string;
+  /** List of files which are part of the example. */
+  files: string[];
+  /** Path to the directory containing the example. */
+  packagePath: string;
+  /** List of additional components which are part of the example. */
+  additionalComponents: string[];
+  /** Path from which to import the xample. */
+  importPath: string;
+}
+
+export const EXAMPLE_COMPONENTS: {[id: string]: LiveExample};
