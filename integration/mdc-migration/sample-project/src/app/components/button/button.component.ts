4b0967cc217437cbdf58a0051d99556689ecc1aa
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2024-04-10 08:53:49 -0400

    build: delete the MDC migration (#28838)
    Removes the MDC migration, because users still on the legacy components haven't been able to update past v17.

--- integration/mdc-migration/sample-project/src/app/components/button/button.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'button-example',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss'],
-})
-export class ButtonComponent {}
