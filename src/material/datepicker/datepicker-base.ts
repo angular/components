27503ed285f3ba964cc861443a7bc1ab7b2cf19a
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2024-06-13 14:17:59 -0700

    docs(multiple): Update color input comments to standarize across the components (#29254)

--- src/material/datepicker/datepicker-base.ts
+++ src/material/datepicker/datepicker-base.ts
@@ -145,7 +145,13 @@ export class MatDatepickerContent<S, D = ExtractDateTypeFromSelection<S>>
   /** Reference to the internal calendar component. */
   @ViewChild(MatCalendar) _calendar: MatCalendar<D>;
 
-  /** Palette color of the internal calendar. */
+  /**
+   * Theme color of the internal calendar. This API is supported in M2 themes
+   * only, it has no effect in M3 themes.
+   *
+   * For information on applying color variants in M3, see
+   * https://material.angular.io/guide/theming#using-component-color-variants.
+   */
   @Input() color: ThemePalette;
 
   /** Reference to the datepicker that created the overlay. */
