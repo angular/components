1d2ceb8e7550af4ec29b1962dd03e873ebf2d145
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2024-06-13 22:10:32 +0000

    docs(material/button-toggle): Update docs and examples

--- src/components-examples/material/button-toggle/button-toggle-mode/button-toggle-mode-example.ts
+++ src/components-examples/material/button-toggle/button-toggle-mode/button-toggle-mode-example.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 /**
  * @title Button toggle selection mode
@@ -8,6 +9,18 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
   selector: 'button-toggle-mode-example',
   templateUrl: 'button-toggle-mode-example.html',
   standalone: true,
-  imports: [MatButtonToggleModule],
+  imports: [MatButtonToggleModule, MatCheckboxModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ButtonToggleModeExample {}
+export class ButtonToggleModeExample {
+  hideSingleSelectionIndicator = signal(false);
+  hideMultipleSelectionIndicator = signal(false);
+
+  toggleSingleSelectionIndicator() {
+    this.hideSingleSelectionIndicator.update(value => !value);
+  }
+
+  toggleMultipleSelectionIndicator() {
+    this.hideMultipleSelectionIndicator.update(value => !value);
+  }
+}
