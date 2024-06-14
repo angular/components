0d66928f157b71d485323f631d3799950de8fc9a
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2024-06-14 06:46:51 -0700

    docs(material/button-toggle): Update docs and examples (#29256)
    (cherry picked from commit 205f5ec737886e95e02bf2e19603b1d03b6ed44c)

--- src/components-examples/material/button-toggle/button-toggle-forms/button-toggle-forms-example.ts
+++ src/components-examples/material/button-toggle/button-toggle-forms/button-toggle-forms-example.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
@@ -10,6 +10,7 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
   templateUrl: 'button-toggle-forms-example.html',
   standalone: true,
   imports: [MatButtonToggleModule, FormsModule, ReactiveFormsModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonToggleFormsExample {
   fontStyleControl = new FormControl('');
