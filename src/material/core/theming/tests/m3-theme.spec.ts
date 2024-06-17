7d62718560dbb9dbde12ea0bf6a8879b09b16b5e
Miles Malerba <mmalerba@google.com>
2024-06-17 16:06:37 -0700

    fix(material/theming): Add validation to shared extend-theme logic
    Adds validation to the shared extend-theme logic along with tests to
    verify its behavior.

--- src/material/core/theming/tests/m3-theme.spec.ts
+++ src/material/core/theming/tests/m3-theme.spec.ts
@@ -1,7 +1,7 @@
-import {parse} from 'postcss';
-import {compileString} from 'sass';
 import {runfiles} from '@bazel/runfiles';
 import * as path from 'path';
+import {parse} from 'postcss';
+import {compileString} from 'sass';
 
 import {createLocalAngularPackageImporter} from '../../../../../tools/sass/local-sass-importer';
 
@@ -111,5 +111,153 @@ describe('M3 theme', () => {
     expect(() => transpile(`@include mat.all-component-themes($theme)`)).toThrowError(
       /Calls to Angular Material theme mixins with an M3 theme must be wrapped in a selector/,
     );
+  });
+
+  describe('theme extension API', () => {
+    it('should allow overriding token value', () => {
+      const css = transpile(`
+        @use '../../tokens/token-utils';
+
+        $theme: token-utils.extend-theme($theme, ((mdc, checkbox), (mat, checkbox)), (
+          selected-checkmark-color: magenta
+        ));
+
+        html {
+          @include mat.checkbox-theme($theme);
+        }
+      `);
+
+      expect(css).toContain('--mdc-checkbox-selected-checkmark-color: magenta');
+    });
+
+    it('should not override token value for other color variant', () => {
+      const css = transpile(`
+        @use '../../tokens/token-utils';
+
+        $theme: token-utils.extend-theme($theme, ((mdc, checkbox), (mat, checkbox)), (
+          selected-checkmark-color: magenta
+        ));
+
+        html {
+          @include mat.checkbox-theme($theme, $color-variant: secondary);
+        }
+      `);
+
+      expect(css).not.toContain('--mdc-checkbox-selected-checkmark-color: magenta');
+    });
+
+    it('should allow overriding specific color variant separately', () => {
+      const css = transpile(`
+        @use '../../tokens/token-utils';
+
+        $theme: token-utils.extend-theme($theme, ((mdc, checkbox), (mat, checkbox)), (
+          selected-checkmark-color: magenta,
+          tertiary: (
+            selected-checkmark-color: cyan,
+          ),
+        ));
+
+        html {
+          @include mat.checkbox-theme($theme);
+        }
+
+        .tertiary {
+          @include mat.checkbox-color($theme, $color-variant: tertiary);
+        }
+      `);
+
+      expect(css).toContain('--mdc-checkbox-selected-checkmark-color: magenta');
+      expect(css).toContain('--mdc-checkbox-selected-checkmark-color: cyan');
+    });
+  });
+
+  it('should error if used on M2 theme', () => {
+    expect(() =>
+      transpile(`
+        @use '../../tokens/token-utils';
+
+        $theme: mat.m2-define-light-theme(mat.$m2-red-palette, mat.$m2-red-palette);
+
+        $theme: token-utils.extend-theme($theme, ((mdc, checkbox), (mat, checkbox)), (
+          selected-checkmark-color: magenta
+        ));
+
+        html {
+          @include mat.checkbox-theme($theme);
+        }
+      `),
+    ).toThrowError(/The `extend-theme` functions are only supported for M3 themes/);
+  });
+
+  it('should error on invalid namespace', () => {
+    expect(() =>
+      transpile(`
+        @use '../../tokens/token-utils';
+
+        $theme: token-utils.extend-theme($theme, ((mdc, checkbox), (mat, checkbocks)), (
+          selected-checkmark-color: magenta
+        ));
+
+        html {
+          @include mat.checkbox-theme($theme);
+        }
+      `),
+    ).toThrowError(
+      /Error extending theme: Theme does not have tokes for namespace `\(mat, checkbocks\)`/,
+    );
+  });
+
+  it('should error on ambiguous shorthand token name', () => {
+    expect(() =>
+      transpile(`
+        @use '../../tokens/token-utils';
+
+        $theme: token-utils.extend-theme($theme, ((mdc, checkbox), (mdc, radio)), (
+          selected-checkmark-color: magenta
+        ));
+
+        html {
+          @include mat.checkbox-theme($theme);
+        }
+      `),
+    ).toThrowError(
+      /Error extending theme: Ambiguous token name `.*` exists in multiple namespaces: `\(mdc, checkbox\)` and `\(mdc, radio\)`/,
+    );
+  });
+
+  it('should error on unknown variant', () => {
+    expect(() =>
+      transpile(`
+        @use '../../tokens/token-utils';
+
+        $theme: token-utils.extend-theme($theme, ((mdc, checkbox), (mat, checkbox)), (
+          accent: (
+            selected-checkmark-color: magenta
+          )
+        ));
+
+        html {
+          @include mat.checkbox-theme($theme);
+        }
+      `),
+    ).toThrowError(
+      /Error extending theme: Unrecognized color variant `accent`. Allowed variants are: primary, secondary, tertiary, error, surface/,
+    );
+  });
+
+  it('should error on unknown token', () => {
+    expect(() =>
+      transpile(`
+        @use '../../tokens/token-utils';
+
+        $theme: token-utils.extend-theme($theme, ((mdc, checkbox), (mat, checkbox)), (
+          fake-token: red
+        ));
+
+        html {
+          @include mat.checkbox-theme($theme);
+        }
+      `),
+    ).toThrowError(/Error extending theme: Unrecognized token `fake-token`. Allowed tokens are: /);
   });
 });
