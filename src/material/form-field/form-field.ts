e9d1c7aaddebe17b92453ac5fa4d428a9becee9c
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2024-06-27 15:15:14 -0400

    docs: remove color property from remaining examples and overview pages and make description comment consistent across components (#29345)

--- src/material/form-field/form-field.ts
+++ src/material/form-field/form-field.ts
@@ -75,8 +75,10 @@ export interface MatFormFieldDefaultOptions {
   /** Default form field appearance style. */
   appearance?: MatFormFieldAppearance;
   /**
-   * Default color of the form field. This API is supported in M2 themes only, it has no effect in
-   * M3 themes. For information on applying color variants in M3, see
+   * Default theme color of the form field. This API is supported in M2 themes only, it has no
+   * effect in M3 themes.
+   *
+   * For information on applying color variants in M3, see
    * https://material.angular.io/guide/theming#using-component-color-variants
    */
   color?: ThemePalette;
