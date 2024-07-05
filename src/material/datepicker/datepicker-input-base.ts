90f94882fea72532df143b576165a8b7e68623f0
hhaasbroek <hhaasbroek@retrorabbit.co.za>
2024-07-05 14:31:16 +0200

    Added context for why the null value is there

--- src/material/datepicker/datepicker-input-base.ts
+++ src/material/datepicker/datepicker-input-base.ts
@@ -56,7 +56,9 @@ export class MatDatepickerInputEvent<D, S = unknown> {
   }
 }
 
-/** Function that can be used to filter out dates from a calendar. */
+/** Function that can be used to filter out dates from a calendar.
+ * Datepicker can sometimes receive a null value as input for the date argument.
+ * This doesn't represent a "null date" but rather signifies that no date has been selected yet in the calendar.*/
 export type DateFilterFn<D> = (date: D | null) => boolean;
 
 /**
