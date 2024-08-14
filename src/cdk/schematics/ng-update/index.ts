811cd6633a00aad28d4c3b8a13d375f6a676a637
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2024-08-14 20:32:21 +0200

    build: set up schematics for v19 (#29584)
    Sets up the update schematics for v19.

--- src/cdk/schematics/ng-update/index.ts
+++ src/cdk/schematics/ng-update/index.ts
@@ -14,9 +14,9 @@ import {createMigrationSchematicRule, NullableDevkitMigration} from './devkit-mi
 const cdkMigrations: NullableDevkitMigration[] = [];
 
 /** Entry point for the migration schematics with target of Angular CDK 18.0.0 */
-export function updateToV18(): Rule {
+export function updateToV19(): Rule {
   return createMigrationSchematicRule(
-    TargetVersion.V18,
+    TargetVersion.V19,
     cdkMigrations,
     cdkUpgradeData,
     onMigrationComplete,
