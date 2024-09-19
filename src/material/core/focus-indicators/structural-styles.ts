b58a28186ee39ebe7ac906f33fa6c6cf9bdb9a92
Wagner Maciel <wagnermaciel@google.com>
2024-09-19 10:42:36 -0400

    fixup! feat(material/core): create focus-indicator structural styles loader

--- src/material/core/focus-indicators/structural-styles.ts
+++ src/material/core/focus-indicators/structural-styles.ts
@@ -14,6 +14,7 @@ import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/co
  */
 @Component({
   standalone: true,
+  selector: 'structural-styles',
   styleUrl: 'structural-styles.css',
   encapsulation: ViewEncapsulation.None,
   template: '',
