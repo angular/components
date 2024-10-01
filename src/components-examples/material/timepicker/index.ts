d3cbd486a02f73892bfd19754cff3a5d1a8d9be5
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-10-01 11:18:18 +0200

    docs(material/timepicker): add a basic docs example
    Adds a basic example to work around a CI failure.

--- src/components-examples/material/timepicker/index.ts
+++ src/components-examples/material/timepicker/index.ts
@@ -1,1 +1,2 @@
-export const TEMP = true;
+export {TimepickerOverviewExample} from './timepicker-overview/timepicker-overview-example';
+export {TimepickerHarnessExample} from './timepicker-harness/timepicker-harness-example';
