73d4d1cb43520efc873dde5febd7d6679b12158c
Dolav Soker <sdolav@gmail.com>
2024-10-26 22:02:27 -0700

    Update progress-spinner-harness.ts

--- src/material/progress-spinner/testing/progress-spinner-harness.ts
+++ src/material/progress-spinner/testing/progress-spinner-harness.ts
@@ -48,13 +48,15 @@ export class MatProgressSpinnerHarness extends ComponentHarness {
 
     /** Gets the progress spinner's diameter. */
   async getDiameter(): Promise<number | null> {
-    const diameterAttr = (await this.host()).getAttribute('data-mat-progress-spinner-diameter');
-    return (await diameterAttr) as ProgressSpinnerMode;
+    const host = await this.host();
+    const diameterAttr = await host.getAttribute('data-mat-progress-spinner-diameter');
+    return diameterAttr ? coerceNumberProperty(diameterAttr) : null;
   }
 
   /** Gets the progress spinner's strokeWidth. */
   async getStrokeWidth(): Promise<number | null> {
-    const strokeWidthAttr = (await this.host()).getAttribute('data-mat-progress-spinner-strokeWidth');
-    return (await strokeWidthAttr) as ProgressSpinnerMode;
+    const host = await this.host();
+    const strokeWidthAttr = await host.getAttribute('data-mat-progress-spinner-strokeWidth');
+    return strokeWidthAttr ? coerceNumberProperty(strokeWidthAttr) : null;
   }
 }
