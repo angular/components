37ee1ac4a0bd40a3eb5b37f9793f5b4391bb8a60
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-10-30 20:57:01 +0100

    Add styling page to the docs
    Adds a "Styling" page to each component that tells users how to customize the component's styles.

---
+++ material.angular.io/material.angular.io/material.angular.io/material.angular.io/src/app/pages/component-viewer/token-name.ts
@@ -0,0 +1,42 @@
+import {Component, input, inject} from '@angular/core';
+import {MatIconButton} from '@angular/material/button';
+import {Clipboard} from '@angular/cdk/clipboard';
+import {MatIcon} from '@angular/material/icon';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatTooltip} from '@angular/material/tooltip';
+
+@Component({
+  selector: 'token-name',
+  standalone: true,
+  template: `
+    <code>{{name()}}</code>
+    <button
+      mat-icon-button
+      matTooltip="Copy name to the clipboard"
+      (click)="copy(name())">
+      <mat-icon>content_copy</mat-icon>
+    </button>
+  `,
+  styles: `
+    :host {
+      display: flex;
+      align-items: center;
+
+      button {
+        margin-left: 8px;
+      }
+    }
+  `,
+  imports: [MatIconButton, MatIcon, MatTooltip],
+})
+export class TokenName {
+  private clipboard = inject(Clipboard);
+  private snackbar = inject(MatSnackBar);
+
+  name = input.required<string>();
+
+  protected copy(name: string): void {
+    const message = this.clipboard.copy(name) ? 'Copied token name' : 'Failed to copy token name';
+    this.snackbar.open(message, undefined, {duration: 2500});
+  }
+}
