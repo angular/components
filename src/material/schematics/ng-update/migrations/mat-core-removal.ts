11e39a2e1c283afad110f495cda6da816d2c707c
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-12-02 11:43:48 +0100

    fix(material/schematics): avoid parsing stylesheets that don't include Material
    Adds a check in the `mat.core` migration so that it avoids parsing stylesheets that don't contain `@angular/material` altogether. This both makes the schematic faster and avoids potential issues for stylesheets we don't care about.

--- src/material/schematics/ng-update/migrations/mat-core-removal.ts
+++ src/material/schematics/ng-update/migrations/mat-core-removal.ts
@@ -16,6 +16,8 @@ import {
   WorkspacePath,
 } from '@angular/cdk/schematics';
 
+const MATERIAL_IMPORT_PATH = '@angular/material';
+
 export class MatCoreMigration extends Migration<UpgradeData, DevkitContext> {
   override enabled = true;
   private _namespace: string | undefined;
@@ -25,6 +27,11 @@ export class MatCoreMigration extends Migration<UpgradeData, DevkitContext> {
   }
 
   override visitStylesheet(stylesheet: ResolvedResource): void {
+    // Avoid parsing the template Material isn't used.
+    if (!stylesheet.content.includes(MATERIAL_IMPORT_PATH)) {
+      return;
+    }
+
     try {
       const processor = new postcss.Processor([
         {
@@ -77,7 +84,7 @@ export class MatCoreMigration extends Migration<UpgradeData, DevkitContext> {
 
   /** Sets the namespace if the given at-rule if it is importing from @angular/material. */
   private _getNamespace(node: postcss.AtRule): void {
-    if (!this._namespace && node.params.startsWith('@angular/material', 1)) {
+    if (!this._namespace && node.params.startsWith(MATERIAL_IMPORT_PATH, 1)) {
       this._namespace = node.params.split(/\s+/)[2] || 'material';
     }
   }
