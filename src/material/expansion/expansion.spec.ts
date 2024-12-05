aafa151e78994a70d7e4509e49e040412fd5adf3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2024-12-05 13:53:15 +0100

    fix(material/expansion): switch away from animations module (#30119)
    Reworks the expansion panel to animate purely with CSS, rather than going through the `@angular/animations` module. This simplifies the setup and allows us to resolve several long-standing bug reports.
    
    Fixes #27946.
    Fixes #22715.
    Fixes #21434.
    Fixes #20517.
    Fixes #17177.
    Fixes #16534.
    Fixes #16503.
    Fixes #14952.
    Fixes #14759.
    Fixes #14075.
    Fixes #11765.

--- src/material/expansion/expansion.spec.ts
+++ src/material/expansion/expansion.spec.ts
@@ -225,36 +225,20 @@ describe('MatExpansionPanel', () => {
     });
   });
 
-  it('should not be able to focus content while closed', fakeAsync(() => {
+  it('should not be able to focus content while closed', () => {
     const fixture = TestBed.createComponent(PanelWithContent);
     fixture.componentInstance.expanded = true;
     fixture.changeDetectorRef.markForCheck();
     fixture.detectChanges();
-    tick(250);
+    const wrapper = fixture.nativeElement.querySelector('.mat-expansion-panel-content-wrapper');
+    expect(wrapper.hasAttribute('inert')).toBe(false);
 
-    const button = fixture.debugElement.query(By.css('button'))!.nativeElement;
-
-    button.focus();
-    expect(document.activeElement)
-      .withContext('Expected button to start off focusable.')
-      .toBe(button);
-
-    button.blur();
     fixture.componentInstance.expanded = false;
     fixture.changeDetectorRef.markForCheck();
     fixture.detectChanges();
-    tick(250);
 
-    // Enforce a style recalculation as otherwise browsers like Safari on iOS 14 require
-    // us to wait until the next tick using actual async/await. Not retrieving the computed
-    // styles would result in the `visibility: hidden` on the expansion content to not apply.
-    getComputedStyle(button).getPropertyValue('visibility');
-
-    button.focus();
-    expect(document.activeElement)
-      .not.withContext('Expected button to no longer be focusable.')
-      .toBe(button);
-  }));
+    expect(wrapper.hasAttribute('inert')).toBe(true);
+  });
 
   it('should restore focus to header if focused element is inside panel on close', fakeAsync(() => {
     const fixture = TestBed.createComponent(PanelWithContent);
@@ -346,26 +330,6 @@ describe('MatExpansionPanel', () => {
       .toBeFalsy();
     expect(content.classList).toContain('mat-content-hide-toggle');
   });
-
-  it('should update the indicator rotation when the expanded state is toggled programmatically', fakeAsync(() => {
-    const fixture = TestBed.createComponent(PanelWithContent);
-
-    fixture.detectChanges();
-    tick(250);
-
-    const arrow = fixture.debugElement.query(By.css('.mat-expansion-indicator'))!.nativeElement;
-
-    expect(arrow.style.transform).withContext('Expected no rotation.').toBe('rotate(0deg)');
-
-    fixture.componentInstance.expanded = true;
-    fixture.changeDetectorRef.markForCheck();
-    fixture.detectChanges();
-    tick(250);
-
-    expect(arrow.style.transform)
-      .withContext('Expected 180 degree rotation.')
-      .toBe('rotate(180deg)');
-  }));
 
   it('should make sure accordion item runs ngOnDestroy when expansion panel is destroyed', () => {
     const fixture = TestBed.createComponent(PanelWithContentInNgIf);
