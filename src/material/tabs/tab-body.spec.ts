8eb19048be00b71728ec5f7bb84169a63e0d0b27
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-01-13 11:28:22 +0100

    fix(material/tabs): switch away from animations module
    Reworks the tabs so they don't depend on the animations module anymore. This is both simpler and avoids some of the pitfalls of the animations module.

--- src/material/tabs/tab-body.spec.ts
+++ src/material/tabs/tab-body.spec.ts
@@ -36,74 +36,12 @@ describe('MatTabBody', () => {
     });
   }));
 
-  describe('when initialized as center', () => {
-    let fixture: ComponentFixture<SimpleTabBodyApp>;
+  it('should be center position if origin is unchanged', () => {
+    const fixture = TestBed.createComponent(SimpleTabBodyApp);
+    fixture.componentInstance.position = 0;
+    fixture.detectChanges();
 
-    it('should be center position if origin is unchanged', () => {
-      fixture = TestBed.createComponent(SimpleTabBodyApp);
-      fixture.componentInstance.position = 0;
-      fixture.detectChanges();
-
-      expect(fixture.componentInstance.tabBody._position).toBe('center');
-    });
-
-    it('should be center position if origin is explicitly set to null', () => {
-      fixture = TestBed.createComponent(SimpleTabBodyApp);
-      fixture.componentInstance.position = 0;
-
-      // It can happen that the `origin` is explicitly set to null through the Angular input
-      // binding. This test should ensure that the body does properly such origin value.
-      // The `MatTab` class sets the origin by default to null. See related issue: #12455
-      fixture.componentInstance.origin = null;
-      fixture.detectChanges();
-
-      expect(fixture.componentInstance.tabBody._position).toBe('center');
-    });
-
-    describe('in LTR direction', () => {
-      beforeEach(() => {
-        dir = 'ltr';
-        fixture = TestBed.createComponent(SimpleTabBodyApp);
-      });
-      it('should be left-origin-center position with negative or zero origin', () => {
-        fixture.componentInstance.position = 0;
-        fixture.componentInstance.origin = 0;
-        fixture.detectChanges();
-
-        expect(fixture.componentInstance.tabBody._position).toBe('left-origin-center');
-      });
-
-      it('should be right-origin-center position with positive nonzero origin', () => {
-        fixture.componentInstance.position = 0;
-        fixture.componentInstance.origin = 1;
-        fixture.detectChanges();
-
-        expect(fixture.componentInstance.tabBody._position).toBe('right-origin-center');
-      });
-    });
-
-    describe('in RTL direction', () => {
-      beforeEach(() => {
-        dir = 'rtl';
-        fixture = TestBed.createComponent(SimpleTabBodyApp);
-      });
-
-      it('should be right-origin-center position with negative or zero origin', () => {
-        fixture.componentInstance.position = 0;
-        fixture.componentInstance.origin = 0;
-        fixture.detectChanges();
-
-        expect(fixture.componentInstance.tabBody._position).toBe('right-origin-center');
-      });
-
-      it('should be left-origin-center position with positive nonzero origin', () => {
-        fixture.componentInstance.position = 0;
-        fixture.componentInstance.origin = 1;
-        fixture.detectChanges();
-
-        expect(fixture.componentInstance.tabBody._position).toBe('left-origin-center');
-      });
-    });
+    expect(fixture.componentInstance.tabBody._position).toBe('center');
   });
 
   describe('should properly set the position in LTR', () => {
@@ -213,14 +151,13 @@ describe('MatTabBody', () => {
 @Component({
   template: `
     <ng-template>Tab Body Content</ng-template>
-    <mat-tab-body [content]="content()" [position]="position" [origin]="origin"></mat-tab-body>
+    <mat-tab-body [content]="content()" [position]="position"></mat-tab-body>
   `,
   imports: [PortalModule, MatRippleModule, MatTabBody],
 })
 class SimpleTabBodyApp implements AfterViewInit {
   content = signal<TemplatePortal | undefined>(undefined);
   position: number;
-  origin: number | null;
 
   @ViewChild(MatTabBody) tabBody: MatTabBody;
   @ViewChild(TemplateRef) template: TemplateRef<any>;
