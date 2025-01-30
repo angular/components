879cbe7557d33c3bf7ecb7c48c91dd9e86c2181e
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-01-30 19:51:23 +0100

    fix(material/menu): remove remaining animation dependencies
    Removes the remaning dependencies on `@angular/animations` from the package.

--- src/material/menu/menu-animations.ts
+++ src/material/menu/menu-animations.ts
@@ -6,15 +6,6 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  AnimationTriggerMetadata,
-} from '@angular/animations';
-
 /**
  * Animations used by the mat-menu component.
  * Animation duration and timing values are based on:
@@ -24,9 +15,31 @@ import {
  * @breaking-change 21.0.0
  */
 export const matMenuAnimations: {
-  readonly transformMenu: AnimationTriggerMetadata;
-  readonly fadeInItems: AnimationTriggerMetadata;
+  readonly transformMenu: any;
+  readonly fadeInItems: any;
 } = {
+  // Represents:
+  // trigger('transformMenu', [
+  //   state(
+  //     'void',
+  //     style({
+  //       opacity: 0,
+  //       transform: 'scale(0.8)',
+  //     }),
+  //   ),
+  //   transition(
+  //     'void => enter',
+  //     animate(
+  //       '120ms cubic-bezier(0, 0, 0.2, 1)',
+  //       style({
+  //         opacity: 1,
+  //         transform: 'scale(1)',
+  //       }),
+  //     ),
+  //   ),
+  //   transition('* => void', animate('100ms 25ms linear', style({opacity: 0}))),
+  // ])
+
   /**
    * This animation controls the menu panel's entry and exit from the page.
    *
@@ -35,40 +48,75 @@ export const matMenuAnimations: {
    * When the menu panel is removed from the DOM, it simply fades out after a brief
    * delay to display the ripple.
    */
-  transformMenu: trigger('transformMenu', [
-    state(
-      'void',
-      style({
-        opacity: 0,
-        transform: 'scale(0.8)',
-      }),
-    ),
-    transition(
-      'void => enter',
-      animate(
-        '120ms cubic-bezier(0, 0, 0.2, 1)',
-        style({
-          opacity: 1,
-          transform: 'scale(1)',
-        }),
-      ),
-    ),
-    transition('* => void', animate('100ms 25ms linear', style({opacity: 0}))),
-  ]),
+  transformMenu: {
+    type: 7,
+    name: 'transformMenu',
+    definitions: [
+      {
+        type: 0,
+        name: 'void',
+        styles: {type: 6, styles: {opacity: 0, transform: 'scale(0.8)'}, offset: null},
+      },
+      {
+        type: 1,
+        expr: 'void => enter',
+        animation: {
+          type: 4,
+          styles: {type: 6, styles: {opacity: 1, transform: 'scale(1)'}, offset: null},
+          timings: '120ms cubic-bezier(0, 0, 0.2, 1)',
+        },
+        options: null,
+      },
+      {
+        type: 1,
+        expr: '* => void',
+        animation: {
+          type: 4,
+          styles: {type: 6, styles: {opacity: 0}, offset: null},
+          timings: '100ms 25ms linear',
+        },
+        options: null,
+      },
+    ],
+    options: {},
+  },
+
+  // Represents:
+  // trigger('fadeInItems', [
+  //   // TODO(crisbeto): this is inside the `transformMenu`
+  //   // now. Remove next time we do breaking changes.
+  //   state('showing', style({opacity: 1})),
+  //   transition('void => *', [
+  //     style({opacity: 0}),
+  //     animate('400ms 100ms cubic-bezier(0.55, 0, 0.55, 0.2)'),
+  //   ]),
+  // ])
 
   /**
    * This animation fades in the background color and content of the menu panel
    * after its containing element is scaled in.
    */
-  fadeInItems: trigger('fadeInItems', [
-    // TODO(crisbeto): this is inside the `transformMenu`
-    // now. Remove next time we do breaking changes.
-    state('showing', style({opacity: 1})),
-    transition('void => *', [
-      style({opacity: 0}),
-      animate('400ms 100ms cubic-bezier(0.55, 0, 0.55, 0.2)'),
-    ]),
-  ]),
+  fadeInItems: {
+    type: 7,
+    name: 'fadeInItems',
+    definitions: [
+      {
+        type: 0,
+        name: 'showing',
+        styles: {type: 6, styles: {opacity: 1}, offset: null},
+      },
+      {
+        type: 1,
+        expr: 'void => *',
+        animation: [
+          {type: 6, styles: {opacity: 0}, offset: null},
+          {type: 4, styles: null, timings: '400ms 100ms cubic-bezier(0.55, 0, 0.55, 0.2)'},
+        ],
+        options: null,
+      },
+    ],
+    options: {},
+  },
 };
 
 /**
