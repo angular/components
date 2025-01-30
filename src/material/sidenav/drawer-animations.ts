712d5e2a403db8dfbcc77a417df383585a288f8d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-01-30 19:52:06 +0100

    fix(material/sidenav): remove remaining animation dependencies
    Removes the remaning dependencies on `@angular/animations` from the package.

--- src/material/sidenav/drawer-animations.ts
+++ src/material/sidenav/drawer-animations.ts
@@ -5,14 +5,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger,
-  AnimationTriggerMetadata,
-} from '@angular/animations';
 
 /**
  * Animations used by the Material drawers.
@@ -21,33 +13,76 @@ import {
  * @breaking-change 21.0.0
  */
 export const matDrawerAnimations: {
-  readonly transformDrawer: AnimationTriggerMetadata;
+  readonly transformDrawer: any;
 } = {
+  // Represents
+  // trigger('transform', [
+  //   // We remove the `transform` here completely, rather than setting it to zero, because:
+  //   // 1. Having a transform can cause elements with ripples or an animated
+  //   //    transform to shift around in Chrome with an RTL layout (see #10023).
+  //   // 2. 3d transforms causes text to appear blurry on IE and Edge.
+  //   state(
+  //     'open, open-instant',
+  //     style({
+  //       'transform': 'none',
+  //       'visibility': 'visible',
+  //     }),
+  //   ),
+  //   state(
+  //     'void',
+  //     style({
+  //       // Avoids the shadow showing up when closed in SSR.
+  //       'box-shadow': 'none',
+  //       'visibility': 'hidden',
+  //     }),
+  //   ),
+  //   transition('void => open-instant', animate('0ms')),
+  //   transition(
+  //     'void <=> open, open-instant => void',
+  //     animate('400ms cubic-bezier(0.25, 0.8, 0.25, 1)'),
+  //   ),
+  // ])
+
   /** Animation that slides a drawer in and out. */
-  transformDrawer: trigger('transform', [
-    // We remove the `transform` here completely, rather than setting it to zero, because:
-    // 1. Having a transform can cause elements with ripples or an animated
-    //    transform to shift around in Chrome with an RTL layout (see #10023).
-    // 2. 3d transforms causes text to appear blurry on IE and Edge.
-    state(
-      'open, open-instant',
-      style({
-        'transform': 'none',
-        'visibility': 'visible',
-      }),
-    ),
-    state(
-      'void',
-      style({
-        // Avoids the shadow showing up when closed in SSR.
-        'box-shadow': 'none',
-        'visibility': 'hidden',
-      }),
-    ),
-    transition('void => open-instant', animate('0ms')),
-    transition(
-      'void <=> open, open-instant => void',
-      animate('400ms cubic-bezier(0.25, 0.8, 0.25, 1)'),
-    ),
-  ]),
+  transformDrawer: {
+    type: 7,
+    name: 'transform',
+    definitions: [
+      {
+        type: 0,
+        name: 'open, open-instant',
+        styles: {
+          type: 6,
+          styles: {transform: 'none', visibility: 'visible'},
+          offset: null,
+        },
+      },
+      {
+        type: 0,
+        name: 'void',
+        styles: {
+          type: 6,
+          styles: {'box-shadow': 'none', visibility: 'hidden'},
+          offset: null,
+        },
+      },
+      {
+        type: 1,
+        expr: 'void => open-instant',
+        animation: {type: 4, styles: null, timings: '0ms'},
+        options: null,
+      },
+      {
+        type: 1,
+        expr: 'void <=> open, open-instant => void',
+        animation: {
+          type: 4,
+          styles: null,
+          timings: '400ms cubic-bezier(0.25, 0.8, 0.25, 1)',
+        },
+        options: null,
+      },
+    ],
+    options: {},
+  },
 };
