5d3f43c15088a16dc995e44261e2207ca56c51df
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-02-02 05:22:52 -0800

    fix(material/tooltip): remove remaining animation dependencies
    Removes the remaning dependencies on `@angular/animations` from the package.

--- src/material/tooltip/tooltip-animations.ts
+++ src/material/tooltip/tooltip-animations.ts
@@ -5,14 +5,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
-import {
-  animate,
-  AnimationTriggerMetadata,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
 
 /**
  * Animations used by MatTooltip.
@@ -21,15 +13,44 @@ import {
  * @breaking-change 21.0.0
  */
 export const matTooltipAnimations: {
-  readonly tooltipState: AnimationTriggerMetadata;
+  readonly tooltipState: any;
 } = {
+  // Represents:
+  // trigger('state', [
+  //   state('initial, void, hidden', style({opacity: 0, transform: 'scale(0.8)'})),
+  //   state('visible', style({transform: 'scale(1)'})),
+  //   transition('* => visible', animate('150ms cubic-bezier(0, 0, 0.2, 1)')),
+  //   transition('* => hidden', animate('75ms cubic-bezier(0.4, 0, 1, 1)')),
+  // ])
+
   /** Animation that transitions a tooltip in and out. */
-  tooltipState: trigger('state', [
-    // TODO(crisbeto): these values are based on MDC's CSS.
-    // We should be able to use their styles directly once we land #19432.
-    state('initial, void, hidden', style({opacity: 0, transform: 'scale(0.8)'})),
-    state('visible', style({transform: 'scale(1)'})),
-    transition('* => visible', animate('150ms cubic-bezier(0, 0, 0.2, 1)')),
-    transition('* => hidden', animate('75ms cubic-bezier(0.4, 0, 1, 1)')),
-  ]),
+  tooltipState: {
+    type: 7,
+    name: 'state',
+    definitions: [
+      {
+        type: 0,
+        name: 'initial, void, hidden',
+        styles: {type: 6, styles: {opacity: 0, transform: 'scale(0.8)'}, offset: null},
+      },
+      {
+        type: 0,
+        name: 'visible',
+        styles: {type: 6, styles: {transform: 'scale(1)'}, offset: null},
+      },
+      {
+        type: 1,
+        expr: '* => visible',
+        animation: {type: 4, styles: null, timings: '150ms cubic-bezier(0, 0, 0.2, 1)'},
+        options: null,
+      },
+      {
+        type: 1,
+        expr: '* => hidden',
+        animation: {type: 4, styles: null, timings: '75ms cubic-bezier(0.4, 0, 1, 1)'},
+        options: null,
+      },
+    ],
+    options: {},
+  },
 };
