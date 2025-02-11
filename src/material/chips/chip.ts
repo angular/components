0ac0290bb98f7ed14fd39fd1797980a7245e6b74
Karl Seamon <bannanafiend@gmail.com>
2025-02-11 12:39:51 -0500

    perf(multiple): Don't double-inject the style loader. (#30467)
    inject() is pretty cheap, but not free.
    
    (cherry picked from commit 1ddb9cf0569f52042c59ec535e21d885245b532a)

--- src/material/chips/chip.ts
+++ src/material/chips/chip.ts
@@ -242,8 +242,9 @@ export class MatChip implements OnInit, AfterViewInit, AfterContentInit, DoCheck
   constructor(...args: unknown[]);
 
   constructor() {
-    inject(_CdkPrivateStyleLoader).load(_StructuralStylesLoader);
-    inject(_CdkPrivateStyleLoader).load(_VisuallyHiddenLoader);
+    const styleLoader = inject(_CdkPrivateStyleLoader);
+    styleLoader.load(_StructuralStylesLoader);
+    styleLoader.load(_VisuallyHiddenLoader);
     const animationMode = inject(ANIMATION_MODULE_TYPE, {optional: true});
     this._animationsDisabled = animationMode === 'NoopAnimations';
     this._monitorFocus();
