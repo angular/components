c5a37f2d84108f8b9027818df64ebf0f9fbf8095
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-02-13 11:50:17 +0100

    fix(material-angular-io): remove animations dependency
    Drops the `@angular/animations` module from the docs site since it's no longer necessary. This removes another ~70kb from the initial bundle.

--- material.angular.io/src/main.ts
+++ material.angular.io/src/main.ts
@@ -5,13 +5,9 @@ import {unregisterServiceWorkers} from './unregister-service-workers';
 import {MaterialDocsApp} from './app/material-docs-app';
 import {MATERIAL_DOCS_ROUTES} from './app/routes';
 import {withInMemoryScrolling, provideRouter} from '@angular/router';
-import {provideAnimations, provideNoopAnimations} from '@angular/platform-browser/animations';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {AnalyticsErrorReportHandler} from './app/shared/analytics/error-report-handler';
 import {LocationStrategy, PathLocationStrategy} from '@angular/common';
-
-const prefersReducedMotion =
-  typeof matchMedia === 'function' ? matchMedia('(prefers-reduced-motion)').matches : false;
 
 // Unregister all installed service workers and force reload the page if there was
 // an old service worker from a previous version of the docs.
@@ -19,7 +15,6 @@ unregisterServiceWorkers().then(hadServiceWorker => hadServiceWorker && location
 
 bootstrapApplication(MaterialDocsApp, {
   providers: [
-    prefersReducedMotion ? provideNoopAnimations() : provideAnimations(),
     {provide: LocationStrategy, useClass: PathLocationStrategy},
     {provide: ErrorHandler, useClass: AnalyticsErrorReportHandler},
     provideRouter(
