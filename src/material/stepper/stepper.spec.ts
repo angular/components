97f25e244f6444577f514e81f67ffd8301bebea9
Dominik Rabij <dominikrabij@google.com>
2025-03-03 18:13:27 +0100

    fix(material/stepper): Replace `any` with `unknown`

--- src/material/stepper/stepper.spec.ts
+++ src/material/stepper/stepper.spec.ts
@@ -1617,7 +1617,7 @@ describe('MatStepper', () => {
 
 /** Asserts that keyboard interaction works correctly. */
 function assertCorrectKeyboardInteraction(
-  fixture: ComponentFixture<any>,
+  fixture: ComponentFixture<unknown>,
   stepHeaders: DebugElement[],
   orientation: StepperOrientation,
 ) {
@@ -1718,7 +1718,7 @@ function assertCorrectKeyboardInteraction(
 
 /** Asserts that arrow key direction works correctly in RTL mode. */
 function assertArrowKeyInteractionInRtl(
-  fixture: ComponentFixture<any>,
+  fixture: ComponentFixture<unknown>,
   stepHeaders: DebugElement[],
 ) {
   const stepperComponent = fixture.debugElement.query(By.directive(MatStepper))!.componentInstance;
@@ -1740,7 +1740,7 @@ function assertArrowKeyInteractionInRtl(
 
 /** Asserts that keyboard interaction works correctly when the user is pressing a modifier key. */
 function assertSelectKeyWithModifierInteraction(
-  fixture: ComponentFixture<any>,
+  fixture: ComponentFixture<unknown>,
   stepHeaders: DebugElement[],
   orientation: StepperOrientation,
   selectionKey: number,
@@ -1799,7 +1799,7 @@ function asyncValidator(minLength: number, validationTrigger: Subject<void>): As
 function createComponent<T>(
   component: Type<T>,
   providers: Provider[] = [],
-  imports: any[] = [],
+  imports: unknown[] = [],
   encapsulation?: ViewEncapsulation,
   declarations = [component],
 ): ComponentFixture<T> {
