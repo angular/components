b102193b299db9a708cbbd698781720fe7ce1571
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-03-31 12:10:16 +0000

    build: update integration tests to use first-party packages
    Updates integration tests to work with the linked first-party packages.

--- integration/ts-compat/helpers.mjs
+++ integration/ts-compat/helpers.mjs
@@ -1,21 +1,12 @@
-import {join} from 'path';
-import {unlinkSync} from 'fs';
+import path from 'path';
 import shelljs from 'shelljs';
 import {fork} from 'child_process';
-import {getNpmPackagesFromRunfiles} from '../npm-packages-from-runfiles.mjs';
-import {runfiles} from '@bazel/runfiles';
 
 // Exit if any command fails.
 shelljs.set('-e');
 
-// List of NPM packages that have been built for the current test target.
-const npmPackages = getNpmPackagesFromRunfiles();
-// Path to the node modules of the workspace.
-const nodeModulesDir = runfiles.resolve('npm/node_modules');
 // Path to the generated file that imports all entry-points.
-const testFilePath = runfiles.resolveWorkspaceRelative(
-  'integration/ts-compat/import-all-entry-points.ts',
-);
+const testFilePath = path.resolve('./import-all-entry-points.ts');
 
 /**
  * Runs the TypeScript compatibility test with the specified tsc binary. The
@@ -24,18 +15,6 @@ const testFilePath = runfiles.resolveWorkspaceRelative(
  */
 export async function runTypeScriptCompatibilityTest(tscBinPath) {
   return new Promise((resolve, reject) => {
-    const angularDir = join(nodeModulesDir, '@angular/');
-
-    // Create the `node_modules/@angular` directory in case it's not present.
-    shelljs.mkdir('-p', angularDir);
-
-    // Symlink npm packages into `node_modules/` so that the project can
-    // be compiled without path mappings (simulating a real project).
-    for (const {name, pkgPath} of npmPackages) {
-      console.info(`Linking "@angular/${name}" into node modules..`);
-      shelljs.ln('-sf', pkgPath, join(angularDir, name));
-    }
-
     const tscArgs = [
       '--strict',
       // Disables automatic type resolution. In non-sandbox environments, the node modules
@@ -43,10 +22,6 @@ export async function runTypeScriptCompatibilityTest(tscBinPath) {
       '--types',
       '--lib',
       'es2015,dom',
-      // Ensures that `node_modules` can be resolved. By default, in sandbox environments the
-      // node modules cannot be resolved because they are wrapped in the `npm/node_modules` folder
-      '--baseUrl',
-      nodeModulesDir,
       testFilePath,
     ];
     // Run `tsc` to compile the project. The stdout/stderr output is inherited, so that
@@ -54,12 +29,6 @@ export async function runTypeScriptCompatibilityTest(tscBinPath) {
     const tscProcess = fork(tscBinPath, tscArgs, {stdio: 'inherit'});
 
     tscProcess.on('exit', exitCode => {
-      // Remove symlinks to keep a clean repository state.
-      for (const {name} of npmPackages) {
-        console.info(`Removing link for "@angular/${name}"..`);
-        unlinkSync(join(angularDir, name));
-        shelljs.rm('-rf', join(angularDir, name));
-      }
       exitCode === 0 ? resolve() : reject();
     });
   });
