731c95267f6cf2b0710720a9617eea7eb3b11e1b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-03-31 14:33:35 +0000

    build: migrate all cross-package targets to use first-party links
    Instead of e.g. depending on `src/cdk/testing`, we should use the
    first-party linked package so that npm-module imports like
    `@angular/cdk` still work. This is the new approach for `rules_js`. It's
    notably slower, but with the simplicity gained, it's an acceptable
    trade-off we are taking.

--- src/universal-app/main.ts
+++ src/universal-app/main.ts
@@ -1,3 +1,6 @@
+// TODO(devversion): Remove when APF ships pre-linked output.
+import '@angular/compiler';
+
 import {bootstrapApplication, provideClientHydration} from '@angular/platform-browser';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {AUTOMATED_KITCHEN_SINK, KitchenSink} from './kitchen-sink/kitchen-sink';
