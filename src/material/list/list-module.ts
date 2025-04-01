b7cae25cea41638f8d667b194b77acd453660658
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-04-01 07:41:45 +0000

    build: fix some remaining relative imports
    Fixes that some remaining imports weren't updated to be relative.

--- src/material/list/list-module.ts
+++ src/material/list/list-module.ts
@@ -8,7 +8,7 @@
 
 import {NgModule} from '@angular/core';
 import {MatPseudoCheckboxModule, MatRippleModule, MatCommonModule} from '../core';
-import {MatDividerModule, MatDivider} from '@angular/material/divider';
+import {MatDividerModule, MatDivider} from '../divider';
 import {MatActionList} from './action-list';
 import {MatList, MatListItem} from './list';
 import {MatListOption} from './list-option';
