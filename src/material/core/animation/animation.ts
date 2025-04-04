f15eae59a654c49512d07487344a89e738f07d07
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-04-04 09:21:16 +0200

    feat(material/core): handle prefers-reduced-motion automatically
    Adds some logic so that animations automatically get disabled if the `prefers-reduced-motion` media query matches.

--- src/material/core/animation/animation.ts
+++ src/material/core/animation/animation.ts
@@ -6,12 +6,13 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
+import {MediaMatcher} from '@angular/cdk/layout';
 import {ANIMATION_MODULE_TYPE, inject, InjectionToken} from '@angular/core';
 
 /** Object used to configure the animation in Angular Material. */
 export interface AnimationsConfig {
   /** Whether all animations should be disabled. */
-  animationsDisabled: boolean;
+  animationsDisabled?: boolean;
 }
 
 /** Injection token used to configure the animations in Angular Material. */
@@ -45,11 +46,13 @@ export class AnimationDurations {
  * @docs-private
  */
 export function _animationsDisabled(): boolean {
-  const customToken = inject(MATERIAL_ANIMATIONS, {optional: true});
-
-  if (customToken) {
-    return customToken.animationsDisabled;
+  if (
+    inject(MATERIAL_ANIMATIONS, {optional: true})?.animationsDisabled ||
+    inject(ANIMATION_MODULE_TYPE, {optional: true}) === 'NoopAnimations'
+  ) {
+    return true;
   }
 
-  return inject(ANIMATION_MODULE_TYPE, {optional: true}) === 'NoopAnimations';
+  const mediaMatcher = inject(MediaMatcher);
+  return mediaMatcher.matchMedia('(prefers-reduced-motion)').matches;
 }
