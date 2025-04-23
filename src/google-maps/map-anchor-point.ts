76cd0a0761c48008a668476bd2b47f88d2e754e6
Joey Perrott <josephperrott@gmail.com>
2025-04-23 18:19:41 +0000

    fix(google-maps): implicitly include google.maps types instead of the triple slash workaround
    Previously, due to an issue with the rules_nodejs toolchain we had to include the google.maps namespace types via a triple
    slash comment with preserve set to true, this changes to instead include the types in the tsconfig as normal

--- src/google-maps/map-anchor-point.ts
+++ src/google-maps/map-anchor-point.ts
@@ -6,9 +6,6 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-// Workaround for: https://github.com/bazelbuild/rules_nodejs/issues/1265
-/// <reference types="google.maps" preserve="true" />
-
 export interface MapAnchorPoint {
   getAnchor(): google.maps.MVCObject | google.maps.marker.AdvancedMarkerElement;
 }
