af0b4638f2a55d4e8280c12a66f5eb9a0c949d9c
Karl Seamon <bannanafiend@gmail.com>
2025-05-13 15:47:58 -0400

    fix(cdk-experimental/column-resize): Obscure weird behavior in tables that are too wide by conditionally disabling resize (#31100)
    (cherry picked from commit 49f99668669b8fb52124450fecbb8e36a738d02a)

--- src/cdk-experimental/column-resize/resizable.ts
+++ src/cdk-experimental/column-resize/resizable.ts
@@ -40,6 +40,7 @@ import {ResizeRef} from './resize-ref';
 import {ResizeStrategy} from './resize-strategy';
 
 const OVERLAY_ACTIVE_CLASS = 'cdk-resizable-overlay-thumb-active';
+const RESIZE_DISABLED_CLASS = 'cdk-resizable-resize-disabled';
 
 /**
  * Base class for Resizable directives which are applied to column headers to make those columns
@@ -181,11 +182,18 @@ export abstract class Resizable<HandleComponent extends ResizeOverlayHandle>
       .pipe(takeUntilDestroyed)
       .subscribe(hoveringRow => {
         if (hoveringRow) {
-          if (!this.overlayRef) {
-            this.overlayRef = this._createOverlayForHandle();
-          }
+          const tooBigToResize =
+            this.maxWidthPxInternal < Number.MAX_SAFE_INTEGER &&
+            element.offsetWidth > this.maxWidthPxInternal;
+          element.classList.toggle(RESIZE_DISABLED_CLASS, tooBigToResize);
 
-          this._showHandleOverlay();
+          if (!tooBigToResize) {
+            if (!this.overlayRef) {
+              this.overlayRef = this._createOverlayForHandle();
+            }
+
+            this._showHandleOverlay();
+          }
         } else if (this.overlayRef) {
           // todo - can't detach during an active resize - need to work that out
           this.overlayRef.detach();
