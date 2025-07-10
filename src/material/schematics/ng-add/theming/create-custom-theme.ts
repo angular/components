b515c78e60485a25350df7b7d4d5dabc25bcdcc3
Andrew Seguin <andrewjs@google.com>
2025-07-10 15:59:01 -0600

    fix(material/schematics): always add a custom theme with ng add

--- src/material/schematics/ng-add/theming/create-custom-theme.ts
+++ src/material/schematics/ng-add/theming/create-custom-theme.ts
@@ -7,21 +7,40 @@
  */
 
 /** Create custom theme for the given application configuration. */
-export function createCustomTheme(name: string = 'app') {
+export function createCustomTheme(userPaletteChoice: string) {
+  const colorPalettes = new Map<string, {primary: string; tertiary: string}>([
+    ['azure-blue', {primary: 'azure', tertiary: 'blue'}],
+    ['rose-red', {primary: 'rose', tertiary: 'red'}],
+    ['magenta-violet', {primary: 'magenta', tertiary: 'violet'}],
+    ['cyan-orange', {primary: 'cyan', tertiary: 'orange'}],
+  ]);
   return `
-// Custom Theming for Angular Material
-// For more information: https://material.angular.dev/guide/theming
+// Include theming for Angular Material with \`mat.theme()\`.
+// This Sass mixin will define CSS variables that are used for styling Angular Material
+// components according to the Material 3 design spec.
+// Learn more about theming and how to use it for your application's
+// custom components at https://material.angular.dev/guide/theming
 @use '@angular/material' as mat;
 
 html {
   @include mat.theme((
     color: (
-      theme-type: light,
-      primary: mat.$azure-palette,
-      tertiary: mat.$blue-palette,
+      primary: mat.$${colorPalettes.get(userPaletteChoice)!.primary}-palette,
+      tertiary: mat.$${colorPalettes.get(userPaletteChoice)!.tertiary}-palette,
     ),
     typography: Roboto,
     density: 0,
   ));
+
+  // Default the application to a light color theme. This can be changed to
+  // \`dark\` to enable the dark color theme, or to \`light dark\` to defer to the
+  // user's system settings.
+  color-scheme: light;
+
+  // Set a default background, font and text colors for the application using
+  // Angular Material's system-level CSS variables.
+  background-color: var(--mat-sys-surface);
+  color: var(--mat-sys-on-surface);
+  font: var(--mat-sys-body-medium);
 }`;
 }
