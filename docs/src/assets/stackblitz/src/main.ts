ced4165db98a3dd4ebd28b68b56ac296351a3e7c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-07-15 18:36:29 +0200

    docs: simplify stackblitz template (#31530)
    Simplifies the Stackblitz template by:
    1. Removing the native date adapter since the individual examples add it themselves.
    2. Dropping animations both from the code and the package.json.
    3. Updating a dependency that was causing an error to be logged when installed.
    
    (cherry picked from commit 2efed4ae32a7a1fedb1a34b6d7cc046460a0e5d4)

--- docs/src/assets/stackblitz/src/main.ts
+++ docs/src/assets/stackblitz/src/main.ts
@@ -8,14 +8,13 @@
 
 import {bootstrapApplication} from '@angular/platform-browser';
 import {provideHttpClient} from '@angular/common/http';
-import {provideAnimations} from '@angular/platform-browser/animations';
 import {VERSION as CDK_VERSION} from '@angular/cdk';
-import {VERSION as MAT_VERSION, provideNativeDateAdapter} from '@angular/material/core';
+import {VERSION as MAT_VERSION} from '@angular/material/core';
 import {MaterialDocsExample} from './example/material-docs-example';
 
 console.info('Angular CDK version', CDK_VERSION.full);
 console.info('Angular Material version', MAT_VERSION.full);
 
 bootstrapApplication(MaterialDocsExample, {
-  providers: [provideAnimations(), provideHttpClient(), provideNativeDateAdapter()],
+  providers: [provideHttpClient()],
 }).catch(err => console.error(err));
