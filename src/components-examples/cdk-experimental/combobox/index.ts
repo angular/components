d9ade63732fd7639b53022cd01cd85e637b169dc
Wagner Maciel <wagnermaciel@google.com>
2025-09-15 11:52:41 -0400

    docs(cdk-experimental/combobox): add component examples

---
+++ src/components-examples/cdk-experimental/combobox/index.ts
@@ -0,0 +1,3 @@
+export {CdkComboboxManualExample} from './cdk-combobox-manual/cdk-combobox-manual-example';
+export {CdkComboboxAutoSelectExample} from './cdk-combobox-auto-select/cdk-combobox-auto-select-example';
+export {CdkComboboxHighlightExample} from './cdk-combobox-highlight/cdk-combobox-highlight-example';
