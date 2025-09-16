0f4fb548a8a59e8aaf64213ce59d388fa9e4ba9d
Wagner Maciel <wagnermaciel@google.com>
2025-09-16 16:55:31 -0400

    fixup! feat(cdk-experimental/ui-patterns): create combobox ui pattern

--- src/cdk-experimental/ui-patterns/listbox/listbox.spec.ts
+++ src/cdk-experimental/ui-patterns/listbox/listbox.spec.ts
@@ -46,7 +46,6 @@ describe('Listbox Pattern', () => {
       orientation: inputs.orientation ?? signal('vertical'),
       selectionMode: inputs.selectionMode ?? signal('explicit'),
       element: signal(document.createElement('div')),
-      combobox: signal(undefined),
     });
   }
 
