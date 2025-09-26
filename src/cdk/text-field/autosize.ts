edb9efbdcccb7d38a735444d3ea98bd8485a23f6
Cheng-Hsuan Tsai <webcat@google.com>
2025-09-26 13:57:34 -0700

    Revert "fix(cdk/text-field): Long multiline textfield focus issue (#30312)" (#31891)
    This reverts commit dc70692abafe578a140b7e2b6020663643cd271e.
    
    The original PR caused autosized textarea no longer auto scroll to caret position when overflow.
    
    (cherry picked from commit a2b2b9bcd8fb19a5d22206ea526105bb0111868d)

--- src/cdk/text-field/autosize.ts
+++ src/cdk/text-field/autosize.ts
@@ -118,8 +118,6 @@ export class CdkTextareaAutosize implements AfterViewInit, DoCheck, OnDestroy {
   private _cachedLineHeight?: number;
   /** Cached height of a textarea with only the placeholder. */
   private _cachedPlaceholderHeight?: number;
-  /** Cached scroll top of a textarea */
-  private _cachedScrollTop: number;
 
   /** Used to reference correct document/window */
   protected _document = inject(DOCUMENT);
@@ -306,7 +304,6 @@ export class CdkTextareaAutosize implements AfterViewInit, DoCheck, OnDestroy {
 
     this._cacheTextareaLineHeight();
     this._cacheTextareaPlaceholderHeight();
-    this._cachedScrollTop = this._textareaElement.scrollTop;
 
     // If we haven't determined the line-height yet, we know we're still hidden and there's no point
     // in checking the height of the textarea.
@@ -371,7 +368,6 @@ export class CdkTextareaAutosize implements AfterViewInit, DoCheck, OnDestroy {
     // it to receive focus on IE and Edge.
     if (!this._destroyed.isStopped && this._hasFocus) {
       textarea.setSelectionRange(selectionStart, selectionEnd);
-      textarea.scrollTop = this._cachedScrollTop;
     }
   }
 }
