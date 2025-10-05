40ab441f7c54354b55789e36868df0388f359896
Ruslan Lekhman <lekhman112@gmail.com>
2025-10-04 22:55:59 -0600

    docs(material/list): single selection example
    - Revert single selection example from #29348
    
    close #31989

--- src/components-examples/material/list/list-single-selection/list-single-selection-example.ts
+++ src/components-examples/material/list/list-single-selection/list-single-selection-example.ts
@@ -1,32 +1,14 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatListModule} from '@angular/material/list';
-interface Shoes {
-  value: string;
-  name: string;
-}
+
 /**
- * @title List with single selection using Reactive Forms
+ * @title List with single selection
  */
 @Component({
   selector: 'list-single-selection-example',
   templateUrl: 'list-single-selection-example.html',
-  imports: [MatListModule, FormsModule, ReactiveFormsModule],
+  imports: [MatListModule],
 })
 export class ListSingleSelectionExample {
-  form: FormGroup;
-  shoes: Shoes[] = [
-    {value: 'boots', name: 'Boots'},
-    {value: 'clogs', name: 'Clogs'},
-    {value: 'loafers', name: 'Loafers'},
-    {value: 'moccasins', name: 'Moccasins'},
-    {value: 'sneakers', name: 'Sneakers'},
-  ];
-  shoesControl = new FormControl();
-
-  constructor() {
-    this.form = new FormGroup({
-      clothes: this.shoesControl,
-    });
-  }
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
