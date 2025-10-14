105d63ac48254fad54251cd9761ccffcdc4fdb81
Wagner Maciel <wagnermaciel@google.com>
2025-10-14 18:35:40 -0400

    feat(aria/ui-patterns): add initial menu pattern
    * Adds the initial implementation of the WAI-ARIA menu, menubar, and menuitem patterns. This
    includes the basic behaviors for keyboard navigation, opening and closing submenus, and
    typeahead support.
    * This also introduces a 'focusElement' option to the list navigation behaviors to allow for
    moving the active item without focusing it.

--- src/aria/ui-patterns/public-api.ts
+++ src/aria/ui-patterns/public-api.ts
@@ -10,6 +10,7 @@ export * from './combobox/combobox';
 export * from './listbox/listbox';
 export * from './listbox/option';
 export * from './listbox/combobox-listbox';
+export * from './menu/menu';
 export * from './radio-group/radio-group';
 export * from './radio-group/radio-button';
 export * from './radio-group/toolbar-radio-group';
