a0322f3073fdfeae17e646d0e9e2e0024595c0f6
Joy Serquina <serquina@google.com>
2025-10-18 02:28:13 +0000

    feature(aria/toolbar): implement radiogroup in toolbar basic example
    Adds radiogroup UI into the toolbar basic horizontal example. Also
    attempts to improve accessibility by adding LiveAnnouncer on button
    activation.

--- src/components-examples/aria/toolbar/toolbar-basic-horizontal/toolbar-basic-horizontal-example.ts
+++ src/components-examples/aria/toolbar/toolbar-basic-horizontal/toolbar-basic-horizontal-example.ts
@@ -1,15 +1,28 @@
 import {Component} from '@angular/core';
 import {Toolbar, ToolbarWidget} from '@angular/aria/toolbar';
+import {RadioGroup, RadioButton} from '@angular/aria/radio-group';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
 
 /** @title Basic Horizontal Toolbar Example */
 @Component({
   selector: 'toolbar-basic-horizontal-example',
   templateUrl: 'toolbar-basic-horizontal-example.html',
   styleUrl: '../toolbar-common.css',
-  imports: [Toolbar, ToolbarWidget],
+  imports: [RadioButton, RadioGroup, Toolbar, ToolbarWidget],
 })
 export class ToolbarBasicHorizontalExample {
+  constructor(private liveAnnouncer: LiveAnnouncer) {}
+  alignments = [
+    {value: 'left', label: 'Left'},
+    {value: 'center', label: 'Center'},
+    {value: 'right', label: 'Right'},
+  ];
   format(tool: string) {
     console.log(`Tool activated: ${tool}`);
+    this.liveAnnouncer.announce(`${tool} applied`, 'polite');
+  }
+  test(action: string) {
+    console.log(`Action triggered: ${action}`);
+    this.liveAnnouncer.announce(`${action} button activated`, 'polite');
   }
 }
