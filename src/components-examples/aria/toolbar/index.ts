38c4dad8743df5a864eb08462328bd09cdbd5036
Joy Serquina <serquina@google.com>
2025-10-20 21:59:53 +0000

    feat(aria/toolbar): adds skip disabled toolbar example to dev-app
    Updates component-examples to add a toolbar skip disabled example
    and adds it to the dev-app for accessibility testing.

--- src/components-examples/aria/toolbar/index.ts
+++ src/components-examples/aria/toolbar/index.ts
@@ -1,2 +1,3 @@
 export {ToolbarBasicHorizontalExample} from './toolbar-basic-horizontal/toolbar-basic-horizontal-example';
 export {ToolbarConfigurableExample} from './toolbar-configurable/toolbar-configurable-example';
+export {ToolbarSkipDisabledExample} from './toolbar-skip-disabled/toolbar-skip-disabled-example';
