1fb58e94615143f9e6758f79d51c9a8dcb9d7726
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-22 20:02:12 +0200

    refactor(cdk/overlay): add opt-in popover behavior
    Adds the option for the connected overlay to opt into being rendered into a native popover, instead of the overlay container.

--- src/cdk/overlay/position/position-strategy.ts
+++ src/cdk/overlay/position/position-strategy.ts
@@ -21,4 +21,31 @@ export interface PositionStrategy {
 
   /** Cleans up any DOM modifications made by the position strategy, if necessary. */
   dispose(): void;
+
+  /**
+   * Creates the structure of the overlay. If not provided or if it returns null,
+   * structure will be created inside the overlay container.
+   */
+  createStructure?(): {pane: HTMLElement; host: HTMLElement} | null;
+
+  /**
+   * Attaches the host element to the DOM. The return value indicates whether the
+   * position strategy ended up attaching the host. If it didn't, it will fall back
+   * to the default behavior.
+   */
+  attachHost?(host: HTMLElement): boolean;
+
+  /**
+   * Attaches the backdrop element to the host. The return value indicates whether the
+   * position strategy ended up attaching the backdrop. If it didn't, it will fall back
+   * to the default behavior.
+   */
+  attachBackdrop?(backdrop: HTMLElement, host: HTMLElement): boolean;
+
+  /**
+   * Updates the stacking order of the overlay. The return value indicates whether the
+   * position strategy ended up updating the stacking order. If it hasn't, the overlay
+   * will fall back to the default stacking update logic.
+   */
+  updateStackingOrder?(host: HTMLElement): boolean;
 }
