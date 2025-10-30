4711d53bbe881495cf036228d12523432ee0785e
Wagner Maciel <wagnermaciel@google.com>
2025-10-30 13:39:05 -0400

    refactor: delete aria radio group (#32216)

--- src/components-examples/aria/toolbar/toolbar-rtl/toolbar-rtl-example.ts
+++ src/components-examples/aria/toolbar/toolbar-rtl/toolbar-rtl-example.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {Toolbar, ToolbarWidget} from '@angular/aria/toolbar';
-import {RadioGroup, RadioButton} from '@angular/aria/radio-group';
 import {LiveAnnouncer} from '@angular/cdk/a11y';
 
 /** @title Basic RTL Toolbar Example */
@@ -8,15 +7,10 @@ import {LiveAnnouncer} from '@angular/cdk/a11y';
   selector: 'toolbar-rtl-example',
   templateUrl: 'toolbar-rtl-example.html',
   styleUrl: '../toolbar-common.css',
-  imports: [RadioButton, RadioGroup, Toolbar, ToolbarWidget],
+  imports: [Toolbar, ToolbarWidget],
 })
 export class ToolbarRtlExample {
   constructor(private _liveAnnouncer: LiveAnnouncer) {}
-  alignments = [
-    {value: 'left', label: 'Left'},
-    {value: 'center', label: 'Center'},
-    {value: 'right', label: 'Right'},
-  ];
   format(tool: string) {
     console.log(`Tool activated: ${tool}`);
     this._liveAnnouncer.announce(`${tool} applied`, 'polite');
