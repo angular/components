c345614c933847582a0f91ff4902221db8c862d5
Wagner Maciel <wagnermaciel@google.com>
2025-11-05 18:17:34 -0500

    fix(aria/combobox): several small fixes (#32202)
    * fix(aria/combobox): do not open on click
    
    * fix(aria/combobox): handle unselectable tree items
    
    * fix(aria/combobox): multi selection
    
    * docs(aria/combobox): multi select examples

--- src/components-examples/aria/combobox/index.ts
+++ src/components-examples/aria/combobox/index.ts
@@ -1,7 +1,9 @@
 export {ComboboxManualExample} from './combobox-manual/combobox-manual-example';
 export {ComboboxAutoSelectExample} from './combobox-auto-select/combobox-auto-select-example';
 export {ComboboxHighlightExample} from './combobox-highlight/combobox-highlight-example';
+export {ComboboxReadonlyExample} from './combobox-readonly/combobox-readonly-example';
+export {ComboboxReadonlyMultiselectExample} from './combobox-readonly-multiselect/combobox-readonly-multiselect-example';
+
 export {ComboboxTreeManualExample} from './combobox-tree-manual/combobox-tree-manual-example';
 export {ComboboxTreeAutoSelectExample} from './combobox-tree-auto-select/combobox-tree-auto-select-example';
 export {ComboboxTreeHighlightExample} from './combobox-tree-highlight/combobox-tree-highlight-example';
-export {ComboboxReadonlyExample} from './combobox-readonly/combobox-readonly-example';
