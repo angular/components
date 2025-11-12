8ff0e7919122ab7d829f44a702a42473e962227f
Wagner Maciel <wagnermaciel@google.com>
2025-11-12 13:36:11 -0500

    fix(aria/combobox): disabled state (#32308)

--- src/components-examples/aria/combobox/index.ts
+++ src/components-examples/aria/combobox/index.ts
@@ -2,8 +2,11 @@ export {ComboboxDialogExample} from './combobox-dialog/combobox-dialog-example';
 export {ComboboxManualExample} from './combobox-manual/combobox-manual-example';
 export {ComboboxAutoSelectExample} from './combobox-auto-select/combobox-auto-select-example';
 export {ComboboxHighlightExample} from './combobox-highlight/combobox-highlight-example';
+export {ComboboxDisabledExample} from './combobox-disabled/combobox-disabled-example';
+
 export {ComboboxReadonlyExample} from './combobox-readonly/combobox-readonly-example';
 export {ComboboxReadonlyMultiselectExample} from './combobox-readonly-multiselect/combobox-readonly-multiselect-example';
+export {ComboboxReadonlyDisabledExample} from './combobox-readonly-disabled/combobox-readonly-disabled-example';
 
 export {ComboboxTreeManualExample} from './combobox-tree-manual/combobox-tree-manual-example';
 export {ComboboxTreeAutoSelectExample} from './combobox-tree-auto-select/combobox-tree-auto-select-example';
