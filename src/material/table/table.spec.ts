1f6504bfd6f57ef5b5fbc9887204fbe9830fe0bb
Dominik Rabij <dominikrabij@google.com>
2025-11-21 14:46:26 +0100

    refactor(multiple): Remove any in sort, stepper, table, timepicker, tooltip, tree

--- src/material/table/table.spec.ts
+++ src/material/table/table.spec.ts
@@ -16,9 +16,9 @@ describe('MatTable', () => {
       const data = fixture.componentInstance.dataSource!.data;
       expectTableToMatchContent(tableElement, [
         ['Column A', 'Column B', 'Column C'],
-        [data[0].a, data[0].b, data[0].c],
-        [data[1].a, data[1].b, data[1].c],
-        [data[2].a, data[2].b, data[2].c],
+        [data[0].a, data[0].b, data[0].c] as string[],
+        [data[1].a, data[1].b, data[1].c] as string[],
+        [data[2].a, data[2].b, data[2].c] as string[],
         ['fourth_row'],
         ['Footer A', 'Footer B', 'Footer C'],
       ]);
@@ -64,10 +64,10 @@ describe('MatTable', () => {
       const data = fixture.componentInstance.dataSource!.data;
       expectTableToMatchContent(tableElement, [
         ['Column A', 'Column B', 'Column C'],
-        [data[0].a, data[0].b, data[0].c],
-        [data[1].a, data[1].b, data[1].c],
-        [data[2].a, data[2].b, data[2].c],
-        [data[3].a, data[3].b, data[3].c],
+        [data[0].a, data[0].b, data[0].c] as string[],
+        [data[1].a, data[1].b, data[1].c] as string[],
+        [data[2].a, data[2].b, data[2].c] as string[],
+        [data[3].a, data[3].b, data[3].c] as string[],
       ]);
     });
 
@@ -159,9 +159,9 @@ describe('MatTable', () => {
     const data = fixture.componentInstance.dataSource!.data;
     expectTableToMatchContent(tableElement, [
       ['Column A', 'Column B', 'Column C'],
-      [data[0].a, data[0].b, data[0].c],
-      [data[1].a, data[1].b, data[1].c],
-      [data[2].a, data[2].b, data[2].c],
+      [data[0].a, data[0].b, data[0].c] as string[],
+      [data[1].a, data[1].b, data[1].c] as string[],
+      [data[2].a, data[2].b, data[2].c] as string[],
     ]);
   });
 
@@ -173,9 +173,9 @@ describe('MatTable', () => {
     const data = fixture.componentInstance.dataSource!.data;
     expectTableToMatchContent(tableElement, [
       ['Column A', 'Column B', 'Column C'],
-      [data[0].a, data[0].b, data[0].c],
-      [data[1].a, data[1].b, data[1].c],
-      [data[2].a, data[2].b, data[2].c],
+      [data[0].a, data[0].b, data[0].c] as string[],
+      [data[1].a, data[1].b, data[1].c] as string[],
+      [data[2].a, data[2].b, data[2].c] as string[],
     ]);
   });
 
@@ -357,7 +357,7 @@ describe('MatTable', () => {
       ]);
 
       // Change the filter to a falsy value that might come in from the view.
-      dataSource.filter = 0 as any;
+      dataSource.filter = 0 as unknown as string;
       flushMicrotasks();
       fixture.detectChanges();
       expectTableToMatchContent(tableElement, [
@@ -604,7 +604,7 @@ describe('MatTable', () => {
         ['Footer A', 'Footer B', 'Footer C'],
       ]);
 
-      dataSource.data = {} as any;
+      dataSource.data = {} as TestData[];
       fixture.changeDetectorRef.markForCheck();
       fixture.detectChanges();
       expectTableToMatchContent(tableElement, [
@@ -1108,7 +1108,7 @@ function getActualTableContent(tableElement: Element): string[][] {
   return actualTableContent.map(row => row.map(cell => cell.textContent!.trim()));
 }
 
-export function expectTableToMatchContent(tableElement: Element, expected: any[]) {
+export function expectTableToMatchContent(tableElement: Element, expected: string[][]) {
   const missedExpectations: string[] = [];
   function checkCellContent(actualCell: string, expectedCell: string) {
     if (actualCell !== expectedCell) {
@@ -1134,7 +1134,7 @@ export function expectTableToMatchContent(tableElement: Element, expected: any[]
     }
 
     row.forEach((actualCell, cellIndex) => {
-      const expectedCell = expectedRow ? expectedRow[cellIndex] : null;
+      const expectedCell = expectedRow[cellIndex];
       checkCellContent(actualCell, expectedCell);
     });
   });
