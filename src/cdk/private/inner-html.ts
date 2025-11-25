3a7b7cf0ac4f9795fd9fbfee12a922570f93f126
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-11-25 14:02:59 +0100

    refactor: set up function for setting innerHTML
    When setting `innerHTML`, we have to go through a different API internally. These changes add a file that we can replace during the sync process.

---
+++ src/cdk/private/inner-html.ts
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
+import {SecurityContext} from '@angular/core';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
+import {trustedHTMLFromString} from './trusted-types';
+
+// !!!Note!!! this file isn't synced into g3, but is replaced with a version that uses
+// internal-specific APIs. The internal version may have to be updated if the signature of
+// the function changes.
+
+/** Sanitizes and sets the `innerHTML` of an element. */
+export function _setInnerHtml(element: HTMLElement, html: SafeHtml, sanitizer: DomSanitizer): void {
+  const cleanHtml = sanitizer.sanitize(SecurityContext.HTML, html);
+
+  if (cleanHtml === null && (typeof ngDevMode === 'undefined' || ngDevMode)) {
+    throw new Error(`Could not sanitize HTML: ${html}`);
+  }
+
+  element.innerHTML = trustedHTMLFromString(cleanHtml || '') as unknown as string;
+}
