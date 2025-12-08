dedc4643f202bf30cb2a1734a3cde1e0d4a3475d
Wagner Maciel <wagnermaciel@google.com>
2025-12-08 15:30:53 -0500

    docs: create aria autocomplete docs

---
+++ src/components-examples/aria/autocomplete/index.ts
@@ -0,0 +1,4 @@
+export {AutocompleteAutoSelectExample} from './autocomplete-auto-select/autocomplete-auto-select-example';
+export {AutocompleteManualExample} from './autocomplete-manual/autocomplete-manual-example';
+export {AutocompleteHighlightExample} from './autocomplete-highlight/autocomplete-highlight-example';
+export {AutocompleteDisabledExample} from './autocomplete-disabled/autocomplete-disabled-example';
