e63c5648e8b08931435c087a12d20305d385f5bc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-17 15:58:41 +0200

    fix(multiple): type issues when importing Aria directives (#32561)
    Similar fix to #32522 that aims to capture the remaining host directives.
    
    Related to https://github.com/angular/angular/issues/66069.

--- src/aria/accordion/public-api.ts
+++ src/aria/accordion/public-api.ts
@@ -13,4 +13,7 @@ export {AccordionContent} from './accordion-content';
 
 // This needs to be re-exported, because it's used by the accordion components.
 // See: https://github.com/angular/components/issues/30663.
-export {DeferredContent as ɵɵDeferredContent} from '../private';
+export {
+  DeferredContent as ɵɵDeferredContent,
+  DeferredContentAware as ɵɵDeferredContentAware,
+} from '../private';
--- src/aria/tabs/public-api.ts
+++ src/aria/tabs/public-api.ts
@@ -14,4 +14,7 @@ export {TabContent} from './tab-content';
 
 // This needs to be re-exported, because it's used by the tab components.
 // See: https://github.com/angular/components/issues/30663.
-export {DeferredContent as ɵɵDeferredContent} from '../private';
+export {
+  DeferredContent as ɵɵDeferredContent,
+  DeferredContentAware as ɵɵDeferredContentAware,
+} from '../private';
