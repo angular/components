54b8a635c3eb6ee021eafc87612251be3223380e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-02 08:36:59 +0100

    fix(multiple): not all symbols exported in some aria packages (#32591)
    Fixes that a couple of Aria packages weren't exporting all the necessary symbols.
    
    Fixes #32584.

--- src/aria/accordion/index.ts
+++ src/aria/accordion/index.ts
@@ -6,11 +6,4 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-export {AccordionPanel} from './accordion-panel';
-export {AccordionGroup} from './accordion-group';
-export {AccordionTrigger} from './accordion-trigger';
-export {AccordionContent} from './accordion-content';
-
-// This needs to be re-exported, because it's used by the accordion components.
-// See: https://github.com/angular/components/issues/30663.
-export {DeferredContent as ɵɵDeferredContent} from '../private';
+export * from './public-api';
