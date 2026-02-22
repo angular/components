ef2f1cb303a04e1d127fbc15819fff843ccd4749
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-02-22 08:36:17 +0100

    build: set up schematics for v22 (#32806)
    One of the pre-requisites to release the next version of v22 is to have schematics set up. These changes add them ahead of time so we don't run into any issues when we go to release.
    
    (cherry picked from commit dce147f6cac375983f1379a366dc40c46e8163e6)

--- src/cdk/schematics/ng-update/index.ts
+++ src/cdk/schematics/ng-update/index.ts
@@ -13,10 +13,20 @@ import {createMigrationSchematicRule, NullableDevkitMigration} from './devkit-mi
 
 const cdkMigrations: NullableDevkitMigration[] = [];
 
-/** Entry point for the migration schematics with target of Angular CDK 20.0.0 */
+/** Entry point for the migration schematics with target of Angular CDK 21.0.0 */
 export function updateToV21(): Rule {
   return createMigrationSchematicRule(
     TargetVersion.V21,
+    cdkMigrations,
+    cdkUpgradeData,
+    onMigrationComplete,
+  );
+}
+
+/** Entry point for the migration schematics with target of Angular CDK 22.0.0 */
+export function updateToV22(): Rule {
+  return createMigrationSchematicRule(
+    TargetVersion.V22,
     cdkMigrations,
     cdkUpgradeData,
     onMigrationComplete,
