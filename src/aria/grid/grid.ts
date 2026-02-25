85645e2833fadf3a08a9992705e896d76d632ebe
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-02-25 13:54:33 -0800

    refactor(aria/combobox): add grid and datepicker simple-combobox examples

--- src/aria/grid/grid.ts
+++ src/aria/grid/grid.ts
@@ -120,6 +120,12 @@ export class Grid {
    */
   readonly selectionMode = input<'follow' | 'explicit'>('follow');
 
+  /** Whether enable range selections (with modifier keys or dragging). */
+  readonly enableRangeSelection = input(false, {transform: booleanAttribute});
+
+  /** Overrides the default tab index of the grid. */
+  readonly tabIndex = input<number | undefined>(undefined);
+
   /** The UI pattern for the grid. */
   readonly _pattern = new GridPattern({
     ...this,
@@ -134,6 +140,11 @@ export class Grid {
     afterRenderEffect(() => this._pattern.resetFocusEffect());
     afterRenderEffect(() => this._pattern.restoreFocusEffect());
     afterRenderEffect(() => this._pattern.focusEffect());
+  }
+
+  /** Scrolls the active cell into view. */
+  scrollActiveCellIntoView(options: ScrollIntoViewOptions = {block: 'nearest'}) {
+    this._pattern.activeCell()?.element().scrollIntoView(options);
   }
 
   /** Gets the cell pattern for a given element. */
