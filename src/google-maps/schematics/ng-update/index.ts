694765de3bdfa9031271756926c4f57dc5e09765
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-03-18 10:29:56 +0100

    build: remove v21 migrations (#32937)
    Now that we're on v22 in `main`, we don't need th v21 migrations anymore.

--- src/google-maps/schematics/ng-update/index.ts
+++ src/google-maps/schematics/ng-update/index.ts
@@ -8,11 +8,6 @@
 
 import {Rule} from '@angular-devkit/schematics';
 
-/** Entry point for the migration schematics with target of Google Maps v21 */
-export function updateToV21(): Rule {
-  return () => {};
-}
-
 /** Entry point for the migration schematics with target of Google Maps v22 */
 export function updateToV22(): Rule {
   return () => {};
