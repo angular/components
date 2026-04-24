f45080123aa742139dc5e4729e00f9699c9c4f55
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-04-24 11:48:38 +0200

    refactor(material/snack-bar): switch to service decorator
    Switches all injectables to use the new `@Service` decorator.

--- src/material/snack-bar/snack-bar.ts
+++ src/material/snack-bar/snack-bar.ts
@@ -18,7 +18,7 @@ import {
 import {
   ComponentRef,
   EmbeddedViewRef,
-  Injectable,
+  Service,
   InjectionToken,
   Injector,
   OnDestroy,
@@ -45,7 +45,7 @@ export const MAT_SNACK_BAR_DEFAULT_OPTIONS = new InjectionToken<MatSnackBarConfi
 /**
  * Service to dispatch Material Design snack bar messages.
  */
-@Injectable({providedIn: 'root'})
+@Service()
 export class MatSnackBar implements OnDestroy {
   private _live = inject(LiveAnnouncer);
   private _injector = inject(Injector);
