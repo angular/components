29f7a4a73d7b79386da1e04e6397371450834432
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-04-24 11:50:58 +0200

    refactor(cdk/collections): switch to service decorator
    Switches all injectables to use the new `@Service` decorator.

--- src/cdk/collections/unique-selection-dispatcher.ts
+++ src/cdk/collections/unique-selection-dispatcher.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {Injectable, OnDestroy} from '@angular/core';
+import {Service, OnDestroy} from '@angular/core';
 
 // Users of the Dispatcher never need to see this type, but TypeScript requires it to be exported.
 export type UniqueSelectionDispatcherListener = (id: string, name: string) => void;
@@ -20,7 +20,7 @@ export type UniqueSelectionDispatcherListener = (id: string, name: string) => vo
  * This service does not *store* any IDs and names because they may change at any time, so it is
  * less error-prone if they are simply passed through when the events occur.
  */
-@Injectable({providedIn: 'root'})
+@Service()
 export class UniqueSelectionDispatcher implements OnDestroy {
   private _listeners: UniqueSelectionDispatcherListener[] = [];
 
