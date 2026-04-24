29e76179aaa79917ab2483f6fe2250e7c702793f
Cheng-Hsuan Tsai <webcat@google.com>
2026-04-24 00:37:45 +0000

    fix(google-maps): fix type errors caused by @types/google.maps update

--- src/google-maps/map-directions-renderer/map-directions-service.ts
+++ src/google-maps/map-directions-renderer/map-directions-service.ts
@@ -38,7 +38,10 @@ export class MapDirectionsService {
       this._getService().then(service => {
         service.route(request, (result, status) => {
           this._ngZone.run(() => {
-            observer.next({result: result || undefined, status});
+            observer.next({
+              result: result || undefined,
+              status: status as google.maps.DirectionsStatus,
+            });
             observer.complete();
           });
         });
