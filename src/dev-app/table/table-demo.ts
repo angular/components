1a9d8fbadea7f3d6abb0b0331cecb7dd12ad068f
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-04-29 14:57:43 +0200

    test: attempting to reproduce

--- src/dev-app/table/table-demo.ts
+++ src/dev-app/table/table-demo.ts
@@ -7,79 +7,100 @@
  */
 
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {
-  CdkTableBasicExample,
-  CdkTableFixedLayoutExample,
-  CdkTableFlexBasicExample,
-  CdkTableRecycleRowsExample,
-  CdkTableVirtualScrollExample,
-} from '@angular/components-examples/cdk/table';
-import {
-  TableBasicExample,
-  TableDragDropExample,
-  TableDynamicColumnsExample,
-  TableExpandableRowsExample,
-  TableFilteringExample,
-  TableFlexBasicExample,
-  TableFlexLargeRowExample,
-  TableFooterRowExample,
-  TableHttpExample,
-  TableMultipleHeaderFooterExample,
-  TableMultipleRowTemplateExample,
-  TableOverviewExample,
-  TablePaginationExample,
-  TableRecycleRowsExample,
-  TableReorderableExample,
-  TableRowContextExample,
-  TableSelectionExample,
-  TableSortingExample,
-  TableStickyColumnsExample,
-  TableStickyComplexExample,
-  TableStickyComplexFlexExample,
-  TableStickyFooterExample,
-  TableStickyHeaderExample,
-  TableTextColumnAdvancedExample,
-  TableTextColumnExample,
-  TableVirtualScrollExample,
-  TableWrappedExample,
-} from '@angular/components-examples/material/table';
+import {MatTableModule} from '@angular/material/table';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {MockedDataType} from './MockedDataType';
+import {TableDataSource} from './table-data-source';
+import {mockedData} from './data';
+import {TableColumn} from './TableExampleColumn';
+import {PagedTableDataSourceNextPageData} from './PagedTableDataSourceNextPageData';
+import {Observable, of} from 'rxjs';
+import {delay, map} from 'rxjs/operators';
+import {PagedDataSource} from './paged-data-source';
+
+export const TABLE_EXAMPLE_COLUMNS: readonly TableColumn[] = [
+  TableColumn.ID,
+  TableColumn.FIRST_NAME,
+  TableColumn.LAST_NAME,
+  // TableColumn.EMAIL,
+  // TableColumn.GENDER,
+  // TableColumn.IP_ADDRESS,
+];
 
 @Component({
   templateUrl: './table-demo.html',
-  imports: [
-    CdkTableFlexBasicExample,
-    CdkTableBasicExample,
-    CdkTableFixedLayoutExample,
-    CdkTableRecycleRowsExample,
-    CdkTableVirtualScrollExample,
-    TableFlexBasicExample,
-    TableBasicExample,
-    TableDynamicColumnsExample,
-    TableExpandableRowsExample,
-    TableFilteringExample,
-    TableFooterRowExample,
-    TableHttpExample,
-    TableMultipleHeaderFooterExample,
-    TableMultipleRowTemplateExample,
-    TableOverviewExample,
-    TablePaginationExample,
-    TableVirtualScrollExample,
-    TableRowContextExample,
-    TableSelectionExample,
-    TableSortingExample,
-    TableStickyColumnsExample,
-    TableStickyComplexFlexExample,
-    TableStickyComplexExample,
-    TableStickyFooterExample,
-    TableStickyHeaderExample,
-    TableTextColumnAdvancedExample,
-    TableTextColumnExample,
-    TableWrappedExample,
-    TableReorderableExample,
-    TableRecycleRowsExample,
-    TableFlexLargeRowExample,
-    TableDragDropExample,
-  ],
+  imports: [MatTableModule, ScrollingModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: `
+    .example-container {
+      height: 600px;
+      overflow: auto;
+    }
+
+    .example-container table {
+      width: 100%;
+    }
+
+    .loading {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: white;
+      background: rgba(0, 0, 0, 0.8);
+    }
+
+    :host {
+      position: relative;
+    }
+
+    td {
+      white-space: nowrap;
+    }
+
+  `,
 })
-export class TableDemo {}
+export class TableDemo {
+  readonly TableExampleColumn = TableColumn;
+
+  displayedColumns: string[] = [...TABLE_EXAMPLE_COLUMNS];
+  dataSource!: TableDataSource<MockedDataType>;
+
+  trackBy = (index: number, el: MockedDataType) => el.id;
+
+  constructor() {
+    this.initStaticDataSource(); // static data, comment the other one
+    // this.initPagedDataSource(); // async data, comment the other one
+  }
+
+  initStaticDataSource(): void {
+    this.dataSource = new TableDataSource();
+    this.dataSource.setData(mockedData);
+  }
+
+  initPagedDataSource(): void {
+    this.dataSource = new PagedDataSource<MockedDataType>({
+      pageSize: 40,
+      itemsUntilReq: 5,
+      nextPageHandler: this._nextPageHandler.bind(this),
+    });
+  }
+
+  private _nextPageHandler(
+    pageSize: number,
+    nextPageNumber: number,
+    nextPageToken?: string,
+  ): Observable<PagedTableDataSourceNextPageData<MockedDataType>> {
+    return of(mockedData).pipe(
+      delay(1000),
+      map((data: MockedDataType[]) => ({
+        content: data.slice(pageSize * (nextPageNumber - 1), pageSize * nextPageNumber),
+        nextPageToken,
+      })),
+    );
+  }
+}
