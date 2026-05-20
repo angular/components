3a8edea4949f7841c4fe79e48dff303765586bc3
Will-thom <shortgamestests@gmail.com>
2026-05-20 02:17:06 -0300

    feat(material/button): add filled and tonal icon buttons

--- src/material/button/icon-button.ts
+++ src/material/button/icon-button.ts
@@ -6,8 +6,17 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, Input, ViewEncapsulation} from '@angular/core';
 import {MatButtonBase} from './button-base';
+
+/** Possible appearances for a `MatIconButton`. */
+export type MatIconButtonAppearance = 'filled' | 'tonal';
+
+/** Classes that need to be set for each appearance of the icon button. */
+const APPEARANCE_CLASSES: Map<MatIconButtonAppearance, readonly string[]> = new Map([
+  ['filled', ['mat-mdc-icon-button-filled']],
+  ['tonal', ['mat-mdc-icon-button-tonal']],
+]);
 
 /**
  * Material Design icon button component. This type of button displays a single interactive icon for
@@ -25,9 +34,50 @@ import {MatButtonBase} from './button-base';
   encapsulation: ViewEncapsulation.None,
 })
 export class MatIconButton extends MatButtonBase {
+  /** Appearance of the icon button. */
+  @Input('matIconButton')
+  get appearance(): MatIconButtonAppearance | null {
+    return this._appearance;
+  }
+  set appearance(value: MatIconButtonAppearance | '') {
+    this.setAppearance(value || null);
+  }
+  private _appearance: MatIconButtonAppearance | null = null;
+
+  /** Same as `appearance`, but using the legacy `mat-icon-button` attribute selector. */
+  @Input('mat-icon-button')
+  set _legacyAppearance(value: MatIconButtonAppearance | '') {
+    this.setAppearance(value || null);
+  }
+
   constructor() {
     super();
     this._rippleLoader.configureRipple(this._elementRef.nativeElement, {centered: true});
+  }
+
+  /** Programmatically sets the appearance of the icon button. */
+  setAppearance(appearance: MatIconButtonAppearance | null): void {
+    if (appearance === this._appearance) {
+      return;
+    }
+
+    const classList = this._elementRef.nativeElement.classList;
+    const previousClasses = this._appearance ? APPEARANCE_CLASSES.get(this._appearance) : null;
+    const newClasses = appearance ? APPEARANCE_CLASSES.get(appearance) : null;
+
+    if ((typeof ngDevMode === 'undefined' || ngDevMode) && appearance && !newClasses) {
+      throw new Error(`Unsupported MatIconButton appearance "${appearance}"`);
+    }
+
+    if (previousClasses) {
+      classList.remove(...previousClasses);
+    }
+
+    if (newClasses) {
+      classList.add(...newClasses);
+    }
+
+    this._appearance = appearance;
   }
 }
 
