73939a835b49f9603cc8026eb373dbef5e9cb96a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-19 11:35:44 +0200

    feat(material/slide-toggle): add full-width support (#33424)
    Adds support for a `fullWidth` input on the slide toggle that'll make it stretch to fill its container. This is useful for mobile-focused UIs.
    
    Fixes #24754.

---
+++ src/components-examples/material/slide-toggle/slide-toggle-full-width/slide-toggle-full-width-example.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {MatSlideToggle} from '@angular/material/slide-toggle';
+
+/**
+ * @title Full-width slide toggle
+ */
+@Component({
+  selector: 'slide-toggle-full-width-example',
+  templateUrl: 'slide-toggle-full-width-example.html',
+  styleUrl: 'slide-toggle-full-width-example.css',
+  imports: [MatSlideToggle],
+})
+export class SlideToggleFullWidthExample {}
