f4686e981e7a8198f121ff370b2254ca85349a9d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-23 12:13:34 +0200

    docs(cdk/overlay): add popover to docs (#33434)
    Updates the overlay docs to mention our usage of the popover API.
    
    Fixes #32729.
    
    (cherry picked from commit f7da3f2eb42705a3ff4297bea95d0cda3c78e6f8)

--- src/cdk/overlay/overlay.ts
+++ src/cdk/overlay/overlay.ts
@@ -34,6 +34,7 @@ import {ScrollStrategyOptions} from './scroll/index';
 
 /** Object used to configure the default options for overlays. */
 export interface OverlayDefaultConfig {
+  /** Whether overlays should be rendered inside popovers by default. */
   usePopover?: boolean;
 }
 
