025ec7d523eda92516f797b75f8914ab359175a9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-12 11:20:06 +0200

    build: switch tests away from protractor (#33664)
    Reworks most e2e tests not to use Protractor. Note that we have some test harnesses functionality built specifically for Protractor that we need to keep around.
    
    (cherry picked from commit d7eb639662fe0c8b5568379c3cdedb5ddab2b8da)

--- src/universal-app/hydration.e2e.spec.ts
+++ src/universal-app/hydration.e2e.spec.ts
@@ -1,15 +1,27 @@
-import {browser, by, element, ExpectedConditions} from 'protractor';
+import * as webdriver from 'selenium-webdriver';
+import {createE2eWebDriver} from '../e2e-app/e2e-setup';
+
+const {builder, port} = createE2eWebDriver();
 
 describe('hydration e2e', () => {
+  let wd: webdriver.WebDriver;
+
+  beforeAll(async () => {
+    wd = await builder.build();
+  });
+
+  afterAll(async () => {
+    await wd.quit();
+  });
+
   beforeEach(async () => {
-    await browser.waitForAngularEnabled(false);
-    await browser.get('/');
-    await browser.wait(ExpectedConditions.presenceOf(element(by.css('.render-marker'))), 5000);
+    await wd.get(`http://localhost:${port}/`);
+    await wd.wait(webdriver.until.elementLocated(webdriver.By.css('.render-marker')), 5000);
   });
 
   it('should enable hydration', async () => {
     const hydrationState = await getHydrationState();
-    const logs = await browser.manage().logs().get('browser');
+    const logs = await wd.manage().logs().get(webdriver.logging.Type.BROWSER);
 
     expect(hydrationState.hydratedComponents).toBeGreaterThan(0);
     expect(logs.map(log => log.message).filter(msg => msg.includes('NG0500'))).toEqual([]);
@@ -19,25 +31,25 @@ describe('hydration e2e', () => {
     const hydrationState = await getHydrationState();
     expect(hydrationState.componentsSkippedHydration).toBe(0);
   });
-});
 
-/** Gets the hydration state from the current app. */
-async function getHydrationState() {
-  return browser.executeScript<{
-    hydratedComponents: number;
-    componentsSkippedHydration: number;
-  }>(() => {
-    const devModeWindow = window as Window &
-      typeof globalThis & {
-        ngDevMode: {
-          hydratedComponents: number;
-          componentsSkippedHydration: number;
+  /** Gets the hydration state from the current app. */
+  async function getHydrationState() {
+    return wd.executeScript<{
+      hydratedComponents: number;
+      componentsSkippedHydration: number;
+    }>(() => {
+      const devModeWindow = window as Window &
+        typeof globalThis & {
+          ngDevMode: {
+            hydratedComponents: number;
+            componentsSkippedHydration: number;
+          };
         };
-      };
 
-    return {
-      hydratedComponents: devModeWindow.ngDevMode.hydratedComponents,
-      componentsSkippedHydration: devModeWindow.ngDevMode.componentsSkippedHydration,
-    };
-  });
-}
+      return {
+        hydratedComponents: devModeWindow.ngDevMode.hydratedComponents,
+        componentsSkippedHydration: devModeWindow.ngDevMode.componentsSkippedHydration,
+      };
+    });
+  }
+});
