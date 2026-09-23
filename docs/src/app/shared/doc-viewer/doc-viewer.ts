17b22c73a70de20cb75192f7073452e3a3ab1e4d
MeAkib <akibh805@gmail.com>
2026-09-23 21:17:01 +0600

    docs: add copy buttons to markdown code blocks
    Add copy functionality to all code blocks in documentation markdown content.
    Previously, only example viewer code and module import snippets had copy
    buttons, but regular markdown code blocks (like configuration examples)
    were missing this feature.

--- docs/src/app/shared/doc-viewer/doc-viewer.ts
+++ docs/src/app/shared/doc-viewer/doc-viewer.ts
@@ -40,6 +40,7 @@ import {HeaderLink} from './header-link';
 import {DeprecatedFieldComponent} from './deprecated-tooltip';
 import {ModuleImportCopyButton} from './module-import-copy-button';
 import {AngularAriaBanner} from './angular-aria-banner/angular-aria-banner';
+import {CodeBlockCopyButton} from './code-block-copy-button/code-block-copy-button';
 
 @Service()
 class DocFetcher {
@@ -166,6 +167,9 @@ export class DocViewer implements OnDestroy {
 
     // Create icon buttons to copy module import
     this._createCopyIconForModule();
+
+    // Create icon buttons to copy code blocks
+    this._createCopyButtonsForCodeBlocks();
 
     // Resolving and creating components dynamically in Angular happens synchronously, but since
     // we want to emit the output if the components are actually rendered completely, we wait
@@ -305,5 +309,32 @@ export class DocViewer implements OnDestroy {
     bannerComponent.instance.componentName = componentName;
 
     this._portalHosts.push(portalHost);
+  }
+
+  /** Adds a button to copy the code to every code block in the document. */
+  private _createCopyButtonsForCodeBlocks() {
+    // Query all <pre> tags that contain <code> elements (markdown code blocks)
+    const codeBlockElements = this._elementRef.nativeElement.querySelectorAll(
+      '.docs-markdown pre:has(code)',
+    );
+
+    [...codeBlockElements].forEach((element: HTMLElement) => {
+      // Extract the text content from the code block
+      const codeElement = element.querySelector('code');
+      const codeSnippet = codeElement?.textContent || '';
+
+      const elementPortalOutlet = new DomPortalOutlet(element, this._appRef, this._injector);
+      const codeBlockCopyButtonPortal = new ComponentPortal(
+        CodeBlockCopyButton,
+        this._viewContainerRef,
+      );
+      const codeBlockCopyButtonOutlet = elementPortalOutlet.attach(codeBlockCopyButtonPortal);
+
+      if (codeSnippet) {
+        codeBlockCopyButtonOutlet.instance.code = codeSnippet;
+      }
+
+      this._portalHosts.push(elementPortalOutlet);
+    });
   }
 }
